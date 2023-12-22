7cf37e7db42c535b5abd8105dc673e4e7b132338
Akos Magyar <92167417+magyarakoos@users.noreply.github.com>
2023-12-22 16:20:24 +0100

    Update main.cpp

--- njudge/nemes/NT22/talalkozas/main.cpp
+++ njudge/nemes/NT22/talalkozas/main.cpp
@@ -1,34 +1,60 @@
 #include <iostream>
 #include <vector>
+#include <array>
 #include <queue>
+#include <climits>
+#include <algorithm>
+#include <numeric>
+
 using namespace std;
+
 #define speed cin.tie(0); ios::sync_with_stdio(0)
 
-struct Guest {
-public:
-    int e;
-    int v;
-    bool operator()(Guest a, Guest b) {
-        return a.v > b.v;
-    }
-};
+const int INF = 100'001;
 
-int main() {
-    speed;
+int main()
+{
+	speed;
 
-    int N;
-    cin >> N;
+	int N, H;
+	cin >> N;
 
-    priority_queue<Guest, vector<Guest>, Guest> pq;
+	H = N / 2 + (N & 1);
+	
+	vector<int> eS(N);
+	vector<int> vS(N);
 
-    while (N--) {
-        int E, V;
-        cin >> E >> V;
-        pq.push({E, V});
-    }
+	for (int i = 0; i < N; i++) {
+		int E, V;
+		cin >> E >> V;
+		eS[i] = E;
+		vS[i] = V;
+	}
 
-    while(!pq.empty()) {
-        // idk tho
-        pq.pop();
-    }
+	std::sort(eS.begin(), eS.end());
+	std::sort(vS.begin(), vS.end());
+
+	array<int, 2> min_v{ 0, INF };
+
+	for (int i = 0; i < N; i++) {
+
+		int e = vS[i],
+			v = eS.back(),
+			curr = N - i + 1,
+			j = N;
+
+		while (e < v && H < curr) {
+
+			v = eS[--j];
+			
+			curr--;
+
+			if (v - e < min_v[1] - min_v[0]) {
+				min_v = { e, v };
+			}
+		}
+	}
+
+    // time limit bruh
+	cout << min_v[1] - min_v[0] + 1 << '\n' << min_v[0] << ' ' << min_v[1];
 }
