8bbdaef8fba00cfafa4b904b81c09ed871a0cb6d
LordOfPumpkins <ninjaoffical08@gmail.com>
2024-01-03 19:53:25 +0100

    Solve stuff

--- njudge/nemes/1/NT23/takaros-sorozat/main.cpp
+++ njudge/nemes/1/NT23/takaros-sorozat/main.cpp
@@ -1,32 +1,54 @@
 #include <bits/stdc++.h>
 
 #define speed cin.tie(0); ios::sync_with_stdio(0)
+#define cinv(v) for (auto& e : v) cin >> e;
+#define all(v) v.begin(), v.end()
+#define has(s, e) s.count(e)
 
 using namespace std;
+using ll = long long;
+using point = array<int, 2>;
 
 int main() {
-    //speed;
+    speed;
 
-    bool done = 0;
-    int N, fo = -1;
-    char c;
+    int N;
     cin >> N;
+    vector<bool> v;
+
+    bool start = 0;
+    while (N--) {
+        char c;
+        cin >> c;
+        start |= c == '1';
+        if (start) {
+            v.push_back(c == '1');
+        }
+    }
+
+    N = v.size();
+
+    int result = 0;
+    auto p1v = v;
 
     for (int i = 0; i < N; i++) {
-        cin >> c;
-        if (!done) {
-            if (c == '1') {
-                fo = i;
-                done = 1;
+        if (!p1v[i]) {
+            result++;
+            if (i + 1 < N) p1v[i + 1] = 1;
+        }
+    }
+
+    cout << result << '\n';
+
+    result = 0;
+    for (int i = 0; i < N; i++) {
+        if (!v[i]) {
+            result++;
+            for (int j = i + 1; j < N && j < i + 4; j++) {
+                v[j] = 1;
             }
         }
     }
 
-    if (fo <= 0) {
-        cout << "0\n0";
-        exit(0);
-    }
-
-    int len = N - fo - 1;
-    cout << len / 2 + (len & 1) << '\n' << len / 4 + (len % 4) / 2 + ((len % 4) & 1);
+    cout << result;
 }
--- njudge/nemes/2-3/NT20/fasor/main.cpp
+++ njudge/nemes/2-3/NT20/fasor/main.cpp
@@ -13,6 +13,56 @@ int main() {
     speed;
 
     int N, K;
+    cin >> N >> K;
+    vector<int> v(N);
+    cinv(v);
 
+    if (N <= K + 1) {
+        cout << max_element(all(v)) - v.begin() + 1;
+        exit(0);
+    }
+
+    deque<point> q;
     
+    for (int i = 0; i < K + 1; i++) {
+
+        while (!q.empty() && q.back()[0] < v[i]) {
+            q.pop_back();
+        }
+
+        q.push_back({v[i], i});
+    }
+
+    if (q.front()[0] == v[0]) {
+        cout << 1;
+        exit(0);
+    }
+
+    int bagoly = 1, left = 1 - K, right = 1 + K;
+
+    while (bagoly < N) {
+
+        if (!q.empty() && q.front()[1] < left) {
+            q.pop_front();
+        }
+
+        if (right < N) {
+            while (!q.empty() && q.back()[0] < v[right]) {
+                q.pop_back();
+            }
+
+            q.push_back({v[right], right});
+        }
+
+        if (!q.empty() && q.front()[0] == v[bagoly]) {
+            cout << bagoly + 1;
+            exit(0);
+        }
+
+        left++;
+        bagoly++;
+        right++;
+    }
+
+    cout << -1;
 }
