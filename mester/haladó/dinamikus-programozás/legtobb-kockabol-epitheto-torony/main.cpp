8d1bc668aaad910a1c5c8b0862da71b2923965bf
LordOfPumpkins <ninjaoffical08@gmail.com>
2024-01-28 15:25:53 +0100

    mester

--- mester/haladó/dinamikus-programozás/legtobb-kockabol-epitheto-torony/main.cpp
+++ mester/haladó/dinamikus-programozás/legtobb-kockabol-epitheto-torony/main.cpp
@@ -1,14 +1,10 @@
 #include <bits/stdc++.h>
 
-#define cinv(v) for (auto& e : v) cin >> e;
 #define all(v) v.begin(), v.end()
 #define rall(v) v.rbegin(), v.rend()
 #define size(v) (int)v.size()
-#define smax(x, y) x = max(x, y)
-#define smin(x, y) x = min(x, y)
 
 using namespace std;
-using ll = long long;
 
 int main() {
     cin.tie(0), ios::sync_with_stdio(0);
@@ -25,20 +21,30 @@ int main() {
 
     sort(all(boxS));
 
-    for (int i = 0; i < N; i++) {
-        cout << boxS[i][0] << ' ' << boxS[i][1] << ' ' << boxS[i][2] << '\n';
-    } 
-
-    vector<int> dp(N); // dp[i] = max magasság, ha boxS[i] a torony legalja
-    dp[0] = 1; // a legkisebb kocka alá nem fér be semmi
+    vector<int> dp(N, 1), prev(N); // dp[i] = max magasság, ha boxS[i] a torony legalja
 
     for (int i = 1; i < N; i++) {
         for (int j = 0; j < i; j++) {
             if (boxS[j][1] <= boxS[i][1]) {
-                smax(dp[i], dp[j] + 1);
+                if (dp[i] < dp[j] + 1) {
+                    dp[i] = dp[j] + 1;
+                    prev[i] = j;
+                }
             }
         }
     }
 
-    cout << *max_element(all(dp)) << '\n';
+    int i = max_element(all(dp)) - dp.begin();
+
+    cout << dp[i] << '\n';
+
+    vector<int> path;
+
+    for (int j = dp[i]; j > 0; j--) {
+        path.push_back(boxS[i][2]);
+        i = prev[i];
+    };
+
+    for (int x : path) cout << x << ' ';
+    cout << '\n';
 }
