ecd61624ac34206708ca61d78b9877f7f0fa72d9
magyarakoos <ninjaoffical08@gmail.com>
2024-02-04 20:30:04 +0100

    grind

--- mester/haladó/dinamikus-programozás/legtobb-csomag-2-kamion/main.cpp
+++ mester/haladó/dinamikus-programozás/legtobb-csomag-2-kamion/main.cpp
@@ -1,3 +1,4 @@
+// it is important to note that this does not work
 #include <bits/stdc++.h>
 
 #define cinv(v) for (auto& e : v) cin >> e;
@@ -8,21 +9,55 @@
 using namespace std;
 using ll = long long;
 
+vector<int> v;
+vector<bool> put; // a jobboldaliba raktuk-e
+int K, N;
+
+int dp(int a, int b, int i) {
+    cerr << a << ' ' << b << ' ' << i << '\n';
+    if (a + v[i] > K && b + v[i] > K) {
+        return i;
+    }
+
+    int left = -1, right = -1;
+    if (a + v[i] <= K) left = dp(a + v[i], b, i + 1);
+    if (b + v[i] <= K) right = dp(a, b + v[i], i + 1);
+
+    if (left != -1 && right != -1) {
+        put[i] = left < right;
+    }
+    else {
+        put[i] = left == -1;
+    }
+
+    return max(left, right);
+}
+
 int main() {
     cin.tie(0), ios::sync_with_stdio(0);
 
-    int K, N;
     cin >> K >> N;
-    
-    vector<int> v(N + 1);
-    for (int i = 1; i <= N; i++) {
-        cin >> v[i];
-    }
 
-    vector<array<int, 2>> dp(K + 1);
+    v.resize(N);    
+    put.resize(N);
+    cinv(v);
 
-    for (int i = 1; i <= K; i++) {
-        dp[i] = dp[i - 1];
-        
+    int result = dp(v[0], 0, 1);
+
+    cerr << '\n';
+    cout << result << '\n';
+
+    for (int i = 0; i < result; i++) {
+        if (put[i]) {
+            cout << i + 1 << ' ';
+        }
     }
+    cout << '\n';
+
+    for (int i = 0; i < result; i++) {
+        if (!put[i]) {
+            cout << i + 1 <<  ' ';
+        }
+    }
+    cout << '\n';
 }
