5f6c543a4edd5b280230ff823401c140241baf94
magyarakoos <ninjaoffical08@gmail.com>
2024-02-10 13:04:56 +0100

    arch descktop sync: 2024-02-10 13:04:56

---
+++ atcoder/abc340/A/main.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+#define cinv(v) for (auto& e : v) cin >> e;
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define size(v) (int)v.size()
+#define has(s, e) s.count(e)
+
+using namespace std;
+using ll = long long;
+
+int main() {
+    cin.tie(0), ios::sync_with_stdio(0);
+
+    
+}
