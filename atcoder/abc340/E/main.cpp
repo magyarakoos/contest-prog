130188316688e908d2c2a81d1b80e078cc6e62f2
magyarakoos <ninjaoffical08@gmail.com>
2024-02-10 14:01:03 +0100

    arch descktop sync: 2024-02-10 14:01:03

--- atcoder/abc340/E/main.cpp
+++ atcoder/abc340/E/main.cpp
@@ -34,7 +34,7 @@ int main() {
 
         int p = 0, i;
 
-        for (i = B + 1; i < N && i < mod + B + 1; i++) {
+        for (i = B + 1; i < N && p < mod; i++) {
             A[i]++;
             p++;
         }
