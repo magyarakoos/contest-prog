18041b8b5b66a2e0930da9263c7a5f91aa7a4045
magyarakoos <ninjaoffical08@gmail.com>
2024-02-10 13:29:04 +0100

    arch descktop sync: 2024-02-10 13:29:04

---
+++ atcoder/abc340/D/main.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+#define cinv(v) for (auto& e : v) cin >> e;
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define size(v) (int)v.size()
+#define has(s, e) s.count(e)
+
+using namespace std;
+using ull = unsigned long long;
+
+unordered_map<ull, ull> cache;
+
+ull solve(ull n) {
+    if (has(cache, n)) {
+        return cache[n];
+    }
+
+    if (n < 2) {
+        return 0;
+    }
+
+    ull a = n / 2,
+        b = n / 2 + (n & 1);
+
+    ull sa = solve(a) + a,
+        sb = solve(b) + b;
+
+    cache[n] = sa + sb;
+    return sa + sb;
+}
+
+int main() {
+    cin.tie(0), ios::sync_with_stdio(0);
+
+    ull N;
+    cin >> N;
+
+    cout << solve(N);
+}
