a9c17f86e6da4fd687c94ae8bfa943d2429fd22c
magyarakoos <ninjaoffical08@gmail.com>
2024-02-10 13:29:21 +0100

    arch descktop sync: 2024-02-10 13:29:21

--- atcoder/abc340/D/main.cpp
+++ atcoder/abc340/D/main.cpp
@@ -7,34 +7,11 @@
 #define has(s, e) s.count(e)
 
 using namespace std;
-using ull = unsigned long long;
+using ll = long long;
 
-unordered_map<ull, ull> cache;
-
-ull solve(ull n) {
-    if (has(cache, n)) {
-        return cache[n];
-    }
-
-    if (n < 2) {
-        return 0;
-    }
-
-    ull a = n / 2,
-        b = n / 2 + (n & 1);
-
-    ull sa = solve(a) + a,
-        sb = solve(b) + b;
-
-    cache[n] = sa + sb;
-    return sa + sb;
-}
 
 int main() {
     cin.tie(0), ios::sync_with_stdio(0);
 
-    ull N;
-    cin >> N;
 
-    cout << solve(N);
 }
