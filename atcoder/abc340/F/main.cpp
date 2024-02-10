8b9d112e01f46ec7f37e6490bdd458e8e4e4ccbb
magyarakoos <ninjaoffical08@gmail.com>
2024-02-10 14:32:13 +0100

    arch descktop sync: 2024-02-10 14:32:13

--- atcoder/abc340/F/main.cpp
+++ atcoder/abc340/F/main.cpp
@@ -20,7 +20,9 @@ int main() {
     ll X3, Y3;
 
     ll bottomX = min({X1, X2, X3}), bottomY = min({Y1, Y2, Y3}),
-       topX = max({X1, X2, X3}), topY =     
+       topX = max({X1, X2, X3}), topY = max({Y1, Y2, Y3});
+    
+        
 
     cout << X3 << ' ' << Y3;
 }
