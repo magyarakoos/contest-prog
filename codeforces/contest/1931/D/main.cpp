8b6014b65d69ad5e982e63926a1807f205c375e4
LordOfPumpkins <ninjaoffical08@gmail.com>
2024-02-13 16:54:44 +0100

    arch descktop sync: 2024-02-13 16:54:44

--- codeforces/contest/1931/D/main.cpp
+++ codeforces/contest/1931/D/main.cpp
@@ -14,47 +14,9 @@ int main() {
     int T;
     cin >> T;
     while (T--) {
-        int N;
-        cin >> N;
-        vector<int> v(N);
-        cinv(v);
+        int N, X, Y;
+        cin >> N >> X >> Y;
 
-        int best = 1;
-
-        if (v[0] != v[N - 1]) {
-            int left = 0, right = 0;
-
-            int c1 = v[0], c2 = v[N - 1];
-
-            int i = 0, j = N - 1;
-            while (i < N && v[i] == c1) {
-                left++;
-                i++;
-            }
-            while (j >= 0 && v[j] == c2) {
-                right++;
-                j--;
-            }
-
-            best = max({best, left, right});
-        } else {
-            int control = v[0];
-
-            int result = 0;
-
-            int i = 0, j = N - 1;
-            while (i < N && v[i] == control) {
-                result++;
-                i++;
-            }
-            while (j >= 0 && i < j && v[j] == control) {
-                result++;
-                j--;
-            }
-
-            best = max(best, result);
-        }
-
-        cout << N - best << '\n';
+        
     }
 }
