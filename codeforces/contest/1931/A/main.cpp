52df6fc4628cdd70b8c9f3b7f91c79a6c1cdb568
LordOfPumpkins <ninjaoffical08@gmail.com>
2024-02-13 16:08:45 +0100

    arch descktop sync: 2024-02-13 16:08:45

---
+++ codeforces/contest/1931/A/main.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+#define cinv(v) for (auto& e : v) cin >> e;
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define size(v) (int)v.size()
+
+using namespace std;
+using ll = long long;
+
+int main() {
+    cin.tie(0), ios::sync_with_stdio(0);
+
+    int T;
+    cin >> T;
+    while (T--) {
+        int N;
+        cin >> N;
+        
+    }
+}
