12942956e3337dc7a6a70b58bb8f6291969b3f8b
magyarakoos <ninjaoffical08@gmail.com>
2024-02-17 17:19:20 +0100

    arch descktop sync: 2024-02-17 17:19:20

---
+++ mester/nt-oktv-ioi/NT17/ritka-szam/main.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define size(v) (int)v.size()
+
+using namespace std;
+using ll = long long;
+
+int main() {
+    cin.tie(0), ios::sync_with_stdio(0);
+
+
+
+}
+
+/*
+1
+10
+101
+1010
+10101
+1010
+
+*/
