22893c0b28e960aa92e6d85564f33519af153a22
magyarakoos <ninjaoffical08@gmail.com>
2024-02-17 18:08:02 +0100

    arch descktop sync: 2024-02-17 18:08:02

--- mester/nt-oktv-ioi/NT17/varolista/main.cpp
+++ mester/nt-oktv-ioi/NT17/varolista/main.cpp
@@ -16,7 +16,7 @@ int main() {
     int last = 0;
     int maxi = 0, maxw = 0;
 
-    vector<bool> waitc()
+    vector<bool> waitc(100 * 720 * 40)
     
     for (int i = 0; i < N; i++) {
         int u, t;
