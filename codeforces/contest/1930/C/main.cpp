59e3a4aab67d770945c3b63deae90d2a0b5c0bae
magyarakoos <ninjaoffical08@gmail.com>
2024-02-17 16:07:10 +0100

    arch descktop sync: 2024-02-17 16:07:10

--- codeforces/contest/1930/C/main.cpp
+++ codeforces/contest/1930/C/main.cpp
@@ -8,14 +8,6 @@ using namespace std;
 using ll = long long;
 
 struct BIT {
-    vector<int> bit;
-    int n;
-
-    BIT(int n) {
-        this->n = n;
-        bit.assign(n, 0);
-    }
-
     int sum(int r) {
         int ret = 0;
         for (; r >= 0; r = (r & (r + 1)) - 1)
@@ -26,12 +18,25 @@ struct BIT {
     int sum(int l, int r) {
         return sum(r) - sum(l - 1);
     }
+    vector<int> bit;
+    int n;
+
+    BIT(int n) {
+        this->n = n;
+        bit.assign(n, 0);
+    }
+
 
     void add(int idx, int delta) {
         for (; idx < n; idx = idx | (idx + 1))
             bit[idx] += delta;
     }
+
+    int query(int idx) {
+        return sum(idx, idx);
+    }
 };
+
 
 int main() {
     cin.tie(0), ios::sync_with_stdio(0);
