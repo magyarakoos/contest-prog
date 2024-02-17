8984ba45859875252aa215b499328bfc950b6c43
magyarakoos <ninjaoffical08@gmail.com>
2024-02-17 18:33:26 +0100

    arch descktop sync: 2024-02-17 18:33:26

--- mester/nt-oktv-ioi/NT17/fuggohid/main.cpp
+++ mester/nt-oktv-ioi/NT17/fuggohid/main.cpp
@@ -29,7 +29,8 @@ int main() {
 
     for (int i = 2; i <= N; i++) {
         dp[i][0] = min(dp[i - 1][0], dp[i - 1][1] + Jobb[i]) + Bal[i];
-
-        dp[i][1] = min(dp[i - 1][1], )
+        dp[i][1] = min(dp[i - 1][1], dp[i - 1][0] + Bal[i]) + Jobb[i];
     }
+
+    cout << min(dp[N][0], dp[N][1]);
 }
