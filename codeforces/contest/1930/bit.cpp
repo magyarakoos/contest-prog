fd7f5e644a2812827366e3ce8106acfb706a7fc0
magyarakoos <ninjaoffical08@gmail.com>
2024-02-17 15:33:54 +0100

    arch descktop sync: 2024-02-17 15:33:54

---
+++ codeforces/contest/1930/bit.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct BIT {
+    int n;
+    vector<int> BITree1, BITree2;
+
+    BIT(int size) : n(size), BITree1(size + 1, 0), BITree2(size + 1, 0) {}
+
+    int getSum(vector<int>& BITree, int index) {
+        int sum = 0;
+        index = index + 1;
+        while (index > 0) {
+            sum += BITree[index];
+            index -= index & (-index);
+        }
+        return sum;
+    }
+
+    void updateBIT(vector<int>& BITree, int index, int val) {
+        index = index + 1;
+        while (index <= n) {
+            BITree[index] += val;
+            index += index & (-index);
+        }
+    }
+
+    void updateRange(int l, int r, int val) {
+        updateBIT(BITree1, l, val);
+        updateBIT(BITTree1, r + 1, -val);
+
+        updateBIT(BITTree2, l, val * (l - 1));
+        updateBIT(BITTree2, r + 1, -val * r);
+    }
+
+    int sum(int x) {
+        return (getSum(BITTree1, x) * x) - getSum(BITTree2, x);
+    }
+
+    int rangeSum(int l, int r) {
+        return sum(r) - sum(l - 1);
+    }
+};
