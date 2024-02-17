a09464bf801090b808eb12afa4aa3d0fd3bec32a
magyarakoos <ninjaoffical08@gmail.com>
2024-02-17 16:32:26 +0100

    arch descktop sync: 2024-02-17 16:32:26

--- atcoder/abc341/B/main.cpp
+++ atcoder/abc341/B/main.cpp
@@ -15,5 +15,11 @@ int main() {
     vector<int> A(N);
     for (int& e : A) cin >> e;
     
+    vector<int> S(N - 1), T(N - 1);
+
+    for (int i = 0; i < N - 1; i++) {
+        cin >> S[i] >> T[i];
+    }
+
     
 }
