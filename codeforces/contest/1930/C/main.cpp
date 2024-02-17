ab8e8b0fe64f9278302125543fa3f73c3feae614
magyarakoos <ninjaoffical08@gmail.com>
2024-02-17 16:01:46 +0100

    arch descktop sync: 2024-02-17 16:01:46

--- codeforces/contest/1930/C/main.cpp
+++ codeforces/contest/1930/C/main.cpp
@@ -7,44 +7,42 @@
 using namespace std;
 using ll = long long;
 
-struct BIT {
-    int n;
-    vector<int> BITree1, BITree2;
+class FenwickTree {
+private:
+    vector<int> BITree;
+    vector<int> lazy;
 
-    BIT(int size) : n(size), BITree1(size + 1, 0), BITree2(size + 1, 0) {}
-
-    int getSum(vector<int>& BITree, int index) {
-        int sum = 0;
-        index = index + 1;
-        while (index > 0) {
-            sum += BITree[index];
-            index -= index & (-index);
-        }
-        return sum;
-    }
-
-    void updateBIT(vector<int>& BITree, int index, int val) {
-        index = index + 1;
-        while (index <= n) {
-            BITree[index] += val;
-            index += index & (-index);
-        }
+public:
+    FenwickTree(int size) {
+        BITree.resize(size + 1, 0);
+        lazy.resize(size + 1, 0);
     }
 
     void updateRange(int l, int r, int val) {
-        updateBIT(BITree1, l, val);
-        updateBIT(BITree1, r + 1, -val);
-
-        updateBIT(BITree2, l, val * (l - 1));
-        updateBIT(BITree2, r + 1, -val * r);
+        update(l, val);
+        update(r + 1, -val);
     }
 
-    int sum(int x) {
-        return (getSum(BITree1, x) * x) - getSum(BITree2, x);
+    void update(int index, int val) {
+        for (; index < BITree.size(); index += index & -index) {
+            BITree[index] += val;
+        }
     }
 
-    int rangeSum(int l, int r) {
-        return sum(r) - sum(l - 1);
+    void rangeUpdate(int l, int r, int val) {
+        updateRange(l, r, val);
+    }
+
+    void pointUpdate(int index, int val) {
+        update(index, val);
+    }
+
+    int pointQuery(int index) {
+        int result = 0;
+        for (; index > 0; index -= index & -index) {
+            result += BITree[index];
+        }
+        return result;
     }
 };
 
