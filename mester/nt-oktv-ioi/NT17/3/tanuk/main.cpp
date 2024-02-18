4e2c01c70fe70b4a99b0b5700090a2b9b473f1a8
LordOfPumpkins <ninjaoffical08@gmail.com>
2024-02-18 13:00:53 +0100

    arch descktop sync: 2024-02-18 13:00:53

--- mester/nt-oktv-ioi/NT17/3/tanuk/main.cpp
+++ mester/nt-oktv-ioi/NT17/3/tanuk/main.cpp
@@ -7,17 +7,27 @@
 using namespace std;
 using ll = long long;
 
+vector<int> beginS, endS(N);
+
 int main() {
     cin.tie(0), ios::sync_with_stdio(0);
 
     int N, M;
     cin >> N >> M;
 
-    vector<int> beginS(N), endS(N);
 
     for (int i = 0; i < N; i++) {
         cin >> beginS[i] >> endS[i];
     }
 
-    vector<int> 
+    vector<int> sus(M);
+
+    for (int i = 0; i < M; i++) {
+        cin >> sus[i];
+    }
+
+    for (int i = 0; i < M; i++) {
+
+
+    }
 }
