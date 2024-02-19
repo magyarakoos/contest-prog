bfee5c61633b266c5f763eb0bb452bfe2e1fd870
LordOfPumpkins <ninjaoffical08@gmail.com>
2024-02-19 16:19:50 +0100

    arch descktop sync: 2024-02-19 16:19:50

--- codeforces/contest/1926/C/main.cpp
+++ codeforces/contest/1926/C/main.cpp
@@ -10,13 +10,15 @@ using ll = long long;
 int main() {
     cin.tie(0), ios::sync_with_stdio(0);
 
-    vector<int> v(200'000);
+    vector<int> v(200'001);
 
     int T;
     cin >> T;
     while (T--) {
         int N;
         cin >> N;
+
+        if () 
 
         int result = 0;
 
