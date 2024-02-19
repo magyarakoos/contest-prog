c72742521b64abae6257786238e7c340c9402f54
LordOfPumpkins <ninjaoffical08@gmail.com>
2024-02-19 16:06:40 +0100

    arch descktop sync: 2024-02-19 16:06:40

--- codeforces/contest/1926/C/main.cpp
+++ codeforces/contest/1926/C/main.cpp
@@ -16,13 +16,6 @@ int main() {
         int N;
         cin >> N;
 
-        const vector<int> v {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-
-        cout <<
-            46 * (N / 10) +
-            accumulate(v.begin(), v.begin() + (N / 10) + 1, 0) +
-            accumulate(v.begin(), v.begin() + (N % 10) + 1, 0) +
-            accumulate(v.begin(), v.begin() + (N / 10), 0)
-        << '\n';
+        for (int i = 1; i < N;)
     }
 }
