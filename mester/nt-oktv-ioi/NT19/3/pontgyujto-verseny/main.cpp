8b1c613f2fed6d88ed415c6ffc58c8d8a82517d0
LordOfPumpkins <ninjaoffical08@gmail.com>
2024-02-21 19:35:50 +0100

    arch descktop sync: 2024-02-21 19:35:50

--- mester/nt-oktv-ioi/NT19/3/pontgyujto-verseny/main.cpp
+++ mester/nt-oktv-ioi/NT19/3/pontgyujto-verseny/main.cpp
@@ -28,9 +28,9 @@ int main() {
     }
 
     for (int j = 2; j <= M; j++) {
-        for (int j = 1; j <= N; i++) {
+        for (int i = 1; i <= N; i++) {
 
-            dp[i][j] = max(dp[i - 1])
+            dp[i][j] = max({dp[i][j - 1]})
 
         }
     }
