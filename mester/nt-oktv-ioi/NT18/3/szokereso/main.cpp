5d64802b483251466952cd79615a9052caff3233
LordOfPumpkins <ninjaoffical08@gmail.com>
2024-02-22 12:08:28 +0100

    arch descktop sync: 2024-02-22 12:08:28

--- mester/nt-oktv-ioi/NT18/3/szokereso/main.cpp
+++ mester/nt-oktv-ioi/NT18/3/szokereso/main.cpp
@@ -33,7 +33,6 @@ void solve(int si, int sj, int i, int j, int p, string path,
         if (di < 0 || dj < 0 || di >= N || dj >= N || vis[di][dj] || grid[di][dj] != s[p]) {
             continue;
         }
-
         
         solve(si, sj, di, dj, p + 1, path + d[k], vis);
     }
