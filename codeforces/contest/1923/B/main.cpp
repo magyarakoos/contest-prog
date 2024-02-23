ce1d3f7c2caecfab7e85503c51ac5a727b3c542b
LordOfPumpkins <ninjaoffical08@gmail.com>
2024-02-23 16:40:42 +0100

    arch descktop sync: 2024-02-23 16:40:42

--- codeforces/contest/1923/B/main.cpp
+++ codeforces/contest/1923/B/main.cpp
@@ -27,7 +27,10 @@ int main() {
         }
 
         for (int i = 0; i < N; i++) {
-            
+            if (v[i] < K) {
+                cout << "NO\n";
+                b
+            }
         }
     }
 }
