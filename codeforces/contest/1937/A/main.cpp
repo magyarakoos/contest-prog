7ee9cac094640b925fa3d5348633c31f080607ff
LordOfPumpkins <ninjaoffical08@gmail.com>
2024-02-29 16:02:34 +0100

    arch descktop sync: 2024-02-29 16:02:34

--- codeforces/contest/1937/A/main.cpp
+++ codeforces/contest/1937/A/main.cpp
@@ -10,14 +10,15 @@ using ll = long long;
 int main() {
     cin.tie(0), ios::sync_with_stdio(0);
 
-    const vector<int> v = {
-
-    };
+    const vector<int> v;
+    for (int i = 0; i < 31; i++) {
+        
+    }
     int T;
     cin >> T;
     while (T--) {
         int N;
         cin >> N;
-        cout << lower_bound(all(v), N) << '\n';
+        cout << *lower_bound(all(v), N) << '\n';
     }
 }
