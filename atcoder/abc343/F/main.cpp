8fceabe76ce05061c8d794b397781b25d2da5b1e
LordOfPumpkins <ninjaoffical08@gmail.com>
2024-03-02 13:32:09 +0100

    arch descktop sync: 2024-03-02 13:32:09

---
+++ atcoder/abc343/F/main.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define size(v) (int)v.size()
+
+using namespace std;
+using ll = long long;
+
+ifstream f("../input");
+
+#ifdef LOCAL
+#define cin f
+#endif
+
+int main() {
+
+}
