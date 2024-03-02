d4a01252ac24ab783e9b71dfd526d3c69c34b83a
LordOfPumpkins <ninjaoffical08@gmail.com>
2024-03-02 14:23:42 +0100

    arch descktop sync: 2024-03-02 14:23:42

---
+++ atcoder/abc343/E/main.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define size(v) (int)v.size()
+
+using namespace std;
+using ll = long long;
+
+ifstream f("../input");
+
+#ifdef LOCAL
+#define cin f
+#endif
+
+bool pal(ll x) {
+    string s = to_string(x);
+    int l = 0, r = size(s) - 1;
+    while (l < r) {
+        if (s[l] != s[r]) return 0;
+        l++;
+        r--;
+    }
+    return 1;
+}
+
+int main() {
+    ll N;
+    cin >> N;
+    ll x = round(cbrt(N));
+    for(;x;x--) {
+        ll cube = x * x * x;
+        if (cube <= N && pal(cube)) {
+            cout << cube;
+            exit(0);
+        }
+    }
+    cout << 1;
+}
