5466a15ce5825bbc4c513046d2dc289cbe3d169e
LordOfPumpkins <ninjaoffical08@gmail.com>
2024-03-02 14:24:04 +0100

    arch descktop sync: 2024-03-02 14:24:04

--- atcoder/abc343/E/main.cpp
+++ atcoder/abc343/E/main.cpp
@@ -13,17 +13,6 @@ ifstream f("../input");
 #define cin f
 #endif
 
-bool pal(ll x) {
-    string s = to_string(x);
-    int l = 0, r = size(s) - 1;
-    while (l < r) {
-        if (s[l] != s[r]) return 0;
-        l++;
-        r--;
-    }
-    return 1;
-}
-
 int main() {
-
+    int
 }
