f89361a717ddcdc8723746acb051cfad2f7b4fd8
magyarakoos <magyar.kende.22f@gmail.com>
2024-03-30 13:54:45 +0100

    arch descktop sync: 2024-03-30 13:54:45

--- njudge/ioi-ceoi/2023/3/labirintus/labirintus.cpp
+++ njudge/ioi-ceoi/2023/3/labirintus/labirintus.cpp
@@ -13,6 +13,6 @@ void init_labyrinth(int R, int C, std::vector<std::vector<int>> L) {
 bool can_escape(int N, std::vector<int> U, std::vector<int> V) {
 
     for (int i = 0; i < N; i++) {
-        
+        l[U[i]][]
     }
 }
