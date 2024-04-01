549cdb4d7d43c6062c33ca753ab0d3d4c07f3e7e
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-01 18:10:13 +0200

    arch descktop sync: 2024-04-01 18:10:13

--- njudge/nemes/1/NT24/dns/main.cpp
+++ njudge/nemes/1/NT24/dns/main.cpp
@@ -5,20 +5,19 @@ using ll = long long;
 int main() {
 	cin.tie(0), ios::sync_with_stdio(0);
     
+    int N;
     string s;
     cin >> s >> s;
 
     int result = 0, before_g = 0, after_g = 0;
 
-    while (N--) {
-        char C;
-        cin >> C;
-        if (C == 'A') after_g++;
-        if (C == 'G') {
+    for (int i = 0; i < N; i++)
+        if (s[i] == 'A') after_g++;
+        if (s[i] == 'G') {
             before_g += after_g;
             after_g = 0;
         }
-        if (C == 'T') {
+        if (s[i] == 'T') {
             result += before_g;
         }
     }
