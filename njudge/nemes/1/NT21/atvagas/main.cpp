63b7f159c1a29c2ffb5ecf82883f2df1208097a0
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-01 15:13:14 +0200

    arch descktop sync: 2024-04-01 15:13:14

--- njudge/nemes/1/NT21/atvagas/main.cpp
+++ njudge/nemes/1/NT21/atvagas/main.cpp
@@ -11,12 +11,16 @@ int main() {
 	cin.tie(0), ios::sync_with_stdio(0);
 
     cin >> N;
-
     if (N <= 3) {
         cout << "0\n0";
         exit(0);
     }
 
+    g.resize(N + 1);
 
+    for (int i = 1; i < N; i++) {
+        
+    }
 
+    cout << bambus() << "\n" << star();
 }
