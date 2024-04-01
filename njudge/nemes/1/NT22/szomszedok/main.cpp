2b32be571d1b52aea9f22600410862b5eb303efa
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-01 17:04:37 +0200

    arch descktop sync: 2024-04-01 17:04:37

--- njudge/nemes/1/NT22/szomszedok/main.cpp
+++ njudge/nemes/1/NT22/szomszedok/main.cpp
@@ -13,10 +13,15 @@ int main() {
     vector<int> v(N);
     for (int& x : v) cin >> x;
 
-    vector<array<int, 2>> befS, aftS;
+    vector<int> befS, aftS;
     for (int i = 0; i < N; i++) {
         if (v[i] > v[M]) {
-            befS.push_back({v[i], i})
+            befS.push_back(i);
+        }
+        else if (v[i] < v[M]) {
+            aftS.push_back(i);
         }
     }
+
+    
 }
