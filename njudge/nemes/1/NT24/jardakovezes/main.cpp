21040ac0d1b3e1346e02493a4c5585fb90a86c6b
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-01 18:28:19 +0200

    arch descktop sync: 2024-04-01 18:28:19

---
+++ njudge/nemes/1/NT24/jardakovezes/main.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main() {
+	cin.tie(0), ios::sync_with_stdio(0);
+    
+    int N;
+    cin >> N;
+
+    ll result = 0;
+    int before_g = 0, after_g = 0;
+
+    while (N--) {
+        char C;
+        cin >> C;
+
+        if (C == 'C') continue;
+        if (C == 'A') before_g++;
+        else if (C == 'G') {
+            after_g += before_g;
+            before_g = 0;
+        }
+        else {
+            result += after_g;
+        }
+    }
+
+    cout << result;
+}
