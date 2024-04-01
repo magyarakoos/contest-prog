4f06a71b8819090fd052d8811dc5cb780d5a0127
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-01 18:53:29 +0200

    arch descktop sync: 2024-04-01 18:53:29

---
+++ njudge/nemes/1/NT24/osztozkodas/main.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main() {
+    int N;
+    cin >> N;
+    vector<int> v(N);
+    for (int& x : v) cin >> x;
+    priority_queue<int> pq()
+}
