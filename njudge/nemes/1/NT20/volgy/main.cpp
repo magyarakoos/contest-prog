5b52bad3e2eff76b4c549fca1d664e2985243931
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-01 11:46:34 +0200

    arch descktop sync: 2024-04-01 11:46:34

--- njudge/nemes/1/NT20/volgy/main.cpp
+++ njudge/nemes/1/NT20/volgy/main.cpp
@@ -15,27 +15,6 @@ int main() {
     int N;
     cin >> N;
 
-
     vector<int> T(N);
-    cinv(T);
-
-    if (N < 3) {
-        cout << -1;
-        exit(0);
-    }
     
-    int max_l = 1;
-
-    for (int i = 0; i < N; i++) {
-        int max_h = 0;
-        for (int j = i + 1; j < N - 1; j++) {
-            if (T[i] <= T[j]) break;
-            max_h = max(max_h, T[j]);
-            if (max_h < T[j + 1]) {
-                max_l = max(max_l, j + 1 - i);
-            }
-        }
-    }
-
-    cout << (max_l == 1 ? -1 : max_l);
 }
