ddec3c97a120ce0f920f2ed7e24abb6d69cf776f
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-01 18:28:54 +0200

    arch descktop sync: 2024-04-01 18:28:54

--- njudge/nemes/1/NT24/jardakovezes/main.cpp
+++ njudge/nemes/1/NT24/jardakovezes/main.cpp
@@ -3,28 +3,11 @@ using namespace std;
 using ll = long long;
 
 int main() {
-	cin.tie(0), ios::sync_with_stdio(0);
-    
+
     int N;
     cin >> N;
-
-    ll result = 0;
-    int before_g = 0, after_g = 0;
-
-    while (N--) {
-        char C;
-        cin >> C;
-
-        if (C == 'C') continue;
-        if (C == 'A') before_g++;
-        else if (C == 'G') {
-            after_g += before_g;
-            before_g = 0;
-        }
-        else {
-            result += after_g;
-        }
+    vector<int> dp(N + 1);
+    for (int i = 1; i <= N; i++) {
+        
     }
-
-    cout << result;
 }
