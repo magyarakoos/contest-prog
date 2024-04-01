943232bea775ee86bdf059f3e12c43bdffcfd0b7
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-01 16:11:48 +0200

    arch descktop sync: 2024-04-01 16:11:48

--- njudge/nemes/1/NT22/tura/main.cpp
+++ njudge/nemes/1/NT22/tura/main.cpp
@@ -4,8 +4,13 @@ using ll = long long;
 
 #define all(v) v.begin(), v.end()
 
+void solve() {
+    
+}
+
 int main() {
 	cin.tie(0), ios::sync_with_stdio(0);
-
-    
+    int T;
+    cin >> T;
+    while (T--) solve();
 }
