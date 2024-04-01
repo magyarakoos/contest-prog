9371591bd5bfd94970d2df61808bccba43f5126e
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-01 18:09:38 +0200

    arch descktop sync: 2024-04-01 18:09:38

--- njudge/nemes/1/NT24/dns/main.cpp
+++ njudge/nemes/1/NT24/dns/main.cpp
@@ -5,8 +5,8 @@ using ll = long long;
 int main() {
 	cin.tie(0), ios::sync_with_stdio(0);
     
-    int N;
-    cin >> N;
+    string s;
+    cin >> s >> s;
 
     int result = 0, before_g = 0, after_g = 0;
 
