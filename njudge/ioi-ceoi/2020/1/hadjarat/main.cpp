00b4b5e4d86c9416a5c77a118a0b355f95bf0ac3
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-01 11:02:39 +0200

    arch descktop sync: 2024-04-01 11:02:39

--- njudge/ioi-ceoi/2020/1/hadjarat/main.cpp
+++ njudge/ioi-ceoi/2020/1/hadjarat/main.cpp
@@ -20,6 +20,10 @@ int main() {
     vector<int> dp(N + 1);
 
     for (int i = 1; i <= N; i++) {
-        
+        for (int j = 1; j < i; j++) {
+            
+        }
     }
+
+    cout << *max_element(all(dp));
 }
