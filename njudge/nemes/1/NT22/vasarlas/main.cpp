170de15ceddff33ad65e89deac6106892127a8fa
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-01 16:02:27 +0200

    arch descktop sync: 2024-04-01 16:02:27

--- njudge/nemes/1/NT22/vasarlas/main.cpp
+++ njudge/nemes/1/NT22/vasarlas/main.cpp
@@ -16,26 +16,6 @@ int main() {
     for (int& x : B) cin >> x;
     for (int i = 0; i < N; i++) S[i] = B[i] - A[i];
 
-    /*for (int i = 0; i < N; i++) {
-    int best_i = 0, best_j = 0, best_save = INT_MIN;
-        for (int j = i; j < N; j++) {
-            int costA = psA[j + 1] - psA[i];
-            int costB = psB[j + 1] - psB[i];
-
-            int save = costB - costA;
-
-            if (best_save < save) {
-                best_save = save;
-                best_i = i;
-                best_j = j;
-            }
-        }
-    cout << min(
-        psB.back(), 
-        psB[best_i] + (psA[best_j + 1] - psA[best_i]) + (psB.back() - psB[best_j + 1])
-    );
-    }*/
-
     int best_l = -1, best_r = -1, best_save = 0, 
         l = 0, r = 0, save = 0;
 
@@ -52,4 +32,11 @@ int main() {
             }
         }
     } 
+
+    int sum = 0;
+    for (int i = 0; i < N; i++) {
+        if (i < best_l || i > best_r) {
+            
+        }
+    }
 }
