e38e33da77efe08184b15f6fb3d71e8bfc573f74
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-01 17:08:44 +0200

    arch descktop sync: 2024-04-01 17:08:44

--- njudge/nemes/1/NT22/szomszedok/main.cpp
+++ njudge/nemes/1/NT22/szomszedok/main.cpp
@@ -15,11 +15,13 @@ int main() {
 
     vector<array<int, 2>> befS({{INT_MAX, M}}), aftS({{0, M}});
 
-    for (int i = 0; i < M; i++) {
+    for (int i = 0; i < M - 1; i++) {
         if (v[i] > v[M] && v[i] < befS.back()[0]) {
-            befS.back() = {v[i], i};
+            befS.back() = {v[i], M};
         }
-        else if (v[i] < v[M] && v[i] > )
+        else if (v[i] < v[M] && v[i] > aftS.back()[0]) {
+            aftS.back() = {v[i], M};
+        }
     }
 
     for (int i = 0; i < N; i++) {
