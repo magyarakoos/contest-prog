2637069b8258748e497d8fbac285b8db26d4b6b5
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-01 12:44:21 +0200

    arch descktop sync: 2024-04-01 12:44:21

--- njudge/nemes/2-3/NT22/logisztikai-kozpont/main.cpp
+++ njudge/nemes/2-3/NT22/logisztikai-kozpont/main.cpp
@@ -34,47 +34,13 @@ vector<T> prefix_sum(const vector<T>& v) {
 
 #pragma endregion
 
+int N;
+vector<
+
 int main() {
     speed;
 
-    int N;
     cin >> N;
 
-    vector<vector<ll>> g(N + 1, vector<ll>(N + 1, 1e18));
-    
-    for (int i = 1; i < N; i++) {
-        int u, v, w;
-        cin >> u >> v >> w;
-        g[u][v] = g[v][u] = w;
-        g[u][u] = g[v][v] = 0;
-    }
 
-    for (int k = 1; k <= N; k++) {
-        for (int i = 1; i <= N; i++) {
-            for (int j = 1; j <= N; j++) {
-                smin(g[i][j], g[i][k] + g[k][j]);
-            }
-        }
-    }
-
-    ll mn = 1e18;
-    vector<ll> values(N + 1);
-
-    for (int i = 1; i <= N; i++) {
-        for (int j = 1; j <= N; j++) {
-            smax(values[i], g[i][j]);
-        }
-        smin(mn, values[i]);
-    }
-
-    vector<int> result;
-
-    for (int i = 1; i <= N; i++) {
-        if (values[i] == mn) {
-            result.push_back(i);
-        }
-    }
-
-    cout << mn << '\n' << size(result) << '\n';
-    for (int x : result) cout << x << ' ';
 }
