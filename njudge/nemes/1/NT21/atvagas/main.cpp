4e378ea6a0ceb98569f0de9f970060ef8911baee
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-01 15:24:21 +0200

    arch descktop sync: 2024-04-01 15:24:21

--- njudge/nemes/1/NT21/atvagas/main.cpp
+++ njudge/nemes/1/NT21/atvagas/main.cpp
@@ -4,30 +4,18 @@ using ll = long long;
 
 #define all(v) v.begin(), v.end()
 
-int N;
-vector<set<int>> g;
-
-int bambus() {
-    int end_count = 0;
-    for (int i = 1; i <= N; i++) {
-        
-    }
-}
-
-int star() {
-    return 0;
-}
 
 int main() {
 	cin.tie(0), ios::sync_with_stdio(0);
 
+    int N;
     cin >> N;
     if (N <= 3) {
         cout << "0\n0";
         exit(0);
     }
 
-    g.resize(N + 1);
+    vector<ve(N + 1);
 
     for (int i = 1; i < N; i++) {
         int U, V;
