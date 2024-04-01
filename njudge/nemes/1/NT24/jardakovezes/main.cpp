f36dc293f21d232430d64155b4020ab4a88fd6a2
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-01 18:30:40 +0200

    arch descktop sync: 2024-04-01 18:30:40

--- njudge/nemes/1/NT24/jardakovezes/main.cpp
+++ njudge/nemes/1/NT24/jardakovezes/main.cpp
@@ -1,15 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
-using ll = long long;
 
 int main() {
-
     int N;
     cin >> N;
     vector<int> dp(N + 1, INT_MAX);
     dp[0] = 0;
     dp[2] = 1;
     for (int i = 3; i <= N; i++) {
-        dp[i] = min(dp[i - 2], dp[i - 3])
+        dp[i] = min(dp[i - 2], dp[i - 3]) + 1;
     }
+    cout << dp[N];
 }
