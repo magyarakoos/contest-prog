0cd44ecea1c14859ccbf6ae720dcaebbc9435ef2
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-01 18:34:12 +0200

    arch descktop sync: 2024-04-01 18:34:12

--- njudge/nemes/1/NT24/jardakovezes/main.cpp
+++ njudge/nemes/1/NT24/jardakovezes/main.cpp
@@ -8,7 +8,7 @@ int main() {
     vector<array<ll, 2>> dp(N + 1, {INT_MAX, INT_MAX});
     dp[0] = {0, 0};
     dp[2] = {1, 0};
-    dp[3] = {0, 1};
+    if (N > 2) dp[3] = {0, 1};
     for (int i = 4; i <= N; i++) {
         if (dp[i - 2][0] + dp[i - 2][1] < dp[i - 3][0] + dp[i - 3][1]) {
             dp[i] = {dp[i - 2][0] + 1, dp[i - 3][0]};
