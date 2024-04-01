3e1ccf521939dbe00c4272730243cd434a71bf8f
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-01 18:30:05 +0200

    arch descktop sync: 2024-04-01 18:30:05

--- njudge/nemes/1/NT24/jardakovezes/main.cpp
+++ njudge/nemes/1/NT24/jardakovezes/main.cpp
@@ -7,7 +7,9 @@ int main() {
     int N;
     cin >> N;
     vector<int> dp(N + 1, INT_MAX);
+    dp[0] = 0;
+    dp[2] = 1;
     for (int i = 3; i <= N; i++) {
-
+        dp[i] = min(dp[i - 2], dp[i - 3])
     }
 }
