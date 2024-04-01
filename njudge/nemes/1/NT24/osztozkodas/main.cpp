3312503e51cf53bc32a81e1972b179e5877e4955
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-01 19:01:08 +0200

    arch descktop sync: 2024-04-01 19:01:08

--- njudge/nemes/1/NT24/osztozkodas/main.cpp
+++ njudge/nemes/1/NT24/osztozkodas/main.cpp
@@ -16,13 +16,18 @@ int main() {
     
     for (int x : ps) cerr << x << " ";
 
-    bool a_turn = 1;
+    array<ll, 2> playerS;
+
+    bool turn = 0;
 
     for (int i = v.size() - 1; i >= 0; i--) {
         if (i + 1 <= K) {
             if (v[i] < ps[i]) {
-                
+                playerS[turn] += ps[i];
+                break;
             }
         }
+
+        turn ^= 1;
     }
 }
