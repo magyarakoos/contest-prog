74895d07e66fd2ac2eaa7135ddafdb97e399c940
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-01 16:39:19 +0200

    arch descktop sync: 2024-04-01 16:39:19

--- njudge/nemes/1/NT22/tura/main.cpp
+++ njudge/nemes/1/NT22/tura/main.cpp
@@ -20,18 +20,17 @@ void solve() {
         int U, V;
         cin >> U >> V;
         if (running) {
+            running = 0;
             if (ugly[U] && ugly[V]) {
                 cout << "NEM\n2\n" << U << " " << V << "\n";
-                running = 0;
             }
             else if (uglyNeighbor[U] && ugly[V]) {
                 cout << "NEM\n3\n" << uglyNeighbor[U] << " " << U << " " << V << "\n";
-                running = 0;
             }
             else if (ugly[U] && uglyNeighbor[V]) {
-                cout << "NEM\n3\n" << uglyNeighbor[V] << " " << V << " " << U << "\n"; 
-                running = 0;
+                cout << "NEM\n3\n" << uglyNeighbor[V] << " " << V << " " << U << "\n";
             }
+            else running = 1;
         }
         
         if (ugly[U]) {
