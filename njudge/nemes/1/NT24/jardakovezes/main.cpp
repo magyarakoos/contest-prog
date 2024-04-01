a27ad3f5b506dabbaf079f9fd2f30daa5c746c93
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-01 18:34:48 +0200

    arch descktop sync: 2024-04-01 18:34:48

--- njudge/nemes/1/NT24/jardakovezes/main.cpp
+++ njudge/nemes/1/NT24/jardakovezes/main.cpp
@@ -1,20 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
-using ll = long long;
 
 int main() {
     int N;
     cin >> N;
-    vector<array<ll, 2>> dp(N + 1, {INT_MAX, INT_MAX});
-    dp[0] = {0, 0};
-    dp[2] = {1, 0};
-    if (N > 2) dp[3] = {0, 1};
-    for (int i = 4; i <= N; i++) {
-        if (dp[i - 2][0] + dp[i - 2][1] < dp[i - 3][0] + dp[i - 3][1]) {
-            dp[i] = {dp[i - 2][0] + 1, dp[i - 3][0]};
-        } else {
-            dp[i] = {dp[i - 2][0], dp[i - 3][0] + 1};
-        }
+    vector<int> dp(N + 1, INT_MAX);
+    dp[0] = 0;
+    dp[2] = 1;
+    for (int i = 3; i <= N; i++) {
+        dp[i] = min(dp[i - 2], dp[i - 3]) + 1;
     }
-    cout << dp[N][0] << " " << dp[N][1];
+    cout << dp[N];
 }
