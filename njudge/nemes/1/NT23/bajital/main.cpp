45bfbaccf85185b13c0293da213f27da7f93eb88
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-01 12:19:19 +0200

    arch descktop sync: 2024-04-01 12:19:19

--- njudge/nemes/1/NT23/bajital/main.cpp
+++ njudge/nemes/1/NT23/bajital/main.cpp
@@ -1,20 +1,11 @@
 #include <bits/stdc++.h>
-
-#define speed cin.tie(0); ios::sync_with_stdio(0)
-#define cinv(v) for (auto& e : v) cin >> e;
-#define all(v) v.begin(), v.end()
-#define has(s, e) s.count(e)
-
 using namespace std;
-using ll = long long;
-using point = array<int, 2>;
 
 int main() {
-    speed;
-
-    int N, X;
+    long long N, X, sum, divS;
     cin >> N >> X;
-    ll sum = 0, divS = 0;
 
+    while (N--) {
 
+    }
 }
