808a40e3326ee560e2ce0d917630061ec118964e
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-01 18:10:48 +0200

    arch descktop sync: 2024-04-01 18:10:48

--- njudge/nemes/1/NT24/dns/main.cpp
+++ njudge/nemes/1/NT24/dns/main.cpp
@@ -1,17 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
-using ll = long long;
 
 int main() {
-	cin.tie(0), ios::sync_with_stdio(0);
-    
     int N;
     string s;
     cin >> s >> s;
 
     int result = 0, before_g = 0, after_g = 0;
 
-    for (int i = 0; i < N; i++)
+    for (int i = 0; i < N; i++) {
         if (s[i] == 'A') after_g++;
         if (s[i] == 'G') {
             before_g += after_g;
