865f2bef9575f19588e675212228c058197c8443
magyarakoos <ninjaoffical08@gmail.com>
2024-04-03 16:38:55 +0200

    arch descktop sync: 2024-04-03 16:38:55

--- njudge/nemes/1/NT24/csalodott/main.cpp
+++ njudge/nemes/1/NT24/csalodott/main.cpp
@@ -1,9 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-ifstream fin("minta(1)/be2.txt");
-#define cin fin
-
 int main() {
     cin.tie(0), ios::sync_with_stdio(0);
 
@@ -12,17 +9,17 @@ int main() {
     vector<int> v(N);
     for (int& x : v) cin >> x;
 
-    int l = 0, r = 1, mx = -1, result_i = -1;
+    int l = 0, r = 1, mx = -1, result = -1;
     while (r < N) {
         if (v[l] < v[r]) {
             if (mx < r - l) {
                 mx = r - l;
-                result_i = l + 1;
+                result = l + 1;
             }
             l = r;
         }
         r++;
     }
 
-    cout << result_i;
+    cout << result;
 }
