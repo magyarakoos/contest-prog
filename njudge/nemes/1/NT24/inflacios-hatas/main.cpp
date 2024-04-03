0b2d6e7a2f641bcbb0ac15ed5c24754e562201e9
magyarakoos <ninjaoffical08@gmail.com>
2024-04-03 16:44:09 +0200

    arch descktop sync: 2024-04-03 16:44:09

--- njudge/nemes/1/NT24/inflacios-hatas/main.cpp
+++ njudge/nemes/1/NT24/inflacios-hatas/main.cpp
@@ -1,5 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
+using ll = long long;
 
 int main() {
     cin.tie(0), ios::sync_with_stdio(0);
@@ -9,5 +10,9 @@ int main() {
     vector<int> v(N);
     for (int& x : v) cin >> x;
 
-    priority_queue<
+    priority_queue<int, vector<int>, greater<int>> pq(v.begin(), v.end());
+
+    ll result = 0;
+
+    while ()
 }
