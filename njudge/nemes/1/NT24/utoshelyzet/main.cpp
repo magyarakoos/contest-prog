063270c50ed81ea1918d7e6df3f95002137821ee
magyarakoos <ninjaoffical08@gmail.com>
2024-04-03 15:41:35 +0200

    arch descktop sync: 2024-04-03 15:41:35

--- njudge/nemes/1/NT24/utoshelyzet/main.cpp
+++ njudge/nemes/1/NT24/utoshelyzet/main.cpp
@@ -2,6 +2,39 @@
 using namespace std;
 
 vector<vector<int>> v(8, vector<int>(8));
+
+void setvals(int X, int Y, int type) {
+    for (int i = Y + 1; i < 8; i++) {
+        v[i][X] = 0;
+    }
+    for (int i = Y - 1; i >= 0; i--) {
+        v[i][X] = 0;
+    }
+    for (int j = X + 1; j < 8; j++) {
+        v[Y][j] = 0;
+    }
+    for (int j = X - 1; j >= 0; j--) {
+        v[Y][j] = 0;
+    }
+
+    if (v[Y][X] == 2) {
+        for (int i = Y + 1, j = X + 1; i < 8 && j < 8; i++, j++) {
+            v[i][j] = 0;
+        }
+        for (int i = Y - 1, j = X - 1; i >= 0 && j >= 0; i--, j--) {
+            v[i][j] = 0;
+        }
+        for (int i = Y - 1, j = X + 1; i >= 0 && j < 8; i--, j++) {
+            v[i][j] = 0;
+        }
+        for (int i = Y + 1, j = X - 1; i < 8 && j >= 0; i++, j--) {
+            v[i][j] = 0;
+        }
+    }
+
+    v[Y][X] = 0;
+}
+
 int main() {
     cin.tie(0), ios::sync_with_stdio(0);
 
@@ -13,35 +46,7 @@ int main() {
         X--; Y--;
         
         if (T == 3) {
-            for (int i = Y + 1; i < 8; i++) {
-                v[i][X] = 0;
-            }
-            for (int i = Y - 1; i >= 0; i--) {
-                v[i][X] = 0;
-            }
-            for (int j = X + 1; j < 8; j++) {
-                v[Y][j] = 0;
-            }
-            for (int j = X - 1; j >= 0; j--) {
-                v[Y][j] = 0;
-            }
-
-            if (v[Y][X] == 2) {
-                for (int i = Y + 1, j = X + 1; i < 8 && j < 8; i++, j++) {
-                    v[i][j] = 0;
-                }
-                for (int i = Y - 1, j = X - 1; i >= 0 && j >= 0; i--, j--) {
-                    v[i][j] = 0;
-                }
-                for (int i = Y - 1, j = X + 1; i >= 0 && j < 8; i--, j++) {
-                    v[i][j] = 0;
-                }
-                for (int i = Y + 1, j = X - 1; i < 8 && j >= 0; i++, j--) {
-                    v[i][j] = 0;
-                }
-            }
-
-            v[Y][X] = 0;
+            
             cout << "-\n";
             continue;
         }
