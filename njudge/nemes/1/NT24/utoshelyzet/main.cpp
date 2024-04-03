b02799f2266dd1e454d0dc3ef7d35df11ac4fad7
magyarakoos <ninjaoffical08@gmail.com>
2024-04-03 15:38:03 +0200

    arch descktop sync: 2024-04-03 15:38:03

--- njudge/nemes/1/NT24/utoshelyzet/main.cpp
+++ njudge/nemes/1/NT24/utoshelyzet/main.cpp
@@ -1,10 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+vector<vector<int>> v(8, vector<int>(8));
 int main() {
     cin.tie(0), ios::sync_with_stdio(0);
 
-    vector<vector<int>> v(8, vector<int>(8));
     int N;
     cin >> N;
     while (N--) {
