b1a46132af17f4aeddab1f11f6733c6383fcc4ca
magyarakoos <ninjaoffical08@gmail.com>
2024-04-03 15:41:00 +0200

    arch descktop sync: 2024-04-03 15:41:00

--- njudge/nemes/1/NT24/utoshelyzet/main.cpp
+++ njudge/nemes/1/NT24/utoshelyzet/main.cpp
@@ -36,6 +36,9 @@ int main() {
                 for (int i = Y - 1, j = X + 1; i >= 0 && j < 8; i--, j++) {
                     v[i][j] = 0;
                 }
+                for (int i = Y + 1, j = X - 1; i < 8 && j >= 0; i++, j--) {
+                    v[i][j] = 0;
+                }
             }
 
             v[Y][X] = 0;
