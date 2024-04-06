fe38403330e908904a338ff5da34e222222a4bd5
magyarakoos <ninjaoffical08@gmail.com>
2024-04-06 11:38:35 +0200

    arch descktop sync: 2024-04-06 11:38:35

--- njudge/nemes/2-3/NT20/kombinacio/main.cpp
+++ njudge/nemes/2-3/NT20/kombinacio/main.cpp
@@ -26,6 +26,23 @@ vector<int> after(vector<int> v) {
     return v;
 }
 
+vector<int> before(vector<int> v) {
+    for (int i = M - 1 ; i >= 0; i--) {
+        if (v[i] < N - (M - i - 1)) {
+            v[i]++;
+            for (int j = i + 1; j < M; j++) {
+                v[j] = v[i] + j - i;
+            }
+            return v;
+        }
+    }
+    // körbeértünk, lex-minimális lesz a megoldás
+    for (int i = 0; i < M; i++) {
+        v[i] = i + 1;
+    }
+    return v;
+}
+
 int main() {
     cin.tie(0), ios::sync_with_stdio(0);
 
@@ -33,7 +50,8 @@ int main() {
     vector<int> v(M);
     for (int& x : v) cin >> x;
     
-    
+    for (int x : before(v)) cout << x << " ";
+    cout << "\n";
     for (int x : after(v)) cout << x << " ";
     cout << "\n";
     
