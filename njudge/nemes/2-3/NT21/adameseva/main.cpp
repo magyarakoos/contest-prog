df1de342e798826e7d65d56e78b2375754240917
magyarakoos <ninjaoffical08@gmail.com>
2024-04-06 12:37:53 +0200

    arch descktop sync: 2024-04-06 12:37:53

--- njudge/nemes/2-3/NT21/adameseva/main.cpp
+++ njudge/nemes/2-3/NT21/adameseva/main.cpp
@@ -11,12 +11,15 @@ int main() {
     cin.tie(0), ios::sync_with_stdio(0);
 
     map<int, vector<pair<char, bool>>> m;
-    int P, A;
+    int P, A, E;
     cin >> P >> A;
 
     while (A--) {
         int AK, AV;
         cin >> AK >> AV;
-        m[AK].push_back()
+        m[AK].push_back({'A', 1});
+        m[AV].push_back({'A', 0});
     }
+
+    int E
 }
