060cdb41ec0c6e750532f4364a578f62b51d3839
magyarakoos <ninjaoffical08@gmail.com>
2024-04-06 13:59:06 +0200

    arch descktop sync: 2024-04-06 13:59:06

--- njudge/nemes/2-3/NT22/logisztikai-kozpont/main.cpp
+++ njudge/nemes/2-3/NT22/logisztikai-kozpont/main.cpp
@@ -2,7 +2,7 @@
 
 #pragma region Utility
 
-#define speed cin.tie(0); ios::sync_with_stdio(0)
+#define speed 
 #define cinv(v) for (auto& e : v) cin >> e;
 
 #define all(v) v.begin(), v.end()
@@ -15,24 +15,6 @@
 #define smax(x, y) x = max(x, y)
 #define smin(x, y) x = min(x, y)
 
-#define sum(v) accumulate(all(v), 0)
-#define product(v, T) accumulate(all(v), 1, multiplies<T>())
-
-using namespace std;
-using ll = long long;
-using point = array<int, 2>;
-
-int max(point p) { return max(p[0], p[1]); }
-int min(point p) { return min(p[0], p[1]); }
-
-template <typename T>
-vector<T> prefix_sum(const vector<T>& v) {
-    vector<T> result(size(v));
-    partial_sum(all(v), result.begin());
-    return result;
-}
-
-#pragma endregion
 
 int N;
 vector<vector<array<ll, 2>>> g;
@@ -63,7 +45,7 @@ void proc(int start) {
 }
 
 int main() {
-    speed;
+    cin.tie(0); ios::sync_with_stdio(0);
     cin >> N;
 
     g.resize(N + 1);
