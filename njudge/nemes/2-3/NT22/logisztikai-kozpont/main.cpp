c7661f20b988e8a543089492666258c21b61fd22
magyarakoos <ninjaoffical08@gmail.com>
2024-04-06 13:02:48 +0200

    arch descktop sync: 2024-04-06 13:02:48

--- njudge/nemes/2-3/NT22/logisztikai-kozpont/main.cpp
+++ njudge/nemes/2-3/NT22/logisztikai-kozpont/main.cpp
@@ -36,45 +36,12 @@ vector<T> prefix_sum(const vector<T>& v) {
 
 int N;
 vector<vector<array<ll, 2>>> g;
-vector<int> prv;
-vector<ll> prv_dist;
-
-// which is the farthest node from s
-int bfs(int s) {
-    vector<bool> vis(N + 1);
-    vector<ll> dist(N + 1, 1e18);
-    queue<int> q;
-
-    dist[0] = -1;
-
-    vis[s] = 1;
-    dist[s] = 0;
-    q.push(s);
-
-    while (!q.empty()) {
-        int u = q.front(); q.pop();
-        for (auto [v, w] : g[u]) {
-            if (!vis[v]) {
-                vis[v] = 1;
-                dist[v] = dist[u] + w;
-                prv[v] = u;
-                prv_dist[v] = w;
-                q.push(v);
-            }
-        }
-    }
-
-    return max_element(all(dist)) - dist.begin();
-}
 
 int main() {
     speed;
     cin >> N;
 
-    prv.resize(N + 1);
-    prv_dist.resize(N + 1);
     g.resize(N + 1);
-
     for (int i = 1; i < N; i++) {
         int U, V, W;
         cin >> U >> V >> W;
@@ -82,42 +49,4 @@ int main() {
         g[V].push_back({U, W});
     }
 
-    int start = bfs(1);
-    int end = bfs(start);
-
-    vector<ll> distS;
-    vector<int> nodeS;
-
-    int curr = end;
-    do {
-        distS.push_back(prv_dist[curr]);
-        nodeS.push_back(prv[curr]);
-        curr = prv[curr];
-    } while (curr != start);
-
-    ll sum = sum(distS);
-    auto pref = prefix_sum(distS);
-
-    vector<ll> valueS(size(distS));
-
-    ll best = sum;
-
-    for (int i = 0; i < size(distS); i++) {
-        valueS[i] = max(pref[i], sum - pref[i]);
-        best = min(best, valueS[i]);
-    }
-
-    cout << best << "\n";
-
-    vector<int> result;
-
-    for (int i = 0; i < size(valueS); i++) {
-        if (valueS[i] == best) {
-            result.push_back(nodeS[i]);
-        }
-    }
-
-    cout << size(result) << "\n";
-    for (int x : result) cout << x << " ";
-    cout << "\n";
 }
