738e058d58e158b3c0e60cc952e60c3f42d8a4f5
magyarakoos <ninjaoffical08@gmail.com>
2024-04-06 11:51:22 +0200

    arch descktop sync: 2024-04-06 11:51:22

--- njudge/nemes/2-3/NT20/siksag/main.cpp
+++ njudge/nemes/2-3/NT20/siksag/main.cpp
@@ -1,32 +1,14 @@
-#include <iostream>
-#include <vector>
+#include <bits/stdc++.h>
+
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define size(v) (int)v.size()
+
 using namespace std;
-#define speed cin.tie(0); ios::sync_with_stdio(0)
+using ll = long long;
 
-
-int N, l, r, bestl, bestr;
 int main() {
-    cin >> N;
-    vector<int> M(N);
-    for (int i = 0; i < N; i++) cin >> M[i];
+    cin.tie(0), ios::sync_with_stdio(0);
 
-    while (++r < N) {
-        cout << l << ' ' << r << '\n';
-        if (abs(M[l] - M[r]) > 1) {
-            if (bestr - bestl < r - l - 1) {
-                bestl = l;
-                bestr = r - 1;
-            }
-
-            while (1) {
-                abs(M[l] - M[r]) > 1
-            }
-
-        } else if (bestr - bestl < r - l) {
-            bestl = l;
-            bestr = r;
-        }
-    }
-
-    cout << bestl << ' ' << bestr;
+    
 }
