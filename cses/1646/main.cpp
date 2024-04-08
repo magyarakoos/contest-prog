72aaf271a98bd097e2c6788054ad24b82e8ce0d9
magyarakoos <ninjaoffical08@gmail.com>
2024-04-08 20:20:04 +0200

    arch descktop sync: 2024-04-08 20:20:04

---
+++ cses/1646/main.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define size(v) (int)v.size()
+
+using namespace std;
+using ll = long long;
+
+int main() {
+    cin.tie(0), ios::sync_with_stdio(0);
+
+    int N, Q;
+    cin >> N >> Q;
+    vector<int> v(N), ps(N + 1);
+    for (int& x : v) cin >> x;
+
+    for (int i = 1; i <= N; i++) {
+        ps[i] = v[i]
+    }
+}
