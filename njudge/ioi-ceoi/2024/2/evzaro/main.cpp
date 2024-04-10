21f99694fe0006175cd04420fcc29033ee02aacf
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-10 08:27:44 +0200

    arch descktop sync: 2024-04-10 08:27:44

--- njudge/ioi-ceoi/2024/2/evzaro/main.cpp
+++ njudge/ioi-ceoi/2024/2/evzaro/main.cpp
@@ -3,19 +3,23 @@ using namespace std;
 using ll = long long;
 using point = array<int, 2>;
 
+map<point, pair<int, bool>> m;
+vector<point> v;
+
 int main() {
     cin.tie(0), ios::sync_with_stdio(0);
 
-    map<point, pair<int, bool>> m;
     int N;
     cin >> N;
-    vector<point> v(N);
+    v.resize(N);
     for (int i = 0; i < N; i++) {
         cin >> v[i][0] >> v[i][1];
         m[{v[i][0], v[i][1]}] = {i, 0};
     }
     string s(N, 'L');
     for (int i = 0; i < N; i++) {
-        if (!m[])
+        if (!m[v[i]].second) {
+            bfs()
+        }
     }
 }
