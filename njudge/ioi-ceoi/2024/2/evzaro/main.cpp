75ce58b1649eb89dfbbe5f6bf83b520cc2690818
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-10 08:24:52 +0200

    arch descktop sync: 2024-04-10 08:24:52

--- njudge/ioi-ceoi/2024/2/evzaro/main.cpp
+++ njudge/ioi-ceoi/2024/2/evzaro/main.cpp
@@ -1,9 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
+using point = array<int, 2>;
 
 int main() {
     cin.tie(0), ios::sync_with_stdio(0);
 
-    
+    map<point, int> m;
+    int N;
+    cin >> N;
+    for (int i = 0; i < N; i++) {
+        int X, Y;
+        cin >> X >> Y;
+        m[{X, Y}] = i;
+    }
+    vector<
 }
