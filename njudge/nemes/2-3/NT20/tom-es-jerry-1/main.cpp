9bc753167b6834b4e4e10d296c437876d83323d7
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-11 09:23:06 +0200

    arch descktop sync: 2024-04-11 09:23:06

--- njudge/nemes/2-3/NT20/tom-es-jerry-1/main.cpp
+++ njudge/nemes/2-3/NT20/tom-es-jerry-1/main.cpp
@@ -5,12 +5,11 @@ int N, M, T, P, E, INF = 1e9;
 vector<vector<array<int, 2>>> g;
 vector<int> tom, jerry;
 
-void bfs(int s, vector<int>& d, int min_w, int goal) {
+void bfs(int s, vector<int>& d, int min_w) {
     queue<int> q({s});
     d[s] = 0;
     while (!q.empty()) {
         int u = q.front(); q.pop();
-        if (u == goal) return;
         for (auto [v, w] : g[u]) {
             if (d[u] + 1 < d[v] && min_w <= w && d[u] + 1 < tom[v]) {
                 d[v] = d[u] + 1;
@@ -27,20 +26,19 @@ int main() {
 
     g.resize(N + 1);
     tom.assign(N + 1, INF);
+    jerry.assign(N + 1, INF);
     while (M--) {
         int A, B, S;
         cin >> A >> B >> S;
-    
         g[A].push_back({B, S});
         g[B].push_back({A, S});
     }
 
-    bfs(T, tom, 2, 0);
-    bfs(K, jerry, 1, E);
+    bfs(T, tom, 2);
+    bfs(E, jerry, 1);
     while (P--) {
         int K;
         cin >> K;
-        jerry.assign(N + 1, INF);
         cout << (jerry[E] < INF ? "IGEN\n" : "NEM\n");
     }
 }
