c74a73cde0d03e9004fe76ee3e40a4f57f6ff3e2
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-11 13:10:47 +0200

    arch descktop sync: 2024-04-11 13:10:47

--- njudge/nemes/2-3/NT20/tom-es-jerry-1/main.cpp
+++ njudge/nemes/2-3/NT20/tom-es-jerry-1/main.cpp
@@ -5,10 +5,6 @@ int N, M, T, P, E, INF = 1e9;
 vector<vector<array<int, 2>>> g;
 vector<int> tom, jerry;
 
-void bfs(int s, vector<int>& d, int min_w, int goal) {
-    
-}
-
 int main() {
     cin.tie(0), ios::sync_with_stdio(0);
 
@@ -23,22 +19,22 @@ int main() {
         g[A].push_back({B, S});
         g[B].push_back({A, S});
     }
+
     queue<int> q({T});
     tom[T] = 0;
     while (!q.empty()) {
         int u = q.front(); q.pop();
         for (auto [v, w] : g[u]) {
-            if (tom[u] + 1 < tom[v] && min_w <= w && d[u] + 1 < tom[v]) {
-                d[v] = d[u] + 1;
+            if (tom[u] + 1 < tom[v] && w == 2) {
+                tom[v] = tom[u] + 1;
                 q.push(v);
             }
         }
     }
+
     while (P--) {
         int K;
         cin >> K;
-        jerry.assign(N + 1, INF);
-        bfs(K, jerry, 1, E);
         cout << (jerry[E] < INF ? "IGEN\n" : "NEM\n");
     }
 }
