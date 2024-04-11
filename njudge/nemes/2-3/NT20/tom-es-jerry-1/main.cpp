f539a4ba815ba926d7507d892bb4eeb7a31a6245
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-11 13:46:57 +0200

    arch descktop sync: 2024-04-11 13:46:57

--- njudge/nemes/2-3/NT20/tom-es-jerry-1/main.cpp
+++ njudge/nemes/2-3/NT20/tom-es-jerry-1/main.cpp
@@ -2,52 +2,52 @@
 using namespace std;
 
 int main() {
-  cin.tie(0), ios::sync_with_stdio(0);
-  int N, M, T, P, E;
-  cin >> N >> M >> T >> P >> E;
-  vector<vector<int>> jg(N + 1), tg(N + 1);
-  while (M--) {
-    int U, V, W;
-    cin >> U >> V >> W;
-    jg[U].push_back(V);
-    jg[V].push_back(U);
-    if (W == 2) {
-      tg[U].push_back(V);
-      tg[V].push_back(U);
+    cin.tie(0), ios::sync_with_stdio(0);
+    int N, M, T, P, E;
+    cin >> N >> M >> T >> P >> E;
+    vector<vector<int>> jg(N + 1), tg(N + 1);
+    while (M--) {
+        int U, V, W;
+        cin >> U >> V >> W;
+        jg[U].push_back(V);
+        jg[V].push_back(U);
+        if (W == 2) {
+            tg[U].push_back(V);
+            tg[V].push_back(U);
+        }
     }
-  }
 
-  vector<int> tom(N + 1, INT_MAX);
-  queue<int> q({T});
-  tom[T] = 0;
-  while (!q.empty()) {
-    int u = q.front(); q.pop();
-    for (auto v : tg[u]) {
-      if (tom[v] == INT_MAX) {
-        tom[v] = tom[u] + 1;
-        q.push(v);
-      }
+    vector<int> tom(N + 1, INT_MAX);
+    queue<int> q({T});
+    tom[T] = 0;
+    while (!q.empty()) {
+        int u = q.front(); q.pop();
+        for (auto v : tg[u]) {
+            if (tom[v] == INT_MAX) {
+                tom[v] = tom[u] + 1;
+                q.push(v);
+            }
+        }
     }
-  }
 
-  vector<bool> jerry(N + 1);
-  jerry[E] = 1;
-  priority_queue<array<int, 2>> pq;
-  pq.push({tom[E], E});
-  while (!pq.empty()) {
-    auto [dist, u] = pq.top(); pq.pop();
-    if (dist < 0) continue;
-    jerry[u] = 1;
-    for (int v : jg[u]) {
-      if (!jerry[v]) {
-        pq.push({min(tom[v] - 1, dist - 1), v});
-      }
+    vector<bool> jerry(N + 1);
+    jerry[E] = 1;
+    priority_queue<array<int, 2>> pq;
+    pq.push({tom[E], E});
+    while (!pq.empty()) {
+        auto [dist, u] = pq.top(); pq.pop();
+        if (dist < 0) continue;
+        jerry[u] = 1;
+        for (int v : jg[u]) {
+            if (!jerry[v]) {
+                pq.push({min(tom[v] - 1, dist - 1), v});
+            }
+        }
     }
-  }
 
-  while (P--) {
-    int jerry_start;
-    cin >> jerry_start;
-    cout << (jerry[jerry_start] ? "IGEN" : "NEM") << endl;
-  }
+    while (P--) {
+        int K;
+        cin >> K;
+        cout << (jerry[K] ? "IGEN" : "NEM") << '\n';
+    }
 }
