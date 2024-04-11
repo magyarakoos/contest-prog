cdf781f4c48c85b192dadb33f311853ed9bc9a28
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-11 07:33:58 +0200

    arch descktop sync: 2024-04-11 07:33:58

--- njudge/nemes/2-3/NT20/tom-es-jerry-1/main.cpp
+++ njudge/nemes/2-3/NT20/tom-es-jerry-1/main.cpp
@@ -1,43 +1,11 @@
 
 #include <bits/stdc++.h>
-
-#pragma region Utility
-
-#define speed cin.tie(0); ios::sync_with_stdio(0)
-#define cinv(v) for (auto& e : v) cin >> e;
-
-#define all(v) v.begin(), v.end()
-#define rall(v) v.rbegin(), v.rend()
-#define size(v) (int)v.size()
-#define has(s, e) s.count(e)
-
-#define max_index(v) max_element(all(v)) - v.begin()
-#define min_index(v) min_element(all(v)) - v.begin()
-#define smax(x, y) x = max(x, y)
-#define smin(x, y) x = min(x, y)
-
-#define sum(v) accumulate(all(v), 0)
-#define product(v, T) accumulate(all(v), 1, multiplies<T>())
-
 using namespace std;
 using ll = long long;
 using point = array<int, 2>;
 
-int max(point p) { return max(p[0], p[1]); }
-int min(point p) { return min(p[0], p[1]); }
-
-template <typename T>
-vector<T> prefix_sum(const vector<T>& v) {
-    vector<T> result(size(v));
-    partial_sum(all(v), result.begin());
-    return result;
-}
-
-#pragma endregion
-
-int N, M, T, P, E;
+int N, M, T, P, E, INF = 1e9;
 vector<vector<point>> g;
-vector<vector<int>> tom, jerry;
 
 int main() {
     speed;
@@ -54,8 +22,9 @@ int main() {
         g[A].push_back({B, S});
         g[B].push_back({A, S});
     }
+    vector<vector<int>> tom(N + 1,), jerry;
 
-    
+
 
     while (P--) {
         int K;
