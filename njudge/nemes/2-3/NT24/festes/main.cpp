567647d852fd937e7742e767a0931fe42a17dcc6
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-12 12:08:56 +0200

    arch descktop sync: 2024-04-12 12:08:56

--- njudge/nemes/2-3/NT24/festes/main.cpp
+++ njudge/nemes/2-3/NT24/festes/main.cpp
@@ -7,11 +7,12 @@ int N, M;
 vec<ll> rowS;
 vec<vec<vec<ll>>> colS;
 
+
 int main() {
     cin.tie(0), ios::sync_with_stdio(0);
     cin >> N >> M;
     rowS.resize(N);
-    colS.assign(M, vec<vec<ll>>)
+    colS.assign(M, vec<vec<ll>>(N, vec<ll>(N)));
 
     for (int i = 0; i < N; i++) {
         cin >> rowS[i];
