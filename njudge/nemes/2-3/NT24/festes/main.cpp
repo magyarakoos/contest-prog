afb621cb20f36043711cdb010e309e3b2ae807e7
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-12 12:07:49 +0200

    arch descktop sync: 2024-04-12 12:07:49

--- njudge/nemes/2-3/NT24/festes/main.cpp
+++ njudge/nemes/2-3/NT24/festes/main.cpp
@@ -3,12 +3,14 @@ using namespace std;
 using ll = long long;
 #define vec vector
 
+int N, M;
+vec<vec<vec<ll>>> colS;
+vec<ll> rowS;
+
 int main() {
     cin.tie(0), ios::sync_with_stdio(0);
     int N, M;
     cin >> N >> M;
-    vec<ll> rowS(N);
-    vec<vec<vec<ll>>> colS(M, vec<vec<ll>>(N, vec<ll>(N)));
 
     for (int i = 0; i < N; i++) {
         cin >> rowS[i];
