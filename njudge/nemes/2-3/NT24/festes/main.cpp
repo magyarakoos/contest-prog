67971d6301964475067cde7be7fcd7021d122d2f
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-12 11:22:19 +0200

    arch descktop sync: 2024-04-12 11:22:19

--- njudge/nemes/2-3/NT24/festes/main.cpp
+++ njudge/nemes/2-3/NT24/festes/main.cpp
@@ -9,12 +9,17 @@ int main() {
     cin >> N >> M;
     vec<ll> rowS(N);
     vec<vec<vec<ll>>> colS(M, vec<vec<ll>>(N, vec<ll>(N)));
+
     for (int i = 0; i < N; i++) {
         cin >> rowS[i];
     }
     for (int i = 0; i < M; i++) {
         for (int j = 0; j < N; j++) {
-            for (int k = )
+            for (int k = j; k < N; k++) {
+                cin >> colS[i][j][k];
+            }
         }
     }
+
+    
 }
