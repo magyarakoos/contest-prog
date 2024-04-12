27cad0099bb7d11d763fa6a8b3f5a6278776a0f2
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-12 11:17:11 +0200

    arch descktop sync: 2024-04-12 11:17:11

--- njudge/nemes/2-3/NT24/festes/main.cpp
+++ njudge/nemes/2-3/NT24/festes/main.cpp
@@ -1,13 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
-using vec = vector;
+#define vec vector;
 
 int main() {
     cin.tie(0), ios::sync_with_stdio(0);
     int N, M;
     cin >> N >> M;
-    vector<ll> rowS(N);
-    vector<vector<vector<ll>>> colS(M, vector<vector<ll>>(N, vector<ll>(N)));
+    vec<ll> rowS(N);
+    vec<vec<vec<ll>>> colS(M, vec<vec<ll>>(N, vec<ll>(N)));
 
 }
