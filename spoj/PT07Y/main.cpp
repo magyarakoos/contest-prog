8e2d5b582c0d764c53368e21fb8f17d81b2b1cfe
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-15 17:22:23 +0200

    arch descktop sync: 2024-04-15 17:22:23

--- spoj/PT07Y/main.cpp
+++ spoj/PT07Y/main.cpp
@@ -1,13 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define end(b) cout << (b ? "YES" : "NO"); exit(0)
+#define over(b) cout << (b ? "YES" : "NO"); exit(0)
 vector<vector<int>> g;
 vector<bool> vis;
 
 void dfs(int u, int p) {
     for (int v : g[u]) {
         if (v == p) continue;
-        if (vis[v]) end(0);
+        if (vis[v]) over(0);
         vis[v] = 1;
         dfs(v, u);
     }
@@ -16,7 +16,7 @@ void dfs(int u, int p) {
 int main() {
     int N, M;
     cin >> N >> M;
-    if (M != N - 1) end(0);
+    if (M != N - 1) over(0);
 
     g.resize(N + 1);
     vis.resize(N + 1);
@@ -26,5 +26,5 @@ int main() {
         g[U].push_back(V);
         g[V].push_back(U);
     }
-    end((count(vis.begin() + 1, vis.end(), 1) == N));
+    over((count(vis.begin() + 1, vis.end(), 1) == N));
 }
