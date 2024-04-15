8aa3f5cf7de01f6674945cf0859bd52ba7043d5b
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-15 17:19:52 +0200

    arch descktop sync: 2024-04-15 17:19:52

--- spoj/PT07Y/main.cpp
+++ spoj/PT07Y/main.cpp
@@ -1,9 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define end(b) cout << (b ? "YES" : "NO"); exit(0)
+vector<vector<int>> g;
 
 int main() {
     int N, M;
     cin >> N >> M;
-    if (M != N - 1)
+    if (M != N - 1) end(0);
+    while (M--) {
+        int U, V;
+        cin >> U >> V;
+    }
 }
