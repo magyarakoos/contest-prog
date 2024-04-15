0234b520347cacc10fbc26ab6940581842968f15
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-15 17:51:15 +0200

    arch descktop sync: 2024-04-15 17:51:15

--- cses/1681/main.cpp
+++ cses/1681/main.cpp
@@ -1,33 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
-using ll = long long;
 constexpr int MOD = 1e9 + 7;
-vector<vector<int>> g;
-vector<bool> vis;
-vector<int> dp;
-
-void dfs(int u) {
-    for (int v : g[u]) {
-        dp[v] = (dp[u] + dp[v]) % MOD;
-        if (!vis[v]) {
-            vis[v] = 1;
-            dfs(v);
-        }
-    }
-}
 
 int main() {
     int N, M;
     cin >> N >> M;
-    g.resize(N + 1);
-    dp.resize(N + 1);
-    vis.resize(N + 1);
+    vector<vector<int>> g(N + 1);
+    vector<bool> vis(N + 1);
+    vector<int> dp(N + 1);
     while (M--) {
         int U, V;
         cin >> U >> V;
         g[U].push_back(V);
     }
     vis[1] = dp[1] = 1;
-    dfs(1);
-    cout << dp[N];
+    queue<
 }
