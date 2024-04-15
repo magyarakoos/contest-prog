2b9665241255a7054f26d7752858e85f75d0d070
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-15 17:43:21 +0200

    arch descktop sync: 2024-04-15 17:43:21

--- cses/1681/main.cpp
+++ cses/1681/main.cpp
@@ -1,10 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
-
+vector<vector<int>> g;
 
 int main() {
     int N, M;
     cin >> N >> M;
-    
+    g.resize(N + 1);
+    while (M--) {
+        int U, V;
+        cin >> U >> V;
+        g[U].push_back(V);
+    }
 }
