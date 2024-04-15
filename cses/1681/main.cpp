97c3540f39b49fd931f3c709d54acfe264fddeb1
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-15 18:02:27 +0200

    arch descktop sync: 2024-04-15 18:02:27

--- cses/1681/main.cpp
+++ cses/1681/main.cpp
@@ -11,28 +11,24 @@ void dfs(int u) {
     for (int v : g[u]) {
         if (!vis[v]) dfs(v);
     }
+    s.push(u);
 }
 
 int main() {
     int N, M;
     cin >> N >> M;
+    g.resize(N + 1);
+    vis.resize(N + 1);
+    dp.resize(N + 1);
     while (M--) {
         int U, V;
         cin >> U >> V;
         g[U].push_back(V);
     }
-    queue<int> q({1});
-    vis[1] = dp[1] = 1;
-    while (!q.empty()) {
-        int u = q.front(); q.pop();
-        cout << u << "\n";
-        for (int v : g[u]) {
-            dp[v] = (dp[u] + dp[v]) % MOD;
-            if (!vis[v]) {
-                vis[v] = 1;
-                q.push(v);
-            }
+    for (int u = 1; u <= N; u++) {
+        if (!vis[u]) {
+            dfs(u);
         }
     }
-    cout << dp[N];
+    
 }
