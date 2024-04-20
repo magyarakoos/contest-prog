e3943ba939f45759646adab52c9faab9cda9bb71
magyarakoos <ninjaoffical08@gmail.com>
2024-04-20 12:09:00 +0200

    arch descktop sync: 2024-04-20 12:09:00

--- atcoder/abc349/E/main.cpp
+++ atcoder/abc349/E/main.cpp
@@ -3,8 +3,12 @@ using namespace std;
 using ll = long long;
 
 ll m[9], d[9], sum, INF = 1e10;
+set<int> s;
 
-
+ll backtrack(int x) {
+    if (s.size() == 9) return 0;
+    ll ans = (x == )
+}
 
 int main() {
     cin.tie(0), ios::sync_with_stdio(0);
