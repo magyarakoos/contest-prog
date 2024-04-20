da4690176f289b57a16ba97dd42c7f16f3357bed
magyarakoos <ninjaoffical08@gmail.com>
2024-04-20 18:32:51 +0200

    arch descktop sync: 2024-04-20 18:32:51

--- codeforces/archive/1951A/main.cpp
+++ codeforces/archive/1951A/main.cpp
@@ -2,8 +2,13 @@
 using namespace std;
 using ll = long long;
 
+void solve() {
+    
+}
+
 int main() {
     cin.tie(0), ios::sync_with_stdio(0);
-
-    
+    int T;
+    cin >> T;
+    while (T--) solve();
 }
