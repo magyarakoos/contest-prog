3af4ad8670e6fdd391d8004c37e8c4abdebc58f0
magyarakoos <ninjaoffical08@gmail.com>
2024-04-20 20:04:00 +0200

    arch descktop sync: 2024-04-20 20:04:00

--- cses/1649/main.cpp
+++ cses/1649/main.cpp
@@ -1,11 +1,8 @@
 #include <bits/stdc++.h>
-
-#define all(v) v.begin(), v.end()
-#define rall(v) v.rbegin(), v.rend()
-#define size(v) (int)v.size()
-
 using namespace std;
 using ll = long long;
+constexpr int MAXN = 2
+
 
 int main() {
     cin.tie(0), ios::sync_with_stdio(0);
