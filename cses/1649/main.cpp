f6e647371d1c8b3827178f96d7a79cc237539158
magyarakoos <ninjaoffical08@gmail.com>
2024-04-20 20:04:36 +0200

    arch descktop sync: 2024-04-20 20:04:36

--- cses/1649/main.cpp
+++ cses/1649/main.cpp
@@ -1,10 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
-constexpr int MAXN = 2
+constexpr int MAXN = 2e5 + 1;
+
+ll tree[MAXN];
+
 
 
 int main() {
     cin.tie(0), ios::sync_with_stdio(0);
 
+    int N, Q;
+    cin >> N >> Q;
 }
