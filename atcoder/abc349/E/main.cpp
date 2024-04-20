acc518175e6da885b2d13fd33b4812e5fbe6e0c5
magyarakoos <ninjaoffical08@gmail.com>
2024-04-20 12:06:38 +0200

    arch descktop sync: 2024-04-20 12:06:38

--- atcoder/abc349/E/main.cpp
+++ atcoder/abc349/E/main.cpp
@@ -2,9 +2,14 @@
 using namespace std;
 using ll = long long;
 
-ll m[9];
-ll d[9];
+ll m[9], d[9], sum;
 int main() {
     cin.tie(0), ios::sync_with_stdio(0);
-    for (int i = 0; i < 9; i++) d[i] = i;
+    for (int i = 0; i < 9; i++) {
+        d[i] = i;
+        cin >> m[i];
+        sum += m[i];
+    }
+
+    
 }
