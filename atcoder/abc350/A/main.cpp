f884f93e9af137af77e735ced5aa18f3079a0b17
magyarakoos <ninjaoffical08@gmail.com>
2024-04-20 13:10:54 +0200

    arch descktop sync: 2024-04-20 13:10:54

--- atcoder/abc350/A/main.cpp
+++ atcoder/abc350/A/main.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 using ll = long long;
 
-
+#define cases = false
 #ifdef LOCAL
 ifstream cin("../input");
 ofstream cout("../output");
@@ -11,4 +11,6 @@ int main() {
     #ifndef LOCAl
     cin.tie(0), ios::sync_with_stdio(0);
     #endif
+    int T = 1;
+
 }
