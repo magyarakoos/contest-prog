afaa4826af9a04c91d131c30bf5751119c1ca838
magyarakoos <ninjaoffical08@gmail.com>
2024-04-20 11:31:32 +0200

    arch descktop sync: 2024-04-20 11:31:32

--- atcoder/abc349/E/main.jl
+++ atcoder/abc349/E/main.jl
@@ -1,6 +1,6 @@
 using Combinatorics
 using StatsBase
-m = zeros(Int, 9)
+m = zeros(Int, 9); d = collect(1:9)
 for i in 0:2
     l = map(x -> parse(Int, x), split(readline()))
     for j in 1:3 m[i * 3 + j] = l[j] end 
@@ -10,5 +10,7 @@ over = () -> begin
     for i in 1:3:7 
         if length(countmap(m[i:i+2])) == 1 return false end 
     end
-    return length(unique([m[1], m[5], m[9]])) > 1 && length(unique([m[3], m[5], m[7]])) > 1
+    return 
+        length(unique([m[1], m[5], m[9]])) > 1 && 
+        length(unique([m[3], m[5], m[7]])) > 1
 end
