55226485133e84cb839112fe2473e32b078d20e5
magyarakoos <ninjaoffical08@gmail.com>
2024-04-20 14:20:16 +0200

    arch descktop sync: 2024-04-20 14:20:16

--- atcoder/abc350/C/main.cpp
+++ atcoder/abc350/C/main.cpp
@@ -2,37 +2,12 @@
 using namespace std;
 using ll = long long;
 
-int partition(vector<int>& v, int begin, int end) {
-    int pivot = v[end];
-    int i = begin - 1;
-
-    for (int j = begin; j < end; j++) {
-        if (v[j] < pivot) {
-            i++;
-            swap(v[i], v[j]);
-        }
-    }
-
-    swap(v[i + 1], v[end]);
-    return i + 1;
-}
-
-void quick_sort(vector<int>& v, int begin, int end) {
-    if (begin < end) {
-        int pivotI = partition(v, begin, end);
-
-        quick_sort(v, begin, pivotI - 1);
-        quick_sort(v, pivotI + 1, end);
-    }
-}
-
 #define cases 0
 void solve() {
     int N;
     cin >> N;
     vector<int> A(N);
     for (int& x : A) cin >> x;
-    auto result = insertion_sort(A);
     cout << result.size() << "\n";
     for (auto [l, r] : result) cout << l + 1 << " " << r + 1 << "\n";
 }
