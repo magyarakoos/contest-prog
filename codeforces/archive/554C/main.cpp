54b5eb77424815d0c698d7b859e0f6bc4ca9182c
magyarakoos <ninjaoffical08@gmail.com>
2024-04-20 18:09:43 +0200

    arch descktop sync: 2024-04-20 18:09:43

---
+++ codeforces/archive/554C/main.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+constexpr int MOD = 1e9 + 7;
+
+int choose[1001][1001];
+int main() {
+	cin.tie(0), ios::sync_with_stdio(0);
+
+    int K;
+    cin >> K;
+    vector<int> v(K);
+    for (int& c : v) cin >> c;
+}
