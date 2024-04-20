4d27a056a97f0c737641c28f8f0b01a33ef46e78
magyarakoos <ninjaoffical08@gmail.com>
2024-04-20 15:00:37 +0200

    arch descktop sync: 2024-04-20 15:00:37

--- atcoder/abc350/E/main.cpp
+++ atcoder/abc350/E/main.cpp
@@ -3,9 +3,13 @@ using namespace std;
 using ll = long long;
 
 int N, A, X, Y;
+map<int, double> cache;
+
+double recur()
+
 #define cases 0
 void solve() {
-    
+    cin >> N >> A >> X >> Y;
 }
 
 int main() {
