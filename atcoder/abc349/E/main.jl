c0c8f2b08b6ca4f5cbc96d9bed2374fce886fa06
magyarakoos <ninjaoffical08@gmail.com>
2024-04-20 11:24:22 +0200

    arch descktop sync: 2024-04-20 11:24:22

--- atcoder/abc349/E/main.jl
+++ atcoder/abc349/E/main.jl
@@ -4,6 +4,11 @@ for i in 0:2
     l = map(x -> parse(Int, x), split(readline()))
     for j in 1:3 m[i * 3 + j] = l[j] end 
 end
+
+over = () -> begin
+    
+end
+
 x = 0
 for perm in permutations(collect(1:9))
     global x += 1
