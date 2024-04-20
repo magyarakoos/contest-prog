65a08ecd0475a8c87ff3dd1ddb9006b5e578da5d
magyarakoos <ninjaoffical08@gmail.com>
2024-04-20 11:36:20 +0200

    arch descktop sync: 2024-04-20 11:36:20

--- atcoder/abc349/E/main.jl
+++ atcoder/abc349/E/main.jl
@@ -1,13 +1,13 @@
 using Combinatorics
 using StatsBase
 
-m = zeros(Int, 9); d = collect(1:9)
+m = zeros(Int, 9)
 for i in 0:2
     l = map(x -> parse(Int, x), split(readline()))
     for j in 1:3 m[i * 3 + j] = l[j] end 
 end
 
-over = () -> begin
+over = (d) -> begin
     for i in 1:3:7 
         if length(countmap(d[i:i+2])) == 1 return false end 
     end
@@ -18,8 +18,11 @@ end
 
 for perm in permutations(collect(1:9))
     scores = [0, 0]
+    d = collect(1:9)
     for i in 1:9
         scores[i % 2 + 1] += m[perm[i]]
         d[perm[i]] = i % 2 + 1
     end
+    if over(d) continue end
+
 end
