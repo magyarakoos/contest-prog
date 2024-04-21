49b54d93c58716278e6f46241bc558ee611fb231
magyarakoos <ninjaoffical08@gmail.com>
2024-04-21 18:34:25 +0200

    arch descktop sync: 2024-04-21 18:34:25

--- codeforces/archive/1957B/main.cpp
+++ codeforces/archive/1957B/main.cpp
@@ -1,6 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+#define size(v) (int)v.size()
+
 void solve() {
     int N, K;
     cin >> N >> K;
@@ -9,9 +11,13 @@ void solve() {
     for (int i = 1 ;; i *= 2) {
         if (sum + i <= K) {
             sum += i;
+            res.push_back(i);
         } else {
             break;
         }
+    }
+    for (int i = 0; i < N; i++) {
+        if (i < size(rse))
     }
 }
 
