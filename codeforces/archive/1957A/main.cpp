0e837e33b48829de60380a4b8222a90cf846d06e
magyarakoos <ninjaoffical08@gmail.com>
2024-04-21 18:25:31 +0200

    arch descktop sync: 2024-04-21 18:25:31

--- codeforces/archive/1957A/main.cpp
+++ codeforces/archive/1957A/main.cpp
@@ -6,9 +6,13 @@ void solve() {
     cin >> N;
     map<int, int> m;
     while (N--) {
-        
+        int X;
+        cin >> X;
+        m[X]++;
     }
     int result = 0;
+    for (auto [k, v] : m) result += v / 3;
+    cout << result << "\n";
 }
 
 int main() {
