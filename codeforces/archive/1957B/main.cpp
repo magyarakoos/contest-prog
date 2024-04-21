3949707ebb0af135ef1a6aa43a54f4fd383e5503
magyarakoos <ninjaoffical08@gmail.com>
2024-04-21 18:33:49 +0200

    arch descktop sync: 2024-04-21 18:33:49

--- codeforces/archive/1957B/main.cpp
+++ codeforces/archive/1957B/main.cpp
@@ -5,7 +5,14 @@ void solve() {
     int N, K;
     cin >> N >> K;
     vector<int> res;
-    int sum;
+    int sum = 0;
+    for (int i = 1 ;; i *= 2) {
+        if (sum + i <= K) {
+            sum += i;
+        } else {
+            break;
+        }
+    }
 }
 
 int main() {
