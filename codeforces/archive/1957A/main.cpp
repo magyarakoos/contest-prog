27a3c61baf7bc27fdffbd5f9448bc8c0a6037fd4
magyarakoos <ninjaoffical08@gmail.com>
2024-04-21 18:24:56 +0200

    arch descktop sync: 2024-04-21 18:24:56

---
+++ codeforces/archive/1957A/main.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    int N;
+    cin >> N;
+    map<int, int> m;
+    while (N--) {
+        
+    }
+    int result = 0;
+}
+
+int main() {
+    int T;
+    cin >> T;
+    while (T--) solve();
+}
