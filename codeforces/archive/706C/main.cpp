502f31434dac85e2e0f879be12dc7688c232faa2
magyarakoos <ninjaoffical08@gmail.com>
2024-04-21 18:12:28 +0200

    arch descktop sync: 2024-04-21 18:12:28

--- codeforces/archive/706C/main.cpp
+++ codeforces/archive/706C/main.cpp
@@ -14,64 +14,7 @@ int main() {
 
     int N;
     cin >> N;
-    vector<int> C(N);
-    for (int& x : C) cin >> x;
-
-    int maxl = 0;
-    vector<vector<array<string, 2>>> v;
-    vector<vector<int>> costS;
-    for (int i = 0; i < N; i++) {
-        string s;
-        cin >> s;
-        if (maxl < size(s)) {
-            maxl = size(s);
-            v.push_back({{s, s}});
-            costS.push_back({C[i]});
-        }
-        else if (maxl == size(s)) {
-            v.back().push_back({s, s});
-            costS.back().push_back(C[i]);
-        }
-        else {
-            cout << -1;
-            exit(0);
-        }
-        reverse(v.back().back()[1].begin(), v.back().back()[1].end());
-    }
-
-    ll result = 0;
-    N = size(v);
-
-    for (int i = 0; i < N; i++) {
-
-        int M = size(v[i]);
-        vector<array<ll, 2>> dp(M, {INF, INF});
-        dp[0] = {0, costS[i][0]};
-
-        for (int j = 1; j < M; j++) {
-
-            if (v[i][j - 1][0] <= v[i][j][0]) {
-                dp[j][0] = min(dp[j][0], dp[j - 1][0]);
-            }
-            if (v[i][j - 1][1] <= v[i][j][0]) {
-                dp[j][0] = min(dp[j][0], dp[j - 1][1]);
-            }
-
-            if (v[i][j - 1][0] <= v[i][j][1]) {
-                dp[j][1] = min(dp[j][1], dp[j - 1][0] + costS[i][j]);
-            }
-            if (v[i][j - 1][1] <= v[i][j][1]) {
-                dp[j][1] = min(dp[j][1], dp[j - 1][1] + costS[i][j]);
-            }
-        }
-
-        if (min(dp[M - 1][0], dp[M - 1][1]) >= INF) {
-            cout << -1;
-            exit(0);
-        }
-
-        result += min(dp[M - 1][0], dp[M - 1][1]);
-    }
-
-    cout << result << "\n";
+    vector<int> costS(N);
+    for (int& x : costS) cin >> x;
+    vector<array<string, 2>> v(N)
 }
