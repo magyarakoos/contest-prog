682792e07d9f8e0589760de9c6f89b896e62e234
magyarakoos <ninjaoffical08@gmail.com>
2024-04-21 09:40:30 +0200

    arch descktop sync: 2024-04-21 09:40:30

--- codeforces/archive/706B/main.cpp
+++ codeforces/archive/706B/main.cpp
@@ -1,10 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
-using ll = long long;
 
 int main() {
     cin.tie(0), ios::sync_with_stdio(0);
-
     int N;
     cin >> N;
     vector<int> v(N);
@@ -14,11 +12,6 @@ int main() {
     while (Q--) {
         int M;
         cin >> M;
-        auto it = upper_bound(v.begin(), v.end(), M);
-        if (it == v.end()) {
-            cout << N << "\n";
-        } else {
-            cout << i
-        }
+        cout << upper_bound(v.begin(), v.end(), M) - v.begin() << "\n";
     }
 }
