3d9a160a724c1dfcf1b72f8a9ca709b412b9edeb
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-22 13:51:13 +0200

    arch descktop sync: 2024-04-22 13:51:13

--- codeforces/archive/1923B/main.cpp
+++ codeforces/archive/1923B/main.cpp
@@ -5,21 +5,24 @@ using ll = long long;
 void solve() {
     int N, K;
     cin >> N >> K;
-    vector<int> hpS(N);
+    vector<int> hpS(N), v(N + 1);
     for (int& x : hpS) cin >> x;
-    vector<int> v(N + 1);
+
     for (int i = 0; i < N; i++) {
         int X;
         cin >> X;
         v[abs(X)] += hpS[i];
     }
+
     int cleared = 0;
     for (int pos = 0; pos <= N; pos++) {
-        
+        if (cleared == N) break;
+
         if (v[pos] > 0) {
             cout << "NO\n";
             return;
         }
+
         int k = K;
         for (int i = cleared + 1; i <= N; i++) {
             if (k < v[i]) {
@@ -31,6 +34,7 @@ void solve() {
             k -= v[i];
         }
     }
+    
     cout << "YES\n";
 }
 
