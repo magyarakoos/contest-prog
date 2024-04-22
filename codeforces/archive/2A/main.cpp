6c73124cac581608b90b7605863a8770cc39b6f8
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-22 08:01:11 +0200

    arch descktop sync: 2024-04-22 08:01:11

--- codeforces/archive/2A/main.cpp
+++ codeforces/archive/2A/main.cpp
@@ -1,49 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct Record {
-    string key;
-    int score;
-};
-
-array<Record, 1000> playerS;
-int length;
-
-int GetRecord(const string& s) {
-    for (int i = 0; i < length; i++) {
-        if (playerS[i].key == s) {
-            return i;
-        }
-    }
-    return -1;
-}
-
 int main() {
     int N;
     cin >> N;
-
-    int result = INT_MIN, playerID = 0;
-
-    for (int i = 0; i < N; i++) {
-        string s;
-        int x;
-        cin >> s >> x;
-
-        int index = GetRecord(s);
-
-        if (index == -1) {
-            playerS[length] = {s, x};
-            index = length;
-            length++;
-        } else {
-            playerS[index].score += x;
-        }
-
-        if (result < playerS[index].score) {
-            result = playerS[index].score;
-            playerID = index;
-        }
-    }
-
-    cout << playerS[playerID].key;
+    int 
 }
