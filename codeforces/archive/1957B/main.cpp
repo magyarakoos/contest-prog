c912e066b115ee1cba8b4175c77c787fbcae83fb
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-22 07:39:51 +0200

    arch descktop sync: 2024-04-22 07:39:51

--- codeforces/archive/1957B/main.cpp
+++ codeforces/archive/1957B/main.cpp
@@ -6,15 +6,13 @@ using namespace std;
 void solve() {
     int N, K;
     cin >> N >> K;
-    int res = 0;
     for (int i = (1 << 30) ; i > 1; i /= 2) {
         if (i - 1 <= K) {
-            res = i - 1;
+            cout << i - 1 << " " << K - (i - 1);
             break;
         }
     }
-    cout << res;
-    for (int i = 1; i < N; i++) cout << " 0";
+    for (int i = 2; i < N; i++) cout << " 0";
     cout << "\n";
 }
 
