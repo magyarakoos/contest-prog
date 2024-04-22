882ee31167c51e0ecc3e6648e794be7a3dd892b8
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-22 07:28:42 +0200

    arch descktop sync: 2024-04-22 07:28:42

--- codeforces/archive/1957C/main.cpp
+++ codeforces/archive/1957C/main.cpp
@@ -3,6 +3,13 @@ using namespace std;
 
 void solve() {
     int N, K;
+    cin >> N >> K;
+    int M = N * N;
+    while (K--) {
+        int I, J;
+        cin >> I >> K;
+        
+    }
 }
 
 int main() {
