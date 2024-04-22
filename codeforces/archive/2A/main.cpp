9ce56b65671bf6a8c991ad740ccdfd0baade11c2
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-22 08:03:57 +0200

    arch descktop sync: 2024-04-22 08:03:57

--- codeforces/archive/2A/main.cpp
+++ codeforces/archive/2A/main.cpp
@@ -4,5 +4,13 @@ using namespace std;
 int main() {
     int N;
     cin >> N;
-    int 
+    int mx = INT_MIN;
+    map<string, int> scoreS, firstS;
+    while (N--) {
+        string name;
+        int score;
+        cin >> name >> score;
+        scoreS[name] += score;
+        if (!)
+    }
 }
