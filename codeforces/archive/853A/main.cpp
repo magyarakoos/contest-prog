ac82ecb20ca35d263aefccee594affa578ad9526
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-22 08:31:27 +0200

    arch descktop sync: 2024-04-22 08:31:27

--- codeforces/archive/853A/main.cpp
+++ codeforces/archive/853A/main.cpp
@@ -8,7 +8,7 @@ bool operator()(array<int, 2> a, array<int, 2> b) {
     if ((a[1] >= t && b[1] >= t) || (a[1] < t && b[1] < t)) {
         return a[0] < b[0];
     }
-    
+    if (a[])
 }};
 
 int main() {
