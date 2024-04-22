bc0ed7eafb006994a6413a8d857f1c8fa9391bff
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-22 08:24:29 +0200

    arch descktop sync: 2024-04-22 08:24:29

--- codeforces/archive/853A/main.cpp
+++ codeforces/archive/853A/main.cpp
@@ -3,10 +3,47 @@ using namespace std;
 using ll = long long;
 
 int main() {
-    cin.tie(0), ios::sync_with_stdio(0);
+	cin.tie(0), ios::sync_with_stdio(0);
 
-    int N, K;
-    cin >> N >> K;
+	int N, K;
+	cin >> N >> K;
 
-    
+	priority_queue<array<int, 2>> pq;
+
+	for (int i = 0; i < N; i++) {
+		int C;
+		cin >> C;
+		pq.push({C, i + 1});
+	}
+
+	ll result = 0, t = K + 1;
+	vector<int> timeS(N);
+
+	while (!pq.empty()) {
+		vector<array<int, 2>> later;
+
+		while (!pq.empty()) {
+			auto [c, i] = pq.top();
+			pq.pop();
+		
+			if (t < i) {
+				later.push_back({c, i});
+			} else {
+				while (!later.empty()) {
+					pq.push(later.back());
+					later.pop_back();
+				}
+
+				result += (t - i) * c;
+				timeS[i - 1] = t;
+
+				break;
+			}
+		}
+		t++;
+	}
+
+	cout << result << '\n';
+	for (int time : timeS) cout << time << ' ';
+	cout << '\n';
 }
