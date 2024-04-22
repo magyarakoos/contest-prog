dc2821054b1598ca2d7b7d150fc26a13601dde75
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-22 07:29:18 +0200

    arch descktop sync: 2024-04-22 07:29:18

--- codeforces/archive/1957C/main.cpp
+++ codeforces/archive/1957C/main.cpp
@@ -4,12 +4,20 @@ using namespace std;
 void solve() {
     int N, K;
     cin >> N >> K;
+
     int M = N * N;
     while (K--) {
         int I, J;
         cin >> I >> K;
-        
+        M--;
+        if (I != J) M--;
     }
+
+    vector<int> dp(M + 1);
+    dp[0] = 1;
+    dp[1] = 1;
+
+    
 }
 
 int main() {
