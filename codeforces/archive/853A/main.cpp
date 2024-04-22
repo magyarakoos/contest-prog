4daf2d3354889e950aa271a6ef37d46d774ec0c1
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-22 10:24:09 +0200

    arch descktop sync: 2024-04-22 10:24:09

--- codeforces/archive/853A/main.cpp
+++ codeforces/archive/853A/main.cpp
@@ -28,6 +28,7 @@ array<int, 2> query(int curr, int tl, int tr, int l, int r) {
 	
 	auto a = query(curr * 2, tl, tmid, l, min(tmid, r));
 	auto b = query(curr * 2 + 1, tmid + 1, tr, max(l, tmid + 1), r);
+	if (a[1] <)
 }
 
 int main() {
