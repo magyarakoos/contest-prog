6e2bcf546685adcdcf8aaa7158f1572e62d2c55d
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-23 21:14:13 +0200

    arch descktop sync: 2024-04-23 21:14:13

--- mester/nt-oktv-ioi/NT19/2/teglalapok/main.cpp
+++ mester/nt-oktv-ioi/NT19/2/teglalapok/main.cpp
@@ -2,31 +2,47 @@
 using namespace std;
 constexpr int MAXN = 1e6;
 
-int t[4 * MAXN + 1];
-
-void update(long long curr, int tl, int tr, int l, int r, int val) {
-    if (l > r) {
+void build(const vector<int>& v, int curr, int tl, int tr) {
+    if (tl == tr) {
+        a[curr] = v[tl];
         return;
     }
-    if (l == tl && r == tr) {
-        t[curr] = max(t[curr], val);
-    } else {
-        int tmid = (tl + tr) / 2;
-        update(curr * 2, tl, tmid, l, min(tmid, r), val);
-        update(curr * 2 + 1, tmid + 1, tr, max(l, tmid + 1), r, val);
-    }
-}
-
-int query(int curr, int tl, int tr, int pos) {
-    if (tl == tr) {
-        return t[curr];
-    }
+ 
     int tmid = (tl + tr) / 2;
-    if (pos <= tmid) {
-        return max(t[curr], query(curr * 2, tl, tmid, pos));
-    } else {
-        return max(t[curr], query(curr * 2 + 1, tmid + 1, tr, pos));
+    build(v, curr * 2, tl, tmid);
+    build(v, curr * 2 + 1, tmid + 1, tr);
+    a[curr] = a[curr * 2] + a[curr * 2 + 1];
+}
+ 
+ll query(int curr, int tl, int tr, int l, int r) {
+    if (l > r) {
+        return 0;
     }
+    if (l == tl && r == tr) {
+        return a[curr];
+    }
+    
+    int tmid = (tl + tr) / 2;
+    return 
+        query(curr * 2, tl, tmid, l, min(tmid, r)) +
+        query(curr * 2 + 1, tmid + 1, tr, max(tmid + 1, l), r)
+    ;
+}
+ 
+void update(int curr, int tl, int tr, int pos, int x) {
+    if (tl == tr) {
+        a[curr] = x;
+        return;
+    }
+ 
+    int tmid = (tl + tr) / 2;
+    
+    if (pos <= tmid) {
+        update(curr * 2, tl, tmid, pos, x);
+    } else {
+        update(curr * 2 + 1, tmid + 1, tr, pos, x);
+    }
+    a[curr] = a[curr * 2] + a[curr * 2 + 1];
 }
 
 int main() {
