3df301f8a72485423c003ce780dccafbfb0c1620
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-23 21:26:53 +0200

    arch descktop sync: 2024-04-23 21:26:53

--- atcoder/abc338/D/main.cpp
+++ atcoder/abc338/D/main.cpp
@@ -19,27 +19,7 @@ void dfs(int u) {
 #define cases 0
 void solve() {
     int N, M;
-    cin >> N >> M;
-    vis.resize(N + 1);
-    g.resize(N + 1);
-    degS.resize(N + 1);
-    for (int i = 0; i < M; i++) {
-        int U, V;
-        cin >> U >> V;
-        degS[U]++;
-        g[U].push_back(V);
-        g[V].push_back(U);
-    }
-    ll result = 0;
-    for (int u = 1; u <= N; u++) {
-        if (!vis[u]) {
-            group_size = 0;
-            group_edges = 0;
-            dfs(u);
-            result += group_size * (group_size - 1) / 2 - group_edges;
-        }
-    }
-    cout << result;
+
 }
 
 int main() {
