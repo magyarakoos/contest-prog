96d73acae74054430e3f2af701ffb7d3bcd97a5a
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-23 17:50:23 +0200

    arch descktop sync: 2024-04-23 17:50:23

--- atcoder/dp/sushi/main.cpp
+++ atcoder/dp/sushi/main.cpp
@@ -2,4 +2,14 @@
 using namespace std;
 using u8 = unsigned char;
 
-double solve()
+double solve(vector<u8> v) {
+
+}
+
+int main() {
+    cin.tie(0), ios::sync_with_stdio(0);
+    int N;
+    cin >> N;
+    vector<u8> v(N);
+    for (u)
+}
