c46ec986b408e35ba25e077457796e1628847b08
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-23 20:52:26 +0200

    arch descktop sync: 2024-04-23 20:52:26

--- mester/nt-oktv-ioi/NT19/2/teglalapok/main.cpp
+++ mester/nt-oktv-ioi/NT19/2/teglalapok/main.cpp
@@ -4,7 +4,9 @@ constexpr int MAXN = 1e6;
 
 int t[4 * MAXN + 1];
 
-void update()
+void update(int curr, int tl, int tr, int l, int r, int val) {
+
+}
 
 int main() {
     cin.tie(0), ios::sync_with_stdio(0);
@@ -13,6 +15,6 @@ int main() {
     while (N--) {
         int P, D;
         cin >> P >> D;
-        
+        update(1, 0, H - 1, P, P + K, D);
     }
 }
