d1637ce56cdb002b471c4a605c72cd859d76573a
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-23 11:36:19 +0200

    arch descktop sync: 2024-04-23 11:36:19

--- atcoder/abc338/B/main.cpp
+++ atcoder/abc338/B/main.cpp
@@ -4,7 +4,16 @@ using ll = long long;
 
 #define cases 0
 void solve() {
-
+    int N;
+    cin >> N;
+    vector<int> Q(N), A(N), B(N);
+    for (int& x : Q) cin >> x;
+    for (int& x : A) cin >> x;
+    for (int& x : B) cin >> x;
+    int res = 0;
+    
+    cout << res;
+}
 
 int main() {
     cin.tie(0), ios::sync_with_stdio(0);
