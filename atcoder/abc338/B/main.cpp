e7679dc588de4ac2f1f582b1ed0cdb05ca155227
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-23 11:34:31 +0200

    arch descktop sync: 2024-04-23 11:34:31

--- atcoder/abc338/B/main.cpp
+++ atcoder/abc338/B/main.cpp
@@ -8,7 +8,8 @@ void solve() {
     cin >> s;
     map<char, int> m;
     for (char c : s) m[c]++;
-    int mx = 0, mxc = '$';
+    int mx = 0;
+    char mxc = '$';
     for (auto [k, v] : m) {
         if (mx < v) {
             mx = v;
