5d1a71cea134fbf62a542029e298857f87239515
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-23 11:47:30 +0200

    arch descktop sync: 2024-04-23 11:47:30

--- atcoder/abc338/C/main.cpp
+++ atcoder/abc338/C/main.cpp
@@ -6,7 +6,7 @@ using ll = long long;
 void solve() {
     int N;
     cin >> N;
-    vector<int> Q(N), A(N), B(N), AC(N), BC(N);
+    vector<int> Q(N), A(N), B(N), AC(N);
     for (int& x : Q) cin >> x;
     for (int& x : A) cin >> x;
     for (int& x : B) cin >> x;
@@ -14,9 +14,7 @@ void solve() {
     int mn_a = INT_MAX, mn_b = INT_MAX;
     for (int i = 0; i < N; i++) {
         AC[i] = Q[i] / A[i];
-        BC[i] = Q[i] / B[i];
         mn_a = min(mn_a, AC[i]);
-        mn_b = min(mn_b, BC[i]);
     }
 
     int res = 0;
@@ -26,6 +24,7 @@ void solve() {
         for (int i = 0; i < N; i++) {
             Q_left[i] -= curr_a * A[i];
         }
+        vector<int> BC(N);
     }
     
     cout << res;
