faaccdcb9b8537820990372898b51b4144cd0baf
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-23 11:35:40 +0200

    arch descktop sync: 2024-04-23 11:35:40

--- atcoder/abc338/B/main.cpp
+++ atcoder/abc338/B/main.cpp
@@ -4,20 +4,7 @@ using ll = long long;
 
 #define cases 0
 void solve() {
-    string s;
-    cin >> s;
-    map<char, int> m;
-    for (char c : s) m[c]++;
-    int mx = 0;
-    char mxc = '$';
-    for (auto [k, v] : m) {
-        if (mx < v) {
-            mx = v;
-            mxc = k;
-        }
-    }
-    cout << mxc;
-}
+
 
 int main() {
     cin.tie(0), ios::sync_with_stdio(0);
