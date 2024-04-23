eb7f7c60457af278e2c5aa753aaea9d5406f33cc
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-23 17:52:07 +0200

    arch descktop sync: 2024-04-23 17:52:07

--- atcoder/dp/sushi/main.cpp
+++ atcoder/dp/sushi/main.cpp
@@ -4,7 +4,10 @@ using u8 = unsigned char;
 #define sum(v) accumulate(v.begin(), v.end(), 0)
 
 double solve(vector<u8> v) {
-    if (!sum(v))
+    if (!sum(v)) {
+        return 0;
+    }
+    // a = (v[1] - 1)
 }
 
 int main() {
