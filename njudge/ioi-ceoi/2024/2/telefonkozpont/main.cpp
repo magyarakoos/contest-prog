ad64cb12b98a0dbc4c99ca01ec44da24fdae7cf2
magyarakoos <ninjaoffical08@gmail.com>
2024-04-24 20:29:46 +0200

    arch descktop sync: 2024-04-24 20:29:46

--- njudge/ioi-ceoi/2024/2/telefonkozpont/main.cpp
+++ njudge/ioi-ceoi/2024/2/telefonkozpont/main.cpp
@@ -4,12 +4,19 @@ constexpr int MAXN = 2e5;
 
 int t[4 * MAXN + 1], lazy[4 * MAXN + 1];
 
+void push(int curr) {
+    t[curr * 2 + ]
+}
+
 void update(int curr, int tl, int tr, int l, int r) {
     if (tr < tl || r < l) {
         return;
     }
     if (tr == tl) {
-
+        t[curr]++;
+        lazy[curr]++;
+    } else {
+        push(curr);
     }
 }
 
