bbd6533b83d53aec9aa642b993b8d99ee433f514
magyarakoos <ninjaoffical08@gmail.com>
2024-04-24 20:29:09 +0200

    arch descktop sync: 2024-04-24 20:29:09

--- njudge/ioi-ceoi/2024/2/telefonkozpont/main.cpp
+++ njudge/ioi-ceoi/2024/2/telefonkozpont/main.cpp
@@ -2,10 +2,15 @@
 using namespace std;
 constexpr int MAXN = 2e5;
 
-int t[4 * MAXN + 1];
+int t[4 * MAXN + 1], lazy[4 * MAXN + 1];
 
 void update(int curr, int tl, int tr, int l, int r) {
+    if (tr < tl || r < l) {
+        return;
+    }
+    if (tr == tl) {
 
+    }
 }
 
 int main() {
