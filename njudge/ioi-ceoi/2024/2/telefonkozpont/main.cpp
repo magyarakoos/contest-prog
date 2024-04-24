dd4aa20a5fad19bb83f223a5a67dcd14734c3f48
magyarakoos <ninjaoffical08@gmail.com>
2024-04-24 20:28:33 +0200

    arch descktop sync: 2024-04-24 20:28:33

--- njudge/ioi-ceoi/2024/2/telefonkozpont/main.cpp
+++ njudge/ioi-ceoi/2024/2/telefonkozpont/main.cpp
@@ -4,10 +4,19 @@ constexpr int MAXN = 2e5;
 
 int t[4 * MAXN + 1];
 
+void update(int curr, int tl, int tr, int l, int r) {
+
+}
+
 int main() {
     cin.tie(0), ios::sync_with_stdio(0);
 
     int M, N, Q;
     cin >> M >> N >> Q;
-    
+
+    while (N--) {
+        int B, E;
+        cin >> B >> E;
+        
+    }
 }
