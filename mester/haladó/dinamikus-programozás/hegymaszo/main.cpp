356dbb8eac10820bb2aaf24fc331ef98ee92cea6
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-24 08:40:56 +0200

    arch descktop sync: 2024-04-24 08:40:56

--- mester/haladó/dinamikus-programozás/hegymaszo/main.cpp
+++ mester/haladó/dinamikus-programozás/hegymaszo/main.cpp
@@ -7,17 +7,16 @@ int main() {
 
     int N, T, H;
     cin >> N >> T >> H;
-    vector<array<int, 2>> v(N + 2);
-    for (int i = 1; i <= N; i++) {
+    vector<array<int, 2>> v(N + 1);
+    for (int i = 0; i < N; i++) {
         cin >> v[i][0] >> v[i][1];
     }
-    v[N + 1] = {T, 0};
+    v[N] = {T, 0};
 
-    vector<array<int, 2>> dp(N + 2, {INF, INF});
-    dp[0] = {0, 0};
-    dp[1] = {1, v[1][1]};
+    vector<array<int, 2>> dp(N + 1, {INF, INF});
+    dp[0] = {1, v[1][1]};
 
-    for (int i = 2; i <= N + 1; i++) {
+    for (int i = 1; i <= N; i++) {
 
     }
 }
