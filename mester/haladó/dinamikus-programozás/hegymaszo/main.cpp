a288106c947fa924ee875b3da68a6484ed6d187a
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-24 08:27:21 +0200

    arch descktop sync: 2024-04-24 08:27:21

--- mester/haladó/dinamikus-programozás/hegymaszo/main.cpp
+++ mester/haladó/dinamikus-programozás/hegymaszo/main.cpp
@@ -6,6 +6,9 @@ int main() {
 
     int N, T, H;
     cin >> N >> T >> H;
+    vector<array<int, 2>> v(N + 1);
+    for (int i = 1; i <= N; i++) {
+        cin >> v[i][0] >> v[i][1];
+    }
 
-    
 }
