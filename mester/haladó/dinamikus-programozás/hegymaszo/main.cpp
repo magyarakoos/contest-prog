66e4da31e7ae7ebe34e15fbe866226d6d6d69718
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-24 08:43:25 +0200

    arch descktop sync: 2024-04-24 08:43:25

--- mester/haladó/dinamikus-programozás/hegymaszo/main.cpp
+++ mester/haladó/dinamikus-programozás/hegymaszo/main.cpp
@@ -18,7 +18,9 @@ int main() {
 
     for (int i = 1; i <= N; i++) {
         for (int j = 0; j < i; j++) {
-            if (v[i][0] - v[] dp[j])
+            if (v[i][0] - v[j][0] <= dp[j][1]) {
+                
+            }
         }
     }
 }
