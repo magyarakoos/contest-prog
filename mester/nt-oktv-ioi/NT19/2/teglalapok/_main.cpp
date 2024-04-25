f0bf91704da3b8bc51f42ac5fd294286b0c29417
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-25 07:36:57 +0200

    arch descktop sync: 2024-04-25 07:36:57

--- mester/nt-oktv-ioi/NT19/2/teglalapok/_main.cpp
+++ mester/nt-oktv-ioi/NT19/2/teglalapok/_main.cpp
@@ -1,19 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
-constexpr int MAXN = 1e6;
 
 int main() {
     cin.tie(0), ios::sync_with_stdio(0);
     int H, N, K;
     cin >> H >> N >> K;
+    vector<int> v(H + 1);
     while (N--) {
         int P, D;
         cin >> P >> D;
-        update(1, 0, H, P + 1, P + K, D);
+        v[P - 1] += D;
+        
     }
-    int mn = INT_MAX;
-    for (int i = 1; i <= H; i++) {
-        mn = min(mn, query(1, 0, H, i));
-    }
-    cout << mn << "\n";
+
 }
