5873829779db7969d32e4199afdf107079ab4fba
magyarakoos <ninjaoffical08@gmail.com>
2024-04-25 18:34:16 +0200

    arch descktop sync: 2024-04-25 18:34:16

--- atcoder/abc343/F/main.cpp
+++ atcoder/abc343/F/main.cpp
@@ -69,7 +69,7 @@ Vertex query(int curr, int tl, int tr, int l, int r) {
     int tmid = (tl + tr) / 2;
     return {
         query(curr * 2, tl, tmid, l, min(tmid, r)),
-        query()
+        query(curr * 2 + 1, tmid + 1, tr, max(tmid + 1, l), r)
     };
 }
 
