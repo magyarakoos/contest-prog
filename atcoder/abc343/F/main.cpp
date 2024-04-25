fd255ef0383dc4d7f60a6948338c38934826d97b
magyarakoos <ninjaoffical08@gmail.com>
2024-04-25 18:22:36 +0200

    arch descktop sync: 2024-04-25 18:22:36

--- atcoder/abc343/F/main.cpp
+++ atcoder/abc343/F/main.cpp
@@ -1,18 +1,9 @@
-// nuh uh
 #include <bits/stdc++.h>
-
 #define all(v) v.begin(), v.end()
 #define rall(v) v.rbegin(), v.rend()
 #define size(v) (int)v.size()
-
 using namespace std;
 using ll = long long;
-
-ifstream f("../input");
-
-#ifdef LOCAL
-#define cin f
-#endif
 
 class SegmentTree {
 private:
