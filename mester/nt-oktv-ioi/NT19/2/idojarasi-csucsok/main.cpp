c02888d8b2688bf02a4acc9c82b922295d3d2156
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-25 08:30:43 +0200

    arch descktop sync: 2024-04-25 08:30:43

--- mester/nt-oktv-ioi/NT19/2/idojarasi-csucsok/main.cpp
+++ mester/nt-oktv-ioi/NT19/2/idojarasi-csucsok/main.cpp
@@ -5,5 +5,30 @@ int main() {
     cin.tie(0), ios::sync_with_stdio(0);
     int N, K, L;
     cin >> N >> K >> L;
-    
+    vector<int> v(N);
+    for (int& x : v) cin >> x;
+    vector<int> sliceS, posS;
+    for (int i = 1; i < N - 1; i++) {
+        if (v[i] < v[i - 1] && v[i] < v[i + 1]) {
+            if (sliceS.empty()) {
+                sliceS.push_back(i);
+            } else {
+                sliceS.push_back(i - posS.back() - 1);
+            }
+            sliceS.push_back(-2);
+            posS.push_back(i);
+            posS.push_back(i);
+        }
+        if (v[i] > v[i - 1] && v[i] > v[i + 1]) {
+            if (sliceS.empty()) {
+                sliceS.push_back(i);
+            } else {
+                sliceS.push_back(i - posS.back() - 1);
+            }
+            sliceS.push_back(-1);
+            posS.push_back(i);
+            posS.push_back(i);
+        }
+    }
+    for (int i = )
 }
