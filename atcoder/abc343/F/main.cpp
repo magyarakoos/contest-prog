7d52d69eaef8301fb3cc4779a69c4669cd8e063b
magyarakoos <ninjaoffical08@gmail.com>
2024-04-25 18:23:13 +0200

    arch descktop sync: 2024-04-25 18:23:13

--- atcoder/abc343/F/main.cpp
+++ atcoder/abc343/F/main.cpp
@@ -1,76 +1,12 @@
 #include <bits/stdc++.h>
-#define all(v) v.begin(), v.end()
-#define rall(v) v.rbegin(), v.rend()
-#define size(v) (int)v.size()
 using namespace std;
 using ll = long long;
 
-class SegmentTree {
-private:
-    vector<pair<int, int>> tree;
-    int n;
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define size(v) (int)v.size()
 
-public:
-    SegmentTree(const vector<int>& arr) {
-        n = size(arr);
-        tree.resize(4 * n);
-        build(arr, 0, 0, n - 1);
-    }
-
-    void build(const vector<int>& arr, int node, int start, int end) {
-        if (start == end) {
-            tree[node] = {arr[start], INT_MIN};
-        } else {
-            int mid = (start + end) / 2;
-            build(arr, 2 * node + 1, start, mid);
-            build(arr, 2 * node + 2, mid + 1, end);
-            updateNode(node);
-        }
-    }
-
-    void updateNode(int node) {
-        vector<int> values = {tree[2 * node + 1].first, tree[2 * node + 1].second,
-                              tree[2 * node + 2].first, tree[2 * node + 2].second};
-        sort(values.begin(), values.end(), greater<int>());
-        tree[node] = {values[0], values[1]};
-    }
-
-    void update(int index, int value, int node, int start, int end) {
-        if (start == end) {
-            tree[node] = {value, INT_MIN};
-        } else {
-            int mid = (start + end) / 2;
-            if (index <= mid) {
-                update(index, value, 2 * node + 1, start, mid);
-            } else {
-                update(index, value, 2 * node + 2, mid + 1, end);
-            }
-            updateNode(node);
-        }
-    }
-
-    pair<int, int> query(int l, int r, int node, int start, int end) {
-        if (l <= start && r >= end) {
-            return tree[node];
-        }
-        int mid = (start + end) / 2;
-
-        pair<int, int> leftResult = query(l, r, 2 * node + 1, start, mid);
-        pair<int, int> rightResult = query(l, r, 2 * node + 2, mid + 1, end);
-
-        vector<int> values = {leftResult.first, leftResult.second, rightResult.first, rightResult.second};
-        sort(rall(values));
-        return {values[0], values[1]};
-    }
-
-    void update(int index, int value) {
-        update(index, value, 0, 0, n - 1);
-    }
-
-    int query(int l, int r) {
-        return query(l, r, 0, 0, n - 1).second;
-    }
-};
+constexpr int MAXN = 
 
 int main() {
     int N, Q;
@@ -78,7 +14,6 @@ int main() {
     vector<int> A(N);
     for (auto& e : A) cin >> e;
 
-    SegmentTree tree(A);
 
     while (Q--) {
         char type;
@@ -86,11 +21,9 @@ int main() {
         if (type == '1') {
             int P, X;
             cin >> P >> X;
-            tree.update(P - 1, X);
         } else {
             int L, R;
             cin >> L >> R;
-            cout << tree.query(L - 1, R - 1) << '\n';
         }
     }
 }
