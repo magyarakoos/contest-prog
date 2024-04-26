7dcbf82f3cd42e922e4476a125e2dd9186b16886
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-26 16:01:18 +0200

    arch descktop sync: 2024-04-26 16:01:18

--- codeforces/gym/492004/E/main.cpp
+++ codeforces/gym/492004/E/main.cpp
@@ -34,6 +34,9 @@ int main() {
 
     for (int i = 2; i <= L; i++) {
         dp[i][0] = (dp[i - 1][0] + dp[i - 1][1]) * M;
-        dp[i][0]
+        dp[i][0] %= MOD;
+
+        dp[i][1] = dp[i - 1][0] * M;
+        dp[i][1] %= MOD;
     }
 }
