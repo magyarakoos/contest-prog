f5ef50fbcb0176aea84c3fd55b7abd8372fde449
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-26 10:44:41 +0200

    arch descktop sync: 2024-04-26 10:44:41

---
+++ codeforces/archive/1956A/main.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    int K, Q;
+    cin >> K >> Q;
+    
+}
+
+int main() {
+    cin.tie(0), ios::sync_with_stdio(0);
+    int T;
+    cin >> T;
+    while (T--) solve();
+}
