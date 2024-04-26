a3fbb493b13a00dc73b1a05097daad42b7a438d0
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-26 15:02:30 +0200

    arch descktop sync: 2024-04-26 15:02:30

--- codeforces/contest/1919/C/main.cpp
+++ codeforces/contest/1919/C/main.cpp
@@ -6,8 +6,8 @@ void solve() {
     cin >> N;
     vector<int> v(N);
     for (int& x : v) cin >> x;
-    vector<array<int, 2>> subS;
-    subS.push_back({})
+    vector<array<int, 2>> v;
+    v.push_back({INT_MAX, INT_MAX});
 }
 
 int main() {
