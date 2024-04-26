5cb2b0d4da1bc7a3e4fcfc594e23aa96c8d307f5
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-26 11:47:34 +0200

    arch descktop sync: 2024-04-26 11:47:34

--- codeforces/contest/1916/A/main.cpp
+++ codeforces/contest/1916/A/main.cpp
@@ -9,27 +9,14 @@ using namespace std;
 using ll = long long;
 using point = array<int, 2>;
 
+void solve() {
+    int N, K;
+    cin >> N >> K;
+    
+}
+
 int main() {
     int T;
     cin >> T;
-    while (T--) {
-        int N, K, mul = 1;
-        cin >> N >> K;
-        while (N--) {
-            int a;
-            cin >> a;
-            mul *= a;
-        }
-
-        if (2023 % mul) {
-            cout << "NO\n";
-        } else {
-            cout << "YES\n" << 2023 / mul;
-            K--;
-            while (K--) {
-                cout << " 1";
-            }
-            cout << '\n';
-        }
-    }
+    while (T--) solve();
 }
