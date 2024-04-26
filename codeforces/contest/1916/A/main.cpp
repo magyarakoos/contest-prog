5edcf53da2a1e87ae324b5b3158501384922fea0
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-26 13:47:03 +0200

    arch descktop sync: 2024-04-26 13:47:03

--- codeforces/contest/1916/A/main.cpp
+++ codeforces/contest/1916/A/main.cpp
@@ -1,23 +1,14 @@
 #include <bits/stdc++.h>
-
-#define speed cin.tie(0); ios::sync_with_stdio(0)
-#define cinv(v) for (auto& e : v) cin >> e;
-#define all(v) v.begin(), v.end()
-#define has(s, e) s.count(e)
-
 using namespace std;
 using ll = long long;
-using point = array<int, 2>;
 
 void solve() {
-    int N, K;
+    ll N, K, prod = 1;
     cin >> N >> K;
-    ll prod = 1;
     while (N--) {
         ll x;
         cin >> x;
         prod *= x;
-        cout << prod << "\n";
     }
 
     if (!prod || prod > 2023 || 2023 % prod) {
