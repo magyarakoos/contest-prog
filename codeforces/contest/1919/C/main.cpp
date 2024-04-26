4138502c5af4887cb3bda7096740a5a95d0bf0ce
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-26 14:53:20 +0200

    arch descktop sync: 2024-04-26 14:53:20

--- codeforces/contest/1919/C/main.cpp
+++ codeforces/contest/1919/C/main.cpp
@@ -1,33 +1,13 @@
 #include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    
+}
 
 int main() {
-    speed;
-
+    cin.tie(0), ios::sync_with_stdio(0);
     int T;
     cin >> T;
-    while (T--) {
-        int N;
-        cin >> N;
-        vector<int> v(N);
-        cinv(v);
-
-        int result = 0, la = INT_MAX, lb = INT_MAX;
-
-        for (int i = 0; i < N; i++) {
-            int mn = min(la, lb), mx = max(la, lb);
-
-            if (v[i] <= mn) {
-                mn = v[i];
-            } else if (mx < v[i]) {
-                result++;
-                mn = v[i];
-            } else {
-                mx = v[i];
-            }
-
-            la = mn, lb = mx;
-        }
-
-        cout << result << '\n';
-    }
+    while (T--) solve();
 }
