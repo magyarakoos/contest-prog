d2b2399f5881eae31c195a8afb512532882091fb
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-26 16:04:45 +0200

    arch descktop sync: 2024-04-26 16:04:45

--- codeforces/gym/492004/E/main.cpp
+++ codeforces/gym/492004/E/main.cpp
@@ -1,11 +1,4 @@
 #include <bits/stdc++.h>
-
-#define cinv(v) for (auto& e : v) cin >> e;
-#define all(v) v.begin(), v.end()
-#define rall(v) v.rbegin(), v.rend()
-#define size(v) (int)v.size()
-#define has(s, e) s.count(e)
-
 using namespace std;
 using ll = long long;
 
