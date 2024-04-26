f0c1cc4a0a09cf1400bba22c42c425f27d08d5fe
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-26 16:00:43 +0200

    arch descktop sync: 2024-04-26 16:00:43

--- codeforces/gym/492004/E/main.cpp
+++ codeforces/gym/492004/E/main.cpp
@@ -9,7 +9,7 @@
 using namespace std;
 using ll = long long;
 
-constexpr int MOD = 1e9 + 7;
+constexpr ll MOD = 1e9 + 7;
 
 int main() {
     ios::sync_with_stdio(0), cin.tie(0);
@@ -34,6 +34,6 @@ int main() {
 
     for (int i = 2; i <= L; i++) {
         dp[i][0] = (dp[i - 1][0] + dp[i - 1][1]) * M;
-        
+        dp[i][0]
     }
 }
