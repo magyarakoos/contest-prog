61dc79dce3351e4b71dcf2ad18d1b5ab71638e37
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-26 15:05:24 +0200

    arch descktop sync: 2024-04-26 15:05:24

--- codeforces/contest/1919/C/main.cpp
+++ codeforces/contest/1919/C/main.cpp
@@ -3,7 +3,7 @@ using namespace std;
 #define all(v) v.begin(), v.end()
 
 void solve() {
-    array<int, 2> a
+    array<int, 2> a {INT_MAX, INT_MAX};
     int N, res = 0;
     cin >> N;
     while (N--) {
@@ -11,6 +11,11 @@ void solve() {
         cin >> X;
         if (a[0] < X && a[1] < X) {
             a[min_element(all(a)) - a.begin()] = X;
+            res++;
+            continue;
+        }
+        if (a[0] == X || a[1] == X) {
+            continue;
         }
     }
 }
