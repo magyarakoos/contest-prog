6b7d9ea302ec7f0190fed0238e5d944e358cd793
magyarakoos <ninjaoffical08@gmail.com>
2024-04-27 11:50:04 +0200

    arch descktop sync: 2024-04-27 11:50:04

---
+++ atcoder/abc351/A/main.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+#define cases 0
+void solve() {
+
+}
+
+int main() {
+    cin.tie(0), ios::sync_with_stdio(0);
+    int T = 1;
+    if (cases) cin >> T;
+    while (T--) solve();
+}
