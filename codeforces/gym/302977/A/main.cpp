a3ed647a70862efe92beb7e69ad0fb0af4883d20
magyarakoos <ninjaoffical08@gmail.com>
2024-04-28 13:50:32 +0200

    arch descktop sync: 2024-04-28 13:50:32

---
+++ codeforces/gym/302977/A/main.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+#define sz(v) (int)v.size()
+#define all(v) v.begin(), v.end()
+#define smin(x, y) x = min(x, y)
+#define smax(x, y) x = max(x, y)
+
+#define cases 0
+void solve() {
+    ll N;
+    cin >> N;
+    cout << (1LL << (N / 2)) * !(N & 1);
+}
+
+int main() {
+    cin.tie(0), ios::sync_with_stdio(0);
+    int T = 1;
+    if (cases) cin >> T;
+    while (T--) solve();
+}
