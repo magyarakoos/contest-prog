dca645dac737ee66e98b5fb129262bff41fd4629
magyarakoos <ninjaoffical08@gmail.com>
2024-05-01 10:53:31 +0200

    arch descktop sync: 2024-05-01 10:53:31

--- atcoder/dp/knapsack-2/main.cpp
+++ atcoder/dp/knapsack-2/main.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
-const ll MAXV = 1e5, INF = 1e18;
+const ll INF = 1e18;
 
 int main() {
     cin.tie(0), ios::sync_with_stdio(0);
@@ -12,5 +12,15 @@ int main() {
         cin >> w[i] >> v[i];
     }
 
-    vector dp(N + 1, vector<ll>(MAXV, INF));
+    int maxv = accumulate(v.begin(), v.end(), 0);
+
+    vector dp(N + 1, vector<ll>(maxv + 1, INF));
+
+    dp[0][0] = 0;
+
+    for (int i = 1; i <= N; i++) {
+        for (int j = 0; j <= maxv; j++) {
+            
+        }
+    }
 }
