6e3e7a4c263e7d107d237c914ae87ed19dc6dae6
magyarakoos <ninjaoffical08@gmail.com>
2024-05-01 16:59:40 +0200

    arch descktop sync: 2024-05-01 16:59:40

--- mester/haladó/visszalépéses-keresés/iskolavalasztas/main.cpp
+++ mester/haladó/visszalépéses-keresés/iskolavalasztas/main.cpp
@@ -5,7 +5,7 @@ int main() {
     int N, M;
     cin >> N >> M;
     vector<array<int, 2>> kidS(N);
-    vector<int> capS(M), currS(M);
+    vector<int> capS(M);
     for (auto& [a, b] : kidS) cin >> a >> b;
     for (int& x : capS) cin >> x;
     
@@ -33,6 +33,4 @@ int main() {
             exit(0);
         }
     }
-
-    assert(0);
 }
