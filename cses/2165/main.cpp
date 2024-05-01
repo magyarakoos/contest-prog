6c753339d201a9efe4630b5e6599a516ba9cbe1d
magyarakoos <ninjaoffical08@gmail.com>
2024-05-01 18:39:15 +0200

    arch descktop sync: 2024-05-01 18:39:15

--- cses/2165/main.cpp
+++ cses/2165/main.cpp
@@ -5,6 +5,10 @@ vector<array<int, 2>> res;
 
 void hanoi(int a, int c, int cnt) {
     if (cnt == 0) return;
+    if (cnt == 1) {
+        res.push_back({a, c});
+        return;
+    }
     int b = 6 - (a + c);
     hanoi(a, b, cnt - 1);
     res.push_back({a, b});
