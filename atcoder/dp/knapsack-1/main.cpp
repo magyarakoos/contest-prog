fa5d73181b2f195a93bd236e7a0d96c091957375
magyarakoos <ninjaoffical08@gmail.com>
2024-05-01 10:40:11 +0200

    arch descktop sync: 2024-05-01 10:40:11

--- atcoder/dp/knapsack-1/main.cpp
+++ atcoder/dp/knapsack-1/main.cpp
@@ -14,12 +14,12 @@ int main() {
         cin >> w[i] >> v[i];
     }
 
-    dp[0].assign(W + 1, 0);
+    dp[0][0] = 0;
 
     for (int i = 1; i <= N; i++) {
         for (int j = 0; j <= W; j++) {
             dp[i][j] = dp[i - 1][j];
-            if (j - w[i] >= 0) {
+            if (j - w[i] >= 0 && dp[i - 1][j - w[i]] != -1) {
                 dp[i][j] = max(dp[i][j], dp[i - 1][j - w[i]] + v[i]);
             }
         }
