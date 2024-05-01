cbd4c1825b0a3af29c5bebfd19b46f45e819db4d
magyarakoos <ninjaoffical08@gmail.com>
2024-05-01 15:34:47 +0200

    arch descktop sync: 2024-05-01 15:34:47

--- atcoder/dp/slimes/main.cpp
+++ atcoder/dp/slimes/main.cpp
@@ -1,6 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
-using ll = int64_t;
+using ll = long long;
+
+const ll INF = 1e18;
 
 int main() {
     cin.tie(0), ios::sync_with_stdio(0);
@@ -9,21 +11,15 @@ int main() {
     vector v(N, 0);
     for (int& x : v) cin >> x;
 
-    vector dp(N, vector<ll>(N));
+    vector dp(N, vector<ll>(N, INF));
 
-    // l = r, answer is trivial
     for (int l = 0; l < N; l++) {
-        dp[l][l] = v[l];
+        
     }
 
     for (int l = N - 1; l >= 0; l--) {
         for (int r = l + 1; r < N; r++) {
-            dp[l][r] = max(
-                v[l] - dp[l + 1][r],
-                v[r] - dp[l][r - 1]
-            );
+
         }
     }
-
-    cout << dp[0][N - 1];
 }
