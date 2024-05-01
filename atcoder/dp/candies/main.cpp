58eb5c6c5eb7570ccd76efbcef74669d8ee99746
magyarakoos <ninjaoffical08@gmail.com>
2024-05-01 14:54:14 +0200

    arch descktop sync: 2024-05-01 14:54:14

--- atcoder/dp/candies/main.cpp
+++ atcoder/dp/candies/main.cpp
@@ -7,11 +7,17 @@ int main() {
     
     int N, K;
     cin >> N >> K;
-    vector<int> v(N);
-    for (int& x : v) cin >> x;
+    vector<int> v(N + 1);
+    for (int i = 1; i <= N; i++) {
+        cin >> v[i];
+    }
 
     vector dp(N + 1, vector<ll>(K + 1));
     dp[0][0] = 1;
 
-
+    for (int i = 1; i <= N; i++) {
+        for (int j = 0; j <= K; j++) {
+            
+        }
+    }
 }
