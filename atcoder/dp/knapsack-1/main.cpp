5efc7c18a01da017ace21e13b8f4815e2726b98e
magyarakoos <ninjaoffical08@gmail.com>
2024-05-01 10:05:41 +0200

    arch descktop sync: 2024-05-01 10:05:41

--- atcoder/dp/knapsack-1/main.cpp
+++ atcoder/dp/knapsack-1/main.cpp
@@ -5,19 +5,19 @@ using ll = long long;
 int N, W;
 map<pair<int, ll>, ll> m;
 
-int v[101], w[101];
+int v[100], w[100];
 
 ll solve(pair<int, ll> state) {
     if (m.count(state)) return m[state];
 
     auto [i, weight] = state;
-    if (i + 1 == N) return weight;
+    if (i == N) return weight;
 
     ll res = 0;
 
     // take
-    if (weight + w[i + 1] <= W) {
-        res = max(res, solve({i + 1, weight + w[i + 1]}) + v[i + 1]);
+    if (weight + w[i] <= W) {
+        res = max(res, solve({i + 1, weight + w[i]}) + v[i]);
     }
 
     // don't take
@@ -32,7 +32,7 @@ int main() {
     cin.tie(0), ios::sync_with_stdio();
     cin >> N >> W;
     for (int i = 0; i < N; i++) {
-        cin >> v[i] >> w[i];
+        cin >> w[i] >> v[i];
     }
-    cout << solve({-1, 0});
+    cout << solve({0, 0});
 }
