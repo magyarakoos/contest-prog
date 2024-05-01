9e142f9f7876fd1dc0b7c9a04fa64fda79ddc4b5
magyarakoos <ninjaoffical08@gmail.com>
2024-05-01 14:42:45 +0200

    arch descktop sync: 2024-05-01 14:42:45

--- atcoder/dp/deque/main.cpp
+++ atcoder/dp/deque/main.cpp
@@ -9,5 +9,16 @@ int main() {
     vector v(N, 0);
     for (int& x : v) cin >> x;
 
-    
+    vector dp(N, vector<ll>(N));
+
+    for (int l = N - 1; l >= 0; l--) {
+        for (int r = l + 1; r < N; r++) {
+            dp[l][r] = max(
+                v[l] - dp[l + 1][r],
+                v[r] - dp[l][r - 1]
+            );
+        }
+    }
+
+    cout << dp[0][N - 1];
 }
