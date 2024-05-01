905b6dcfbcdf996edd857002717553a8e75b1e28
magyarakoos <ninjaoffical08@gmail.com>
2024-05-01 14:08:54 +0200

    arch descktop sync: 2024-05-01 14:08:54

--- atcoder/dp/deque/main.cpp
+++ atcoder/dp/deque/main.cpp
@@ -1,9 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
 int N;
+vector<int> a;
 
 int main() {
     cin.tie(0), ios::sync_with_stdio(0);
 
     cin >> N;
+    a.resize(N);
+    for (int i = 0; i < N; i++) cin >> a[i];
+    
 }
