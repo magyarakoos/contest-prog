e4799f0ddbde8b0762a9f61aae7591487e485805
magyarakoos <ninjaoffical08@gmail.com>
2024-05-01 18:20:59 +0200

    arch descktop sync: 2024-05-01 18:20:59

--- cses/1068/main.cpp
+++ cses/1068/main.cpp
@@ -1,22 +1,14 @@
 #include <bits/stdc++.h>
-
-#define all(v) v.begin(), v.end()
-#define rall(v) v.rbegin(), v.rend()
-#define size(v) (int)v.size()
-
 using namespace std;
-using ll = long long;
 
 int main() {
-    cin.tie(0), ios::sync_with_stdio(0);
-
     int N;
     cin >> N;
     while (1) {
         cout << N << " ";
         if (N != 1) {
-            if (N % 2) N /= 2;
-            else N *= 3, N += 1;
+            if (N % 2) N *= 3, N++;
+            else N /= 2;
         } else break;
     }
     cout << "\n";
