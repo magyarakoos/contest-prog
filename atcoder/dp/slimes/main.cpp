e45059f5eda2683ba014d702af29b5cb979d7f21
magyarakoos <ninjaoffical08@gmail.com>
2024-05-01 15:41:26 +0200

    arch descktop sync: 2024-05-01 15:41:26

--- atcoder/dp/slimes/main.cpp
+++ atcoder/dp/slimes/main.cpp
@@ -8,25 +8,19 @@ int main() {
     cin.tie(0), ios::sync_with_stdio(0);
     int N;
     cin >> N;
-    vector v(N, 0);
-    for (int& x : v) cin >> x;
+    vector v(N, 0LL);
+    for (ll& x : v) cin >> x;
 
     vector dp(N, vector<ll>(N, INF));
 
-    // cost of combining a 1-length segment: it's free
-    for (int l = 0; l < N; l++) {
-        dp[l][l] = 0;
-    }
-
     for (int l = N - 1; l >= 0; l--) {
-        for (int r = l + 1; r < N; r++) {
+        for (int r = l; r < N; r++) {
 
-            if (l + 1 == r) {
+            if (l == r) {
+                dp[l][r] = 0;
+            }
+            else if (l + 1 == r) {
                 dp[l][r] = v[l] + v[r];
-            } else {
-                for (int m = l; m <= r; m++) {
-                    dp[l][r] = min(dp[l][r], dp[l][m] + (m < r ? dp[m + 1][r] : 0));
-                }
             }
         }
     }
