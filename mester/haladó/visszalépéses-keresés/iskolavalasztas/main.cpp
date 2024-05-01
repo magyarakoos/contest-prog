ec6725bb797de17d76eca9ddf94e6d478121a98d
magyarakoos <ninjaoffical08@gmail.com>
2024-05-01 16:50:54 +0200

    arch descktop sync: 2024-05-01 16:50:54

--- mester/haladó/visszalépéses-keresés/iskolavalasztas/main.cpp
+++ mester/haladó/visszalépéses-keresés/iskolavalasztas/main.cpp
@@ -11,7 +11,6 @@ int main() {
     
     for (int mask = 0; mask < (1 << N); mask++) {
         cout << bitset<9>(mask) << "\n";
-        continue;
         bool valid = 1;
         vector<int> countS(M), placeS(N);
         for (int i = 0; i < N; i++) {
