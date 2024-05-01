4af1221c3f8bdc70f3db623f34d210319b94f3ad
magyarakoos <ninjaoffical08@gmail.com>
2024-05-01 10:49:16 +0200

    arch descktop sync: 2024-05-01 10:49:16

--- atcoder/dp/knapsack-2/main.cpp
+++ atcoder/dp/knapsack-2/main.cpp
@@ -1,3 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int main() {
+    cin.tie(0), ios::sync_with_stdio(0);
+    int N, W;
+    cin >> N >> W;
+    vector<int> v(N + 1), w(N + 1);
+    for (int i = 1; i <= N; i++) {
+        cin >> w[i] >> v[i];
+    }
+
+    
+}
