85ea18c8d496c1a995b170667a299186ea0ee894
magyarakoos <ninjaoffical08@gmail.com>
2024-05-01 10:30:24 +0200

    arch descktop sync: 2024-05-01 10:30:24

--- atcoder/dp/knapsack-1/main.cpp
+++ atcoder/dp/knapsack-1/main.cpp
@@ -1,30 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
-const int MAXN = 1e5;
+const int MAXN = 100, MAXW = 1e5;
 
-int N, W, v[100], w[100];
-int m[101][MAXN + 1];
+int N, W, v[MAXN], w[MAXN];
+int m[MAXN][MAXW + 1];
 
 ll solve(int i, ll weight) {
-
-    if (m[i][weight]) return m[i][weight];
     if (i == N) return 0;
+    if (m[i][weight] != -1) return m[i][weight];
+
     // don't take
-    ll res = solve({i + 1, weight});
+    ll res = solve(i + 1, weight);
+
     // take
     if (weight + w[i] <= W) {
-        res = max(res, solve({i + 1, weight + w[i]}) + v[i]);
+        res = max(res, solve(i + 1, weight + w[i]) + v[i]);
     }
-    m[state] = res;
+
+    m[i][weight] = res;
     return res;
 }
 
 int main() {
+    memset(m, -1, sizeof(m));
     cin.tie(0), ios::sync_with_stdio();
     cin >> N >> W;
     for (int i = 0; i < N; i++) {
         cin >> w[i] >> v[i];
     }
-    cout << solve({0, 0});
+    cout << solve(0, 0);
 }
