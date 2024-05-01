223500b8b27defc2c7d411bc6a8feb644cf82aa5
magyarakoos <ninjaoffical08@gmail.com>
2024-05-01 16:33:03 +0200

    arch descktop sync: 2024-05-01 16:33:03

--- mester/haladó/visszalépéses-keresés/iskolavalasztas/main.cpp
+++ mester/haladó/visszalépéses-keresés/iskolavalasztas/main.cpp
@@ -6,4 +6,9 @@ vector<int> schoolS, capS;
 int main() {
     int N, M;
     cin >> N >> M;
+    schoolS.resize(M);
+    capS.resize(M);
+    for (int i = 0; i < M; i++) {
+        cin >> capS[i];
+    }
 }
