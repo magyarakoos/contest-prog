76da7569bfbd057f9bf594dc822585a7ee8c8116
magyarakoos <ninjaoffical08@gmail.com>
2024-05-01 09:54:11 +0200

    arch descktop sync: 2024-05-01 09:54:11

--- atcoder/dp/knapsack-1/main.cpp
+++ atcoder/dp/knapsack-1/main.cpp
@@ -1,9 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
+using state = struct { int i; ll curr; };
+
 const ll MAXN = 1e5, INF = 1e18;
 
 int N, W;
+map<
 
 int main() {
 
