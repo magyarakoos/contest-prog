280ec7d88ab2786517658da33b4d6e980e793e81
magyarakoos <ninjaoffical08@gmail.com>
2024-05-01 14:38:32 +0200

    arch descktop sync: 2024-05-01 14:38:32

--- atcoder/dp/deque/main.cpp
+++ atcoder/dp/deque/main.cpp
@@ -2,41 +2,12 @@
 using namespace std;
 using ll = int64_t;
 
-int N;
-vector<int> v;
-map<array<int, 3>, ll> m;
-
-ll solve(int l, int r, int turn) {
-    if (m.count({l, r, turn})) return m[{l, r, turn}];
-
-    ll res = 0;
-
-    if (l == r) {
-        res += v[l] * (turn ^ 1);
-    }
-    else if (l < r) {
-        ll a = solve(l + 1, r, turn ^ 1),
-           b = solve(l, r - 1, turn ^ 1);
-        if (turn) {
-            res = min(a, b);
-        } else {
-            if (a > b) {
-                res = a + v[l];
-            } else {
-                res = b + v[r];
-            }
-        }
-    }
-
-    m[{l, r, turn}] = res;
-    return res;
-}
-
 int main() {
     cin.tie(0), ios::sync_with_stdio(0);
-
+    int N;
     cin >> N;
-    v.resize(N);
-    for (int i = 0; i < N; i++) cin >> v[i];
-    cout << solve(0, N - 1, 0);
+    vector v(N, 0);
+    for (int& x : v) cin >> x;
+
+    
 }
