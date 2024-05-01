227a0066100b79bf6bf133ab429453869e1a361d
magyarakoos <ninjaoffical08@gmail.com>
2024-05-01 11:21:15 +0200

    arch descktop sync: 2024-05-01 11:21:15

--- atcoder/dp/knapsack-2/main.cpp
+++ atcoder/dp/knapsack-2/main.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
-const ll INF = 1e12;
+const ll INF = 1e15;
 
 int main() {
     cin.tie(0), ios::sync_with_stdio(0);
@@ -14,26 +14,12 @@ int main() {
 
     int maxv = accumulate(v.begin(), v.end(), 0);
 
-    vector dp(N + 1, vector<ll>(maxv + 1, INF));
-
-    dp[0][0] = 0;
+    vector<ll> dp(maxv + 1, INF);
+    dp[0] = 0;
 
     for (int i = 1; i <= N; i++) {
-        for (int j = 0; j <= maxv; j++) {
-            dp[i][j] = dp[i - 1][j];
-            if (j - v[i] >= 0) {
-                auto& choose = dp[i - 1][j - v[i]];
-                if (choose + w[i] <= W) {
-                    dp[i][j] = min(dp[i][j], choose + w[i]);
-                }
-            }
-        }
-    }
-
-    for (int j = maxv; j >= 0; j--) {
-        if (dp[N][j] != -1) {
-            cout << j;
-            exit(0);
+        for (int j = maxv; j >= 0; j--) {
+            
         }
     }
 }
