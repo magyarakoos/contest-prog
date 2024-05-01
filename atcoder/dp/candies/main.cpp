0e4c3bb0f1eb5c8cef7e4dae0fbe47ec9215a470
magyarakoos <ninjaoffical08@gmail.com>
2024-05-01 14:53:38 +0200

    arch descktop sync: 2024-05-01 14:53:38

--- atcoder/dp/candies/main.cpp
+++ atcoder/dp/candies/main.cpp
@@ -4,7 +4,14 @@ using ll = int64_t;
 
 int main() {
     cin.tie(0), ios::sync_with_stdio(0);
+    
     int N, K;
     cin >> N >> K;
-    
+    vector<int> v(N);
+    for (int& x : v) cin >> x;
+
+    vector dp(N + 1, vector<ll>(K + 1));
+    dp[0][0] = 1;
+
+
 }
