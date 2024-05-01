590465c60cf3e0399768e8bd108382586e5bdcf4
magyarakoos <ninjaoffical08@gmail.com>
2024-05-01 16:57:32 +0200

    arch descktop sync: 2024-05-01 16:57:32

--- mester/haladó/visszalépéses-keresés/iskolavalasztas/main.cpp
+++ mester/haladó/visszalépéses-keresés/iskolavalasztas/main.cpp
@@ -9,26 +9,15 @@ int main() {
     for (auto& [a, b] : kidS) cin >> a >> b;
     for (int i = 1; i <= M; i++) cin >> capS[i];
     
-    for (int mask = 0; mask < (1 << N); mask++) {
-        cout << bitset<9>(mask) << "\n";
-        bool valid = 1;
-        vector<int> countS(M + 1), placeS(N + 1);
-        for (int i = 0; i < N; i++) {
-            bool bit = (mask << i) & 1;
+    int mask = 0b010101110;
 
-            placeS[i] = kidS[i][bit];
-            countS[placeS[i]]++;
+    vector<int> countS(M + 1), placeS(N + 1);
+    for (int i = 0; i < N; i++) {
+        bool bit = (mask >> i) & 1;
 
-            if (countS[placeS[i]] > capS[placeS[i]]) {
-                valid = 0;
-            }
-        }
-        
-        if (!valid) {
-            for (int p : countS) cout << p << " ";
-            cout << "\n";
-        }
+        placeS[i] = kidS[i][bit];
+        countS[placeS[i]]++;
+
     }
 
-    assert(0);
 }
