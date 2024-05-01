f0528edb04a796b0b09d7254230dcc193adfa65c
magyarakoos <ninjaoffical08@gmail.com>
2024-05-01 11:32:07 +0200

    arch descktop sync: 2024-05-01 11:32:07

--- atcoder/dp/longest-path/main.cpp
+++ atcoder/dp/longest-path/main.cpp
@@ -1,11 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+vector<vector<int>> g;
+
 int main() {
     cin.tie(0), ios::sync_with_stdio(0);
 
     int N, M;
     cin >> N >> M;
     
-    
+
 }
