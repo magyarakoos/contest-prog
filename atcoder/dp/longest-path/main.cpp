777457c0f4438e6267ba9f9a7235e8a0ec571039
magyarakoos <ninjaoffical08@gmail.com>
2024-05-01 13:13:23 +0200

    arch descktop sync: 2024-05-01 13:13:23

--- atcoder/dp/longest-path/main.cpp
+++ atcoder/dp/longest-path/main.cpp
@@ -1,8 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
+const int INF = 1e9;
 
 vector<vector<int>> g;
-vector<int> indegS;
+vector<int> indegS, dp;
 
 int main() {
     cin.tie(0), ios::sync_with_stdio(0);
@@ -12,6 +13,7 @@ int main() {
     
     g.resize(N + 1);
     indegS.resize(N + 1);
+    dp
     while (M--) {
         int U, V;
         cin >> U >> V;
@@ -24,5 +26,5 @@ int main() {
         g[0].push_back(u);
     }
 
-    
+
 }
