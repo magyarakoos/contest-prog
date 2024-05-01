a1f67b0eb217acc15546090dd4fcaf391b241e63
magyarakoos <ninjaoffical08@gmail.com>
2024-05-01 18:53:09 +0200

    arch descktop sync: 2024-05-01 18:53:09

--- cses/1625/main.cpp
+++ cses/1625/main.cpp
@@ -2,12 +2,19 @@
 
 #define all(v) v.begin(), v.end()
 #define rall(v) v.rbegin(), v.rend()
-#define size(v) (int)v.size()
+#define sz(v) (int)v.size()
 
 using namespace std;
 using ll = long long;
 
+int result;
+string s, t;
+bool grid[4][4];
+
 int main() {
     cin.tie(0), ios::sync_with_stdio(0);
+    cin >> s;
+    t.assign(sz(s), '?');
+
 
 }
