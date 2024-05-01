46f82129dc5fe1c366656df14b4ab18bc41557e5
magyarakoos <ninjaoffical08@gmail.com>
2024-05-01 15:40:13 +0200

    arch descktop sync: 2024-05-01 15:40:13

--- atcoder/dp/slimes/main.cpp
+++ atcoder/dp/slimes/main.cpp
@@ -13,13 +13,23 @@ int main() {
 
     vector dp(N, vector<ll>(N, INF));
 
+    // cost of combining a 1-length segment: it's free
     for (int l = 0; l < N; l++) {
-        
+        dp[l][l] = 0;
     }
 
     for (int l = N - 1; l >= 0; l--) {
         for (int r = l + 1; r < N; r++) {
 
+            if (l + 1 == r) {
+                dp[l][r] = v[l] + v[r];
+            } else {
+                for (int m = l; m <= r; m++) {
+                    dp[l][r] = min(dp[l][r], dp[l][m] + (m < r ? dp[m + 1][r] : 0));
+                }
+            }
         }
     }
+
+    cout << dp[0][N - 1];
 }
