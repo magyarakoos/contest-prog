8d790e83fad33daa3c99562e788581cbbc0aa956
magyarakoos <ninjaoffical08@gmail.com>
2024-05-01 09:57:49 +0200

    arch descktop sync: 2024-05-01 09:57:49

--- atcoder/dp/knapsack-1/main.cpp
+++ atcoder/dp/knapsack-1/main.cpp
@@ -1,17 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
-using state = struct { int i; ll weight; };
 
 const ll MAXN = 1e5, INF = 1e18;
 
 int N, W;
-map<state, ll> m;
+map<pair<int, ll>, ll> m;
 
-ll solve(state s) {
-    if (m.count(s)) return m[s];
+ll solve(pair<int, ll> state) {
+    if (m.count(state)) return m[state];
 
-    auto [i, weight] = s;
+    auto [i, weight] = state;
     if (i == N) return weight;
 
     
