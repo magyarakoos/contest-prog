2285e231b4712de2ebabe3d388503c63fa917705
magyarakoos <ninjaoffical08@gmail.com>
2024-05-01 18:25:54 +0200

    arch descktop sync: 2024-05-01 18:25:54

--- cses/2165/main.cpp
+++ cses/2165/main.cpp
@@ -2,14 +2,23 @@
 
 #define all(v) v.begin(), v.end()
 #define rall(v) v.rbegin(), v.rend()
-#define size(v) (int)v.size()
+#define sz(v) (int)v.size()
 
 using namespace std;
 using ll = long long;
+
+vector<int> a, b;
+
+
 
 int main() {
     cin.tie(0), ios::sync_with_stdio(0);
 
     int N;
     cin >> N;
+
+    cout << sz(a) << "\n";
+    for (int i = 0; i < sz(a); i++) {
+        cout << a[i] << " " << b[i] << "\n";
+    }
 }
