3986d228e7e6b69c80425f5006a902529fa6f4d5
magyarakoos <ninjaoffical08@gmail.com>
2024-05-01 16:41:45 +0200

    arch descktop sync: 2024-05-01 16:41:45

--- mester/haladó/visszalépéses-keresés/iskolavalasztas/main.cpp
+++ mester/haladó/visszalépéses-keresés/iskolavalasztas/main.cpp
@@ -5,8 +5,34 @@ int main() {
     int N, M;
     cin >> N >> M;
     vector<array<int, 2>> kidS(N);
-    vector<int> capS(M);
+    vector<int> capS(M), currS(M);
     for (auto& [a, b] : kidS) cin >> a >> b;
     for (int& x : capS) cin >> x;
-    cout << "YAP;";
+    
+    for (int mask = 0; mask < (1 << N); mask++) {
+        bool valid = 1;
+        vector<int> countS(M), placeS(N);
+        for (int i = 0; i < N; i++) {
+            bool bit = (mask << i) & 1;
+
+            if (!kidS[i][bit]) {
+                valid = 0;
+                break;
+            }
+
+            placeS[i] = kidS[i][bit];
+
+            if (++countS[placeS[i] - 1] > capS[placeS[i] - 1]) {
+                valid = 0;
+                break;
+            }
+        }
+        
+        if (valid) {
+            for (int p : placeS) cout << p << " ";
+            exit(0);
+        }
+    }
+
+    assert(0);
 }
