2f30330ff2fdcc941f143c4304d074fe70f23fb0
magyarakoos <ninjaoffical08@gmail.com>
2024-05-01 20:02:57 +0200

    arch descktop sync: 2024-05-01 20:02:57

--- codeforces/contest/1955/C/main.cpp
+++ codeforces/contest/1955/C/main.cpp
@@ -7,45 +7,9 @@ using ll = long long;
 #define smin(x, y) x = min(x, y)
 #define smax(x, y) x = max(x, y)
 
-const ll INF = 1e18;
-
-vector<ll> v;
-ll N, K;
-
-ll check(ll x) {
-    ll cost = 0;
-    for (ll y : v) {
-        if (y < x) {
-            cost += x - y;
-        }
-    }
-    return cost;
-}
-
 #define cases 1
 void solve() {
-    cin >> N >> K;
-    v.assign(N, 0);
-    ll mn = INF;
-    for (ll& x : v) {
-        cin >> x;
-        smin(mn, x);
-    }
-
-    ll left = mn, right = mn + K + 1;
-    while (right - left > 1) {
-        ll mid = (left + right) / 2;
-        if (check(mid) <= K) {
-            left = mid;
-        } else {
-            right = mid;
-        }
-    }
-    ll res = left + (N - 1) * (left - 1) + 
-        (K - check(left)) + 
-        count_if(all(v), [&](ll x){ return x >= left + 1; });
     
-    cout << res << "\n";
 }
 
 int main() {
