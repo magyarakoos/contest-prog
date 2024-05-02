28ed553d286c0b8569e930dd76f84484467f66c4
magyarakoos <magyar.kende.22f@gmail.com>
2024-05-02 08:44:44 +0200

    arch descktop sync: 2024-05-02 08:44:44

---
+++ codeforces/contest/1955/C/main.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+#define sz(v) (int)v.size()
+#define all(v) v.begin(), v.end()
+#define smin(x, y) x = min(x, y)
+#define smax(x, y) x = max(x, y)
+
+#define cases 1
+void solve() {
+    
+}
+
+int main() {
+    cin.tie(0), ios::sync_with_stdio(0);
+    int T = 1;
+    if (cases) cin >> T;
+    while (T--) solve();
+}
