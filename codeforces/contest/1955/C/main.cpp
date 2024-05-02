e87cc65bae937e6a13f50a55f73cd377369eef94
magyarakoos <magyar.kende.22f@gmail.com>
2024-05-02 10:54:36 +0200

    arch descktop sync: 2024-05-02 10:54:36

--- codeforces/contest/1955/C/main.cpp
+++ codeforces/contest/1955/C/main.cpp
@@ -11,58 +11,13 @@ using ll = long long;
 void solve() {
     ll N, K;
     cin >> N >> K;
-    vector v(N, 0LL);
-    for (ll& x : v) cin >> x;
-
-    int l = 0, r = N - 1, res = 0;
-    while (l <= r) {
-        assert(K >= 0);
-        if (K == 0) break;
-        if (l == r) {
-            v[l] -= K;
-            if (v[l] <= 0) res++;
-            break;
-        }
-        if (v[l] == v[r]) {
-            if (K >= v[l] * 2) {
-                K -= v[l] * 2;
-                res += 2;
-                l++;
-                r--;
-                continue;
-            }
-            if (K + 1 == v[l] * 2) {
-                res++;
-            }
-            break;
-        }
-        if (v[l] < v[r]) {
-            if (K >= v[l] * 2) {
-                K -= v[l] * 2;
-                res++;
-                l++;
-                continue;
-            }
-            if (K + 1 == v[l] * 2) {
-                res++;
-            }
-            break;
-        }
-        if (v[r] < v[l]) {
-            if (K >= v[r] * 2) {
-                K -= v[r] * 2;
-                res++;
-                r--;
-                continue;
-            }
-            if (K + 1 == v[r] * 2) {
-                res++;
-            }
-            break;
-        }
-        assert(0);
+    deque<int> q;
+    while (N--) {
+        int X;
+        cin >> X;
+        q.push_back(X);
     }
-    cout << res << "\n";
+    while ()
 }
 
 int main() {
