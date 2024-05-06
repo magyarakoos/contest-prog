2f09c980247e876514380ee881bebdacfe62ffe4
magyarakoos <ninjaoffical08@gmail.com>
2024-05-06 13:43:48 +0200

    arch descktop sync: 2024-05-06 13:43:48

--- cses/1712/main.cpp
+++ cses/1712/main.cpp
@@ -1,30 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
-const ll MOD = 1e9 + 7;
 
-ll mmul(ll a, ll b) {
-    return (a * b) % MOD;
+void mmul(ll a, ll b, ll m) {
+    a = a * b % m;
 }
 
-ll mpow(ll a, ll b) {
+ll mpow(ll a, ll b, ll m) {
     ll res = 1;
+    a %= m;
     while (b > 0) {
         if (b & 1) {
-            res = mmul(res, a);
+            mmul(res, a, m);
         }
-        a = mmul(a, a);
+        mmul(a, a, m);
         b >>= 1;
     }
     return res;
 }
 
 int main() {
+    const ll MOD = 1e9 + 7;
     int T;
     cin >> T;
     while (T--) {
         ll A, B, C;
         cin >> A >> B >> C;
-        cout << mpow(A, mpow(B, C)) << "\n";
+        cout << mpow(A, mpow(B, C, MOD - 1), MOD) << "\n";
     }
 }
