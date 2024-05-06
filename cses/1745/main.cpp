2cbc2625ade6eb8674907275f0604c75710a327b
magyarakoos <ninjaoffical08@gmail.com>
2024-05-06 14:51:38 +0200

    arch descktop sync: 2024-05-06 14:51:38

--- cses/1745/main.cpp
+++ cses/1745/main.cpp
@@ -1,17 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
+const int MAXN = 1e5;
 
 int main() {
     int N;
     cin >> N;
-    vector<bool> dp(100'001);
+    vector<bool> dp(MAXN + 1);
     dp[0] = 1;
 
     while (N--) {
         int X;
         cin >> X;
         dp[X] = 1;
-        for (int i = N; i >= X; i--) {
+        for (int i = MAXN; i >= X; i--) {
             if (dp[i - X]) dp[i] = 1;
         }
         for (int i = 1; i <= 13; i++) cout << dp[i];
@@ -19,7 +20,7 @@ int main() {
     }
 
     vector<int> result;
-    for (int i = 1; i <= 100'000; i++) {
+    for (int i = 1; i <= MAXN; i++) {
         if (dp[i]) result.push_back(i);
     }
 
