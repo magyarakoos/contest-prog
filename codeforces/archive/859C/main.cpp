0193d1cd362dee3e9c51d2ac7fd8b7e46ccab9e6
magyarakoos <ninjaoffical08@gmail.com>
2024-05-06 14:26:45 +0200

    arch descktop sync: 2024-05-06 14:26:45

--- codeforces/archive/859C/main.cpp
+++ codeforces/archive/859C/main.cpp
@@ -11,31 +11,19 @@ int main() {
     cin.tie(0), ios::sync_with_stdio(0);
     int N;
     cin >> N;
-    vector<int> v(N), ss(N), dp(N);
+    vector<int> v(N), dp(N);
     for (int& x : v) cin >> x;
 
-    ss[N - 1] = v[N - 1];
-    for (int i = N - 2; i >= 0; i--) {
-        ss[i] = ss[i + 1] + v[i];
-    }
-
     dp[N - 1] = v[N - 1];
+    int suffix_sum = v[N - 1];
 
     for (int i = N - 2; i >= 0; i--) {
         dp[i] = max(
             dp[i + 1],
-            v[i] + (ss[i + 1] - dp[i + 1])
+            v[i] + suffix_sum - dp[i + 1]
         );
+        suffix_sum += v[i];
     }
 
-    for (int i = 0; i < N; i++) {
-        cout << dp[i] << " ";
-    }
-    cout << "\n";
-    for (int i = 0; i < N; i++) {
-        cout << ss[i] << " ";
-    }
-    cout << "\n";
-
-    cout << dp[0] << " " << ss[0] - dp[0] << "\n";
+    cout << suffix_sum - dp[0] << " " << "\n";
 }
