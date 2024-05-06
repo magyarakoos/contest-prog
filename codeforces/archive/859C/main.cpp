ce34be945ebc3a5bc7be436d54b9947228f28dc4
magyarakoos <ninjaoffical08@gmail.com>
2024-05-06 14:19:27 +0200

    arch descktop sync: 2024-05-06 14:19:27

--- codeforces/archive/859C/main.cpp
+++ codeforces/archive/859C/main.cpp
@@ -2,7 +2,7 @@
 
 #define all(v) v.begin(), v.end()
 #define rall(v) v.rbegin(), v.rend()
-#define size(v) (int)v.size()
+#define sz(v) (int)v.size()
 
 using namespace std;
 using ll = long long;
@@ -11,7 +11,22 @@ int main() {
     cin.tie(0), ios::sync_with_stdio(0);
     int N;
     cin >> N;
-    vector<int> v(N);
+    vector<int> v(N), ss(N), dp(N);
     for (int& x : v) cin >> x;
-    
+
+    ss[N - 1] = v[N - 1];
+    for (int i = N - 2; i >= 0; i--) {
+        ss[i] = ss[i + 1] + v[i];
+    }
+
+    dp[N - 1] = v[N - 1];
+
+    for (int i = N - 2; i >= 0; i--) {
+        dp[i] = max(
+            ss[i] - dp[i + 1],
+            dp[i + 1] - v[i]
+        );
+    }
+
+    cout << dp[0] << " " << ss[0] - dp[0] << "\n";
 }
