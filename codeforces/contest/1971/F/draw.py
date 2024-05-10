477d590182a7590a271f20694af6a19d425f18f3
magyarakoos <ninjaoffical08@gmail.com>
2024-05-10 18:07:38 +0200

    arch descktop sync: 2024-05-10 18:07:38

---
+++ codeforces/contest/1971/F/draw.py
@@ -0,0 +1,21 @@
+def draw_circle(x0, y0, radius):
+    x = radius
+    y = 0
+    err = 0
+
+    while x >= y:
+        print(x0 + x, y0 + y)
+        print(x0 + y, y0 + x)
+        print(x0 - y, y0 + x)
+        print(x0 - x, y0 + y)
+        print(x0 - x, y0 - y)
+        print(x0 - y, y0 - x)
+        print(x0 + y, y0 - x)
+        print(x0 + x, y0 - y)
+
+        if err <= 0:
+            y += 1
+            err += 2*y + 1
+        if err > 0:
+            x -= 1
+            err -= 2*x + 1
