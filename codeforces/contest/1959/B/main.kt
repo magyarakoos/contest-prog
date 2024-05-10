b3395ea3e8254f87c686934cf0a771f3483e926a
magyarakoos <ninjaoffical08@gmail.com>
2024-05-10 09:49:23 +0200

    arch descktop sync: 2024-05-10 09:49:23

--- codeforces/contest/1959/B/main.kt
+++ codeforces/contest/1959/B/main.kt
@@ -1,9 +1,41 @@
+// a < b ->  1
+// a = b ->  0
+// a > b -> -1
+fun cmp(a: String, b: String): Int {
+    if (a == b) return 0
+
+    val cntA = a.count { it == 'X' }
+    val cntB = b.count { it == 'X' }
+
+    val lA = a.last()
+    val lB = b.last()
+
+    if (lA == 'M') {
+        return if (lB == 'S') -1 else 1
+    }
+    if (lB == 'M') {
+        return if (lA == 'S') 1 else -1
+    }
+
+    if (lA == 'S' && lB == 'L') {
+        return 1
+    }
+    if (lA == 'L' && lB == 'S') {
+        return -1
+    }
+
+    if (lA == 'S' && lB == 'S') {
+        return if (cntA > cntB) 1 else -1
+    }
+
+    return if (cntA < cntB) 1 else -1
+}
+
 fun main() {
-    var T = readln().toInt()
-    repeat (T) {
-        val A = readln().split(" ")
-        val a = A[0]
-        val b = A[1]
-        
+    val T = readln().toInt()
+    repeat(T) {
+        val (a, b) = readln().split(" ")
+        val c = cmp(a, b)
+        println(if (c == 0) "=" else if (c == 1) "<" else ">")
     }
 }
