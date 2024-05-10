903e0a98b077875a835694596675f71080fd5489
magyarakoos <ninjaoffical08@gmail.com>
2024-05-10 09:18:38 +0200

    arch descktop sync: 2024-05-10 09:18:38

---
+++ codeforces/contest/1959/A/Main.kt
@@ -0,0 +1,23 @@
+import java.util.Scanner
+
+fun solve() {
+    val scanner = Scanner(System.`in`)
+    var N = scanner.nextInt()
+    val a = IntArray(100)
+    while (N-- > 0) {
+        val X = scanner.nextInt()
+        a[X - 1]++
+    }
+    for (i in 0 until 100) {
+        if (a[i] == 1) {
+            println(i + 1)
+            return
+        }
+    }
+}
+
+fun main() {
+    val scanner = Scanner(System.`in`)
+    var T = scanner.nextInt()
+    while (T-- > 0) solve()
+}
