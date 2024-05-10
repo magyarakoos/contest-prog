14c72977871ce902f8cbc3beda1917c8d5cba567
magyarakoos <ninjaoffical08@gmail.com>
2024-05-10 10:08:06 +0200

    arch descktop sync: 2024-05-10 10:08:06

--- codeforces/contest/1959/C/main.kt
+++ codeforces/contest/1959/C/main.kt
@@ -1,7 +1,25 @@
+import kotlin.math.abs
+import kotlin.math.min
+
 fun main() {
-    var T = readln().toInt()
-    repeat (T) {
-        val (N, K) = readln().split(" ")
-        
+    val T = readLine()!!.toInt()
+    repeat(T) {
+        val (N, K) = readLine()!!.split(" ").map { it.toInt() }
+        val lC = IntArray(26)
+        val uC = IntArray(26)
+        readLine()!!.forEach { c ->
+            if (c.isLowerCase()) {
+                lC[c - 'a']++
+            } else {
+                uC[c - 'A']++
+            }
+        }
+        var res = 0
+        var diffSum = 0
+        for (i in 0 until 26) {
+            res += min(lC[i], uC[i])
+            diffSum += abs(lC[i] - uC[i]) / 2
+        }
+        println(res + min(diffSum, K))
     }
 }
