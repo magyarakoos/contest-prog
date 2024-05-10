bebda05f735be5bb8f493bcc54cc280b751cdbb6
magyarakoos <ninjaoffical08@gmail.com>
2024-05-10 18:11:16 +0200

    arch descktop sync: 2024-05-10 18:11:16

--- codeforces/contest/1971/F/draw.py
+++ codeforces/contest/1971/F/draw.py
@@ -2,16 +2,17 @@ def draw_circle(x0, y0, radius):
     x = radius
     y = 0
     err = 0
+    points = set()
 
     while x >= y:
-        print(x0 + x, y0 + y)
-        print(x0 + y, y0 + x)
-        print(x0 - y, y0 + x)
-        print(x0 - x, y0 + y)
-        print(x0 - x, y0 - y)
-        print(x0 - y, y0 - x)
-        print(x0 + y, y0 - x)
-        print(x0 + x, y0 - y)
+        points.add((x0 + x, y0 + y))
+        points.add((x0 + y, y0 + x))
+        points.add((x0 - y, y0 + x))
+        points.add((x0 - x, y0 + y))
+        points.add((x0 - x, y0 - y))
+        points.add((x0 - y, y0 - x))
+        points.add((x0 + y, y0 - x))
+        points.add((x0 + x, y0 - y))
 
         if err <= 0:
             y += 1
@@ -20,4 +21,6 @@ def draw_circle(x0, y0, radius):
             x -= 1
             err -= 2*x + 1
 
-draw_circle(0, 0, 2)
+    return len(points)
+
+print(draw_circle(0, 0, 3))
