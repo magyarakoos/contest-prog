7a393a6a97c1e3279cc78cd4992084189b81a83d
magyarakoos <ninjaoffical08@gmail.com>
2024-05-10 18:22:46 +0200

    arch descktop sync: 2024-05-10 18:22:46

--- codeforces/contest/1971/F/draw.py
+++ codeforces/contest/1971/F/draw.py
@@ -1,26 +1,11 @@
-def draw_circle(x0, y0, radius):
-    x = radius
-    y = 0
-    err = 0
-    points = set()
+import math
 
-    while x >= y:
-        points.add((x0 + x, y0 + y))
-        points.add((x0 + y, y0 + x))
-        points.add((x0 - y, y0 + x))
-        points.add((x0 - x, y0 + y))
-        points.add((x0 - x, y0 - y))
-        points.add((x0 - y, y0 - x))
-        points.add((x0 + y, y0 - x))
-        points.add((x0 + x, y0 - y))
+def count_lattice_points(r):
+    count = 0
+    for x in range(0, r+1):
+        y = math.sqrt(r**2 - x**2)
+        count += math.floor(y)
+    return 4*count + 1  # multiply by 4 and add 1 for the origin
 
-        if err <= 0:
-            y += 1
-            err += 2*y + 1
-        if err > 0:
-            x -= 1
-            err -= 2*x + 1
-
-    return len(points)
-
-print(draw_circle(0, 0, 3))
+r = 2  # example input
+print(count_lattice_points(r))
