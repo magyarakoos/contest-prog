00e5fe83485640b78cf908a204a39d25a99a17d9
magyarakoos <ninjaoffical08@gmail.com>
2024-05-10 10:13:52 +0200

    arch descktop sync: 2024-05-10 10:13:52

--- codeforces/contest/1959/D/main.kt
+++ codeforces/contest/1959/D/main.kt
@@ -3,23 +3,7 @@ import kotlin.math.*
 fun main() {
     val T = readln().toInt()
     repeat(T) {
-        val K = readln().split(" ")[1].toInt()
-        val lC = IntArray(26)
-        val uC = IntArray(26)
-        readln().forEach { c ->
-            if (c.isLowerCase()) {
-                lC[c - 'a']++
-            }
-            else if (c.isUpperCase()) {
-                uC[c - 'A']++
-            }
-        }
-        var res = 0
-        var diffSum = 0
-        for (i in 0 until 26) {
-            res += min(lC[i], uC[i])
-            diffSum += abs(lC[i] - uC[i]) / 2
-        }
-        println(res + min(diffSum, K))
+        val A = readln().split(" ")
+        val N = A[0].toInt();
     }
 }
