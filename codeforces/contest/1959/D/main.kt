f2b47523efc1684db1e460abb1a8d5e64c1ac5ea
magyarakoos <ninjaoffical08@gmail.com>
2024-05-10 10:14:08 +0200

    arch descktop sync: 2024-05-10 10:14:08

--- codeforces/contest/1959/D/main.kt
+++ codeforces/contest/1959/D/main.kt
@@ -4,6 +4,7 @@ fun main() {
     val T = readln().toInt()
     repeat(T) {
         val A = readln().split(" ")
-        val N = A[0].toInt();
+        val N = A[0].toInt()
+        val C = A[1][0]
     }
 }
