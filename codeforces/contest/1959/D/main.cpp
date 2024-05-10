22c4c62a29edf39faaec3f9d46ed59dca3fc7267
magyarakoos <ninjaoffical08@gmail.com>
2024-05-10 10:18:05 +0200

    arch descktop sync: 2024-05-10 10:18:05

---
+++ codeforces/contest/1959/D/main.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        int N;
+        char c;
+        cin >> N >> c;
+        string s;
+        cin >> s;
+        s += s;
+
+        vector<int> fc(s.size());
+    }
+}
