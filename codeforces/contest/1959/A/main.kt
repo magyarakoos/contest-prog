7f27972f44efa3ef781dd721621b3f5b98e9e3fc
magyarakoos <ninjaoffical08@gmail.com>
2024-05-10 09:32:28 +0200

    arch descktop sync: 2024-05-10 09:32:28

--- codeforces/contest/1959/A/main.kt
+++ codeforces/contest/1959/A/main.kt
@@ -1,7 +1,7 @@
 fun main() {
     var T = readln().toInt()
     repeat (T) {
-        val N = readln().toInt()
+        readln()
         val A = readln().split(" ").map { it.toInt() }
         println(A.indexOfFirst { x -> A.count({it == x}) == 1 } + 1)
     }
