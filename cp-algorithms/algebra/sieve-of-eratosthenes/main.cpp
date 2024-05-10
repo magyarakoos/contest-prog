d48457b2df123b76d749a9e21fcb38b67443bfe9
magyarakoos <ninjaoffical08@gmail.com>
2024-05-10 14:21:30 +0200

    arch descktop sync: 2024-05-10 14:21:30

--- cp-algorithms/algebra/sieve-of-eratosthenes/main.cpp
+++ cp-algorithms/algebra/sieve-of-eratosthenes/main.cpp
@@ -1,26 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
-using ll = long long;
 
 int main(int argc, char* argv[]) {
-    ll n = stoll(argv[1]);
-
+    size_t n = stoul(argv[1]);
+    
     vector is_prime(n + 1, true);
 
     is_prime[0] = is_prime[1] = 0;
 
-    for (long long i = 2; i * i <= n; i++) {
-
+    for (size_t i = 2; i * i <= n; i++) {
         if (is_prime[i]) {
-            for (int j = i * i; j <= n; j += i) {
+            for (size_t j = i * i; j <= n; j += i) {
                 is_prime[j] = 0;
             }
         }
     }
      
-    for (int i = 2; i <= n; i++) {
+    for (size_t i = 2; i <= n; i++) {
         if (is_prime[i]) {
-            cout << i << '\n';
+            cout << i << "\n";
         }
     }
 }
