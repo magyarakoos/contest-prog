864fde2f50330af056b61351ec8dd41808f467b0
magyarakoos <ninjaoffical08@gmail.com>
2024-05-10 09:10:34 +0200

    arch descktop sync: 2024-05-10 09:10:34

--- codeforces/contest/1959/A/main.cpp
+++ codeforces/contest/1959/A/main.cpp
@@ -1,2 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+void solve() {
+    int N;
+    cin >> N;
+    vector<int> a(100);
+    while (N--) {
+        int X;
+        cin >> X;
+        a[X - 1]++;
+    }
+    for (int i = 0; i < 100; i++) {
+        if (a[i] == 1) {
+            cout << i + 1 << "\n";
+            return;
+        }
+    }
+}
+
+int main() {
+    int T;
+    cin >> T;
+    while (T--) solve();
+}
