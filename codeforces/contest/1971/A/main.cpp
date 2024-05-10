49361f75578fe84eb8c4315d8280b5c0cce15c65
magyarakoos <ninjaoffical08@gmail.com>
2024-05-10 19:02:42 +0200

    arch descktop sync: 2024-05-10 19:02:42

---
+++ codeforces/contest/1971/A/main.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define sz(v) (int)v.size()
+#define smin(x, y) x = min(x, y)
+#define smax(x, y) x = max(x, y)
+
+const ll INF = 2e9, MAXN = 2e5, MOD = 1e9 + 7;
+
+#define cases 1
+void solve() {
+    int X, Y;
+    cin >> X >> Y;
+    cout << min(X, Y) << " " << max(X, Y) << "\n";
+}
+
+int main() {
+    cin.tie(0), ios::sync_with_stdio(0);
+    int T = 1;
+    if (cases) cin >> T;
+    while (T--) solve();
+}
