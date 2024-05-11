d11bbe6b27219854b1db0df7a58c44217d557033
magyarakoos <magyar.kende.22f@gmail.com>
2024-05-11 22:41:46 +0200

    arch descktop sync: 2024-05-11 22:41:46

--- codeforces/archive/1970A1/main.cpp
+++ codeforces/archive/1970A1/main.cpp
@@ -26,7 +26,11 @@ int main() {
     for (int i = 0; i < N; i++) {
         v[i] = {i, bal[i], s[i]};
     }
-    sort(all(v), [](Item i) {
-        
+    sort(all(v), [](Item a, Item b) {
+        if (a.balance < b.balance) return true;
+        if (a.balance > b.balance) return false;
+        return a.index > b.index;
     });
+    for (Item x : v) cout << x.c;
+    cout << "\n";
 }
