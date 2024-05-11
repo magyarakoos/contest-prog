5fc6b356068ad4db4b39a2c9500e987f3c84d2f9
magyarakoos <magyar.kende.22f@gmail.com>
2024-05-11 22:38:07 +0200

    arch descktop sync: 2024-05-11 22:38:07

--- codeforces/archive/1970A1/main.cpp
+++ codeforces/archive/1970A1/main.cpp
@@ -2,7 +2,7 @@
 
 #define all(v) v.begin(), v.end()
 #define rall(v) v.rbegin(), v.rend()
-#define size(v) (int)v.size()
+#define sz(v) (int)v.size()
 
 using namespace std;
 using ll = long long;
@@ -11,5 +11,9 @@ int main() {
     cin.tie(0), ios::sync_with_stdio(0);
     string s;
     cin >> s;
-    
+    int N = sz(s);
+    vector<int> bal(N + 1);
+    for (int i = 1; i <= N; i++) {
+        bal[i] = bal[i - 1] + (s[i - 1] == '(' ? 1 : -1);
+    }
 }
