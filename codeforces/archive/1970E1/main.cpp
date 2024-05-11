4407a9466c883cdbc309593320c0912364cc0959
magyarakoos <magyar.kende.22f@gmail.com>
2024-05-11 21:59:06 +0200

    arch descktop sync: 2024-05-11 21:59:06

--- codeforces/archive/1970E1/main.cpp
+++ codeforces/archive/1970E1/main.cpp
@@ -7,11 +7,26 @@
 using namespace std;
 using ll = long long;
 
+const int MOD = 1e9 + 7;
+
 int main() {
     cin.tie(0), ios::sync_with_stdio(0);
     
     int M, N;
     cin >> M >> N;
 
+    vector<int> S(M), L(M);
+    for (int& e : S) cin >> e;
+    for (int& e : L) cin >> e;
+
+    int S_sum = accumulate(all(S), 0),
+        L_sum = accumulate(all(L), 0);
     
+    int res = 0;
+
+    for (int i = 0; i < N; i++) {
+        res += S_sum * S_sum;
+        res %= MOD;
+
+    }
 }
