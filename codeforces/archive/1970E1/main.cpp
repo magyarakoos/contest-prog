33a29d00ce2ec0e59977940e4f5a0dfaaf30b30b
magyarakoos <magyar.kende.22f@gmail.com>
2024-05-11 22:31:27 +0200

    arch descktop sync: 2024-05-11 22:31:27

--- codeforces/archive/1970E1/main.cpp
+++ codeforces/archive/1970E1/main.cpp
@@ -15,9 +15,9 @@ int main() {
     int M, N;
     cin >> M >> N;
 
-    vector<ll> S(M), L(M);
-    for (ll& e : S) cin >> e;
-    for (ll& e : L) cin >> e;
+    vector<int> S(M), L(M);
+    for (int& e : S) cin >> e;
+    for (int& e : L) cin >> e;
 
     vector dp(N + 1, vector<ll>(M));
     dp[0][0] = 1;
@@ -25,8 +25,10 @@ int main() {
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < M; j++) {
             for (int k = 0; k < M; k++) {
-                dp[i + 1][j] += ((S[j] + L[j]) * (S[k] + L[k]) - L[j] * L[k]) * dp[i][k];
-                dp[i + 1][j] %= MOD;
+                dp[i + 1][k] += dp[i][j] * (S[j] + L[j]) * S[k];
+                dp[i + 1][k] %= MOD;
+                dp[i + 1][k] += dp[i][j] * S[j] * L[k];   
+                dp[i + 1][k] %= MOD;
             }
         }
     }
