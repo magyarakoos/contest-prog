d3c39b6b6500cace05c1095d03c88bba74342c44
magyarakoos <ninjaoffical08@gmail.com>
2024-05-14 18:29:24 +0200

    arch desktop sync: 2024-05-14 18:29:24

--- codeforces/archive/1950D/main.cpp
+++ codeforces/archive/1950D/main.cpp
@@ -7,7 +7,33 @@
 using namespace std;
 using ll = long long;
 
+const int MAXN = 1e5;
+vector<int> bindecS;
+
+bool isbindec(int x) {
+    return 0;
+}
+
+void solve() {
+    int N;
+    cin >> N;
+    for (int x : bindecS) {
+        if (N == 1) break;
+        if (N % x == 0) {
+            N /= x;
+        }
+    }
+    cout << (N == 1 ? "YES\n" : "NO\n");
+}
+
 int main() {
     cin.tie(0), ios::sync_with_stdio(0);
-
+    for (int i = 1; i <= MAXN; i++) {
+        if (isbindec(i)) {
+            bindecS.push_back(i);
+        }
+    }
+    int T;
+    cin >> T;
+    while (T--) solve();
 }
