f67d472be9b60d309de9ada2297b63cebabb9748
magyarakoos <magyar.kende.22f@gmail.com>
2024-05-16 13:32:10 +0200

    arch desktop sync: 2024-05-16 13:32:10

--- mester/haladó/gráfok-legrövidebb-utak/autobusz-jaratok/main.cpp
+++ mester/haladó/gráfok-legrövidebb-utak/autobusz-jaratok/main.cpp
@@ -23,6 +23,8 @@ int main() {
     int S, E;
     cin >> S >> E;
 
+    distS[S] = 0;
+
     for (int i = 1; i <= N - 1; i++) {
         for (auto [u, v, w] : edgeS) {
             if (distS[u] + w < distS[v]) {
