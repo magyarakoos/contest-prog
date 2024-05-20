1c0c808d3a606fdcb0f889783263af050f2d149e
magyarakoos <ninjaoffical08@gmail.com>
2024-05-20 14:27:42 +0200

    arch desktop sync: 2024-05-20 14:27:42

--- mester/nt-oktv-ioi/OKTV17/legalabb-2-bolygo/main.cpp
+++ mester/nt-oktv-ioi/OKTV17/legalabb-2-bolygo/main.cpp
@@ -10,4 +10,25 @@ using ll = long long;
 int main() {
     cin.tie(0), ios::sync_with_stdio(0);
 
+    int A, B, C;
+    map<int, vector<int>> m;
+    while (A--) {
+        int K, V;
+        cin >> K >> V;
+        m[K].push_back(1);
+        m[V].push_back(-1);
+    }
+    while (B--) {
+        int K, V;
+        cin >> K >> V;
+        m[K].push_back(1);
+        m[V].push_back(-1);
+    }
+    while (C--) {
+        int K, V;
+        cin >> K >> V;
+        m[K].push_back(1);
+        m[V].push_back(-1);
+    }
+
 }
