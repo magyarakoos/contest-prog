47ecb53905a894f3d823550732c0326bf5520feb
magyarakoos <ninjaoffical08@gmail.com>
2024-05-20 14:53:42 +0200

    arch desktop sync: 2024-05-20 14:53:42

--- mester/nt-oktv-ioi/OKTV17/legalabb-2-bolygo/main.cpp
+++ mester/nt-oktv-ioi/OKTV17/legalabb-2-bolygo/main.cpp
@@ -14,25 +14,28 @@ int main() {
     cin >> A >> B >> C;
     N = A + B + C;
 
-    map<int, int> m;
+    map<int, array<int, 2>> m;
 
     while (N--) {
         int K, V;
         cin >> K >> V;
-        m[K]++;
-        m[V + 1]--;
+        m[K][0]++;
+        m[V + 1][1]++;
     }
 
     int curr = 0, curr_K = 0;
     vector<array<int, 2>> res;
     for (const auto& [K, D] : m) {
-        if (curr >= 2 && curr + D < 2) {
+        auto [DK, DV] = D;
+        if (DV && curr >= 2 && curr + (DK - DV) < 2) {
             res.push_back({curr_K, K - 1});
         }
-        curr += D;
-        curr_K = K;
+        if (DK && curr < 2 && curr + (DK - DV) >= 2) {
+            curr_K = K;
+        }
+        curr += DK - DV;
     }
-    
+
     cout << size(res) << "\n";
     for (auto [K, V] : res) cout << K << " " << V << "\n";
 }
