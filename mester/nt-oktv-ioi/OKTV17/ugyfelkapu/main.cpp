2a2559f60d28118b575fff639e3f6e7f942a61ab
magyarakoos <ninjaoffical08@gmail.com>
2024-05-20 15:14:21 +0200

    arch desktop sync: 2024-05-20 15:14:21

--- mester/nt-oktv-ioi/OKTV17/ugyfelkapu/main.cpp
+++ mester/nt-oktv-ioi/OKTV17/ugyfelkapu/main.cpp
@@ -7,7 +7,29 @@
 using namespace std;
 using ll = long long;
 
+struct Guy {
+    int i;
+    int K;
+    int T;
+    bool operator()(Guy A, Guy B) {
+        if (A.T > B.T) return 1;
+        if (A.T < B.T) return 0;
+        if (A.K > B.K) return 1;
+        if (A.K < B.K) return 0;
+        return A.i > B.i;
+    }
+};
+
 int main() {
     cin.tie(0), ios::sync_with_stdio(0);
 
+    int N;
+    cin >> N;
+    vector<Guy> v(N);
+    for (int i = 0; i < N; i++) {
+        cin >> v[i].K >> v[i].T;
+        v[i].i = i;
+    }
+
+    for (Guy g : v) cout << g.i << " " << g.K << " " << g.T << "\n";
 }
