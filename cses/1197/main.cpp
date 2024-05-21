8641774e37f41299126000f8f0337039c25a3145
magyarakoos <ninjaoffical08@gmail.com>
2024-05-21 17:00:48 +0200

    arch desktop sync: 2024-05-21 17:00:48

---
+++ cses/1197/main.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+const ll INF = 1e18;
+
+int main() {
+	cin.tie(0), ios::sync_with_stdio(0);
+
+    int N, M;
+    cin >> N >> M;
+    
+    vector<array<int, 3>> edgeS(M);
+    for (auto& [U, V, W] : edgeS) cin >> U >> V >> W;
+
+    vector distS(N + 1, INF);
+    vector<int> prv(N + 1);
+
+    distS[1] = 0;
+
+    for (int i = 0; i < N - 1; i++) {
+        for (auto [u, v, w] : edgeS) {
+            if (distS[u] + w < distS[v]) {
+                prv[v] = u;
+                distS[v] = distS[u] + w;
+            }
+        }
+    }
+
+    
+}
