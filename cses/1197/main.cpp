4842457f1d33260f5401296f96641bf81a88ade6
magyarakoos <ninjaoffical08@gmail.com>
2024-05-21 17:29:21 +0200

    arch desktop sync: 2024-05-21 17:29:21

--- cses/1197/main.cpp
+++ cses/1197/main.cpp
@@ -4,6 +4,19 @@ using ll = long long;
 
 const ll INF = 1e18;
 
+vector<int> path(int u, const vector<int>& prv) {
+    vector<int> res({u});
+
+    int curr = prv[u];
+    do {
+        res.push_back(curr);
+        curr = prv[curr];
+    } while(res.front() != res.back());
+
+    reverse(res.begin(), res.end());
+    return res;
+}
+
 int main() {
 	cin.tie(0), ios::sync_with_stdio(0);
 
@@ -14,14 +27,14 @@ int main() {
     for (auto& [U, V, W] : edgeS) cin >> U >> V >> W;
 
     vector distS(N + 1, INF);
-    vector<int> nxt(N + 1, 1);
+    vector<int> prv(N + 1);
 
     distS[1] = 0;
 
-    for (int i = 0; i < N - 1; i++) {
+    for (int i = 0; i < 2 * N; i++) {
         for (auto [u, v, w] : edgeS) {
             if (distS[u] + w < distS[v]) {
-                nxt[u] = v;
+                prv[v] = u;
                 distS[v] = distS[u] + w;
             }
         }
@@ -29,12 +42,10 @@ int main() {
 
     for (auto [u, v, w] : edgeS) {
         if (distS[u] + w < distS[v]) {
-            cout << "YES\n" << u;
-            int curr = u;
-            do {
-                curr = nxt[curr];
-                cout << " " << curr;
-            } while (curr != u);
+            prv[v] = u;
+            cout << "YES\n";
+            for (int x : path(v, prv)) cout << x << " ";
+            cout << "\n";
             exit(0);
         }
     }
