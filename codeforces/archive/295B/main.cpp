81a8836f5275d038cc54c8794802d4ea9d14ed3d
magyarakoos <ninjaoffical08@gmail.com>
2024-05-21 17:46:56 +0200

    arch desktop sync: 2024-05-21 17:46:56

--- codeforces/archive/295B/main.cpp
+++ codeforces/archive/295B/main.cpp
@@ -10,4 +10,14 @@ using ll = long long;
 int main() {
     cin.tie(0), ios::sync_with_stdio(0);
 
+    int N;
+    cin >> N;
+
+    vector distS(N + 1, vector<int>(N + 1));
+
+    for (int i = 1; i <= N; i++) {
+        for (int j = 1; j <= N; j++) {
+            cin >> distS[i][j];
+        }
+    }
 }
