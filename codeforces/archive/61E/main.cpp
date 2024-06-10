5f29af05df3baa075b1c45cfe0b553fe75083279
magyarakoos <magyar.kende.22f@gmail.com>
2024-06-10 18:05:55 +0200

    sync: 2024-06-10 18:05:55

--- codeforces/archive/61E/main.cpp
+++ codeforces/archive/61E/main.cpp
@@ -28,7 +28,7 @@ vector<ll> more_cnt(const vector<int>& v) {
 	ordered_set os;
 	vector<ll> result(v.size());
 	for (int i = v.size() - 1; ~i; i--) {
-		result[i] = os.order_of_key(v[i] - 1);
+		result[i] = os.size() - os.order_of_key(v[i]);
 		os.insert(v[i]);
 	}
 	return result;
