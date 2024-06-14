c865c704908d0479ba8a96b90cc31eb96cf1f926
magyarakoos <magyar.kende.22f@gmail.com>
2024-06-14 19:18:42 +0200

    sync: 2024-06-14 19:18:42

---
+++ cses/1675/main.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+vector<int> lnk, crd;
+
+bool same(int u, int v) {
+    return lnk[u] == lnk[v];
+}
+
+int find(int u) {
+    while (u != lnk[u]) u = lnk[u];
+    return lnk[u];
+}
+
+void unite(int u, int v) {
+    u = find(u);
+    v = find(v);
+    if (crd[v] < crd[u]) swap(u, v);
+    crd[u] += crd[v];
+    lnk[v] = u;
+}
+
+int main() {
+    cin.tie(0), ios::sync_with_stdio(0);
+
+    int N, M;
+    cin >> N >> M;
+
+    vector<array<int, 3>> edgeS(M);
+    lnk.resize(N + 1);
+    crd.resize(N + 1);
+
+    for (int i = 0; i < M; i++) {
+        cin >> edgeS[i][1] >> edgeS[i][2] >> edgeS[i][0];
+    }
+
+    sort(edgeS.begin(), edgeS.end());
+
+    ll result = 0;
+
+    for (auto [w, u, v] : edgeS) {
+        if (!same(u, v)) {
+            unite(u, v);
+            result += w;
+        }
+    }
+
+    cout << result << "\n";
+}
