c4056cfea70b6a50f1a5b2db7de06caf09b36950
magyarakoos <magyar.kende.22f@gmail.com>
2024-06-16 19:37:39 +0200

    sync: 2024-06-16 19:37:39

---
+++ codeforces/contest/1978/A/main.cpp
@@ -0,0 +1,35 @@
+#include <unistd.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#ifdef LOCAL
+#include <magyarakoos-debug.h>
+#else
+#define DB(...)
+#endif
+using namespace std;
+using ll = long long;
+
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define sz(v) (int)v.size()
+#define smin(x, y) x = min(x, y)
+#define smax(x, y) x = max(x, y)
+
+// const ll INF = 2e9, MAXN = 2e5, MOD = 1e9 + 7;
+
+#define cases 1
+void solve() {
+    int N;
+    cin >> N;
+    vector<int> v(N);
+    for (int& x : v) cin >> x;
+    cout << *max_element(v.begin(), v.end() - 1) + v[N - 1] << "\n";
+}
+
+int main() {
+    cin.tie(0), ios::sync_with_stdio(0);
+    int T = 1;
+    if (cases) cin >> T;
+    while (T--) solve();
+}
---
+++ codeforces/contest/1978/B/main.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+#ifdef LOCAL
+#include <magyarakoos-debug.h>
+#else
+#define DB(...)
+#endif
+using namespace std;
+using ll = long long;
+
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define sz(v) (int)v.size()
+#define smin(x, y) x = min(x, y)
+#define smax(x, y) x = max(x, y)
+
+const ll INF = 2e9, MAXN = 2e5, MOD = 1e9 + 7;
+
+#define cases 1
+void solve() {
+    ll N, A, B;
+    cin >> N >> A >> B;
+    if (B <= A) {
+        cout << N * A << "\n";
+        return;
+    }
+    ll d = B - A;
+    if (d > N) {
+        cout << B * (B + 1) / 2 - (B - N) * (B - N + 1) / 2 << "\n";
+    } else {
+        ll nn = B * (B + 1) / 2 - A * (A + 1) / 2;
+        cout << nn + (N - d) * A << "\n";
+    }
+}
+
+int main() {
+    cin.tie(0), ios::sync_with_stdio(0);
+    int T = 1;
+    if (cases) cin >> T;
+    while (T--) solve();
+}
