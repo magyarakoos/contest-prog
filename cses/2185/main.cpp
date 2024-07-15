441fc0f7b4c4caf910adcc65a9b3a033628c2cdf
magyarakoos <magyar.kende.22f@gmail.com>
2024-07-15 14:30:00 +0200

    sync: 2024-07-15 14:30:00

--- cses/2185/main.cpp
+++ cses/2185/main.cpp
@@ -4,34 +4,27 @@
 using namespace std;
 using ll = long long;
 
-ll prod(const vector<ll>& a, int mask, int N) {
-    int n = a.size();
-    ll result = 1;
-    for (int i = 0; i < n; i++) {
-        if (mask >> i & 1) {
-            result *= a[i];
-            if (result >= N) {
-                return N + 1;
-            }
-        }
-    }
-    return result;
-}
-
 int main() {
     cin.tie(0), ios::sync_with_stdio(0);
 
-    int N, K;
+    ll N, K;
     cin >> N >> K;
 
     ll result = 0;
 
     vector<ll> a(K);
-    for (ll& x : a) cin >> x, result += N / x;
+    for (ll& x : a) cin >> x;
 
-    for (int i = 0; i < K; i++) {
-        for (int j = 0; j < i; j++) {
-            result -=
+    for (int mask = 1; mask < (1 << K); mask++) {
+        ll curr = N;
+        for (int i = 0; i < K; i++) {
+            if (mask >> i & 1) curr /= a[i];
+        }
+
+        if (__builtin_popcount(mask) & 1) {
+            result += curr;
+        } else {
+            result -= curr;
         }
     }
 
