090598ee091766d68e705887b0dd2f45c0ae303c
magyarakoos <magyar.kende.22f@gmail.com>
2024-07-17 10:10:06 +0200

    sync: 2024-07-17 10:10:06

--- mester/haladó/visszalépéses-keresés/iskolavalasztas/main.cpp
+++ mester/haladó/visszalépéses-keresés/iskolavalasztas/main.cpp
@@ -1,3 +1,4 @@
+#include <cassert>
 #include <iostream>
 #include <utility>
 #include <vector>
@@ -9,6 +10,33 @@ int main() {
     cin >> N >> M;
 
     vector<pair<int, int>> choiceS(N);
+    vector<int> capS(M);
 
-     
+    for (int i = 0; i < N; i++) cin >> choiceS[i].first >> choiceS[i].second;
+    for (int i = 0; i < M; i++) cin >> capS[i];
+
+    for (int mask = 0; mask < (1 << N); mask++) {
+        vector<int> a(N), b(M);
+        bool ok = 1;
+
+        for (int i = 0; i < N; i++) {
+            a[i] = (mask >> i & 1 ? choiceS[i].second : choiceS[i].first);
+
+            if ((mask >> i & 1 && !choiceS[i].second) ||
+                b[a[i] - 1] == capS[a[i] - 1]) {
+                goto next;
+            }
+
+            b[a[i] - 1]++;
+        }
+
+        for (int x : a) cout << x << " ";
+        cout << "\n";
+        return 0;
+
+    next:
+        continue;
+    }
+
+    assert(0);
 }
