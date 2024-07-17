34ef5efe8a8446d2603ceeb97e7eac5d7a439fd2
magyarakoos <magyar.kende.22f@gmail.com>
2024-07-17 09:46:53 +0200

    sync: 2024-07-17 09:46:53

--- mester/haladó/visszalépéses-keresés/iskolavalasztas/main.cpp
+++ mester/haladó/visszalépéses-keresés/iskolavalasztas/main.cpp
@@ -1,39 +1,14 @@
 #include <iostream>
+#include <utility>
 #include <vector>
-#include <array>
+
 using namespace std;
 
 int main() {
     int N, M;
     cin >> N >> M;
-    vector<array<int, 2>> kidS(N);
-    vector<int> capS(M);
-    for (auto& [a, b] : kidS) cin >> a >> b;
-    for (int& x : capS) cin >> x;
-    
-    for (int mask = 0; mask < (1 << N); mask++) {
-        bool valid = 1;
-        vector<int> countS(M), placeS(N);
-        for (int i = 0; i < N; i++) {
-            bool bit = (mask >> i) & 1;
 
-            if (!kidS[i][bit]) {
-                valid = 0;
-                break;
-            }
+    vector<pair<int, int>> choiceS(N);
 
-            placeS[i] = kidS[i][bit];
-
-            if (++countS[placeS[i] - 1] > capS[placeS[i] - 1]) {
-                valid = 0;
-                break;
-            }
-        }
-        
-        if (valid) {
-            for (int p : placeS) cout << p << " ";
-            cout << "\n";
-            exit(0);
-        }
-    }
+     
 }
