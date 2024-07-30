d216a2c8a3fa1b3d5164cf19d9609c3be63b6c30
magyarakoos <magyar.kende.22f@gmail.com>
2024-07-30 16:24:17 +0200

    sync: 2024-07-30 16:24:17

---
+++ atcoder/abc364/C/main.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+#ifdef LOCAL
+#include <magyarakoos-debug.h>
+#else
+#define DB(...)
+#endif
+using namespace std;
+using ll = long long;
+
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define sz(v) (int)v.size()
+#define smin(x, y) x = min(x, y)
+#define smax(x, y) x = max(x, y)
+
+const ll INF = 2e9, MAXN = 2e5, MOD = 1e9 + 7;
+
+#define cases 0
+void solve() {
+    ll N, X, Y;
+    cin >> N >> X >> Y;
+    vector<ll> a(N), b(N);
+    for (ll& x : a) cin >> x;
+    for (ll& x : b) cin >> x;
+
+    sort(rall(a));
+    sort(rall(b));
+
+    int res = N;
+    for (int i = 0; i < N; i++) {
+        if (X < 0) {
+            res = min(res, i);
+            break;
+        }
+        X -= a[i];
+    }
+
+    for (int i = 0; i < N; i++) {
+        if (Y < 0) {
+            res = min(res, i);
+            break;
+        }
+        Y -= b[i];
+    }
+
+    cout << res << "\n";
+}
+
+int main() {
+    cin.tie(0), ios::sync_with_stdio(0);
+    int T = 1;
+    if (cases) cin >> T;
+    while (T--) solve();
+}
---
+++ atcoder/abc364/D/main.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+#ifdef LOCAL
+#include <magyarakoos-debug.h>
+#else
+#define DB(...)
+#endif
+using namespace std;
+using ll = long long;
+
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define sz(v) (int)v.size()
+#define smin(x, y) x = min(x, y)
+#define smax(x, y) x = max(x, y)
+
+const ll INF = 2e9, MAXN = 2e5, MOD = 1e9 + 7;
+
+int rcnt(const vector<int>& a, int l, int r) {
+    auto itl = lower_bound(all(a), l);
+    auto itr = upper_bound(all(a), r);
+    return itr - itl;
+}
+
+#define cases 0
+void solve() {
+    int N, Q;
+    cin >> N >> Q;
+    vector<int> a(N);
+    for (int& x : a) cin >> x;
+    sort(all(a));
+
+    while (Q--) {
+        int b, k;
+        cin >> b >> k;
+
+        int l = -1, r = 1e9;
+        while (r - l > 1) {
+            int m = (l + r) / 2;
+            if (rcnt(a, b - m, b + m) >= k) {
+                r = m;
+            } else {
+                l = m;
+            }
+        }
+
+        cout << r << "\n";
+    }
+}
+
+int main() {
+    cin.tie(0), ios::sync_with_stdio(0);
+    int T = 1;
+    if (cases) cin >> T;
+    while (T--) solve();
+}
