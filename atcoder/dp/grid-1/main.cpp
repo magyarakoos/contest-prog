7c650e9c8104ff288815acf754af6e256f6d5947
magyarakoos <magyar.kende.22f@gmail.com>
2024-08-07 23:10:05 +0200

    sync: 2024-08-07 23:10:05

--- atcoder/dp/grid-1/main.cpp
+++ atcoder/dp/grid-1/main.cpp
@@ -1,24 +1,14 @@
 #include <bits/stdc++.h>
-
-#define speed cin.tie(0); ios::sync_with_stdio(0)
-#define cinv(v) for (auto& e : v) cin >> e;
-#define all(v) v.begin(), v.end()
-#define has(s, e) s.count(e)
-
 using namespace std;
-using ll = long long;
-using point = array<int, 2>;
 
 const int MOD = 1e9 + 7;
 
 int main() {
-    speed;
-
     int H, W;
     cin >> H >> W;
 
-    vector<vector<char>> grid(H, vector<char>(W));
-    vector<vector<int>> dp(H, vector<int>(W));
+    vector grid(H, vector<char>(W));
+    vector dp(H, vector<int>(W));
 
     for (int i = 0; i < H; i++) {
         for (int j = 0; j < W; j++) {
@@ -32,11 +22,11 @@ int main() {
     for (int i = 1; i < H; i++) {
         for (int j = 1; j < W; j++) {
             if (grid[i][j] == '.') {
-                dp[i][j] = (long)(dp[i - 1][j] + dp[i][j - 1]) % MOD;
+                dp[i][j] =(dp[i - 1][j] + dp[i][j - 1]) % MOD;
             }
         }
     }
 
     cout << dp[H - 1][W - 1];
-
 }
+
