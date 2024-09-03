5aa268b5681ec6c2cc07f260f63e0eeca19f3e19
magyarakoos <magyar.kende.22f@gmail.com>
2024-09-03 15:17:01 +0200

    sync: 2024-09-03 15:17:01

--- atcoder/abc369/F/main.cpp
+++ atcoder/abc369/F/main.cpp
@@ -63,7 +63,8 @@ void solve() {
     for (int i = 0; i < N; i++) { m[b[i]] = i; }
 
     for (int i = 0; i < N; i++) {
-        auto [mx, j] = query(1, 0, N - 1, 0, i - 1);
+        // auto [mx, j] = query(1, 0, N - 1, 0, i - 1);
+        j = 0;
         cout << a[j][0] << " " << a[j][1] << " -> " << a[i][0] << " " << a[i][1]
              << "\n";
         // update(1, 0, N - 1, m[a[i]], {mx + 1, i});
