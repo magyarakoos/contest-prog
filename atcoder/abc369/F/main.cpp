7301e50f6af13fa89799d4cdeeb657cfae3cc8f7
magyarakoos <magyar.kende.22f@gmail.com>
2024-09-03 15:05:01 +0200

    sync: 2024-09-03 15:05:01

--- atcoder/abc369/F/main.cpp
+++ atcoder/abc369/F/main.cpp
@@ -39,7 +39,7 @@ void solve() {
     map<array<int, 2>, int> m;
     for (int i = 0; i < N; i++) { m[b[i]] = i; }
 
-    for (int i = 0; i < N; i++) {}
+    for (int i = 0; i < N; i++) { auto [mx, i] = query(1, 0, N - 1, 0, i - 1); }
 }
 
 int main() {
