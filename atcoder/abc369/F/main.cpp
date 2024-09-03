9c3b2cbc03ab71dcbc08002204764d23f2c05044
magyarakoos <magyar.kende.22f@gmail.com>
2024-09-03 16:04:01 +0200

    sync: 2024-09-03 16:04:01

--- atcoder/abc369/F/main.cpp
+++ atcoder/abc369/F/main.cpp
@@ -81,8 +81,7 @@ void solve() {
     int result = tc[nxt][0];
     int i = a[tc[nxt][1]][0], j = a[tc[nxt][1]][1];
     string p;
-    if (i == H - 1 && j == W - 1) { result++ }
-    string p = /* path({i, j}, {H - 1, W - 1}); */;
+    if (i != H - 1 || j != W - 1) { p = path({i, j}, {H - 1, W - 1}); }
 
     nxt = tc[nxt][2];
 
