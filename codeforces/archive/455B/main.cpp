029855236af47c4c76db05e1436eeb2327d03412
magyarakoos <magyar.kende.22f@gmail.com>
2024-09-09 14:56:14 +0200

    sync: 2024-09-09 14:56:14

---
+++ codeforces/archive/455B/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+const int MAXN = 2e6;
+
+int trie[MAXN][26], cnt[MAXN], node_count = 1;
+bool dp[MAXN][26];
+
+void insert(const string& s) {
+    int node = 1;
+    for (char c : s) {
+        int a = c - 'a';
+        if (!trie[node][a]) { trie[node][a] = ++node_count; }
+        node = trie[node][a];
+        cnt[node]++;
+    }
+}
+
+int main() {
+    cin.tie(0), ios::sync_with_stdio(0);
+
+    int N, K;
+    cin >> N >> K;
+
+    for (int i = 0; i < N; i++) {
+        string s;
+        cin >> s;
+        insert(s);
+    }
+
+    dfs()
+}
