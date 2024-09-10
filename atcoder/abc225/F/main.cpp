f5ad37e16fd1f5ebfbe1909527388907fe676b89
magyarakoos <magyar.kende.22f@gmail.com>
2024-09-10 17:45:00 +0200

    sync: 2024-09-10 17:45:00

--- atcoder/abc225/F/main.cpp
+++ atcoder/abc225/F/main.cpp
@@ -6,10 +6,18 @@ int main() {
     cin >> N >> K;
     vector<string> a(N);
     for (string& s : a) cin >> s;
+
     sort(a.begin(), a.end(),
          [](const string& s, const string& t) { return s + t > t + s; });
 
-    vector<string> dp(K, string(50, 'z'));
-
     // knapsack
+    vector<string> dp(K + 1, "[");
+    dp[0] = "";
+
+    for (string& s : a) {
+        for (int i = K; i > 0; i--) { dp[i] = min(dp[i], s + dp[i - 1]); }
+    }
+
+    cout << dp[K];
 }
+
