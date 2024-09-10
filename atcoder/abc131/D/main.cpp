0175af8d72e100a14a342fa9a386d1311b8e2b94
magyarakoos <magyar.kende.22f@gmail.com>
2024-09-10 16:44:01 +0200

    sync: 2024-09-10 16:44:01

--- atcoder/abc131/D/main.cpp
+++ atcoder/abc131/D/main.cpp
@@ -1,4 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {}
+int main() {
+    int N;
+    cin >> N;
+    vector<int> A(N), B(N);
+    for (int i = 0; i < N; i++) cin >> A[i] >> B[i];
+}
