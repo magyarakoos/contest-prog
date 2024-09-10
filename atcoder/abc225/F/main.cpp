b380dbd0183ab04a1de75b0926563ee44780c611
magyarakoos <magyar.kende.22f@gmail.com>
2024-09-10 17:49:01 +0200

    sync: 2024-09-10 17:49:01

--- atcoder/abc225/F/main.cpp
+++ atcoder/abc225/F/main.cpp
@@ -1,4 +1,3 @@
-#include "debug.h"
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -12,12 +11,13 @@ int main() {
          [](const string& s, const string& t) { return s + t > t + s; });
 
     // knapsack
-    vector<string> dp(K + 1, "[");
+    vector<string> dp(K + 1, string(50, '['));
     dp[0] = "";
 
     for (string& s : a) {
         for (int i = K; i > 0; i--) { dp[i] = min(dp[i], s + dp[i - 1]); }
-        DB(dp);
+        for (int i = 0; i <= K; i++) cout << dp[i] << " ";
+        cout << "\n";
     }
 
     cout << dp[K];
