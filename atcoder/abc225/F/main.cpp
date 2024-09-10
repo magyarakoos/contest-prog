de3db2b718727b50fed2a60d9fc3d9157673c1c6
magyarakoos <magyar.kende.22f@gmail.com>
2024-09-10 17:14:00 +0200

    sync: 2024-09-10 17:14:00

--- atcoder/abc225/F/main.cpp
+++ atcoder/abc225/F/main.cpp
@@ -8,11 +8,13 @@ int main() {
     vector<string> a(N);
     for (string& s : a) cin >> s;
 
-    for (int i = 0; i < N; i++) {
-        for (int j = i + 1; j < N; j++) {
+    for (int i = 0; i < K; i++) {
+        for (int j = i + 1; j < K; j++) {
             if (a[j - 1] + a[j] >= a[j] + a[j - 1]) { swap(a[j - 1], a[j]); }
         }
     }
+
+    while (N > K) a.pop_back(), N--;
 
     DB(a);
 }
