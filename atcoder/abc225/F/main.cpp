6fa22adc586d786994ee3a8fdb842aa0cc818b14
magyarakoos <magyar.kende.22f@gmail.com>
2024-09-10 17:29:00 +0200

    sync: 2024-09-10 17:29:00

--- atcoder/abc225/F/main.cpp
+++ atcoder/abc225/F/main.cpp
@@ -10,5 +10,13 @@ int main() {
     sort(a.begin(), a.end(),
          [](const string& s, const string& t) { return s + t > t + s; });
 
+    // dp[i][j] = az első `i` string értelmezési tartományában,
+    // `j` string felhasználásával az optimális megoldás
+    // lépések:
+    // dp[i][j] = min(dp[i + 1][j], a[i] + dp[i + 1][j - 1]);
     vector dp(K, vector<string>(K));
+
+    for (int i = 0; i < K; i++) {
+        for (int j = 0; j < K; j++) {}
+    }
 }
