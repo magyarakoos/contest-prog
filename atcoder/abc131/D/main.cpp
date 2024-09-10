9946c08b3a158c2660e5c486fc7ceae0aad8856b
magyarakoos <magyar.kende.22f@gmail.com>
2024-09-10 16:45:00 +0200

    sync: 2024-09-10 16:45:00

--- atcoder/abc131/D/main.cpp
+++ atcoder/abc131/D/main.cpp
@@ -4,6 +4,9 @@ using namespace std;
 int main() {
     int N;
     cin >> N;
-    vector<int> A(N), B(N);
-    for (int i = 0; i < N; i++) cin >> A[i] >> B[i];
+    vector<array<int, 2>> a(N);
+    for (int i = 0; i < N; i++) cin >> a[i][1] >> a[i][0];
+    sort(a.begin(), a.end());
+    ll ps = 0;
+    for (int i = 0; i < N; i++) {}
 }
