2ec668239cef3227a4a054721b7cb4670df4f18e
magyarakoos <magyar.kende.22f@gmail.com>
2024-09-12 08:54:01 +0200

    sync: 2024-09-12 08:54:01

--- codeforces/archive/2009E/main.cpp
+++ codeforces/archive/2009E/main.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
 
 using namespace std;
+using ll = long long;
+
+ll sum(ll a, ll b) {}
+
+void solve() {
+    ll N, K;
+    cin >> N >> K;
+}
 
 int main() {
     cin.tie(0), ios::sync_with_stdio(0);
-
-
+    int T;
+    cin >> T;
+    while (T--) solve();
 }
