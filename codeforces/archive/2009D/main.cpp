1e5c5f4dca2f6d13f50ccf8df42510aa37ae39b3
magyarakoos <magyar.kende.22f@gmail.com>
2024-09-12 08:49:00 +0200

    sync: 2024-09-12 08:49:00

--- codeforces/archive/2009D/main.cpp
+++ codeforces/archive/2009D/main.cpp
@@ -1,6 +1,14 @@
+#include <array>
 #include <iostream>
+#include <vector>
 
 using namespace std;
+
+void solve() {
+    int N;
+    cin >> N;
+    vector<array<bool, 2>> a(N, {0, 0});
+}
 
 int main() {
     cin.tie(0), ios::sync_with_stdio(0);
