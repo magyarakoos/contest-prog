e4dde522fe34a07b3b4918fb20117a8eeb82f730
magyarakoos <magyar.kende.22f@gmail.com>
2024-09-12 11:46:00 +0200

    sync: 2024-09-12 11:46:00

---
+++ codeforces/contest/2000/A/main.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#ifdef LOCAL
+#include <debug.h>
+#else
+#define DB(...)
+#endif
+
+using namespace std;
+using ll = long long;
+
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define sz(v) (int)v.size()
+
+const ll INF = 2e9, MAXN = 2e5, MOD = 1e9 + 7;
+
+#define cases 1
+void solve() {
+    string s;
+    cin >> s;
+    cout << (s.size() >= 3 && s.substr(0, 2) == "10" && s[2] >= '1' && stoi(s.substr(2)) >= 2 ? "YES\n" : "NO\n");
+}
+
+int main() {
+    cin.tie(0), ios::sync_with_stdio(0);
+    int T = 1;
+    if (cases) cin >> T;
+    while (T--) solve();
+}
---
+++ codeforces/contest/2000/B/main.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+#ifdef LOCAL
+#include <debug.h>
+#else
+#define DB(...)
+#endif
+
+using namespace std;
+using ll = long long;
+
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define sz(v) (int)v.size()
+
+const ll INF = 2e9, MAXN = 2e5, MOD = 1e9 + 7;
+
+#define cases 1
+void solve() {
+    int N;
+    cin >> N;
+    vector<bool> a(N + 2);
+    int x;
+    cin >> x;
+    a[x] = 1;
+    for (int i = 1; i < N; i++) {
+        cin >> x;
+        if (a[x + 1] || a[x - 1]) {
+            a[x] = 1;
+        } else {
+            cout << "NO\n";
+            while (++i < N) cin >> x;
+            return;
+        }
+    }
+    cout << "YES\n";
+}
+
+int main() {
+    cin.tie(0), ios::sync_with_stdio(0);
+    int T = 1;
+    if (cases) cin >> T;
+    while (T--) solve();
+}
