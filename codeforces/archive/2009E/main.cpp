b075feef95ceb8ec4b7e7ca1052b926471777b20
magyarakoos <magyar.kende.22f@gmail.com>
2024-09-12 09:05:01 +0200

    sync: 2024-09-12 09:05:01

--- codeforces/archive/2009E/main.cpp
+++ codeforces/archive/2009E/main.cpp
@@ -3,6 +3,8 @@
 using namespace std;
 using ll = long long;
 
+const ll INF = 1e15;
+
 void solve() {
     ll N, K;
     cin >> N >> K;
@@ -11,12 +13,12 @@ void solve() {
         return b * (b + 1) / 2 - a * (a - 1) / 2;
     };
 
-    auto f = [&](ll i) -> ll { return sum(N, i) - sum(i + 1, N + K - 1); };
+    auto f = [&](ll i) -> ll { return abs(sum(N, i) - sum(i + 1, N + K - 1)); };
 
     ll l = N, r = N + K - 1;
     while (r - l > 2) {
         ll m1 = l + (r - l) / 3, m2 = r - (r - l) / 3;
-        if (f(m1) < f(m2)) {
+        if (INF - f(m1) < INF - f(m2)) {
             l = m1;
         } else {
             r = m2;
