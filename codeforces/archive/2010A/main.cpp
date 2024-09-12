b3a685332e343b848f53cf76523c3848301fa143
magyarakoos <magyar.kende.22f@gmail.com>
2024-09-12 08:09:01 +0200

    sync: 2024-09-12 08:09:01

--- codeforces/archive/2010A/main.cpp
+++ codeforces/archive/2010A/main.cpp
@@ -4,6 +4,11 @@ using namespace std;
 
 int main() {
     cin.tie(0), ios::sync_with_stdio(0);
-
-
+    int T;
+    cin >> T;
+    while (T--) {
+        int N, x, i = 0, res = 0;
+        cin >> N;
+        while (N--) { int x; }
+    }
 }
