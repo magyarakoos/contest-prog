89368752ca393942ffdfcbf081cc107746445f4f
magyarakoos <magyar.kende.22f@gmail.com>
2024-09-12 08:13:01 +0200

    sync: 2024-09-12 08:13:01

--- codeforces/archive/2010B/main.cpp
+++ codeforces/archive/2010B/main.cpp
@@ -6,11 +6,16 @@ using namespace std;
 int main() {
     cin.tie(0), ios::sync_with_stdio(0);
 
-    array<bool, 3> a;
+    array<bool, 3> a = {0, 0, 0};
     for (int i = 0; i < 3; i++) {
         int x;
         cin >> x;
         a[x - 1] = 1;
     }
-    for (int i = 0; i < 3; i++) { if (!a[i]) }
+    for (int i = 0; i < 3; i++) {
+        if (!a[i]) {
+            cout << i + 1;
+            exit(0);
+        }
+    }
 }
