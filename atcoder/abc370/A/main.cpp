b9372188f5b9edbdeed1a16c71634d4c668a5ba8
magyarakoos <ninjaoffical08@gmail.com>
2024-09-14 16:12:00 +0200

    sync: 2024-09-14 16:12:00

---
+++ atcoder/abc370/A/main.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#ifdef LOCAL
+#include <debug.h>
+#else
+#define DB(...)
+#endif
+
+using namespace std;
+using ll = long long;
+
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define sz(v) (int)v.size()
+
+const ll INF = 2e9, MAXN = 2e5, MOD = 1e9 + 7;
+
+#define cases 0
+void solve() {
+    int L, R;
+    cin >> L >> R;
+    cout << (L == R ? "Invalid" : L ? "Yes" : "No");
+}
+
+int main() {
+    cin.tie(0), ios::sync_with_stdio(0);
+    int T = 1;
+    if (cases) cin >> T;
+    while (T--) solve();
+}
