96a5bea5ee4691f9878e7c402d7ae39b49faf08d
magyarakoos <magyar.kende.22f@gmail.com>
2024-09-17 16:58:00 +0200

    sync: 2024-09-17 16:58:00

---
+++ atcoder/abc125/D/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    cin.tie(0), ios::sync_with_stdio(0);
+
+    int N;
+    cin >> N;
+    vector<int> a(N + 1);
+    for (int& x : a) cin >> x;
+}
