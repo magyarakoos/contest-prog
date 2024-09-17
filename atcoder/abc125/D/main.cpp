bc52f77e386ad301e318d82d9ac156afb1d4db76
magyarakoos <magyar.kende.22f@gmail.com>
2024-09-17 17:42:00 +0200

    sync: 2024-09-17 17:42:00

--- atcoder/abc125/D/main.cpp
+++ atcoder/abc125/D/main.cpp
@@ -1,6 +1,5 @@
 #include <algorithm>
 #include <iostream>
-#include <vector>
 
 using namespace std;
 using ll = long long;
@@ -10,19 +9,18 @@ int main() {
 
     int N;
     cin >> N;
-    vector<int> a(N);
-    ll sum = 0;
-    int neg_cnt = 0, abs_mn = 0;
-    for (int& x : a) {
+    ll sum = 0, neg_cnt = 0, abs_mn = 1e18;
+    for (int i = 0; i < N; i++) {
+        ll x;
         cin >> x;
+        sum += abs(x);
         neg_cnt += x < 0;
         abs_mn = min(abs_mn, abs(x));
     }
 
     if (neg_cnt & 1) {
-        cout << sum - abs_mn;
+        cout << sum - 2 * abs_mn;
     } else {
         cout << sum;
     }
 }
-
