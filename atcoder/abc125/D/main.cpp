0500f82b5b199bca0be34193fda6378ed7f6d5b5
magyarakoos <magyar.kende.22f@gmail.com>
2024-09-17 17:13:01 +0200

    sync: 2024-09-17 17:13:01

--- atcoder/abc125/D/main.cpp
+++ atcoder/abc125/D/main.cpp
@@ -20,7 +20,8 @@ int main() {
 
     for (int i = 1; i < N; i++) {
         dp[i][0] = max(dp[i - 1][0], dp[i - 1][1]) + a[i];
-        dp[i][1] = max(a[0] + a[1], -a[0] + -a[1]);
+        dp[i][1] = max(a[0] + a[1], -a[0] + -a[1]) +
+                   (i >= 2) * max(dp[i - 2][0], dp[i - 2][1]);
     }
 
     cout << max(dp[N - 1][0], dp[N - 1][1]);
