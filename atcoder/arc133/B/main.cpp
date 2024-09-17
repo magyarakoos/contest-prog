7f585517a4904c3cea81f66b1086eb7283c1aa65
magyarakoos <magyar.kende.22f@gmail.com>
2024-09-17 17:44:00 +0200

    sync: 2024-09-17 17:44:00

--- atcoder/arc133/B/main.cpp
+++ atcoder/arc133/B/main.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 int main() {
     cin.tie(0), ios::sync_with_stdio(0);
 
-
+    int N;
+    cin >> N;
+    vector<int> P(N), Q(N);
+    for (int& x : P) cin >> x;
+    for (int& x : Q) cin >> x;
 }
