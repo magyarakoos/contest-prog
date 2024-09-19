f6ed8afb55314bb8e96d92f6966a8ac7545ebc60
magyarakoos <magyar.kende.22f@gmail.com>
2024-09-19 08:38:01 +0200

    sync: 2024-09-19 08:38:01

--- codeforces/gym/550995/A/main.cpp
+++ codeforces/gym/550995/A/main.cpp
@@ -1,16 +1,22 @@
 #include <iostream>
-
 using namespace std;
 
-int N, prv, x;
+int T, N, prv, x;
+
 int main() {
-    cin >> N;
-    for (int i = 0; i < N; i++) {
-        cin >> x;
-        if (prv && x != prv) {
-            cout << 1;
-            exit(0);
+    cin >> T;
+    while (T--) {
+        cin >> N;
+
+        for (int i = 0; i < N; i++) {
+            cin >> x;
+
+            if (prv && x != prv) {
+                cout << 1;
+                exit(0);
+            }
         }
+
+        cout << N;
     }
-    cout << N;
 }
