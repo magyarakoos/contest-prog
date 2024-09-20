befc0e1657b297c27a533c79e5a0ee2c84bd213e
magyarakoos <magyar.kende.22f@gmail.com>
2024-09-20 16:39:00 +0200

    sync: 2024-09-20 16:39:00

--- codeforces/gym/550995/D/main.cpp
+++ codeforces/gym/550995/D/main.cpp
@@ -13,12 +13,15 @@ int main() {
     int N;
     cin >> N;
 
-    vector<array<int, 2>> ptS(N);
-    for (auto& [x, y] : ptS) cin >> x >> y;
+    vector<array<int, 3>> ptS(N);
+    for (int i = 0; i < N; i++) {
+        cin >> ptS[i][0] >> ptS[i][1];
+        ptS[i][2] = i;
+    }
 
     sort(ptS.begin(), ptS.end());
 
-    vector<array<int, 2>> tri = {ptS[0], ptS[1], ptS[2]};
+    vector<array<int, 3>> tri = {ptS[0], ptS[1], ptS[2]};
 
     for (int i = 3; i < N; i++) {
         if (in_triangle(tri, ptS[i])) {
