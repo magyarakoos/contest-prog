4a4a0f60d22ab41db853a74541cb7a4097a18c5a
magyarakoos <magyar.kende.22f@gmail.com>
2024-09-20 16:40:01 +0200

    sync: 2024-09-20 16:40:01

--- codeforces/gym/550995/D/main.cpp
+++ codeforces/gym/550995/D/main.cpp
@@ -5,7 +5,7 @@
 
 using namespace std;
 
-bool in_triangle(vector<array<int, 2>> tri, array<int, 2> p) { return 0; }
+bool in_triangle(vector<array<int, 2>> tri, array<int, 2> p) {}
 
 int main() {
     cin.tie(0), ios::sync_with_stdio(0);
@@ -13,20 +13,17 @@ int main() {
     int N;
     cin >> N;
 
-    vector<array<int, 3>> ptS(N);
-    for (int i = 0; i < N; i++) {
-        cin >> ptS[i][0] >> ptS[i][1];
-        ptS[i][2] = i;
-    }
+    vector<array<int, 2>> ptS(N);
+    for (auto& [x, y] : ptS) cin >> x >> y;
 
     sort(ptS.begin(), ptS.end());
 
-    vector<array<int, 3>> tri = {ptS[0], ptS[1], ptS[2]};
+    vector<array<int, 2>> tri = {ptS[0], ptS[1], ptS[2]};
 
-    for (int i = 3; i < N; i++) {
+    for (int i = 3; i < N - 1; i++) {
         if (in_triangle(tri, ptS[i])) {
             sort(tri.begin(), tri.end());
-            tri[2] = ptS[i];
+            tri[0] = ptS[i];
         }
     }
 
