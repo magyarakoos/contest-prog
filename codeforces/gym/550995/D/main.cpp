d2430a4c1b4048dd03a74333d3285ae3b2c17af9
magyarakoos <magyar.kende.22f@gmail.com>
2024-09-20 16:43:00 +0200

    sync: 2024-09-20 16:43:00

--- codeforces/gym/550995/D/main.cpp
+++ codeforces/gym/550995/D/main.cpp
@@ -4,11 +4,11 @@
 #include <vector>
 
 using namespace std;
-using point;
+using Point = array<int, 2>;
 
 int turn();
 
-bool in_triangle(vector<array<int, 2>> tri, array<int, 2> p) {}
+bool in_triangle(vector<Point> tri, Point p) {}
 
 int main() {
     cin.tie(0), ios::sync_with_stdio(0);
@@ -16,12 +16,12 @@ int main() {
     int N;
     cin >> N;
 
-    vector<array<int, 2>> ptS(N);
+    vector<Point> ptS(N);
     for (auto& [x, y] : ptS) cin >> x >> y;
 
     sort(ptS.begin(), ptS.end());
 
-    vector<array<int, 2>> tri = {ptS[0], ptS[1], ptS[2]};
+    vector<Point> tri = {ptS[0], ptS[1], ptS[2]};
     for (int i = 2; i < N; i++) {
         if (ptS[i][0] > ptS[0][0]) {
             tri[2] = ptS[i];
