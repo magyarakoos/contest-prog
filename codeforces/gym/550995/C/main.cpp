57d816e262bbc7a5d8431a07ca793d625ea24234
magyarakoos <magyar.kende.22f@gmail.com>
2024-09-20 16:00:01 +0200

    sync: 2024-09-20 16:00:01

--- codeforces/gym/550995/C/main.cpp
+++ codeforces/gym/550995/C/main.cpp
@@ -1,7 +1,4 @@
 #include <iostream>
-#include <vector>
-
-#include <debug.h>
 
 using namespace std;
 
@@ -9,11 +6,10 @@ int main() {
     int N, P;
     cin >> N >> P;
 
-    vector<int> a;
-    for (int x = 0; (1 << x) + P <= 5 * N; x++) { a.push_back((1 << x) + P); }
-    int M = a.size();
-
-    int result = INT_MAX;
-
-    cout << (result == INT_MAX ? -1 : result);
+    for (int x = 0; x < N; x++) {
+        if (__builtin_popcount(N + x * P) == x) {
+            cout << x;
+            exit(0);
+        }
+    }
 }
