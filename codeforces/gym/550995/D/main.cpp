517a0a47cb35f8ec6cec385ba7e3982a44842ab2
magyarakoos <magyar.kende.22f@gmail.com>
2024-09-20 16:22:01 +0200

    sync: 2024-09-20 16:22:01

--- codeforces/gym/550995/D/main.cpp
+++ codeforces/gym/550995/D/main.cpp
@@ -1,5 +1,18 @@
+#include <algorithm>
+#include <array>
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-int main() { cin.tie(0), ios::sync_with_stdio(0); }
+int main() {
+    cin.tie(0), ios::sync_with_stdio(0);
+
+    int N;
+    cin >> N;
+
+    vector<array<int, 2>> a(N);
+    for (auto& [x, y] : a) cin >> x >> y;
+
+    sort(a.begin(), a.end());
+}
