e73fb473890cd5c932db9eb135bcc783f3637e26
magyarakoos <magyar.kende.22f@gmail.com>
2024-09-20 16:07:01 +0200

    sync: 2024-09-20 16:07:01

--- codeforces/gym/550995/C/main.cpp
+++ codeforces/gym/550995/C/main.cpp
@@ -7,12 +7,18 @@ int dcnt(int x) {
     for (int d = 2; d <= x && x > 0; d++) {
         while (x > 0 && x % d == 0) x /= d, result++;
     }
-    return result + x;
+    return result;
 }
 
 int main() {
     int N, P;
     cin >> N >> P;
 
-    for (int i = 0; i < N; i++) { cout << i << " " << dcnt(i) << "\n"; }
+    for (int x = 0; x <= N; x++) {
+        if (N - x * P >= 0 && dcnt(N - x * P) == x) {
+            cout << x;
+            exit(0);
+        }
+    }
+    cout << -1;
 }
