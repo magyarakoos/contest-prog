47d21e73161538f670fcaa28b124e631399d98a5
magyarakoos <magyar.kende.22f@gmail.com>
2024-09-20 16:01:01 +0200

    sync: 2024-09-20 16:01:01

--- codeforces/gym/550995/C/main.cpp
+++ codeforces/gym/550995/C/main.cpp
@@ -6,8 +6,8 @@ int main() {
     int N, P;
     cin >> N >> P;
 
-    for (int x = 0; x < N; x++) {
-        if (__builtin_popcount(N + x * P) == x) {
+    for (int x = 0; x <= N; x++) {
+        if (__builtin_popcount(N - x * P) == x) {
             cout << x;
             exit(0);
         }
