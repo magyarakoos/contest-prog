11771023e7f68244e452609986734aef2443bee7
magyarakoos <magyar.kende.22f@gmail.com>
2024-09-20 16:04:00 +0200

    sync: 2024-09-20 16:04:00

--- codeforces/gym/550995/C/main.cpp
+++ codeforces/gym/550995/C/main.cpp
@@ -2,12 +2,18 @@
 
 using namespace std;
 
+int dcnt(int x) {
+    for (int d = 2; d <= x && x > 0; d++) {
+        while (x > 0) {}
+    }
+}
+
 int main() {
     int N, P;
     cin >> N >> P;
 
     for (int x = 0; x <= N; x++) {
-        if (N - x * P >= 0 && __builtin_popcount(N - x * P) == x) {
+        if (N - x * P >= 0 && == x) {
             cout << x;
             exit(0);
         }
