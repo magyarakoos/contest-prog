6aebb53985d0ffceb1ea01494b8c27b932cec338
magyarakoos <ninjaoffical08@gmail.com>
2024-09-21 12:06:00 +0200

    sync: 2024-09-21 12:06:00

--- codeforces/gym/550995/C/main.cpp
+++ codeforces/gym/550995/C/main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main() {
     int N, P;
     cin >> N >> P;
-    for (long long x = 0; x <= 1e6; x++) {
+    for (long long x = 0; x <= 1000; x++) {
         if (N >= x * (P + 1) && __popcount(N - x * P) <= x) {
             cout << x;
             exit(0);
