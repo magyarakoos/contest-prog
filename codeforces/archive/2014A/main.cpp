c523004d5e63402d4131131cac4db6b3e9affae0
magyarakoos <ninjaoffical08@gmail.com>
2024-09-23 16:51:01 +0200

    sync: 2024-09-23 16:51:01

--- codeforces/archive/2014A/main.cpp
+++ codeforces/archive/2014A/main.cpp
@@ -1,4 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {}
+void solve() {
+    int N, K, cash = 0, result = 0;
+    cin >> N >> K;
+    while (N--) {
+        int x;
+        cin >> x;
+        if (x >= K) cash += x;
+        if (x == 0) cash--, result++;
+    }
+    cout << result << "\n";
+}
+
+int main() {
+    int T;
+    cin >> T;
+    while (T--) solve();
+}
