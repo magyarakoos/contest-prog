cee608da7512b0c9f578bdc277d6fa1fac578bdf
magyarakoos <ninjaoffical08@gmail.com>
2024-09-23 17:59:00 +0200

    sync: 2024-09-23 17:59:00

---
+++ codeforces/archive/2014D/main.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+void solve() {
+    int N, D, K;
+    cin >> N >> D >> K;
+}
+
+int main() {
+    int T;
+    cin >> T;
+    while (T--) solve();
+}
