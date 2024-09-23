5e0ed81cceed77e597573736da104e8fec1a1fdb
magyarakoos <ninjaoffical08@gmail.com>
2024-09-23 18:00:01 +0200

    sync: 2024-09-23 18:00:01

--- codeforces/archive/2014D/main.cpp
+++ codeforces/archive/2014D/main.cpp
@@ -5,6 +5,14 @@ using ll = long long;
 void solve() {
     int N, D, K;
     cin >> N >> D >> K;
+    vector<int> a(N + 1);
+    while (K--) {
+        int L, R;
+        cin >> L >> R;
+        a[L - 1]++;
+        a[R]--;
+    }
+    for (int i = 1; i < N; i++) { a[i] += a[i - 1]; }
 }
 
 int main() {
