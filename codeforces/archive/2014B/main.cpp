d69a488e9dd97e3a85943d6c04398f61a86d5b92
magyarakoos <ninjaoffical08@gmail.com>
2024-09-23 17:02:00 +0200

    sync: 2024-09-23 17:02:00

--- codeforces/archive/2014B/main.cpp
+++ codeforces/archive/2014B/main.cpp
@@ -4,7 +4,8 @@ using namespace std;
 void solve() {
     int N, K;
     cin >> N >> K;
-    cout << (K % 2 ? (N % 2 ? "NO" : "YES") : (N % 2 ? "NO" : "YES")) << "\n";
+    cout << (K % 2 ? (N % 2 ? "NO" : "YES") : (K / 2 % 2 ? "NO" : "YES"))
+         << "\n";
 }
 
 int main() {
