5315b96c0bffd1aef980a57500fa55c3c2ee0a05
magyarakoos <ninjaoffical08@gmail.com>
2024-09-23 17:34:00 +0200

    sync: 2024-09-23 17:34:00

--- codeforces/archive/2014C/main.cpp
+++ codeforces/archive/2014C/main.cpp
@@ -7,15 +7,16 @@ void solve() {
     cin >> N;
     vector<int> a(N);
     for (int& x : a) cin >> x;
+
     sort(a.begin(), a.end());
+    ll sum = accumulate(a.begin(), a.end(), 0LL);
 
     if (N == 1) {
         cout << "-1\n";
         return;
     }
 
-    auto f = [&](int i) -> bool {};
-
+    auto f = [&](int i) -> bool { return a[N / 2] * N < sum; };
     int l = -1, r = 1e9;
     while (r - l > 1) {
         int m = (l + r) / 2;
