f5d96bd8e3bcd84564369677230c68bfa3e2f616
magyarakoos <magyar.kende.22f@gmail.com>
2024-10-01 12:38:01 +0200

    sync: 2024-10-01 12:38:01

--- codeforces/gym/550995/G/main.cpp
+++ codeforces/gym/550995/G/main.cpp
@@ -1,6 +1,21 @@
 #include <array>
+#include <iostream>
 #include <map>
+#include <vector>
 
 using namespace std;
 
 map<array<int, 2>, int> dp;
+int N;
+vector<int> a;
+
+int solve(int i, int j) {
+    return 0;
+}
+
+int main() {
+    cin >> N;
+    a.resize(N);
+    for (int& x : a) cin >> x;
+    cout << solve(0, N - 1) << "\n";
+}
