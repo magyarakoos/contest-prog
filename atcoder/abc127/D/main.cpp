04c3609ea84b3b8152c05e0c82818dcc905c59f2
magyarakoos <magyar.kende.22f@gmail.com>
2024-10-01 17:36:00 +0200

    sync: 2024-10-01 17:36:00

--- atcoder/abc127/D/main.cpp
+++ atcoder/abc127/D/main.cpp
@@ -7,13 +7,17 @@ int main() {
     cin.tie(0), ios::sync_with_stdio(0);
 
     map<int, int> m;
-
     int N, M;
     cin >> N >> M;
+
     while (N--) {
         int A;
         cin >> A;
         m[-A]++;
     }
-    while (M--) { int B, C; }
+
+    while (M--) {
+        int B, C;
+        m[-C] += B;
+    }
 }
