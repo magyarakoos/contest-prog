d6fabdb4a7dce2165d4198f6f681d7398e2d1f79
magyarakoos <magyar.kende.22f@gmail.com>
2024-10-01 16:56:01 +0200

    sync: 2024-10-01 16:56:00

--- planets/flight-maximums/main.cpp
+++ planets/flight-maximums/main.cpp
@@ -1,13 +1,6 @@
-#include <bit>
-#include <cmath>
 #include <iostream>
-#include <vector>
 
 using namespace std;
-
-int log2_floor(unsigned i) {
-    return bit_width(i) - 1;
-}
 
 int st[25][100005];
 int main() {
@@ -16,10 +9,8 @@ int main() {
     int N, Q;
     cin >> N >> Q;
 
-    vector<int> H(N);
-    for (int& x : H) cin >> x;
+    for (int i = 0; i < N; i++) { cin >> st[0][i]; }
 
-    copy(H.begin(), H.end(), st[0]);
     for (int i = 1; i < 25; i++) {
         for (int j = 0; j + (1 << i) <= N; j++) {
             st[i][j] =
@@ -30,9 +21,7 @@ int main() {
     while (Q--) {
         int S, K;
         cin >> S >> K;
-        int L = S - 1, R = S + (1 << K) - 2;
-
-        int i = log2_floor(R - L + 1);
-        cout << max(st[i][L], st[i][R - (1 << i) + 1]) << "\n";
+        cout << st[K][S] << "\n";
     }
 }
+
