5eb4dc7c0a432eb624d556f2a3ea97442529b8e4
magyarakoos <magyar.kende.22f@gmail.com>
2024-10-01 17:32:01 +0200

    sync: 2024-10-01 17:32:01

--- atcoder/abc127/D/main.cpp
+++ atcoder/abc127/D/main.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
+#include <map>
 
 using namespace std;
 
 int main() {
     cin.tie(0), ios::sync_with_stdio(0);
 
+    map<int, int> m;
 
+    int N, M;
+    cin >> N >> M;
+    while (N--) {
+        int A;
+        cin >> A;
+        m[-A]++;
+    }
+    while (M--) { int B, C; }
 }
