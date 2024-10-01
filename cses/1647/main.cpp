f4b4226441557d051ff0a77e2fd77846f200e872
magyarakoos <magyar.kende.22f@gmail.com>
2024-10-01 17:05:00 +0200

    sync: 2024-10-01 17:05:00

--- cses/1647/main.cpp
+++ cses/1647/main.cpp
@@ -2,10 +2,22 @@
 
 using namespace std;
 
-int log2_floor(unsigned long long i) {
+int lg(unsigned long long i) {
     return i ? __builtin_clzll(1) - __builtin_clzll(i) : -1;
 }
 
 int st[25][200005];
-int main() {}
+int main() {
+    int N, Q;
+    cin >> N >> Q;
+
+    for (int i = 1; i <= N; i++) cin >> st[0][i];
+
+    for (int i = 1; i <= lg(N); i++) {
+        for (int j = 1; j + (1 << i) <= N + 1; j++) {
+            st[i][j] =
+                min(st[i - 1][j], st[i - 1][j + (1 << (i - 1))]);
+        }
+    }
+}
 
