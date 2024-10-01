ce9aba90218897e3eca5352f41ad1011faadf375
magyarakoos <magyar.kende.22f@gmail.com>
2024-10-01 16:59:00 +0200

    sync: 2024-10-01 16:59:00

--- planets/flight-maximums/main.cpp
+++ planets/flight-maximums/main.cpp
@@ -1,16 +1,12 @@
 #include <iostream>
-
 using namespace std;
 
 int st[25][100005];
 int main() {
-    cin.tie(0), ios::sync_with_stdio(0);
-
     int N, Q;
     cin >> N >> Q;
 
     for (int i = 1; i <= N; i++) cin >> st[0][i];
-
     for (int i = 1; i < 25; i++) {
         for (int j = 1; j + (1 << i) <= N + 1; j++) {
             st[i][j] =
@@ -24,4 +20,3 @@ int main() {
         cout << st[K][S] << "\n";
     }
 }
-
