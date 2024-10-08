9339ef4580e1704c684188bc5ed3473c9345a4f0
magyarakoos <ninjaoffical08@gmail.com>
2024-10-08 16:25:01 +0200

    sync: 2024-10-08 16:25:01

--- codeforces/archive/701C/main.cpp
+++ codeforces/archive/701C/main.cpp
@@ -4,8 +4,23 @@
 using namespace std;
 
 int main() {
+    vector<int> last(52, -1);
     int N;
     string s;
     cin >> N >> s;
-    vector<int> last(52, -1);
+
+    int result = N;
+
+    for (int i = 0; i < N; i++) {
+        last[s[i] - (s[i] < 'a' ? 'A' : 'a')] = i;
+        int mn = i + 1;
+        for (int j = 0; j < 52; j++) {
+            if (last[j] == -1) {
+                mn = i + 1;
+                break;
+            }
+            mn = min(mn, last[j]);
+        }
+        result = min(result, i - mn + 1);
+    }
 }
