98e81b926225219decb980b4d7bc69896f883dea
magyarakoos <ninjaoffical08@gmail.com>
2024-10-08 17:00:01 +0200

    sync: 2024-10-08 17:00:01

--- codeforces/archive/514D/main.cpp
+++ codeforces/archive/514D/main.cpp
@@ -16,4 +16,10 @@ int main() {
 
     for (int i = 2; i <= MAXN; i++) { lg[i] = lg[i / 2] + 1; }
 
-    for (int i) }
+    for (int i = 1; i < 25; i++) {
+        for (int j = 0; j + (1 << i) <= N; i++) {
+            st[i][j] =
+                max(st[i - 1][j], st[i - 1][j + (1 << (i - 1))]);
+        }
+    }
+}
