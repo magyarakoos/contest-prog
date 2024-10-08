bf9096798f05938a8b1dc9d7dd53b1ecf5f0bd2b
magyarakoos <ninjaoffical08@gmail.com>
2024-10-08 16:43:01 +0200

    sync: 2024-10-08 16:43:01

--- codeforces/archive/701C/main.cpp
+++ codeforces/archive/701C/main.cpp
@@ -13,16 +13,18 @@ int main() {
 
     for (int i = 0; i < N; i++) {
         last[s[i]] = i;
-        int mn = -N;
+        int mn = -1;
         for (auto [_, j] : last) {
             if (j == -1) {
-                mn = -N;
+                mn = -1;
                 break;
             }
             mn = min(mn, j);
         }
-        cout << mn << " " << i << "  ";
-        result = min(result, i - mn + 1);
+        if (mn >= 0) {
+            cout << mn << " " << i << "  ";
+            result = min(result, i - mn + 1);
+        }
     }
 
     cout << result << "\n";
