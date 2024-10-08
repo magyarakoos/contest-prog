2c6ce87ce4788462531e1807d5d99dfeb7e2daa7
magyarakoos <ninjaoffical08@gmail.com>
2024-10-08 17:11:00 +0200

    sync: 2024-10-08 17:11:00

--- codeforces/archive/514D/main.cpp
+++ codeforces/archive/514D/main.cpp
@@ -2,9 +2,9 @@
 using namespace std;
 using ll = long long;
 
-const int MAXN = 1e5;
+const int MAXN = 1e5, MAXM = 5;
 
-int lg[MAXN + 1], st[25][MAXN];
+int lg[MAXN + 1], st[MAXM][25][MAXN];
 
 int main() {
     int N, M, K;
@@ -24,15 +24,15 @@ int main() {
         }
     }
 
-    auto rmq = [&](int l, int r) -> int {
-        int i = lg[r - l + 1];
-        return max(st[i][l], st[i][r - (1 << i) + 1]);
+    auto rmq = [&](int i, int l, int r) -> int {
+        int j = lg[r - l + 1];
+        return max(st[i][j][l], st[i][j][r - (1 << j) + 1]);
     };
 
     auto f = [&](int l, int r) -> bool {
         if (l > r) return 1;
         int result = 0;
-        for (int i = 0; i < M; i++) {}
+        for (int i = 0; i < M; i++) { result += rmq() }
         return result <= K;
     };
 
