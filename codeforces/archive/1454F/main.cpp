bc9d192f3d8abc55555a6dc5711e4a189aa9f84b
magyarakoos <ninjaoffical08@gmail.com>
2024-10-08 17:49:00 +0200

    sync: 2024-10-08 17:49:00

--- codeforces/archive/1454F/main.cpp
+++ codeforces/archive/1454F/main.cpp
@@ -1,4 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {}
+void solve() {
+    int N;
+    cin >> N;
+
+    vector<vector<int>> st(25, vector<int>(N));
+    vector<int> lg(N + 1);
+    for (int i = 2; i <= N; i++) { lg[i] = lg[i / 2] + 1; }
+
+    for (int i = 0; i < N; i++) { cin >> st[0][i]; }
+}
+
+int main() {
+    int T;
+    cin >> T;
+    while (T--) solve();
+}
