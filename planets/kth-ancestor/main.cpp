884672d18935b469a1c451f4ef9a171f4ddbd67b
magyarakoos <magyar.kende.22f@gmail.com>
2024-10-15 17:04:01 +0200

    sync: 2024-10-15 17:04:01

--- planets/kth-ancestor/main.cpp
+++ planets/kth-ancestor/main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 int main() {
     cin.tie(0), ios::sync_with_stdio(0);
 
+    int N, Q;
+    cin >> N >> Q;
 
+    vector<vector<int>> anc(ceil(log2(2e5)), vector<int>(N, 0));
 }
