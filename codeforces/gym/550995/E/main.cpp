e9d8cc22ea43985361c97ce7d3fab05fd5a4038a
magyarakoos <magyar.kende.22f@gmail.com>
2024-10-17 08:47:00 +0200

    sync: 2024-10-17 08:47:00

--- codeforces/gym/550995/E/main.cpp
+++ codeforces/gym/550995/E/main.cpp
@@ -12,8 +12,8 @@ int main() {
 
     for (int i = 1; i <= N; i++) {
         for (int j = 1; j <= M; j++) {
-            if (s[i - 1] == s[j - 1]) {
-                dp[i][j] = dp[i - 1][j - 1] + 1;
+            if (s[i - 1] == t[j - 1]) {
+                dp[i][j] = dp[i - 1][j - 1] + 2;
             } else {
                 dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
             }
