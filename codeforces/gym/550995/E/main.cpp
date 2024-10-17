0b66832f88d2e6eb1ecab497f01c4c507ae764df
magyarakoos <magyar.kende.22f@gmail.com>
2024-10-17 08:45:00 +0200

    sync: 2024-10-17 08:45:00

--- codeforces/gym/550995/E/main.cpp
+++ codeforces/gym/550995/E/main.cpp
@@ -6,4 +6,14 @@ int main() {
     int N, M;
     string s, t;
     cin >> N >> M >> s >> t;
+
+    vector dp(N + 1, vector<int>(M + 1));
+
+    for (int i = 1; i <= N; i++) {
+        for (int j = 1; j <= M; j++) {
+            if (s[i - 1] == s[j - 1]) {
+                dp[i][j] = dp[i - 1][j - 1] + 1;
+            }
+        }
+    }
 }
