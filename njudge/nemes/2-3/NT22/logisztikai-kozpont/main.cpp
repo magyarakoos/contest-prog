95d5de6d7645e2cf97a6028d8360eac186395737
magyarakoos <ninjaoffical08@gmail.com>
2024-10-19 10:42:00 +0200

    sync: 2024-10-19 10:42:00

--- njudge/nemes/2-3/NT22/logisztikai-kozpont/main.cpp
+++ njudge/nemes/2-3/NT22/logisztikai-kozpont/main.cpp
@@ -1,108 +1,22 @@
 #include <bits/stdc++.h>
 
-#define all(v) v.begin(), v.end()
-#define rall(v) v.rbegin(), v.rend()
-#define size(v) (int)v.size()
-#define has(s, e) s.count(e)
-
 using namespace std;
 using ll = long long;
 
-int N;
-vector<vector<pair<int, ll>>> g;
-vector<ll> distS, par_distS;
-vector<int> parS;
-
-ll result_t = 1e18;
-set<int> result_nodeS;
-
-void dfs(int u, int par) {
-    for (auto [v, w] : g[u]) {
-        if (v == par) continue;
-        if (distS[v] < distS[u] + w) {
-            distS[v] = distS[u] + w;
-            parS[v] = u;
-            par_distS[v] = w;
-        }
-        dfs(v, u);
-    }
-}
-
-void proc(int end) {
-    vector<pair<int, ll>> path;
-    int curr = end;
-
-    do {
-        path.push_back({curr, par_distS[curr]});
-        curr = parS[curr];
-    } while (curr);
-
-    ll sum = 0;
-    for (auto [u, w] : path) sum += w;
-
-    ll running_sum = 0;
-    for (int i = 0; i < size(path); i++) {
-        ll curr = max(running_sum, sum - running_sum);
-
-        if (curr < result_t) {
-            result_t = curr;
-            result_nodeS.clear();
-            result_nodeS.insert(path[i].first);
-        }
-        else if (curr == result_t) {
-            result_nodeS.insert(path[i].first);
-        }
-
-        running_sum += path[i].second;
-    }
-}
-
-void get_ends(int start) {
-    distS    .assign(N + 1, 0);
-    parS     .assign(N + 1, 0);
-    par_distS.assign(N + 1, 0);
-
-    dfs(start, 0);
-    int end = max_element(all(distS)) - distS.begin();
-
-    for (int u = 1; u <= N; u++) {
-        if (distS[u] == distS[end]) {
-            proc(u);
-        }
-    }
-}
+vector<vector<array<int, 3>>> g;
+vector<ll> dp;
 
 int main() {
-    cin.tie(0); ios::sync_with_stdio(0);
+    int N;
     cin >> N;
 
-    if (N == 1) {
-        cout << "0\n1\n1";
-        exit(0);
-    }
+    g.resize(N + 1);
+    dp.resize(N + 1);
 
-    g        .resize(N + 1);
-    distS    .resize(N + 1);
-    parS     .resize(N + 1);
-    par_distS.resize(N + 1);
-
-    for (int i = 1; i < N; i++) {
+    for (int i = 0; i < N - 1; i++) {
         int U, V, W;
         cin >> U >> V >> W;
         g[U].push_back({V, W});
         g[V].push_back({U, W});
     }
-
-    dfs(1, 0);
-    int start = max_element(all(distS)) - distS.begin();
-
-    for (int u = 1; u <= N; u++) {
-        if (distS[u] == distS[start]) {
-            get_ends(u);
-        }
-    }
-
-    cout << result_t << "\n" << size(result_nodeS) << "\n";
-    for (int u : result_nodeS) cout << u << " ";
-    cout << "\n";
 }
