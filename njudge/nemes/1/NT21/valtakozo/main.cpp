4846ad5bf70b0c4267078befb7025ea8d7b879f6
magyarakoos <ninjaoffical08@gmail.com>
2024-10-19 10:09:00 +0200

    sync: 2024-10-19 10:09:00

--- njudge/nemes/1/NT21/valtakozo/main.cpp
+++ njudge/nemes/1/NT21/valtakozo/main.cpp
@@ -4,8 +4,18 @@ using namespace std;
 int main() {
     string s;
     cin >> s;
-    int N = s.size();
 
     array<int, 26> h = {0};
     for (char c : s) h[c - 'a']++;
+
+    string result;
+    for (int i = 0; i < s.size(); i++) {
+        for (int j = 0; j < 26; j++) {
+            if (result.empty() || result.back() != j + 'a') {
+                result += j + 'a';
+                h[j]--;
+                break;
+            }
+        }
+    }
 }
