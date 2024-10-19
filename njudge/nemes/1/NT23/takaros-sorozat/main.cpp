e5e24a42dac16c7b1ef25bff369df365d8b24396
magyarakoos <ninjaoffical08@gmail.com>
2024-10-19 09:51:00 +0200

    sync: 2024-10-19 09:51:00

--- njudge/nemes/1/NT23/takaros-sorozat/main.cpp
+++ njudge/nemes/1/NT23/takaros-sorozat/main.cpp
@@ -1,10 +1,16 @@
 #include <bits/stdc++.h>
+
 using namespace std;
+using ll = long long;
 
 int main() {
     int N;
     cin >> N;
-    vector<int> a(N), ps(N + 1);
+    vector<int> a(N), ps(N);
     for (int& x : a) cin >> x;
-    partial_sum(a.begin(), a.end(), ps.begin() + 1);
+    partial_sum(a.begin(), a.end(), ps.begin());
+    ll result = 0;
+    for (int i = 0; i < N; i++) {
+        if (!a[i]) result += ps[i];
+    }
 }
