b41012c987aedf9f241e8543880e25bbc259aaf1
magyarakoos <ninjaoffical08@gmail.com>
2024-10-19 09:59:00 +0200

    sync: 2024-10-19 09:59:00

--- njudge/nemes/1/NT23/takaros-sorozat/main.cpp
+++ njudge/nemes/1/NT23/takaros-sorozat/main.cpp
@@ -6,11 +6,12 @@ using ll = long long;
 int main() {
     int N;
     cin >> N;
-    vector<int> a(N), po(N), sz(N);
+    vector<int> a(N), na(N), po(N), sz(N);
     for (int i = 0; i < N; i++) {
         char c;
         cin >> c;
-        a[i] = c == '1';
+        na[i] = !(a[i] = c == '1');
     }
     partial_sum(a.begin(), a.end(), po.begin());
+    partial_sum(na.rbegin(), na.rend(), sz.begin());
 }
