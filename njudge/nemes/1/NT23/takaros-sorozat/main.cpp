56330e63689c252b3f0e57764402515194bf7780
magyarakoos <ninjaoffical08@gmail.com>
2024-10-19 09:56:01 +0200

    sync: 2024-10-19 09:56:01

--- njudge/nemes/1/NT23/takaros-sorozat/main.cpp
+++ njudge/nemes/1/NT23/takaros-sorozat/main.cpp
@@ -6,10 +6,10 @@ using ll = long long;
 int main() {
     int N;
     cin >> N;
-    vector<int> a(N), pz(N), so(N);
+    vector<int> a(N), po(N), sz(N);
     for (int i = 0; i < N; i++) {
         char c;
         cin >> c;
-        po[i] = a[i] = c == '1';
+        po[i] = (a[i] = c == '1');
     }
 }
