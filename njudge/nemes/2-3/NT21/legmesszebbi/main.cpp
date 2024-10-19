27d6e9d6c8936defd1f04e5b583816f71f91417d
magyarakoos <ninjaoffical08@gmail.com>
2024-10-19 10:27:01 +0200

    sync: 2024-10-19 10:27:01

--- njudge/nemes/2-3/NT21/legmesszebbi/main.cpp
+++ njudge/nemes/2-3/NT21/legmesszebbi/main.cpp
@@ -4,6 +4,11 @@ using namespace std;
 int main() {
     int N;
     cin >> N;
-    vector<int> a(N);
+    vector<int> a(N), pmx(N);
     for (int& x : a) cin >> x;
+
+    pmx[0] = a[0];
+    for (int i = 1; i < N; i++) {
+        pmx[i] = max(pmx[i - 1], a[i]);
+    }
 }
