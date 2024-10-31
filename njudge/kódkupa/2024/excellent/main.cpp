79ec8a4607357c15067c907b0b4278a246fa3234
magyarakoos <magyar.kende.22f@gmail.com>
2024-10-31 09:23:01 +0100

    sync: 2024-10-31 09:23:00

--- njudge/kódkupa/2024/excellent/main.cpp
+++ njudge/kódkupa/2024/excellent/main.cpp
@@ -1,23 +1,23 @@
-// NOTE: it is recommended to use this even if you don't
-// understand the following code.
-
-#include <fstream>
 #include <iostream>
-#include <vector>
-
 using namespace std;
 
 int main() {
     int N;
     cin >> N;
 
+    if (N == 1) {
+        cout << "-1";
+        exit(0);
+    }
+
     switch (N % 3) {
     case 0:
         for (int i = 0; i < N; i++) { cout << "1"; }
         break;
+    case 1:
+        cout << "111";
+        for (int i = 0; i < N - 3; i += 2) { cout << "15"; }
     }
     cout << "\n";
-
-    return 0;
 }
 
