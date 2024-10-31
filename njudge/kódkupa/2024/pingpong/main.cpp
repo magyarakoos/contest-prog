7935fa67cafdd7554f2a25cbedabebdf3f761ea4
magyarakoos <magyar.kende.22f@gmail.com>
2024-10-31 09:29:01 +0100

    sync: 2024-10-31 09:29:00

--- njudge/kódkupa/2024/pingpong/main.cpp
+++ njudge/kódkupa/2024/pingpong/main.cpp
@@ -1,9 +1,27 @@
+#include <array>
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 int main() {
     cin.tie(0), ios::sync_with_stdio(0);
-
-
+    int T;
+    cin >> T;
+    while (T--) {
+        int A, B;
+        cin >> A >> B;
+        if (A <= B) {
+            cout << "-1 -1\n";
+            continue;
+        }
+        int ro = A / 11;
+        int rem = A % 11;
+        vector<array<int, 2>> result;
+        for (int i = 0; i < ro; i++) {
+            int bsc = min(10, B);
+            B -= bsc;
+            result.push_back({11, bsc});
+        }
+    }
 }
