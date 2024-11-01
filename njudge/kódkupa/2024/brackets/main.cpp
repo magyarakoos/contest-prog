a2b85b5105227335c2ca54baf47e80a616883ee2
magyarakoos <magyar.kende.22f@gmail.com>
2024-11-01 12:07:01 +0100

    sync: 2024-11-01 12:07:01

--- njudge/kódkupa/2024/brackets/main.cpp
+++ njudge/kódkupa/2024/brackets/main.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
+#include <unordered_set>
+#include <vector>
 
 using namespace std;
+using ll = long long;
 
 int main() {
     cin.tie(0), ios::sync_with_stdio(0);
 
+    int N;
+    cin >> N;
+    vector<int> a(N);
+    for (int& x : a) cin >> x;
 
+    unordered_set<int> s;
+    s.insert(a[0]);
 }
