fb6b193592034a2baa88cb50c67d2d8d861e5cc3
magyarakoos <magyar.kende.22f@gmail.com>
2024-11-01 11:38:01 +0100

    sync: 2024-11-01 11:38:01

--- njudge/kódkupa/2024/pingpong/main.cpp
+++ njudge/kódkupa/2024/pingpong/main.cpp
@@ -12,44 +12,11 @@ int main() {
         int A, B;
         cin >> A >> B;
 
-        if (A < 33 || A > 53) {
+        if (A < 33 || A > 53 || (A - 33 > 10 && B < 22) ||
+            (A - 33 == 1 && B - 30 > 11) ||
+            (A - 33 > 1 && B - 30 > 22)) {
             cout << "-1 -1\n";
             continue;
-        }
-
-        A -= 33;
-
-        vector<array<int, 2>> result;
-
-        if (A) {
-            if (B < 11) {
-                cout << "-1 -1\n";
-                continue;
-            }
-            if (A == 1 || B < 22) {
-                result.push_back({A, 11});
-                B -= 11;
-            } else {
-                int ra = min(10, A - 1);
-                result.push_back({ra, 11});
-                result.push_back({A - ra, 11});
-                B -= 22;
-            }
-        }
-
-        for (int i = 0; i < 3; i++) {
-            int bsc = min(10, B);
-            result.push_back({11, bsc});
-            B -= bsc;
-        }
-
-        if (B > 0) {
-            cout << "-1 -1\n";
-            continue;
-        }
-
-        for (auto [ac, bc] : result) {
-            cout << ac << " " << bc << "\n";
         }
     }
 }
