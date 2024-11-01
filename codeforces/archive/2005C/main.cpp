17114f7d56350c1c575b0a2b1711a9dbcff60491
magyarakoos <ninjaoffical08@gmail.com>
2024-11-01 15:06:01 +0100

    sync: 2024-11-01 15:06:01

--- codeforces/archive/2005C/main.cpp
+++ codeforces/archive/2005C/main.cpp
@@ -11,11 +11,13 @@ void solve() {
     for (string& s : a) cin >> s;
 
     auto score = [&](int i) -> array<int, 3> {
-
+        array<int, 3> result = {0, 0, 0};
+        return {0, 0, 0};
     };
 
     vector<array<array<int, 3>, 2>> dp(N);
-    dp[0] = {{0, 0, 0}, score(0)};
+    dp[0][0] = {0, 0, 0};
+    dp[0][1] = score(0);
 }
 
 int main() {
