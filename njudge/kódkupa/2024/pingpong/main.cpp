a02283a83a5c7f0b4d1b2d213f34f1ea56882529
magyarakoos <magyar.kende.22f@gmail.com>
2024-11-01 11:49:00 +0100

    sync: 2024-11-01 11:49:00

--- njudge/kódkupa/2024/pingpong/main.cpp
+++ njudge/kódkupa/2024/pingpong/main.cpp
@@ -1,45 +1,53 @@
 #include <iostream>
-
 using namespace std;
+
+void win_a(int B) {
+    for (int i = 0; i < 3; i++) {
+        cout << "11 " << min(10, B) << "\n";
+        B -= min(10, B);
+    }
+}
+
+void solve() {
+    int A, B;
+    cin >> A >> B;
+
+    if (A == 33 && B <= 30) {
+        for (int i = 0; i < 3; i++) {
+            cout << "11 " << min(10, B) << "\n";
+            B -= min(10, B);
+        }
+        continue;
+    }
+
+    if (A >= 33 && A <= 43 && B >= 11 && B <= 41) {
+        A -= 33;
+        B -= 11;
+        cout << A << " 11\n";
+        for (int i = 0; i < 3; i++) {
+            cout << "11 " << min(10, B) << "\n";
+            B -= min(10, B);
+        }
+        continue;
+    }
+
+    if (A >= 44 && A <= 53 && B >= 22 && B <= 52) {
+        A -= 43;
+        B -= 22;
+        cout << "10 11\n" << A << " 11\n";
+        for (int i = 0; i < 3; i++) {
+            cout << "11 " << min(10, B) << "\n";
+            B -= min(10, B);
+        }
+        continue;
+    }
+
+    cout << "-1 -1\n";
+}
 
 int main() {
     cin.tie(0), ios::sync_with_stdio(0);
     int T;
     cin >> T;
-    while (T--) {
-        int A, B;
-        cin >> A >> B;
-
-        if (A == 33 && B <= 30) {
-            for (int i = 0; i < 3; i++) {
-                cout << "11 " << min(10, B) << "\n";
-                B -= min(10, B);
-            }
-            continue;
-        }
-
-        if (A >= 33 && A <= 43 && B >= 11 && B <= 41) {
-            A -= 33;
-            B -= 11;
-            cout << A << " 11\n";
-            for (int i = 0; i < 3; i++) {
-                cout << "11 " << min(10, B) << "\n";
-                B -= min(10, B);
-            }
-            continue;
-        }
-
-        if (A >= 44 && A <= 53 && B >= 22 && B <= 52) {
-            A -= 43;
-            B -= 22;
-            cout << "10 11\n" << A << " 11\n";
-            for (int i = 0; i < 3; i++) {
-                cout << "11 " << min(10, B) << "\n";
-                B -= min(10, B);
-            }
-            continue;
-        }
-
-        cout << "-1 -1\n";
-    }
+    while (T--) solve();
 }
