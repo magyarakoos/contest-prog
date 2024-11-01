ec257bca20714e2fa1b91f62aac1caa131093aca
magyarakoos <magyar.kende.22f@gmail.com>
2024-11-01 11:48:01 +0100

    sync: 2024-11-01 11:48:01

--- njudge/kódkupa/2024/pingpong/main.cpp
+++ njudge/kódkupa/2024/pingpong/main.cpp
@@ -1,6 +1,4 @@
-#include <array>
 #include <iostream>
-#include <vector>
 
 using namespace std;
 
@@ -11,13 +9,6 @@ int main() {
     while (T--) {
         int A, B;
         cin >> A >> B;
-
-        if (A < 33 || A > 53 || (A - 33 > 10 && B < 22) ||
-            (A - 33 == 1 && B - 30 > 11) ||
-            (A - 33 > 1 && B - 30 > 22)) {
-            cout << "-1 -1\n";
-            continue;
-        }
 
         if (A == 33 && B <= 30) {
             for (int i = 0; i < 3; i++) {
@@ -42,6 +33,13 @@ int main() {
             A -= 43;
             B -= 22;
             cout << "10 11\n" << A << " 11\n";
+            for (int i = 0; i < 3; i++) {
+                cout << "11 " << min(10, B) << "\n";
+                B -= min(10, B);
+            }
+            continue;
         }
+
+        cout << "-1 -1\n";
     }
 }
