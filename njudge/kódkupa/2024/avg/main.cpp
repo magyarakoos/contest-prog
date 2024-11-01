4e5f485097426cad307c47ae2010e60606875c2f
magyarakoos <magyar.kende.22f@gmail.com>
2024-11-01 11:58:00 +0100

    sync: 2024-11-01 11:58:00

--- njudge/kódkupa/2024/avg/main.cpp
+++ njudge/kódkupa/2024/avg/main.cpp
@@ -1,8 +1,17 @@
+#include <algorithm>
 #include <iostream>
+#include <vector>
 
 using namespace std;
 using ll = long long;
 
 int main() {
     cin.tie(0), ios::sync_with_stdio(0);
+
+    ll N, K;
+    cin >> N >> K;
+
+    vector<ll> p(N);
+    for (ll& x : p) cin >> x;
+    sort(p.rbegin(), p.rend());
 }
