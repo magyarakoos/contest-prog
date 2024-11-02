94f14932bfbfb1df8638a0c0121e43f9a6d26cc5
magyarakoos <magyar.kende.22f@gmail.com>
2024-11-02 11:43:01 +0100

    sync: 2024-11-02 11:43:01

--- njudge/nemes/2-3/NT22/fozet-keszites/main.py
+++ njudge/nemes/2-3/NT22/fozet-keszites/main.py
@@ -8,10 +8,8 @@ for i in range(1, 501):
     for j in range(1, 501):
         R.add((i // gcd(i, j), j // gcd(i, j)))
 
-R = sorted(list(R))
-for i in range(20)
-    print(R[i])
+print(len(R))
 
-# T = int(input())
-# for _ in range(T):
-#     A, B = map(int, input().split())
+T = int(input())
+for _ in range(T):
+    A, B = map(int, input().split())
