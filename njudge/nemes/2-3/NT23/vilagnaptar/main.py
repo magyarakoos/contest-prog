dc4bf61ffc4f7719d80e600ec9029f6f644bde1f
magyarakoos <magyar.kende.22f@gmail.com>
2024-11-02 11:19:00 +0100

    sync: 2024-11-02 11:19:00

--- njudge/nemes/2-3/NT23/vilagnaptar/main.py
+++ njudge/nemes/2-3/NT23/vilagnaptar/main.py
@@ -2,43 +2,19 @@ Y, M, D = map(int, input().split())
 A = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31] if Y % 4 == 0 else [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 B = [31, 30, 30, 31, 30, 31, 31, 30, 30, 31, 30, 31] if Y % 4 == 0 else [31, 30, 30, 31, 30, 30, 31, 30, 30, 31, 30, 31]
 
-for i in range(M - 1)
+for i in range(M - 1):
     D += A[i]
-wm = 1
+M = 1
 for i in range(12):
     if B[i] < D:
-        wm += 1
+        M += 1
         D -= B[i]
     else:
         break
 
-# if Y % 4 == 0:
-#     for i in range(M - 1):
-#         D += LDL[i]
-#     wm = 1
-#     for i in range(12):
-#         if WLDL[i] < D:
-#             wm += 1
-#             D -= WLDL[i]
-#         else:
-#             break
-#     if wm == 6 and D == 31:
-#         print(Y, wm, "SZN")
-#     elif wm == 12 and D == 31:
-#         print(Y, wm, "VN")
-#     else:
-#         print(Y, wm, D)
-# else:
-#     for i in range(M - 1):
-#         D += DL[i]
-#     wm = 1
-#     for i in range(12):
-#         if WDL[i] < D:
-#             wm += 1
-#             D -= WDL[i]
-#         else:
-#             break
-#     if wm == 12 and D == 31:
-#         print(Y, wm, "VN")
-#     else:
-#         print(Y, wm, D)
+if M == 6 and D == 31:
+    print(Y, M, "SZN")
+elif M == 12 and D == 31:
+    print(Y, M, "VN")
+else:
+    print(Y, M, D)
