f13546b7a97faa3cd4ec4752a43505a125e14a93
magyarakoos <magyar.kende.22f@gmail.com>
2024-11-02 11:33:01 +0100

    sync: 2024-11-02 11:33:01

--- njudge/nemes/2-3/NT22/fozet-keszites/main.py
+++ njudge/nemes/2-3/NT22/fozet-keszites/main.py
@@ -1,4 +1,13 @@
+def gcd(a, b):
+    while b:
+        a, b = b, a % b
+    return abs(a)
+
 R = set()
+for i in range(1, 501):
+    for j in range(1, 501):
+        R.add((i // gcd(i, j), j // gcd(i, j)))
+
 
 T = int(input())
 for _ in range(T):
