9d12c80a8c47e17a6bca4f7d43bcf82e54561d34
magyarakoos <magyar.kende.22f@gmail.com>
2024-11-02 09:53:00 +0100

    sync: 2024-11-02 09:53:00

--- njudge/kódkupa/2024/2/farmula1/main.py
+++ njudge/kódkupa/2024/2/farmula1/main.py
@@ -1,8 +1,6 @@
+S = [25, 18, 15, 12, 10, 8, 6, 4, 2, 1, 0]
 T = int(input())
 for _ in range(T):
     N = int(input())
     P = list(map(int, input().split()))
-    S = [25, 18, 15, 12, 10, 8, 6, 4, 2, 1, 0]
-    score = sum([S[min(x - 1, 10)] for x in P])
-    mx_score = 18 * P.count(1) + 25 * (N - P.count(1))
-    print(score, mx_score)
+    print("Champion" if sum([S[min(x - 1, 10)] for x in P]) >= 18 * P.count(1) + 25 * (N - P.count(1)) else "Practice harder")
