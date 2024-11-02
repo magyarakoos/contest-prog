223ebb5a3469dc2bd281cb69cc61bc9f64d0b1e5
magyarakoos <magyar.kende.22f@gmail.com>
2024-11-02 11:59:00 +0100

    sync: 2024-11-02 11:59:00

--- njudge/nemes/2-3/NT22/fozet-keszites/main.cpp
+++ njudge/nemes/2-3/NT22/fozet-keszites/main.cpp
@@ -5,8 +5,8 @@ int main() {
     cin.tie(0), ios::sync_with_stdio(0);
 
     vector dp(501, vector<int>(501));
-    for (int i = 0; i < 32; i++) {
-        for (int j = 0; j < 32; j++) {
+    for (int i = 1; i < 25; i++) {
+        for (int j = 1; j < 25; j++) {
             if (gcd(i, j) > 1) continue;
             for (int k = 500; k >= i; k--) {
                 for (int l = 500; l >= j; l--) {
