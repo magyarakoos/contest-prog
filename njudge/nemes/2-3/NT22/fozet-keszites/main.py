8649489de0430585bab04601777038e2802f8930
magyarakoos <magyar.kende.22f@gmail.com>
2024-11-02 11:51:01 +0100

    sync: 2024-11-02 11:51:01

--- njudge/nemes/2-3/NT22/fozet-keszites/main.py
+++ njudge/nemes/2-3/NT22/fozet-keszites/main.py
@@ -4,6 +4,13 @@ def gcd(a, b):
     return abs(a)
 
 dp = [[0] * 501 for _ in range(501)]
+for i in range(25):
+    for j in range(25):
+        if gcd(i, j) != 1:
+            continue
+        for k in range(i, 501):
+            for l in range(j, 501):
+                dp[k][l] = min(dp[k][l], )
 
 T = int(input())
 for _ in range(T):
