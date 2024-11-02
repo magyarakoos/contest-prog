3da3d8b4fc2734514322a2b3d7b560388668dfe0
magyarakoos <magyar.kende.22f@gmail.com>
2024-11-02 09:52:01 +0100

    sync: 2024-11-02 09:52:01

--- njudge/kódkupa/2024/2/farmula1/main.py
+++ njudge/kódkupa/2024/2/farmula1/main.py
@@ -4,4 +4,5 @@ for _ in range(T):
     P = list(map(int, input().split()))
     S = [25, 18, 15, 12, 10, 8, 6, 4, 2, 1, 0]
     score = sum([S[min(x - 1, 10)] for x in P])
-    print(score)
+    mx_score = 18 * P.count(1) + 25 * (N - P.count(1))
+    print(score, mx_score)
