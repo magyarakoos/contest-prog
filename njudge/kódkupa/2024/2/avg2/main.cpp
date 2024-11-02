f4b166b9afe54976c6ec14cb4801f4a7e0a01787
magyarakoos <magyar.kende.22f@gmail.com>
2024-11-02 09:57:01 +0100

    sync: 2024-11-02 09:57:01

--- njudge/kódkupa/2024/2/avg2/main.cpp
+++ njudge/kódkupa/2024/2/avg2/main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
+#define int long long
 
 int main() {
     cin.tie(0), ios::sync_with_stdio(0);
-
-
+    int N, K;
+    cin >> N >> K;
+    vector<int> p(N);
+    for (int& x : p) cin >> x;
 }
