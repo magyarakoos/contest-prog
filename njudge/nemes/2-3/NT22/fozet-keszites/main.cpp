8c8d9e9130a4e6d5b51db5a375955f0a932edcce
magyarakoos <magyar.kende.22f@gmail.com>
2024-11-02 11:55:00 +0100

    sync: 2024-11-02 11:55:00

--- njudge/nemes/2-3/NT22/fozet-keszites/main.cpp
+++ njudge/nemes/2-3/NT22/fozet-keszites/main.cpp
@@ -1,29 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int MAXN = 501;
+const int MAXN = 500;
 
 int main() {
     cin.tie(0), ios::sync_with_stdio(0);
 
+    vector dp(MAXN + 1, vector<int>(MAXN + 1));
     for (int i = 0; i < 25; i++) {
         for (int j = 0; j < 25; j++) {
             if (gcd(i, j) != 1) continue;
-            for (int k = i; k < MAXN; k++) {
-                for (int l = j; l < MAXN; l++) {
+            for (int k = i; k <= MAXN; k++) {
+                for (int l = j; l <= MAXN; l++) {
                     dp[k][l] =
                         max(dp[k][l], dp[k - i][l - j] + 1);
                 }
             }
         }
     }
-}
 
-int T;
-cin >> T;
-while (T--) {
-    int A, B;
-    cin >> A >> B;
-    cout << dp[A][B] << "\n";
-}
+    int T;
+    cin >> T;
+    while (T--) {
+        int A, B;
+        cin >> A >> B;
+        cout << dp[A][B] << "\n";
+    }
 }
