68c79fe629bf2a3c48d4610827b3a2021fb054e4
magyarakoos <magyar.kende.22f@gmail.com>
2024-11-02 11:14:00 +0100

    sync: 2024-11-02 11:14:00

--- njudge/nemes/2-3/NT23/vilagnaptar/main.py
+++ njudge/nemes/2-3/NT23/vilagnaptar/main.py
@@ -5,7 +5,21 @@ WLDL = [31, 30, 30, 31, 30, 31, 31, 30, 30, 31, 30, 31]
 
 Y, M, D = map(int, input().split())
 if Y % 4 == 0:
-    pass
+    for i in range(M - 1):
+        D += LDL[i]
+    wm = 1
+    for i in range(12):
+        if WLDL[i] < D:
+            wm += 1
+            D -= WLDL[i]
+        else:
+            break
+    if wm == 6 and D == 31:
+        print(Y, wm, "SZN")
+    elif wm == 12 and D == 31:
+        print(Y, wm, "VN")
+    else:
+        print(Y, wm, D)
 else:
     for i in range(M - 1):
         D += DL[i]
