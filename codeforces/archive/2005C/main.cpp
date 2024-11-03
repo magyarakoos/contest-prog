d3a48993cafe165f96580599f45af2ebafa39b84
magyarakoos <ninjaoffical08@gmail.com>
2024-11-03 16:35:01 +0100

    sync: 2024-11-03 16:35:01

--- codeforces/archive/2005C/main.cpp
+++ codeforces/archive/2005C/main.cpp
@@ -3,58 +3,49 @@ using namespace std;
 
 const string NAREK = "narek";
 
+void solve() {
+    int N, M;
+    cin >> N >> M;
+
+    vector<string> s(N);
+    for (int i = 0; i < N; i++) cin >> s[i];
+
+    vector<int> dp(5, int(-1e9)), ndp;
+    dp[0] = 0;
+
+    for (int i = 0; i < N; i++) {
+        ndp = dp;
+        for (int j = 0; j < 5; j++) {
+            if (dp[j] == int(-1e9)) continue;
+            int score = 0, nxt = j;
+            for (int k = 0; k < M; k++) {
+                int pos = NAREK.find(s[i][k]);
+
+                if (pos == -1) continue;
+                if (nxt == pos) { // if s[i][k] is the
+                                  // next letter
+                    nxt = (nxt + 1) % 5;
+                    score++;
+                } else
+                    score--;
+            }
+
+            ndp[nxt] = max(ndp[nxt], dp[j] + score);
+        }
+
+        dp = ndp;
+    }
+
+    int ans = 0;
+    for (int i = 0; i < 5; i++)
+        ans = max(ans, dp[i] - 2 * i);
+    cout << ans << "\n";
+}
+
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(0);
-
-    int t;
-    cin >> t;
-
-    while (t--) {
-        int n, m;
-        cin >> n >> m;
-
-        vector<string> s(n);
-        for (int i = 0; i < n; i++) cin >> s[i];
-
-        vector<int> dp(5, int(-1e9)), ndp;
-        dp[0] = 0;
-
-        for (int i = 0; i < n; i++) {
-            ndp = dp; // overwriting dp
-
-            for (int j = 0; j < 5; j++) {
-                if (dp[j] == int(-1e9)) continue;
-
-                int counted_score = 0, next = j;
-
-                for (int k = 0; k < m; k++) {
-                    int ind = narek.find(s[i][k]);
-
-                    if (ind == -1)
-                        continue; // if s[i][k] is not a
-                                  // letter of "narek"
-
-                    if (next == ind) { // if s[i][k] is the
-                                       // next letter
-                        next = (next + 1) % 5;
-                        counted_score++;
-                    } else
-                        counted_score--;
-                }
-
-                ndp[next] =
-                    max(ndp[next], dp[j] + counted_score);
-            }
-
-            dp = ndp; // overwriting dp back
-        }
-
-        int ans = 0;
-        for (int i = 0; i < 5; i++)
-            ans = max(
-                ans, dp[i] - 2 * i); // checking all letters
-
-        cout << ans << "\n";
-    }
+    int T;
+    cin >> T;
+    while (T--) solve();
 }
