38b2af443b15d808c44ef6706034cd72492dc133
magyarakoos <ninjaoffical08@gmail.com>
2024-11-03 16:54:00 +0100

    sync: 2024-11-03 16:54:00

--- njudge/nemes/2-3/NT24/maxi-szorzat/main.cpp
+++ njudge/nemes/2-3/NT24/maxi-szorzat/main.cpp
@@ -1,5 +1,26 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
+using ll = long long;
 
-int main() {}
+int main() {
+    ll N, K, B;
+    cin >> N >> K >> B;
+
+    vector<ll> neg, pos;
+    for (int i = 0; i < N; i++) {
+        int x;
+        cin >> x;
+        if (x < 0) {
+            neg.push_back(x);
+        } else {
+            pos.push_back(x);
+        }
+    }
+
+    if (neg.size() < B) {
+        cout << "-1\n";
+        exit(0);
+    }
+}
