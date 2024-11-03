b115a65134268ce352075de3f91055632da7e7d6
magyarakoos <ninjaoffical08@gmail.com>
2024-11-03 16:49:00 +0100

    sync: 2024-11-03 16:49:00

--- njudge/nemes/2-3/NT24/ontozes/main.py
+++ njudge/nemes/2-3/NT24/ontozes/main.py
@@ -1,7 +1,6 @@
 Q, M = map(int, input().split())
 for _ in range(Q):
-    A, B, X, Y = map(int, input().split())
+    A, X, B, Y = map(int, input().split())
     A = (A - 1) * 24 + X
     B = (B - 1) * 24 + Y
-    print(A, B)
-    print((B - A + M - 1) // M)
+    print((B - A) // M)
