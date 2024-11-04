4c82f0980ed3431e6b60f8ea4bcaed75bb43bd3c
magyarakoos <ninjaoffical08@gmail.com>
2024-11-04 16:25:01 +0100

    sync: 2024-11-04 16:25:01

--- njudge/nemes/2-3/NT23/szines-szobak/main.cpp
+++ njudge/nemes/2-3/NT23/szines-szobak/main.cpp
@@ -1,5 +1,19 @@
+#include <deque>
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-int main() {}
+int main() {
+    int N, K, M;
+    cin >> N >> K >> M;
+
+    vector<int> d(K);
+    vector<deque<int>> personS(K);
+
+    for (int i = 0; i < K; i++) {
+        int C;
+        cin >> C;
+        personS[C - 1].push_back(i);
+    }
+}
