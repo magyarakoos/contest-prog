830a225b3de8f467b166e9ca518432f02877ceb1
magyarakoos <ninjaoffical08@gmail.com>
2024-11-04 17:19:00 +0100

    sync: 2024-11-04 17:19:00

--- atcoder/abc378/B/main.cpp
+++ atcoder/abc378/B/main.cpp
@@ -7,5 +7,14 @@ using namespace std;
 int main() {
     int N;
     cin >> N;
-    vector<array<int, 2>> QR(N);
+    vector<int> M(N), R(N);
+    for (int i = 0; i < N; i++) { cin >> M[i] >> R[i]; }
+    int Q;
+    cin >> Q;
+    while (Q--) {
+        int T, D;
+        cin >> T >> D;
+        int m = M[T - 1], r = R[T - 1];
+        // D / Q
+    }
 }
