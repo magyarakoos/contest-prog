7a25dfc2bb5660a2024e24fb6d7447cbaa0562b3
magyarakoos <magyar.kende.22f@gmail.com>
2024-11-04 10:19:00 +0100

    sync: 2024-11-04 10:19:00

--- njudge/nemes/2-3/NT24/fertozesi-sorozat/main.cpp
+++ njudge/nemes/2-3/NT24/fertozesi-sorozat/main.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 int main() {
     cin.tie(0), ios::sync_with_stdio(0);
 
+    int N, M, K;
+    cin >> N >> M >> K;
 
+    vector<int> a(K);
+    for (int& x : a) cin >> x;
+
+    vector<vector<int>> g(N + 1);
+    while (M--) {
+        int U, V;
+        cin >> U >> V;
+        g[U].push_back(V);
+        g[V].push_back(U);
+    }
 }
