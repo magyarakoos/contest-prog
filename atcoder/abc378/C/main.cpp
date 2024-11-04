025e15a4ea8f37d038e389c56f3f1ebdb2006a39
magyarakoos <ninjaoffical08@gmail.com>
2024-11-04 17:25:00 +0100

    sync: 2024-11-04 17:25:00

--- atcoder/abc378/C/main.cpp
+++ atcoder/abc378/C/main.cpp
@@ -1,5 +1,16 @@
 #include <iostream>
+#include <map>
 
 using namespace std;
 
-int main() {}
+int main() {
+    int N;
+    cin >> N;
+    map<int, int> m;
+    for (int i = 0; i < N; i++) {
+        int A;
+        cin >> A;
+        cout << (m.count(A) ? m[A] : -1) << "\n";
+        m[A] = i + 1;
+    }
+}
