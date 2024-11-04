41266731c65fd60caabbfea947b65a3f260230b6
magyarakoos <magyar.kende.22f@gmail.com>
2024-11-04 09:51:00 +0100

    sync: 2024-11-04 09:51:00

--- njudge/nemes/2-3/NT24/darabolas/main.cpp
+++ njudge/nemes/2-3/NT24/darabolas/main.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
+using ll = long long;
 
 int main() {
     cin.tie(0), ios::sync_with_stdio(0);
 
+    ll M, N;
+    cin >> M >> N;
 
+    vector<ll> MC(M - 1), NC(N - 1);
+    for (ll& x : MC) cin >> x;
+    for (ll& x : NC) cin >> x;
 }
