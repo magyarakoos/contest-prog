e213ea3ff3898b7775f7df19c9a898f96a2db582
magyarakoos <ninjaoffical08@gmail.com>
2024-11-04 16:40:00 +0100

    sync: 2024-11-04 16:40:00

--- njudge/nemes/1/NT24/zebra/main.cpp
+++ njudge/nemes/1/NT24/zebra/main.cpp
@@ -1,5 +1,18 @@
+#include <algorithm>
+#include <array>
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-int main() {}
+int main() {
+    int N;
+    cin >> N;
+
+    vector<array<int, 2>> a(N + 1);
+    for (int i = 1; i <= N; i++) cin >> a[i][1];
+    for (int i = 1; i <= N; i++) cin >> a[i][0];
+    sort(a.begin(), a.end());
+
+    vector<int> dp(N + 1);
+}
