385f6435854fe206acd252c15c8ba075b70c2f8c
magyarakoos <magyar.kende.22f@gmail.com>
2024-11-05 11:22:00 +0100

    sync: 2024-11-05 11:22:00

--- njudge/kódkupa/2024/2/stringimbalance/main.cpp
+++ njudge/kódkupa/2024/2/stringimbalance/main.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
+using ll = long long;
+
+void solve() {
+    vector<ll> h(52);
+    int Q;
+    cin >> Q;
+    while (Q--) {
+        ll F, K;
+        char C;
+        cin >> F >> C >> K;
+        C -= "aA"[C < 'a'];
+        h[C] += F;
+    }
+}
 
 int main() {
     cin.tie(0), ios::sync_with_stdio(0);
-
-
+    int T;
+    cin >> T;
+    while (T--) solve();
 }
