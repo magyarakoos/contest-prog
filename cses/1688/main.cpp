1832ab41920b614140234e63be3a05f02f5721a9
magyarakoos <ninjaoffical08@gmail.com>
2024-11-05 18:39:01 +0100

    sync: 2024-11-05 18:39:01

--- cses/1688/main.cpp
+++ cses/1688/main.cpp
@@ -3,39 +3,38 @@ using namespace std;
 
 const int MAXK = 20;
 
-vector<vector<int>> sz, p;
+vector<vector<int>> g, p;
 vector<int> tin, tout;
 int t;
 
-void dfs(int cs) {
-    tin[cs] = t++;
+void dfs(int u) {
+    tin[u] = t++;
     for (int i = 1; i <= MAXK; i++) {
-        p[cs][i] = p[p[cs][i - 1]][i - 1];
+        p[u][i] = p[p[u][i - 1]][i - 1];
     }
-    for (int x : sz[cs]) { dfs(x); }
-    tout[cs] = t++;
+    for (int x : g[u]) { dfs(x); }
+    tout[u] = t++;
 }
 
 int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
+    cin.tie(0), ios::sync_with_stdio(0);
 
     int n, q;
     cin >> n >> q;
 
-    sz.resize(n + 1);
+    g.resize(n + 1);
     p.resize(n + 1, vector<int>(MAXK + 1, 1));
     tin.resize(n + 1);
     tout.resize(n + 1);
 
     for (int i = 2; i <= n; i++) {
         cin >> p[i][0];
-        sz[p[i][0]].push_back(i);
+        g[p[i][0]].push_back(i);
     }
 
     dfs(1);
 
-    auto is_parent = [&](int u, int v) {
+    auto is_anc = [&](int u, int v) {
         return (tin[u] <= tin[v] && tout[v] <= tout[u]);
     };
 
@@ -43,17 +42,17 @@ int main() {
         int a, b;
         cin >> a >> b;
 
-        if (is_parent(a, b)) {
+        if (is_anc(a, b)) {
             cout << a << "\n";
             continue;
         }
-        if (is_parent(b, a)) {
+        if (is_anc(b, a)) {
             cout << b << "\n";
             continue;
         }
 
         for (int i = MAXK; i >= 0; i--) {
-            if (!is_parent(p[a][i], b)) { a = p[a][i]; }
+            if (!is_anc(p[a][i], b)) { a = p[a][i]; }
         }
 
         cout << p[a][0] << "\n";
