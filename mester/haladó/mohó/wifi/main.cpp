4992dadf46e98c226d6d4f7479d6624fd7ee2d43
magyarakoos <magyar.kende.22f@gmail.com>
2024-11-10 08:18:01 +0100

    sync: 2024-11-10 08:18:01

--- mester/haladó/mohó/wifi/main.cpp
+++ mester/haladó/mohó/wifi/main.cpp
@@ -7,19 +7,22 @@ int main() {
     vector<int> v(N);
     for (int& x : v) cin >> x;
 
-    int right =
-        -H - 1; // a legjobboldalabbi elérési pont pozíciója
+    // a legjobboldalabbi elérési pont pozíciója
+    int right = -H - 1;
 
     vector<int> result;
 
     for (int i = 0; i < N; i++) {
         if (right + H < v[i]) {
+            // átugorjuk azokat a pontokat, amiket a
+            // legjobboldalabbi elérési pontpból még el
+            // lehet érni és azokat is,
             int j = i;
-            while (j + 1 < N && v[j + 1] - H <= v[i]) {
+            while (j + 1 < N && v[j + 1] <= v[i] + H) {
                 j++;
             }
-            i = j;
 
+            i = j;
             result.push_back(i);
             right = v[i];
         }
