59ad1f79a4a6611a44ba3c9edad7c665c5363598
magyarakoos <ninjaoffical08@gmail.com>
2024-11-16 11:05:00 +0100

    sync: 2024-11-16 11:05:00

--- mester/nt-oktv-ioi/NTO25/amoba/main.cpp
+++ mester/nt-oktv-ioi/NTO25/amoba/main.cpp
@@ -2,4 +2,13 @@
 
 using namespace std;
 
-int main() {}
+int board[3][3];
+int main() {
+    int N;
+    cin >> N;
+    for (int i = 0; i < N; i++) {
+        int X, Y;
+        cin >> X >> Y;
+        board[X - 1][Y - 1] = i % 2 + 1;
+    }
+}
