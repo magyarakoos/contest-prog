6accad06a91520ec58b67588f07d9658c39bdece
magyarakoos <ninjaoffical08@gmail.com>
2024-11-16 15:19:01 +0100

    sync: 2024-11-16 15:19:01

--- codeforces/contest/1980/E/main.cpp
+++ codeforces/contest/1980/E/main.cpp
@@ -19,6 +19,7 @@ void solve() {
 
     vector<int> pos1i(N * M), pos2i(N * M), pos1j(N * M),
         pos2j(N * M);
+
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < M; j++) {
             int x = A[i][j];
@@ -27,14 +28,17 @@ void solve() {
             pos1j[x] = pos2j[y] = j;
         }
     }
-    vector<set<int>> pi(nm), pj(nm);
-    for (int x = 0; x < nm; ++x) {
+
+    vector<set<int>> pi(N * M), pj(N * M);
+
+    for (int x = 0; x < N * M; x++) {
         int i1 = pos1i[x], i2 = pos2i[x];
         int j1 = pos1j[x], j2 = pos2j[x];
         pi[i1].insert(i2);
         pj[j1].insert(j2);
     }
-    for (int x = 0; x < nm; ++x) {
+
+    for (int x = 0; x < N * M; ++x) {
         if (pi[x].size() > 1 || pj[x].size() > 1) {
             cout << "NO\n";
             return;
@@ -44,8 +48,9 @@ void solve() {
 }
 
 int main() {
-    int t;
-    cin >> t;
-    for (int _ = 0; _ < t; ++_) { solve(); }
-    return 0;
+    cin.tie(0);
+    ios::sync_with_stdio(0);
+    int T;
+    cin >> T;
+    while (T--) solve();
 }
