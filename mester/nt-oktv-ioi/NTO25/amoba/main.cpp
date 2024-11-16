c5e5893e38d409473d6888bd8127fdf357736e50
magyarakoos <ninjaoffical08@gmail.com>
2024-11-16 11:06:00 +0100

    sync: 2024-11-16 11:06:00

--- mester/nt-oktv-ioi/NTO25/amoba/main.cpp
+++ mester/nt-oktv-ioi/NTO25/amoba/main.cpp
@@ -11,4 +11,10 @@ int main() {
         cin >> X >> Y;
         board[X - 1][Y - 1] = i % 2 + 1;
     }
+
+    cout << "+-+-+-+\n";
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {}
+        cout << "+-+-+-+\n";
+    }
 }
