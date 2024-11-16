1d15c25a063c2da86de80c005a2cb76a3e92ce0c
magyarakoos <ninjaoffical08@gmail.com>
2024-11-16 15:22:00 +0100

    sync: 2024-11-16 15:22:00

--- codeforces/contest/1980/E/main.cpp
+++ codeforces/contest/1980/E/main.cpp
@@ -40,12 +40,12 @@ void solve() {
             return;
         }
     }
+
     cout << "YES\n";
 }
 
 int main() {
-    cin.tie(0);
-    ios::sync_with_stdio(0);
+    cin.tie(0), ios::sync_with_stdio(0);
     int T;
     cin >> T;
     while (T--) solve();
