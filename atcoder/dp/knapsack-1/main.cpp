ec19685690cb835478183d4bb176ce3a5850ad81
magyarakoos <magyar.kende.22f@gmail.com>
2024-11-19 16:29:00 +0100

    sync: 2024-11-19 16:29:00

--- atcoder/dp/knapsack-1/main.cpp
+++ atcoder/dp/knapsack-1/main.cpp
@@ -1,29 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
-using ll = long long;
 
 int main() {
-    cin.tie(0), ios::sync_with_stdio();
-    int N, W;
+    int N, W, w, v;
     cin >> N >> W;
-
-    vector<int> w(N + 1), v(N + 1);
-    vector dp(N + 1, vector<ll>(W + 1, -1));
-
-    for (int i = 1; i <= N; i++) {
-        cin >> w[i] >> v[i];
-    }
-
-    dp[0][0] = 0;
-
-    for (int i = 1; i <= N; i++) {
-        for (int j = 0; j <= W; j++) {
-            dp[i][j] = dp[i - 1][j];
-            if (j - w[i] >= 0 && dp[i - 1][j - w[i]] != -1) {
-                dp[i][j] = max(dp[i][j], dp[i - 1][j - w[i]] + v[i]);
-            }
+    vector<long long> dp(W + 1);
+    while (N--) {
+        cin >> w >> v;
+        for (int i = W; i >= w; i--) {
+            dp[i] = max(dp[i], dp[i - w] + v);
         }
     }
-
-    cout << *max_element(dp[N].begin(), dp[N].end());
+    cout << *max_element(dp.begin(), dp.end());
 }
+
