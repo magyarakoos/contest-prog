360a177880e6b5ba1629d1a4ae61ebc09c6a44fe
magyarakoos <magyar.kende.22f@gmail.com>
2025-01-01 12:42:01 +0100

    sync: 2025-01-01 12:42:01

---
+++ njudge/ioi-ceoi/2022/5/main.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int int64_t
+
+int32_t main() {
+    int N, M, Q;
+    cin >> N >> M >> Q;
+
+    vector H(N, vector<int>(M));
+    for (auto& row : H) {
+        for (int& x : row) cin >> x;
+    }
+
+    while (Q--) {
+        int i1, j1, i2, j2;
+        cin >> i1 >> j1 >> i2 >> j2;
+        i1--, j1--, i2--, j2--;
+        cout << format("{} {} {} {}\n", i1, j1, i2, j2);
+    }
+}
