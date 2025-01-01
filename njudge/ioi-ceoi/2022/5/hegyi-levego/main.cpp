ce3438adf0831a9cbbcaed22998ffc9513eac134
magyarakoos <magyar.kende.22f@gmail.com>
2025-01-01 12:46:01 +0100

    sync: 2025-01-01 12:46:01

--- njudge/ioi-ceoi/2022/5/hegyi-levego/main.cpp
+++ njudge/ioi-ceoi/2022/5/hegyi-levego/main.cpp
@@ -11,13 +11,19 @@ int32_t main() {
         for (int& x : row) cin >> x;
     }
 
+    auto h = [&](array<int, 2> a) -> int { return 0; };
+    auto lca = [&](array<int, 2> a,
+                   array<int, 2> b) -> array<int, 2> {
+        return {};
+    };
+
     while (Q--) {
         int i1, j1, i2, j2;
         cin >> i1 >> j1 >> i2 >> j2;
         i1--, j1--, i2--, j2--;
         cout << format("{} {} {} {}\n", i1, j1, i2, j2);
-        cout << h(i1, j1) + h(i2, j2) -
-                    2 * lca(i1, j1, i2, j2)
+        cout << h({i1, j1}) + h({i2, j2}) -
+                    2 * h(lca({i1, j1}, {i2, j2}))
              << "\n";
     }
 }
