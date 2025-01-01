81dd79a5e2f58de38f50bf0d4766b3e65b6d63da
magyarakoos <magyar.kende.22f@gmail.com>
2025-01-01 18:04:00 +0100

    sync: 2025-01-01 18:04:00

--- njudge/ioi-ceoi/2022/1/hazszamok/main.cpp
+++ njudge/ioi-ceoi/2022/1/hazszamok/main.cpp
@@ -18,10 +18,8 @@ int32_t main() {
     auto f = [&](int n) -> bool {
         vector<int> req(K);
         for (int i = 1; i <= MAXW; i++) {
-            int div = max((int)0, n - (pw[i - 1] - 1)) /
-                      pw[i],
-                rem = max((int)0, n - (pw[i - 1] - 1)) %
-                      pw[i];
+            int num = max((int)0, n - (pw[i - 1] - 1));
+            int div = num / pw[i], rem = num % pw[i];
             for (int j = 0; j < K; j++) {
                 req[j] += div * pw[i - 1];
                 req[j] += min(rem, pw[i - 1]);
@@ -32,7 +30,7 @@ int32_t main() {
         return 1;
     };
 
-    int l = 0, r = 1e18;
+    int l = 0, r = 1e17;
     while (r - l > 1) {
         int m = (r + l) / 2;
         if (!f(m)) {
