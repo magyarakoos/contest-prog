0c6978f2586ac4ecb160357c7be4b0986b786768
magyarakoos <magyar.kende.22f@gmail.com>
2025-01-01 12:54:00 +0100

    sync: 2025-01-01 12:54:00

--- njudge/ioi-ceoi/2022/5/hegyi-levego/main.cpp
+++ njudge/ioi-ceoi/2022/5/hegyi-levego/main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 const int di[] = {1, -1, 0, 0}, dj[] = {0, 0, 1, -1};
 
 int32_t main() {
-    int N, M, Q, K = 0;
+    int N, M, Q;
     cin >> N >> M >> Q;
 
     vector H(N, vector<int>(M));
@@ -15,7 +15,7 @@ int32_t main() {
 
     vector<array<int, 5>> edgeS;
     for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
+        for (int j = 0; j < M; j++) {
             for (int k = 0; k < 4; k++) {
                 int ni = i + di[k], nj = j + dj[k];
                 if (ni < 0 || nj < 0 || ni >= N ||
@@ -24,11 +24,25 @@ int32_t main() {
                 }
                 edgeS.push_back({max(H[i][j], H[ni][nj]), i,
                                  j, ni, nj});
-                K++;
             }
         }
     }
     sort(edgeS.begin(), edgeS.end());
+
+    vector rnk(N, vector<int>(M, 1));
+    vector par(N, vector<array<int, 2>>(M));
+
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) { par[i][j] = {i, j}; }
+    }
+
+    auto same = [&](array<int, 2> a,
+                    array<int, 2> b) -> bool { return 0; };
+    auto find = [&](array<int, 2> a) -> array<int, 2> {
+        return a;
+    };
+    auto unite = [&](array<int, 2> a,
+                     array<int, 2> b) -> void {};
 
     auto h = [&](array<int, 2> a) -> int { return 0; };
     auto lca = [&](array<int, 2> a,
