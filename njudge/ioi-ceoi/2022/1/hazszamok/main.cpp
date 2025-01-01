6546ae7e328218c426c33c25564ded0b6f3c8b26
magyarakoos <magyar.kende.22f@gmail.com>
2025-01-01 16:00:00 +0100

    sync: 2025-01-01 16:00:00

--- njudge/ioi-ceoi/2022/1/hazszamok/main.cpp
+++ njudge/ioi-ceoi/2022/1/hazszamok/main.cpp
@@ -16,7 +16,8 @@ int32_t main() {
 
     auto f = [&](int n) -> bool {
         for (int i = 1; i <= MAXW; i++) {
-            int div = n / pw[i - 1], rem = n % pw[i - 1];
+            int div = (n - (pw[i - 1] - 1)) / pw[i - 1],
+                rem = (n - (pw[i - 1] - 1)) % pw[i - 1];
             cout << div << " " << rem << "\n";
         }
         cout << "\n";
