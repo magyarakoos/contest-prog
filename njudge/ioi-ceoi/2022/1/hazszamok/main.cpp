b5210058af5a6383a9b9ed5f60d8d02e39447c8c
magyarakoos <magyar.kende.22f@gmail.com>
2025-01-01 18:40:00 +0100

    sync: 2025-01-01 18:40:00

--- njudge/ioi-ceoi/2022/1/hazszamok/main.cpp
+++ njudge/ioi-ceoi/2022/1/hazszamok/main.cpp
@@ -2,8 +2,6 @@
 using namespace std;
 #define int int64_t
 
-const int MAXW = 17;
-
 int32_t main() {
     cin.tie(0), ios::sync_with_stdio(0);
     int K;
@@ -11,12 +9,12 @@ int32_t main() {
     vector<int> a(K);
     for (int& x : a) cin >> x;
 
-    vector<int> pw(MAXW + 1, 1);
-    for (int i = 1; i <= MAXW; i++) pw[i] = pw[i - 1] * K;
+    vector<int> pw(18, 1);
+    for (int i = 1; i < 18; i++) pw[i] = pw[i - 1] * K;
 
     auto f = [&](int n) -> bool {
         vector<int> req(K);
-        for (int i = 1; i <= MAXW && n / pw[i - 1]; i++) {
+        for (int i = 1; n / pw[i - 1]; i++) {
             int div = n / pw[i], rem = n % pw[i];
             for (int j = 0; j < K; j++) {
                 req[j] += div * pw[i - 1];
