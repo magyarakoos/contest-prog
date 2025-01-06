4d3c61d5faabb566baed838631fa1db5b5417aa5
magyarakoos <magyar.kende.22f@gmail.com>
2025-01-06 16:51:00 +0100

    sync: 2025-01-06 16:51:00

--- njudge/nemes/2-3/NT21/inverzio/main.cpp
+++ njudge/nemes/2-3/NT21/inverzio/main.cpp
@@ -1,6 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-using point = array<int, 2>;
-
-int main() {}
+int main() {
+    int N;
+    cin >> N;
+    vector<array<int, 2>> a(N);
+    for (int i = 0; i < N; i++) {
+        cin >> a[i][0];
+        a[i][1] = i;
+    }
+    sort(a.rbegin(), a.rend());
+    int mx_j = -1, result = -1;
+    for (auto [x, i] : a) {
+        if (result > i) { result = i; }
+        mx_j = max(mx_j, i);
+    }
+}
