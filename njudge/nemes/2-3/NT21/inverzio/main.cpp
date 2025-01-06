a5e119dde5acca3b9bc382115c2cabc60990a23f
magyarakoos <magyar.kende.22f@gmail.com>
2025-01-06 17:00:00 +0100

    sync: 2025-01-06 17:00:00

--- njudge/nemes/2-3/NT21/inverzio/main.cpp
+++ njudge/nemes/2-3/NT21/inverzio/main.cpp
@@ -10,7 +10,7 @@ int main() {
         a[i][1] = i;
     }
     sort(a.rbegin(), a.rend());
-    int mx_j = -1, mn_i = 1e9;
+    int mx_j = -1, result_i = 1e9, result_j = -1;
     for (auto [x, i] : a) {
         if (mn_i > i) mn_i = i;
         mx_j = max(mx_j, i);
