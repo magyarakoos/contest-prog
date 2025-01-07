54fbc17724b7411e051d393b9b0240bc19928b8b
magyarakoos <magyar.kende.22f@gmail.com>
2025-01-07 17:18:01 +0100

    sync: 2025-01-07 17:18:01

--- njudge/nemes/2-3/NT23/mi-broker/main.cpp
+++ njudge/nemes/2-3/NT23/mi-broker/main.cpp
@@ -1,42 +1,7 @@
 #include <bits/stdc++.h>
-
-#define all(v) v.begin(), v.end()
-#define rall(v) v.rbegin(), v.rend()
-#define size(v) (int)v.size()
-
 using namespace std;
-using ll = long long;
+#define int long long
 
-int main() {
+int32_t main() {
     cin.tie(0), ios::sync_with_stdio(0);
-
-    int N, Q;
-    cin >> N >> Q;
-
-    vector<int> priceS(N);
-    for (int i = 0; i < N; i++) {
-        cin >> priceS[i];
-    }
-
-    while (Q--) {
-        int result = 0;
-
-        bool has = 0;
-
-        int V, E;
-        cin >> V >> E;
-
-        for (int i = 0; i < N; i++) {
-            if (!has && V >= priceS[i]) {
-                has = 1;
-                result -= priceS[i];
-            }
-            else if (has && E <= priceS[i]) {
-                has = 0;
-                result += priceS[i];
-            }
-        }
-
-        cout << result << '\n';
-    }
 }
