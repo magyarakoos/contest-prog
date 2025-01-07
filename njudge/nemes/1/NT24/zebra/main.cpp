1046b043afda3749b3d4d96e6f8db195d6941bd1
magyarakoos <magyar.kende.22f@gmail.com>
2025-01-07 16:54:01 +0100

    sync: 2025-01-07 16:54:01

--- njudge/nemes/1/NT24/zebra/main.cpp
+++ njudge/nemes/1/NT24/zebra/main.cpp
@@ -15,16 +15,33 @@ int32_t main() {
     for (int i = 1; i <= N; i++) cin >> a[i][0];
     sort(a.begin() + 1, a.end());
 
-    vector<int> sideA, sideB;
+    vector<int> sideA(1), sideB(1);
     for (int i = 1; i <= N; i++) {
         (a[i][1] ? sideB : sideA).push_back(a[i][0]);
     }
-    int M = sideA.size(), K = sideB.size();
+    int M = sideA.size() - 1, K = sideB.size() - 1;
 
     vector dp(M + 1, vector<int>(K + 1, INF));
     dp[0][0] = 0;
 
-    for (int i = 1; i <= N; i++) {
-        for (int j = 0; j < i; j++) {}
+    vector<int> psA(M + 1), psB(M + 1);
+    partial_sum(sideA.begin(), sideA.end(),
+                psA.begin() + 1);
+    partial_sum(sideB.begin(), sideB.end(),
+                psB.begin() + 1);
+
+    auto f = [&](int i, int j, int k, int l) {
+
+    };
+
+    for (int i = 1; i <= M; i++) {
+        for (int j = 1; j <= K; j++) {
+            for (int k = 0; k < i; k++) {
+                for (int l = 0; l < j; l++) {
+                    dp[i][j] = min(
+                        dp[i][j], dp[k][l] + f(i, j, k, l));
+                }
+            }
+        }
     }
 }
