8686ace2e6bd4411ab1fe3695a014e6d4ae27356
magyarakoos <magyar.kende.22f@gmail.com>
2025-01-07 16:43:00 +0100

    sync: 2025-01-07 16:43:00

--- njudge/nemes/1/NT24/zebra/main.cpp
+++ njudge/nemes/1/NT24/zebra/main.cpp
@@ -8,10 +8,11 @@ int32_t main() {
     cin.tie(0), ios::sync_with_stdio(0);
     int N;
     cin >> N;
-    vector<array<int, 2>> a(N);
-    for (int i = 0; i < N; i++) cin >> a[i][1];
-    for (int i = 0; i < N; i++) cin >> a[i][0];
-    sort(a.begin(), a.end());
+    vector<array<int, 2>> a(N + 1);
+    for (int i = 1; i <= N; i++) cin >> a[i][1];
+    for (int i = 1; i <= N; i++) cin >> a[i][0];
+    sort(a.begin() + 1, a.end());
 
-    vector<array<int, 2>> dp(N, INF);
+    vector<array<int, 2>> dp(N + 1, {INF, INF});
+    dp[0] = {0, 0};
 }
