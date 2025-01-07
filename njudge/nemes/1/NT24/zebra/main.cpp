f80cfe7ea3c132ce919fb88e9cdb93990838a5d8
magyarakoos <magyar.kende.22f@gmail.com>
2025-01-07 16:50:01 +0100

    sync: 2025-01-07 16:50:01

--- njudge/nemes/1/NT24/zebra/main.cpp
+++ njudge/nemes/1/NT24/zebra/main.cpp
@@ -6,12 +6,19 @@ const int INF = 1e9;
 
 int32_t main() {
     cin.tie(0), ios::sync_with_stdio(0);
+
     int N;
     cin >> N;
-    vector<int> a, b;
+
+    vector<array<int, 2>> a(N + 1);
     for (int i = 1; i <= N; i++) cin >> a[i][1];
     for (int i = 1; i <= N; i++) cin >> a[i][0];
     sort(a.begin() + 1, a.end());
+
+    vector<int> sideA, sideB;
+    for (int i = 1; i <= N; i++) {
+        (a[i][1] ? sideB : sideA).push_back(a[i][0]);
+    }
 
     vector dp(N + 1, {INF, INF});
     dp[0] = {0, 0};
