e923cca2a43d94f5825d54d5c6e706834c106ea4
magyarakoos <magyar.kende.22f@gmail.com>
2025-01-07 17:21:01 +0100

    sync: 2025-01-07 17:21:01

--- njudge/nemes/2-3/NT23/mi-broker/main.cpp
+++ njudge/nemes/2-3/NT23/mi-broker/main.cpp
@@ -2,6 +2,21 @@
 using namespace std;
 #define int long long
 
+const int MAXN = 500;
+
 int32_t main() {
     cin.tie(0), ios::sync_with_stdio(0);
+
+    int N, Q;
+    cin >> N >> Q;
+
+    vector<int> a(N);
+    for (int& x : a) cin >> x;
+
+    // transition: maximal profit where dp[i][j] (i < j)
+    // means a pair (V, E)
+    vector dp(MAXN + 1, vector<int>(MAXN + 1));
+    for (int V = 1; V <= 500; V++) {
+        for (int E = V + 1; E <= 500; E++) {}
+    }
 }
