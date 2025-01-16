0d631ce95b4c9db468854aa7ccd65b880d7dd3fe
magyarakoos <magyar.kende.22f@gmail.com>
2025-01-16 08:29:01 +0100

    sync: 2025-01-16 08:29:01

--- codeforces/gym/550995/H/main.cpp
+++ codeforces/gym/550995/H/main.cpp
@@ -1,11 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+map<array<char, 2>, int> adj;
+map<int, int> cache;
+
+int dp(int mask) {}
+
 int main() {
     int N, M;
     string s;
     cin >> N >> M >> s;
 
-    map<array<char, 2>, int> adj;
     for (int i = 1; i < N; i++) adj[{s[i - 1], s[i]}]++;
 }
