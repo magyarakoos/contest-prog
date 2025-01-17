f89a4166fac9f8d6f889778a254c86263a065082
magyarakoos <magyar.kende.22f@gmail.com>
2025-01-17 22:24:00 +0100

    sync: 2025-01-17 22:24:00

--- codeforces/gym/550995/I/main.cpp
+++ codeforces/gym/550995/I/main.cpp
@@ -15,26 +15,16 @@ vector<int> create(int n) {
     return a;
 }
 
-int a[MAXN], b[MAXK], bp = 0;
-int main() {
+void io(const vector<int>& primes) {
     cin.tie(0);
     ios::sync_with_stdio(0);
-    a[0] = a[1] = 1;
-    int result = 0;
-    for (int i = 2; i < MAXN; i++) {
-        if (!a[i]) {
-            for (int j = 2 * i; j < MAXN; j += i) {
-                a[j] = 1;
-            }
-            b[bp++] = i;
-        }
-    }
-    solve();
     int Q;
     cin >> Q;
     while (Q--) {
         int K;
         cin >> K;
-        cout << b[K - 1] << "\n";
+        cout << primes[K - 1] << "\n";
     }
 }
+
+int main() {}
