14b1b5bc9c18d366bdd61177ab38a4541e0f8240
magyarakoos <magyar.kende.22f@gmail.com>
2025-01-17 19:20:00 +0100

    sync: 2025-01-17 19:20:00

--- codeforces/gym/550995/I/main.cpp
+++ codeforces/gym/550995/I/main.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 using namespace std;
 
-inline constexpr int MAXN = 7368790, MAXK = 5e6;
+inline constexpr int MAXN = 7368790, MAXK = 5e5;
 
 int a[MAXN], b[MAXK], bp = 0;
 int main() {
@@ -17,11 +17,14 @@ int main() {
             b[bp++] = i;
         }
     }
-    int Q;
-    cin >> Q;
-    while (Q--) {
-        int K;
-        cin >> K;
-        cout << b[K - 1] << "\n";
+    for (int i = 0; i < MAXK; i++) {
+        cout << b[i] << ",\n";
     }
+    // int Q;
+    // cin >> Q;
+    // while (Q--) {
+    //     int K;
+    //     cin >> K;
+    //     cout << b[K - 1] << "\n";
+    // }
 }
