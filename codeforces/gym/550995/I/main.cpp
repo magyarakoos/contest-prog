8c14696f8ea3692e10fc9d624f0d975e45b125f2
magyarakoos <magyar.kende.22f@gmail.com>
2025-01-17 22:29:00 +0100

    sync: 2025-01-17 22:29:00

--- codeforces/gym/550995/I/main.cpp
+++ codeforces/gym/550995/I/main.cpp
@@ -1,33 +1,28 @@
 #include <iostream>
-#include <vector>
 using namespace std;
 
-inline constexpr int MAXN = 7368790, MAXK = 5e5;
+inline constexpr int MAXN = 7368790, MAXK = 5e6;
 
-vector<int> create(int n) {
-    vector<int> a(n, 1);
-    a[0] = a[1] = 0;
-    for (int i = 2; i < n; i++) {
-        if (a[i]) {
-            for (int j = 2 * i; j < n; j += i) a[j] = 0;
-        }
-    }
-    return a;
-}
-
-void io(const vector<int>& primes) {
+int a[MAXN], b[MAXK], bp = 0;
+int main() {
     cin.tie(0);
     ios::sync_with_stdio(0);
+
+    a[0] = a[1] = 1;
+    for (int i = 2; i < MAXN; i++) {
+        if (!a[i]) {
+            for (int j = 2 * i; j < MAXN; j += i) {
+                a[j] = 1;
+            }
+            b[bp++] = i;
+        }
+    }
+
     int Q;
     cin >> Q;
     while (Q--) {
         int K;
         cin >> K;
-        cout << primes[K - 1] << "\n";
+        cout << b[K - 1] << "\n";
     }
-}
-
-int main() {
-    const int stage_a = 1e4;
-    vector<int> a = create(stage_a);
 }
