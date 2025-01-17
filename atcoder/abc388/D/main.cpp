6eb3558e1bbaff37f947e2787bb6caccc373b764
magyarakoos <magyar.kende.22f@gmail.com>
2025-01-17 17:03:01 +0100

    sync: 2025-01-17 17:03:01

--- atcoder/abc388/D/main.cpp
+++ atcoder/abc388/D/main.cpp
@@ -4,6 +4,14 @@ using namespace std;
 int main() {
     int N;
     cin >> N;
-    vector<int> A(N), adultS;
+    vector<int> A(N);
     for (int& x : A) cin >> x;
+
+    multiset<int> adultS;
+    for (int i = 0; i < N; i++) {
+        adultS.insert(A[i]);
+        while (!adultS.empty() && *adultS.begin() < i + 1) {
+            adultS.erase(adultS.begin());
+        }
+    }
 }
