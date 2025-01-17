d17fc9464c682de6dc0f2ccfd6e3ec87a84d70e9
magyarakoos <magyar.kende.22f@gmail.com>
2025-01-17 18:40:00 +0100

    sync: 2025-01-17 18:40:00

--- codeforces/gym/550995/I/main.cpp
+++ codeforces/gym/550995/I/main.cpp
@@ -4,4 +4,13 @@ using namespace std;
 const int MAXN = 1e8;
 
 int a[MAXN] = {1};
-int main() {}
+int main() {
+    a[0] = a[1] = 0;
+    for (int i = 2; i < MAXN; i++) {
+        if (a[i]) {
+            for (int j = 2 * i; j < MAXN; j += i) {
+                a[i] = 0;
+            }
+        }
+    }
+}
