242c4494e12e353ec72dc01515d9d40aeb04667b
magyarakoos <magyar.kende.22f@gmail.com>
2025-01-17 19:15:01 +0100

    sync: 2025-01-17 19:15:01

--- codeforces/gym/550995/I/main.cpp
+++ codeforces/gym/550995/I/main.cpp
@@ -7,14 +7,16 @@ inline constexpr int MAXN = 7368790;
 int a[MAXN];
 int main() {
     a[0] = a[1] = 1;
+    int result = 0;
     for (int i = 2; i < MAXN; i++) {
         if (!a[i]) {
             for (int j = 2 * i; j < MAXN; j += i) {
                 a[j] = 1;
             }
+            result++;
         }
     }
-    int result = 0;
-    for (int i = 2; i < MAXN; i++) { result += !a[i]; }
+    // int result = 0;
+    // for (int i = 2; i < MAXN; i++) { result += !a[i]; }
     cout << result << "\n";
 }
