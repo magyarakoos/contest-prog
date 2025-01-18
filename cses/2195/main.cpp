1aec3183107ea272a21042c800729ef32dbad196
magyarakoos <magyar.kende.22f@gmail.com>
2025-01-18 13:09:00 +0100

    sync: 2025-01-18 13:09:00

--- cses/2195/main.cpp
+++ cses/2195/main.cpp
@@ -19,33 +19,22 @@ bool kozte(point A, point B, point C) {
 int main() {
     cin.tie(0), ios::sync_with_stdio(0);
 
-    Point base;
     int N;
     cin >> N;
 
-    vector<Point> ptS(N);
-    base = {INT_MAX, INT_MAX};
-
-    for (int i = 0; i < N; i++) {
-        cin >> ptS[i].x >> ptS[i].y;
-
-        if (ptS[i].x < base.x) {
-            base = ptS[i];
-        } else if (base.x == ptS[i].x &&
-                   ptS[i].y < base.y) {
-            base.y = ptS[i].y;
-        }
-    }
+    vector<point> ptS(N);
+    for (auto& [x, y] : ptS) cin >> x >> y;
 
     sort(ptS.begin(), ptS.end());
-    sort(ptS.begin() + 1, ptS.end(), [&](pointa, pointb) {
-        if (fordul(base, a, b) == 0 && !kozte(base, a, b)) {
+    sort(ptS.begin() + 1, ptS.end(), [&](point a, point b) {
+        if (fordul(ptS[0], a, b) == 0 &&
+            !kozte(ptS[0], a, b)) {
             return true;
         }
-        return fordul(base, a, b) == 1;
+        return fordul(ptS[0], a, b) == 1;
     });
 
-    vector<Point> result({base});
+    vector<point> result({ptS[0]});
     int K = 0;
 
     for (int i = 1; i < N; i++) {
@@ -59,12 +48,13 @@ int main() {
     }
 
     for (int i = N - 2; i >= 1; i--) {
-        if (!fordul(base, ptS.back(), ptS[i])) {
+        if (!fordul(ptS[0], ptS.back(), ptS[i])) {
             result.push_back(ptS[i]);
         } else
             break;
     }
 
     cout << result.size() << "\n";
-    for (pointp : result) cout << p.x << " " << p.y << "\n";
+    for (auto [x, y] : result)
+        cout << x << " " << y << "\n";
 }
