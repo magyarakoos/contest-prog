3fc7e204d55581c8e771ea4a4e978d25e340339d
magyarakoos <magyar.kende.22f@gmail.com>
2025-01-18 10:17:00 +0100

    sync: 2025-01-18 10:17:00

--- cses/2195/main.cpp
+++ cses/2195/main.cpp
@@ -1,6 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+bool polar(array<int, 2> a, array<int, 2> b) {
+    return a < b;
+}
+
 int main() {
     cin.tie(0);
     ios::sync_with_stdio(0);
@@ -8,5 +12,8 @@ int main() {
     cin >> N;
     vector<array<int, 2>> a(N);
     for (auto& [x, y] : a) cin >> x >> y;
-    sort(a.begin(), a.end());
+    sort(a.begin(), a.end(),
+         [](array<int, 2> a, array<int, 2> b) {
+             return a < b;
+         });
 }
