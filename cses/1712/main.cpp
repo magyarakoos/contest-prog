e9f26d8c5235e0ae478fcbee37c9e88cc6bfdfb0
magyarakoos <magyar.kende.22f@gmail.com>
2025-01-18 15:34:01 +0100

    sync: 2025-01-18 15:34:01

--- cses/1712/main.cpp
+++ cses/1712/main.cpp
@@ -1,15 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define int int64_t
+using ll = int64_t;
 
-int mpow(int a, int b, int mod) {
+ll mpow(ll a, ll b, ll mod) {
     if (!b) return 1;
     return mpow(a * a % mod, b / 2, mod) * (b & 1 ? a : 1) %
            mod;
 }
 
-int32_t main() {
-    int Q, A, B, C;
+int main() {
+    ll Q, A, B, C;
     cin >> Q;
     while (Q--) {
         cin >> A >> B >> C;
