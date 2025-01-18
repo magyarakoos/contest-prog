e1f45910d1c621dbea23aed8a2850da072112c5a
magyarakoos <magyar.kende.22f@gmail.com>
2025-01-18 15:23:00 +0100

    sync: 2025-01-18 15:23:00

--- cses/1712/main.cpp
+++ cses/1712/main.cpp
@@ -1,6 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int mpow(int a, int b, int mod) {
+    return (!a || !b
+                ? mpow(a * 2, b / 2, mod) * (b & 1 ? a : 1)
+                : 1);
+}
+
 int main() {
     int Q, A, B, C;
     cin >> Q;
