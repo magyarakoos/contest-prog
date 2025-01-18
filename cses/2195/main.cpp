a8b30aab0453d22e9f12208bec9ca370565a679e
magyarakoos <magyar.kende.22f@gmail.com>
2025-01-18 11:01:00 +0100

    sync: 2025-01-18 11:01:00

--- cses/2195/main.cpp
+++ cses/2195/main.cpp
@@ -3,7 +3,8 @@ using namespace std;
 using point = array<int64_t, 2>;
 
 int turn(point a, point b, point c) {
-    int p = 0;
+    int p = (b[0] - a[0]) * (c[1] - a[1]) -
+            (b[1] - a[1]) * (c[0] - a[0]);
     return (p > 0) - (p < 0);
 }
 
