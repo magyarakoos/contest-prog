02dfa2f8fd122a7c35e19aa2cbd95eeecfd32ef8
magyarakoos <magyar.kende.22f@gmail.com>
2025-01-19 14:08:00 +0100

    sync: 2025-01-19 14:08:00

--- cses/1713/main.cpp
+++ cses/1713/main.cpp
@@ -1,20 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int d(int n) {
-    int result = 0;
-    for (int i = 1; i * i <= n; i++) {
-        if (n % i == 0) { result += 1 + (i * i != n); }
-    }
-    return result;
-}
-
 int main() {
     cin.tie(0), ios::sync_with_stdio(0);
     int N, X;
     cin >> N;
     while (N--) {
         cin >> X;
-        cout << d(X) << "\n";
+        int d = 0;
+        for (int i = 1; i * i <= X; i++) {
+            if (X % i == 0) d += 1 + (i * i != X);
+        }
+        cout << d << "\n";
     }
 }
