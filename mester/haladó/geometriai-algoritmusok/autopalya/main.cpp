a863be0fe64dbd9cbb6835dd2706bf2d3c205853
magyarakoos <magyar.kende.22f@gmail.com>
2025-01-19 10:46:01 +0100

    sync: 2025-01-19 10:46:01

--- mester/haladó/geometriai-algoritmusok/autopalya/main.cpp
+++ mester/haladó/geometriai-algoritmusok/autopalya/main.cpp
@@ -1,7 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define int int64_t
+using point = array<int, 2>;
 
 int32_t main() {
     cin.tie(0), ios::sync_with_stdio(0);
+
+    auto read = []() {
+        int N;
+        cin >> N;
+        vector<pair<point, int>> a(N);
+        for (int i = 0; i < N; i++) {
+            cin >> a[i].first[0] >> a[i].first[1];
+            a[i].second = i;
+        }
+        return a;
+    };
+    auto A = read(), B = read();
 }
