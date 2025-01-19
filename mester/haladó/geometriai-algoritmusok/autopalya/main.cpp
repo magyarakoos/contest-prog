1547283c9e0c15985dba5a51c4b081367590d9f9
magyarakoos <magyar.kende.22f@gmail.com>
2025-01-19 10:52:01 +0100

    sync: 2025-01-19 10:52:01

--- mester/haladó/geometriai-algoritmusok/autopalya/main.cpp
+++ mester/haladó/geometriai-algoritmusok/autopalya/main.cpp
@@ -8,18 +8,22 @@ using node = array<point, 2>;
 int32_t main() {
     cin.tie(0), ios::sync_with_stdio(0);
 
-    vector<node> A(2e5);
+    vector<node> P(2e5);
     int N;
     cin >> N;
     for (int i = 0; i < N; i++) {
-        cin >> A[i][0][0] >> A[i][0][1];
-        A[i][1] = {0, i};
+        cin >> P[i][0][0] >> P[i][0][1];
+        P[i][1] = {0, i};
     }
     int M;
     cin >> M;
     for (int i = 0; i < M; i++) {
-        cin >> A[i + N][0][0] >> A[i + N][0][1];
-        A[i + N][1] = {1, i - N};
+        cin >> P[i + N][0][0] >> P[i + N][0][1];
+        P[i + N][1] = {1, i - N};
     }
-    A.resize(N + M);
+    P.resize(N + M);
+
+    sort(P.begin(), P.end());
+    sort(P.begin() + 1, P.end(),
+         [&](auto A, auto B) { return 0; });
 }
