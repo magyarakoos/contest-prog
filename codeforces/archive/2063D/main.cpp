6045dc2573bcb3d7d4e1c1392d6128640c244da1
magyarakoos <magyar.kende.22f@gmail.com>
2025-01-29 11:44:01 +0100

    sync: 2025-01-29 11:44:01

--- codeforces/archive/2063D/main.cpp
+++ codeforces/archive/2063D/main.cpp
@@ -2,7 +2,17 @@
 using namespace std;
 #define int int64_t
 
-int32_t main() {
-    cin.tie(0), ios::sync_with_stdio(0);
+void solve() {
+    int N, M;
+    cin >> N >> M;
+    vector<int> A(N), B(M);
+    for (int& x : A) cin >> x;
+    for (int& x : B) cin >> x;
 }
 
+int32_t main() {
+    cin.tie(0), ios::sync_with_stdio(0);
+    int T;
+    cin >> T;
+    while (T--) solve();
+}
