da17e9f5510b87a55e5eb92e94ae59362c15f212
magyarakoos <magyar.kende.22f@gmail.com>
2025-01-29 11:51:00 +0100

    sync: 2025-01-29 11:51:00

--- codeforces/archive/2063D/main.cpp
+++ codeforces/archive/2063D/main.cpp
@@ -11,7 +11,11 @@ void solve() {
 
     vector<int> result(1);
     int al = 0, ar = N - 1, bl = 0, br = M - 1;
-    while (1) { int la = ar - al + 1, lb = br - bl + 1; }
+    while (1) {
+        int la = ar - al + 1, lb = br - bl + 1;
+        bool can_a = la >= 2 && lb >= 1,
+             can_b = lb >= 2 && la >= 1;
+    }
 }
 
 int32_t main() {
