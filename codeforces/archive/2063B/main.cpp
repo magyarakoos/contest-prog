bcd20089512ab76658c90c7844b1ebce6abc984f
magyarakoos <magyar.kende.22f@gmail.com>
2025-01-29 08:01:01 +0100

    sync: 2025-01-29 08:01:00

--- codeforces/archive/2063B/main.cpp
+++ codeforces/archive/2063B/main.cpp
@@ -4,4 +4,29 @@ using namespace std;
 
 int32_t main() {
     cin.tie(0), ios::sync_with_stdio(0);
+    int T;
+    cin >> T;
+    while (T--) {
+        int N, L, R;
+        cin >> N >> L >> R;
+        vector<int> a, b, c;
+        for (int i = 0; i < L; i++) {
+            int x;
+            cin >> x;
+            a.push_back(x);
+        }
+        for (int i = 0; i < R - L + 1; i++) {
+            int x;
+            cin >> x;
+            b.push_back(x);
+        }
+        for (int i = R; i < N; i++) {
+            int x;
+            cin >> x;
+            c.push_back(x);
+        }
+        sort(a.begin(), a.end());
+        sort(b.rbegin(), b.rend());
+        sort(c.begin(), c.end());
+        for (int i = 0; i <) }
 }
