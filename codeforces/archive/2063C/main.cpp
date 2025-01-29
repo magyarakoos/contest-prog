35093d8c3132910f94a5a1d5d6924cd05c892808
magyarakoos <magyar.kende.22f@gmail.com>
2025-01-29 08:17:01 +0100

    sync: 2025-01-29 08:17:01

--- codeforces/archive/2063C/main.cpp
+++ codeforces/archive/2063C/main.cpp
@@ -2,7 +2,24 @@
 using namespace std;
 #define int int64_t
 
-int32_t main() {
-    cin.tie(0), ios::sync_with_stdio(0);
+void solve() {
+    int N;
+    cin >> N;
+    vector<int> degS(N + 1);
+    vector<vector<int>> g(N + 1);
+    for (int i = 0; i < N - 1; i++) {
+        int U, V;
+        cin >> U >> V;
+        degS[U]++;
+        degS[V]++;
+        g[U].push_back(V);
+        g[V].push_back(U);
+    }
 }
 
+int32_t main() {
+    cin.tie(0), ios::sync_with_stdio(0);
+    int T;
+    cin >> T;
+    while (T--) solve();
+}
