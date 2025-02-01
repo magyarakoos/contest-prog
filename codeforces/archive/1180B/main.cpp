bcabc3ec1e48ed858cb8fa6e5c9e34f731e11278
magyarakoos <ninjaoffical08@gmail.com>
2025-02-01 15:23:01 +0100

    sync: 2025-02-01 15:23:01

--- codeforces/archive/1180B/main.cpp
+++ codeforces/archive/1180B/main.cpp
@@ -4,5 +4,17 @@ using namespace std;
 
 int32_t main() {
     cin.tie(0), ios::sync_with_stdio(0);
+    int N;
+    cin >> N;
+    vector<int> posS, negS;
+    for (int i = 0; i < N; i++) {
+        int x;
+        cin >> x;
+        if (x > 0)
+            posS.push_back(x);
+        else
+            negS.push_back(x);
+    }
+    sort(posS.begin(), posS.end());
+    sort(negS.begin(), negS.end());
 }
-
