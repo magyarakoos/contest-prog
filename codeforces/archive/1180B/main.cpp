9233642c4818e50ce25f48c954b1e5d857124699
magyarakoos <ninjaoffical08@gmail.com>
2025-02-01 15:27:01 +0100

    sync: 2025-02-01 15:27:01

--- codeforces/archive/1180B/main.cpp
+++ codeforces/archive/1180B/main.cpp
@@ -6,15 +6,33 @@ int32_t main() {
     cin.tie(0), ios::sync_with_stdio(0);
     int N;
     cin >> N;
-    vector<int> posS, negS;
+    vector<array<int, 2>> posS, negS;
     for (int i = 0; i < N; i++) {
         int x;
         cin >> x;
         if (x > 0)
-            posS.push_back(x);
+            posS.push_back({x, i});
         else
-            negS.push_back(x);
+            negS.push_back({x, i});
     }
     sort(posS.begin(), posS.end());
     sort(negS.begin(), negS.end());
+    if (negS.size() % 2 == 1) {
+        if (posS.empty()) {
+
+            negS[0][0] = -negS[0][0] - 1;
+        } else {
+            posS.back()[0] = -posS.back()[0] - 1;
+            for (int i = posS.size() - 1; i > 0; i -= 2) {
+                posS[i][0] = -posS[i][0] - 1;
+                posS[i - 1][0] = -posS[i - 1][0] - 1;
+            }
+        }
+    } else {
+
+        for (int i = posS.size() - 2; i > 0; i -= 2) {
+            posS[i][0] = -posS[i][0] - 1;
+            posS[i - 1][0] = -posS[i - 1][0] - 1;
+        }
+    }
 }
