25cf3cc1645a524d7ba8e398cec4dbe027fe8d6f
magyarakoos <magyar.kende.22f@gmail.com>
2025-02-03 11:37:01 +0100

    sync: 2025-02-03 11:37:01

--- codeforces/contest/2059/C/main.cpp
+++ codeforces/contest/2059/C/main.cpp
@@ -9,6 +9,12 @@ void solve() {
     for (auto& row : A) {
         for (int& x : row) cin >> x;
     }
+    multiset<array<int, 2>> ms;
+    for (int i = 0; i < N; i++) {
+        int suf = 0;
+        ms.insert({0, i});
+        for (int j = N - 1; j >= 0; j--) {}
+    }
 }
 
 int32_t main() {
