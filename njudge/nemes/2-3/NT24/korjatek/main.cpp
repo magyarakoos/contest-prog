5710f4ff81e7438dfe41e7e1ca0d34a79e770121
magyarakoos <magyar.kende.22f@gmail.com>
2025-02-04 12:35:01 +0100

    sync: 2025-02-04 12:35:01

--- njudge/nemes/2-3/NT24/korjatek/main.cpp
+++ njudge/nemes/2-3/NT24/korjatek/main.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 #define int int64_t
 
-const int INF = 1e12;
+const int INF = 2e5;
 
 int32_t main() {
     cin.tie(0), ios::sync_with_stdio(0);
@@ -12,4 +12,16 @@ int32_t main() {
     for (int& x : P) cin >> x;
     P.insert(P.end(), P.begin(), P.end());
     P.push_back(INF);
+
+    vector<int> ng(2 * N + 1);
+    for (int i = 2 * N - 1; i >= 0; i--) {
+        ng[i] = ng[i + 1];
+        while (P[i] > P[ng[i]]) { ng[i] = ng[ng[i]]; }
+    }
+    for (int i = 0; i < 2 * N; i++) { cout << P[i] << " "; }
+    cout << "\n";
+    for (int i = 0; i < 2 * N; i++) {
+        cout << ng[i] << " ";
+    }
+    cout << "\n";
 }
