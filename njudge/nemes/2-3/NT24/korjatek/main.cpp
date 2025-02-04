7aedf924198f84d7023ff4d9ab249354593f8b1e
magyarakoos <magyar.kende.22f@gmail.com>
2025-02-04 13:05:00 +0100

    sync: 2025-02-04 13:05:00

--- njudge/nemes/2-3/NT24/korjatek/main.cpp
+++ njudge/nemes/2-3/NT24/korjatek/main.cpp
@@ -22,12 +22,13 @@ int32_t main() {
 
     vector<int> result(N);
     for (int j = N - 1; j >= 0; j--) {
+        swap(pos[j], pos[N - 1]);
         int curr = 0;
         for (int i = 0; i < N - 1; i++) {
             curr += d(pos[i], pos[i + 1]);
         }
         result[opos[j]] = curr;
-        swap(j, N - 1);
+        swap(pos[j], pos[N - 1]);
     }
 
     for (int x : result) cout << x << " ";
