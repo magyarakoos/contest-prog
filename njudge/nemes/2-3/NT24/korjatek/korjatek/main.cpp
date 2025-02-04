2a7137df7bbd83987b5ce21e5535dbbde47b00af
magyarakoos <magyar.kende.22f@gmail.com>
2025-02-04 12:53:00 +0100

    sync: 2025-02-04 12:53:00

--- njudge/nemes/2-3/NT24/korjatek/korjatek/main.cpp
+++ njudge/nemes/2-3/NT24/korjatek/korjatek/main.cpp
@@ -4,4 +4,11 @@ using namespace std;
 
 int32_t main() {
     cin.tie(0), ios::sync_with_stdio(0);
+    int N;
+    cin >> N;
+    vector<int> P(N), pos(N);
+    for (int i = 0; i < N; i++) {
+        cin >> P[i];
+        pos[--P[i]] = i;
+    }
 }
