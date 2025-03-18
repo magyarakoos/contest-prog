fa774cfec681230ca88e23769608f10cce51338b
magyarakoos <magyar.kende.22f@gmail.com>
2025-03-18 14:30:00 +0100

    sync: 2025-03-18 14:30:00

--- codeforces/gym/582252/A/main.py
+++ codeforces/gym/582252/A/main.py
@@ -1,5 +1,7 @@
 for _ in range(int(input())):
     a, b = map(int, input().split())
     ans = 0
-    while a > 0:
+    while 1:
         a, b = min(a, b), max(a, b)
+        ans += b / a
+        b -= b / a
