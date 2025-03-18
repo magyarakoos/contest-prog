506166df2b98bb2cb65e65f562abba7e41daac0f
magyarakoos <magyar.kende.22f@gmail.com>
2025-03-18 14:53:01 +0100

    sync: 2025-03-18 14:53:01

--- codeforces/gym/582252/B/main.rs
+++ codeforces/gym/582252/B/main.rs
@@ -2,12 +2,10 @@ use std::convert::TryInto;
 use std::io;
 
 fn main() {
-    let mut line = String::new();
-    io::stdin().read_line(&mut line).unwrap();
-    let (a, b, c): (i32, i32, i32) = line
+    let mut nums = String::new();
+    io::stdin().read_line(&mut nums).unwrap();
+    let nums = nums
         .split_whitespace()
         .map(|x| x.parse::<i32>().unwrap())
-        .collect::<Vec<_>>()
-        .try_into()
-        .unwrap();
+        .collect();
 }
