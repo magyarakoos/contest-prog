bafb0a6b907660cd983a8cba6887c2488ccc01b2
magyarakoos <magyar.kende.22f@gmail.com>
2025-03-18 14:52:05 +0100

    sync: 2025-03-18 14:52:05

--- codeforces/gym/582252/B/main.rs
+++ codeforces/gym/582252/B/main.rs
@@ -1,9 +1,10 @@
+use std::convert::TryInto;
 use std::io;
 
 fn main() {
     let mut line = String::new();
     io::stdin().read_line(&mut line).unwrap();
-    let (a, b, c): (i32, i32, i32) = input
+    let (a, b, c): (i32, i32, i32) = line
         .split_whitespace()
         .map(|x| x.parse::<i32>().unwrap())
         .collect::<Vec<_>>()
