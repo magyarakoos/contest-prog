1542542d884321bc693bae1a84d0f39052aeaa11
magyarakoos <magyar.kende.22f@gmail.com>
2025-03-18 14:54:01 +0100

    sync: 2025-03-18 14:54:01

--- codeforces/gym/582252/B/main.rs
+++ codeforces/gym/582252/B/main.rs
@@ -4,8 +4,10 @@ use std::io;
 fn main() {
     let mut nums = String::new();
     io::stdin().read_line(&mut nums).unwrap();
-    let nums = nums
+    let nums: Vec<i32> = nums
         .split_whitespace()
         .map(|x| x.parse::<i32>().unwrap())
         .collect();
+
+    let (a, b, n) = (nums[0], nums[1], nums[2]);
 }
