35798f73eb3b0e118c75c10defd49c9eef966cdd
magyarakoos <magyar.kende.22f@gmail.com>
2025-03-18 14:42:00 +0100

    sync: 2025-03-18 14:42:00

--- codeforces/gym/582252/B/main.py
+++ codeforces/gym/582252/B/main.py
@@ -3,7 +3,7 @@ a, b, n = map(int, input().split())
 
 
 def is_good(i):
-    return all([c == a + '0' or c == b + '0' for c in str(i)])
+    return all([int(c) == a or int(c) == b for c in str(i)])
 
 
 good_numbers = []
@@ -12,4 +12,4 @@ for i in range(1, max(a, b) * n + 1):
     if is_good(i):
         good_numbers.append(i)
 
-print(*good_numbers)
+print(len(good_numbers))
