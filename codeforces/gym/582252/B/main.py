272e9c4d36cf510a992c4525d89e93c120caf1aa
magyarakoos <magyar.kende.22f@gmail.com>
2025-03-18 14:41:01 +0100

    sync: 2025-03-18 14:41:01

--- codeforces/gym/582252/B/main.py
+++ codeforces/gym/582252/B/main.py
@@ -1,6 +1,15 @@
 MOD = 10**9 + 7
 a, b, n = map(int, input().split())
+
+
+def is_good(i):
+    return all([c == a + '0' or c == b + '0' for c in str(i)])
+
+
 good_numbers = []
+
 for i in range(1, max(a, b) * n + 1):
     if is_good(i):
         good_numbers.append(i)
+
+print(*good_numbers)
