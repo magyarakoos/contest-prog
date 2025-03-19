57940012e3e21afd8402f7496082ab47844bbf64
magyarakoos <magyar.kende.22f@gmail.com>
2025-03-19 13:38:00 +0100

    sync: 2025-03-19 13:38:00

--- codeforces/gym/582252/B/main.py
+++ codeforces/gym/582252/B/main.py
@@ -1,4 +1,10 @@
 MOD = 10**9 + 7
 a, b, n = map(int, input().split())
 
+
+def is_good(n):
+    return all([int(c) in [a, b] for c in str(n)])
+
+
 for i in range(max(a, b) * n + 1):
+    if is_good(i):
