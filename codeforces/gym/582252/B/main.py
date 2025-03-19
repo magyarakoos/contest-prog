1727dc994618430b8e11188287e504359f87afb6
magyarakoos <magyar.kende.22f@gmail.com>
2025-03-19 13:53:00 +0100

    sync: 2025-03-19 13:53:00

--- codeforces/gym/582252/B/main.py
+++ codeforces/gym/582252/B/main.py
@@ -1,14 +1,10 @@
 MOD, fact, ans = 10**9 + 7, [1], 0
 a, b, n = map(int, input().split())
-
-
 for i in range(100):
     fact.append(fact[-1] * (i + 1) % MOD)
 
-
 def choose(n, k):
     return fact[n] * pow(fact[k] * fact[n - k] % MOD, MOD - 2, MOD) % MOD
-
 
 for i in range(max(a, b) * n + 1):
     if all([int(c) in [a, b] for c in str(i)]):
