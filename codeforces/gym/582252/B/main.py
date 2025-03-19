b1da1e14757623d0437277bafd47b989ae007746
magyarakoos <magyar.kende.22f@gmail.com>
2025-03-19 13:39:00 +0100

    sync: 2025-03-19 13:39:00

--- codeforces/gym/582252/B/main.py
+++ codeforces/gym/582252/B/main.py
@@ -6,5 +6,10 @@ def is_good(n):
     return all([int(c) in [a, b] for c in str(n)])
 
 
+def inv(n):
+    return pow(n, MOD - 2, MOD)
+
+
 for i in range(max(a, b) * n + 1):
     if is_good(i):
+        print(i)
