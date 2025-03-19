7ba2d2c01d39a449a2b5b75c3e945609829f98e8
magyarakoos <magyar.kende.22f@gmail.com>
2025-03-19 13:41:01 +0100

    sync: 2025-03-19 13:41:01

--- codeforces/gym/582252/B/main.py
+++ codeforces/gym/582252/B/main.py
@@ -1,6 +1,12 @@
 MOD = 10**9 + 7
 a, b, n = map(int, input().split())
 
+fact = [1]
+for i in range(n):
+    fact.append(fact[-1] * (i + 1) % MOD)
+
+print(fact)
+
 
 def is_good(n):
     return all([int(c) in [a, b] for c in str(n)])
