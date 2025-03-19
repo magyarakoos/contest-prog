5d9318f41e251f117a5ebf4e39172535d6cbe83a
magyarakoos <magyar.kende.22f@gmail.com>
2025-03-19 13:51:02 +0100

    sync: 2025-03-19 13:51:02

--- codeforces/gym/582252/B/main.py
+++ codeforces/gym/582252/B/main.py
@@ -10,8 +10,10 @@ def choose(n, k):
     return fact[n] * pow(fact[k] * fact[n - k] % MOD, MOD - 2, MOD) % MOD
 
 
+ans = 0
 for i in range(max(a, b) * n + 1):
     if all([int(c) in [a, b] for c in str(i)]):
         for j in range(0, n + 1):
             if j * a + (n - j) * b == i:
-                print(j, (n - j))
+                (ans += choose(n, j)) %= MOD
+print(ans)
