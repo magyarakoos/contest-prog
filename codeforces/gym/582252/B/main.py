c4b2a2de938d0e4e39de138ef85c38a8653e800e
magyarakoos <magyar.kende.22f@gmail.com>
2025-03-19 13:50:02 +0100

    sync: 2025-03-19 13:50:02

--- codeforces/gym/582252/B/main.py
+++ codeforces/gym/582252/B/main.py
@@ -6,14 +6,12 @@ for i in range(100):
     fact.append(fact[-1] * (i + 1) % MOD)
 
 
-def is_good(n):
-    return all([int(c) in [a, b] for c in str(n)])
-
-
 def choose(n, k):
     return fact[n] * pow(fact[k] * fact[n - k] % MOD, MOD - 2, MOD) % MOD
 
 
 for i in range(max(a, b) * n + 1):
-    if is_good(i):
-        print(i)
+    if all([int(c) in [a, b] for c in str(i)]):
+        for j in range(0, n + 1):
+            if j * a + (n - j) * b == i:
+                print(j, (n - j))
