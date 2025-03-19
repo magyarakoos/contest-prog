356909b41865286581a4754a9e07cb0b1bc27335
magyarakoos <magyar.kende.22f@gmail.com>
2025-03-19 13:34:00 +0100

    sync: 2025-03-19 13:34:00

--- codeforces/gym/582252/B/main.rs
+++ codeforces/gym/582252/B/main.rs
@@ -2,6 +2,17 @@ use std::io;
 
 const MOD: i32 = 1e9 as i32 + 7;
 
+fn is_good(n: u32, nums: &[u32; 2]) -> bool {
+    for c in n.to_string().chars() {
+        if !nums.contains(&c.to_digit(10).unwrap()) {
+            return false;
+        }
+    }
+    true
+}
+
+fn choose(n: u32, k: u32) {}
+
 fn main() {
     let mut nums = String::new();
     io::stdin().read_line(&mut nums).unwrap();
@@ -10,15 +21,6 @@ fn main() {
         .map(|x| x.parse::<u32>().unwrap())
         .collect();
     let (a, b, n) = (nums[0], nums[1], nums[2]);
-
-    fn is_good(n: u32, nums: &[u32; 2]) -> bool {
-        for c in n.to_string().chars() {
-            if !nums.contains(&c.to_digit(10).unwrap()) {
-                return false;
-            }
-        }
-        true
-    }
 
     for i in 1..=(a.max(b) * n) {
         if is_good(i, &[a, b]) {
