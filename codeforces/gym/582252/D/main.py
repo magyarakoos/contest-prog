569b6a53206034dc4630475c65586deed85ee807
magyarakoos <ninjaoffical08@gmail.com>
2025-03-22 18:50:01 +0100

    sync: 2025-03-22 18:50:01

--- codeforces/gym/582252/D/main.py
+++ codeforces/gym/582252/D/main.py
@@ -4,16 +4,16 @@ for _ in range(int(input())):
     N = int(input())
     a = list(map(int, input().split()))
     used, done = [0] * N, [0] * N
-    pq = [(i, i) for i in range(N)]
+    pq = [(0, i, i) for i in range(N)]
     result = 0
     while len(pq) > 0:
-        i, j = heappop(pq)
+        d, i, j = heappop(pq)
         if done[i]:
             continue
         if not used[j]:
             used[j] = 1
             done[i] = 1
-            result += abs(i - j)
+            result += d
             continue
         heappush(pq, (i, j + 1))
         heappush(pq, (i, j - 1))
