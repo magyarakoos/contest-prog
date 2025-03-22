766abdae56c7c799204aa98d723a1d074357891a
magyarakoos <ninjaoffical08@gmail.com>
2025-03-22 18:11:00 +0100

    sync: 2025-03-22 18:11:00

--- codeforces/gym/582252/C/main.py
+++ codeforces/gym/582252/C/main.py
@@ -4,8 +4,9 @@ f = open("input.txt")
 n = int(f.readline())
 a = []
 for _ in range(n):
-    a.append(["".join(seq) for seq in combinations(f.readline(), 4)])
+    a.append(["".join(seq) for seq in combinations(f.read(), 4)])
+
+print(a)
 
 # f = open("output.txt", 'w')
-print(a)
 
