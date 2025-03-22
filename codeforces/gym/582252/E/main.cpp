3f2e6235e0d3626044034e5ec805efededec5409
magyarakoos <ninjaoffical08@gmail.com>
2025-03-22 21:20:00 +0100

    sync: 2025-03-22 21:20:00

--- codeforces/gym/582252/E/main.cpp
+++ codeforces/gym/582252/E/main.cpp
@@ -6,12 +6,13 @@ using node = array<point, 2>;
 
 // Cross product to determine orientation
 int turn(const point& A, const point& B, const point& C) {
-    return (B[0] - A[0]) * (C[1] - A[1]) - (B[1] - A[1]) * (C[0] - A[0]);
+    return (B[0] - A[0]) * (C[1] - A[1]) -
+           (B[1] - A[1]) * (C[0] - A[0]);
 }
 
 int32_t main() {
     cin.tie(0), ios::sync_with_stdio(0);
-    
+
     // Read polygon A
     int N;
     cin >> N;
@@ -21,7 +22,7 @@ int32_t main() {
         cin >> x >> y;
         P.push_back({{x, y}, {0, i}});
     }
-    
+
     // Read polygon B
     int M;
     cin >> M;
@@ -30,47 +31,54 @@ int32_t main() {
         cin >> x >> y;
         P.push_back({{x, y}, {1, i}});
     }
-    
+
     // Check for duplicate points
     sort(P.begin(), P.end());
     for (int i = 1; i < P.size(); i++) {
-        if (P[i-1][0][0] == P[i][0][0] && P[i-1][0][1] == P[i][0][1]) {
-            // Duplicate point found - one polygon vertex lies on another
+        if (P[i - 1][0][0] == P[i][0][0] &&
+            P[i - 1][0][1] == P[i][0][1]) {
+            // Duplicate point found - one polygon vertex
+            // lies on another
             cout << "NO\n";
             return 0;
         }
     }
-     
+
     // Sort points by polar angle with respect to P[0]
-    sort(P.begin() + 1, P.end(), [&](node A,node B) {
+    sort(P.begin() + 1, P.end(), [&](node A, node B) {
         int t = turn(P[0][0], A[0], B[0]);
         if (t != 0) return t > 0;
         // If collinear, sort by distance
-        int d1 = (A[0][0] - P[0][0])*(A[0][0] - P[0][0]) + (A[0][1] - P[0][1])*(A[0][1] - P[0][1]);
-        int d2 = (B[0][0] - P[0][0])*(B[0][0] - P[0][0]) + (B[0][1] - P[0][1])*(B[0][1] - P[0][1]);
+        int d1 = (A[0][0] - P[0][0]) * (A[0][0] - P[0][0]) +
+                 (A[0][1] - P[0][1]) * (A[0][1] - P[0][1]);
+        int d2 = (B[0][0] - P[0][0]) * (B[0][0] - P[0][0]) +
+                 (B[0][1] - P[0][1]) * (B[0][1] - P[0][1]);
         return d1 < d2;
     });
-    
+
     // Graham scan to find convex hull
     vector<node> hull;
     hull.push_back(P[0]);
     hull.push_back(P[1]);
-    
+
     for (int i = 2; i < P.size(); i++) {
-        while (hull.size() >= 2 && 
-               turn(hull[hull.size()-2][0], hull[hull.size()-1][0], P[i][0]) <= 0) {
+        while (hull.size() >= 2 &&
+               turn(hull[hull.size() - 2][0],
+                    hull[hull.size() - 1][0],
+                    P[i][0]) <= 0) {
             hull.pop_back();
         }
         hull.push_back(P[i]);
     }
-    
+
     // Check if any point of B is part of the convex hull
     for (const auto& node : hull) {
-        if (node[1][0] == 1) {  // It's a point from polygon B
+        if (node[1][0] ==
+            1) { // It's a point from polygon B
             cout << "NO\n";
             return 0;
         }
     }
-    
+
     cout << "YES\n";
 }
