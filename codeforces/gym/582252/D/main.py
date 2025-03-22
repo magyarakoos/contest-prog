cb7f805bd879e50911e997ce14572777c52c5eea
magyarakoos <ninjaoffical08@gmail.com>
2025-03-22 18:54:01 +0100

    sync: 2025-03-22 18:54:01

--- codeforces/gym/582252/D/main.py
+++ codeforces/gym/582252/D/main.py
@@ -3,15 +3,16 @@ from heapq import heappush, heappop
 for _ in range(int(input())):
     N = int(input())
     a = list(map(int, input().split()))
-    used, done = [0] * N, [0] * N
+    done = [0] * N
+    used = set()
     pq = [(0, i, x, x) for i, x in enumerate(a)]
     result = 0
     while len(pq) > 0:
         d, i, x, y = heappop(pq)
         if done[i]:
             continue
-        if not used[y]:
-            used[y] = 1
+        if not y in used:
+            used.add(y)
             done[i] = 1
             result += d
             continue
@@ -19,3 +20,4 @@ for _ in range(int(input())):
             heappush(pq, (d + 1, i, x, y - 1))
         if x <= y:
             heappush(pq, (d + 1, i, x, y + 1))
+    print(result)
