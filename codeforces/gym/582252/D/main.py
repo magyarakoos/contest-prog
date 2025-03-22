430af66613178e4896b462a19e09a12f9dd3f795
magyarakoos <ninjaoffical08@gmail.com>
2025-03-22 18:48:00 +0100

    sync: 2025-03-22 18:48:00

--- codeforces/gym/582252/D/main.py
+++ codeforces/gym/582252/D/main.py
@@ -3,8 +3,13 @@ from heapq import heappush, heappop
 for _ in range(int(input())):
     N = int(input())
     a = list(map(int, input().split()))
-    used = [0] * N
+    used, done = [0] * N, [0] * N
     pq = [(i, i) for i in range(N)]
+    result = 0
     while len(pq) > 0:
         i, j = heappop(pq)
         d = abs(i - j)
+        if not used[j]:
+            used[j] = 1
+            result += d
+            continue
