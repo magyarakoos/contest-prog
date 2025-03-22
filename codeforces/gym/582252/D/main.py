1b8e6e2ebdd80ad457347e606e155a5f060d95fd
magyarakoos <ninjaoffical08@gmail.com>
2025-03-22 18:57:00 +0100

    sync: 2025-03-22 18:57:00

--- codeforces/gym/582252/D/main.py
+++ codeforces/gym/582252/D/main.py
@@ -16,7 +16,7 @@ for _ in range(int(input())):
             done[i] = 1
             result += d
             continue
-        if x >= y:
+        if x >= y and y > 1:
             heappush(pq, (d + 1, i, x, y - 1))
         if x <= y:
             heappush(pq, (d + 1, i, x, y + 1))
