28dc273a6dc507ba72cfa96cbc236da6b441ce25
magyarakoos <ninjaoffical08@gmail.com>
2025-03-22 19:10:01 +0100

    sync: 2025-03-22 19:10:01

--- codeforces/gym/582252/D/main.py
+++ codeforces/gym/582252/D/main.py
@@ -3,8 +3,7 @@ for _ in range(int(input())):
     a = [0] * N
     for x in input().split():
         a[int(x) - 1] += 1
-    print(a)
-    result = 0
+
     dp = [[0] * N for _ in range(N)]
     for i in range(N):
         for j in range(N):
