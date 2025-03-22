5d5537fed31d54434e044147fc7acb85f791b47f
magyarakoos <ninjaoffical08@gmail.com>
2025-03-22 19:17:02 +0100

    sync: 2025-03-22 19:17:02

--- codeforces/gym/582252/D/main.py
+++ codeforces/gym/582252/D/main.py
@@ -7,10 +7,9 @@ for _ in range(int(input())):
     dp = [[10**9] * N for _ in range(N)]
     for i in range(N):
         for j in range(N):
-            if j + 1 + (i > 0) < a[i]:
-                continue
-            for k in range(j - a[i] + 1, j + 1):
-                dp[i][j] += abs(k - i)
-            if i > 0:
-                dp[i][j] += min(dp[i - 1][:j - a[i] + 1])
+            if j - a[i] + 1 + (i > 0) >= 0:
+                for k in range(j - a[i] + 1, j + 1):
+                    dp[i][j] += abs(k - i)
+                if i > 0:
+                    dp[i][j] += min(dp[i - 1][:j - a[i] + 1])
     print(dp)
