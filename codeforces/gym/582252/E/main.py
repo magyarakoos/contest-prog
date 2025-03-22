e6ca64d77862d284450d7b91d9574a47df5c4832
magyarakoos <ninjaoffical08@gmail.com>
2025-03-22 19:51:00 +0100

    sync: 2025-03-22 19:51:00

--- codeforces/gym/582252/E/main.py
+++ codeforces/gym/582252/E/main.py
@@ -1,6 +1,12 @@
-A = []
 def read():
-N = int(input())
-for _ in range(N):
-    x, y = map(int, input().split())
-    A.append(complex(x, y))
+    n = int(input())
+    a = []
+    for _ in range(n):
+        x, y = map(int, input().split())
+        a.append(complex(x, y))
+    return (n, a)
+N, A = read()
+M, B = read()
+
+print(N, A)
+print(M, B)
