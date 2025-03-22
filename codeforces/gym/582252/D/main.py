58eb9a736d2d879bad4f0675d6f6e049f460ad18
magyarakoos <ninjaoffical08@gmail.com>
2025-03-22 19:28:00 +0100

    sync: 2025-03-22 19:28:00

--- codeforces/gym/582252/D/main.py
+++ codeforces/gym/582252/D/main.py
@@ -4,7 +4,6 @@ for _ in range(int(input())):
     for x in input().split():
         a[int(x) - 1] += 1
 
-    print(a)
     dp = [[0] * N for _ in range(N)]
     for i in range(N):
         for j in range(N):
@@ -13,8 +12,7 @@ for _ in range(int(input())):
                 for l in range(k, j + 1):
                     dp[i][j] += abs(l - i)
                 if i > 0 and sum(a[:i]) > 0:
-                    if j - a[i] + 1
-                    dp[i][j] += min(dp[i - 1][:j - a[i] + 1]) if j - a[i] + 1 > 0 else 10**9
+                    dp[i][j] += min(dp[i - 1][:k]) if k > 0 else 10**9
             else:
                 dp[i][j] = 10**9
-        print(dp[i])
+    print(min(dp[-1]))
