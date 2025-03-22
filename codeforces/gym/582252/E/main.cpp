0b764f575ff329953587443e0c371ac964bfcc4d
magyarakoos <ninjaoffical08@gmail.com>
2025-03-22 21:39:00 +0100

    sync: 2025-03-22 21:39:00

--- codeforces/gym/582252/E/main.cpp
+++ codeforces/gym/582252/E/main.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define int int64_t
+
 using point = array<int, 2>;
 using node = array<point, 2>;
 
@@ -28,47 +29,30 @@ int32_t main() {
     P.resize(N + M);
 
     sort(P.begin(), P.end());
-    for (int i = 1; i < P.size(); i++) {
-        if (P[i - 1][0][0] == P[i][0][0] &&
-            P[i - 1][0][1] == P[i][0][1]) {
-            cout << "NO\n";
-            return 0;
-        }
-    }
-
     sort(P.begin() + 1, P.end(), [&](node A, node B) {
         int t = turn(P[0][0], A[0], B[0]);
-        if (t != 0) return t > 0;
-        int d1 =
-            (A[0][0] - P[0][0][0]) *
-                (A[0][0] - P[0][0][0]) +
-            (A[0][1] - P[0][0][1]) * (A[0][1] - P[0][0][1]);
-        int d2 =
-            (B[0][0] - P[0][0][0]) *
-                (B[0][0] - P[0][0][0]) +
-            (B[0][1] - P[0][0][1]) * (B[0][1] - P[0][0][1]);
-        return d1 < d2;
+        return t ? t > 0 : A[0] < B[0];
     });
 
-    vector<node> hull;
-    hull.push_back(P[0]);
-    hull.push_back(P[1]);
-
-    for (int i = 2; i < P.size(); i++) {
-        while (hull.size() >= 2 &&
-               turn(hull[hull.size() - 2][0],
-                    hull[hull.size() - 1][0],
+    vector<node> hull({P[0], P[1]});
+    int K = 2;
+    for (int i = 2; i < N + M; i++) {
+        while (K >= 2 &&
+               turn(hull[K - 2][0], hull[K - 1][0],
                     P[i][0]) < 0) {
             hull.pop_back();
+            K--;
         }
         hull.push_back(P[i]);
+        K++;
     }
 
-    for (const auto& node : hull) {
-        if (node[1][0] == 1) {
+    for (int i = 0; i < K; i++) {
+        if (hull[i][1][0]) {
             cout << "NO\n";
             return 0;
         }
     }
     cout << "YES\n";
 }
+
