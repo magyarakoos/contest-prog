fcd386bd2db2d12bade73987ff82c30d379aca9d
magyarakoos <ninjaoffical08@gmail.com>
2025-03-22 19:07:00 +0100

    sync: 2025-03-22 19:07:00

--- codeforces/gym/582252/D/main.py
+++ codeforces/gym/582252/D/main.py
@@ -1,6 +1,9 @@
 for _ in range(int(input())):
     N = int(input())
-    a = list(map(int, input().split()))
+    a = [0] * N
+    map(lambda x: a[int(x) - 1] += 1, input().split())
     result = 0
-    dp = [[0] * (N + 1) for _ in range(N + 1)]
-    print(dp)
+    dp = [[0] * N for _ in range(N)]
+    for i in range(N):
+        for j in range(N):
+            
