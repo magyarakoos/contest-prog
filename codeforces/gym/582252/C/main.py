4ccfc467124e56cee2ec7f06864b5ebd65b6f350
magyarakoos <ninjaoffical08@gmail.com>
2025-03-22 18:18:01 +0100

    sync: 2025-03-22 18:18:01

--- codeforces/gym/582252/C/main.py
+++ codeforces/gym/582252/C/main.py
@@ -3,6 +3,15 @@ f = open("input.txt")
 
 n = int(f.readline())
 a = []
+vis = [0] * n
+g = [[] for _ in range(n)]
+
+def dfs(i):
+    if vis[i]:
+        return 0
+    vis[i] = 1
+
+
 for _ in range(n):
     l = f.readline().strip()
     # if len(l) >= 4:
