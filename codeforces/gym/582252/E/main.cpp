821b408f6f16412699f7424253c302d0def33523
magyarakoos <ninjaoffical08@gmail.com>
2025-03-22 20:42:01 +0100

    sync: 2025-03-22 20:42:01

--- codeforces/gym/582252/E/main.cpp
+++ codeforces/gym/582252/E/main.cpp
@@ -1,72 +1,63 @@
 #include <bits/stdc++.h>
 using namespace std;
-using point = array<int64_t, 2>;
+#define int int64_t
 
-int fordul(point A, point B, point C) {
+using point = array<int, 2>;
+using node = array<point, 2>;
+
+int turn(point A, point B, point C) {
     return (B[0] - A[0]) * (C[1] - A[1]) -
-           (C[0] - A[0]) * (B[1] - A[1]);
+           (B[1] - A[1]) * (C[0] - A[0]);
 }
 
-bool kozte(point A, point B, point C) {
-    return (fordul(A, B, C) == 0 &&
-            (max(A[0], B[0]) >= C[0] &&
-             min(A[0], B[0]) <= C[0]) &&
-            (max(A[1], B[1]) >= C[1] &&
-             min(A[1], B[1]) <= C[1]));
-}
-
-vector<point> hull(vector<point> ptS) {
-    int N = ptS.size();
-
-    sort(ptS.begin(), ptS.end());
-    sort(ptS.begin() + 1, ptS.end(), [&](point a, point b) {
-        if (fordul(ptS[0], a, b) == 0 &&
-            !kozte(ptS[0], a, b)) {
-            return true;
-        }
-        return fordul(ptS[0], a, b) > 0;
-    });
-
-    vector<point> result({ptS[0]});
-    int K = 0;
-
-    for (int i = 1; i < N; i++) {
-        while (K > 0 && fordul(result[K - 1], result[K],
-                               ptS[i]) < 0) {
-            K--;
-            result.pop_back();
-        }
-        K++;
-        result.push_back(ptS[i]);
-    }
-
-    for (int i = N - 2; i >= 1; i--) {
-        if (!fordul(ptS[0], ptS.back(), ptS[i])) {
-            result.push_back(ptS[i]);
-        } else
-            break;
-    }
-    return result;
-}
-
-vector<point> read(vector<point> a) {
-    int N;
-    cin >> N;
-    while (N--) {
-        int x, y;
-        cin >> x >> y;
-        a.push_back({x, y});
-    }
-    return a;
-}
-
-int main() {
+int32_t main() {
     cin.tie(0), ios::sync_with_stdio(0);
 
-    auto A = read({});
-    auto AB = hull(read(A));
-    sort(A.begin(), A.end());
-    sort(AB.begin(), AB.end());
+    vector<node> P(2e5);
+    int N;
+    cin >> N;
+    for (int i = 0; i < N; i++) {
+        cin >> P[i][0][0] >> P[i][0][1];
+        P[i][1] = {0, i};
+    }
+    int M;
+    cin >> M;
+    for (int i = 0; i < M; i++) {
+        cin >> P[i + N][0][0] >> P[i + N][0][1];
+        P[i + N][1] = {1, i};
+    }
+    P.resize(N + M);
 
-    cout << (A == AB ? "YES" : "NO") << "\n";
+    sort(P.begin(), P.end());
+    sort(P.begin() + 1, P.end(), [&](node A, node B) {
+        int t = turn(P[0][0], A[0], B[0]);
+        return t ? t > 0 : A[0] < B[0];
+    });
+
+    vector<node> hull({P[0], P[1]});
+    int K = 2;
+    for (int i = 2; i < N + M; i++) {
+        while (K >= 2 &&
+               turn(hull[K - 2][0], hull[K - 1][0],
+                    P[i][0]) < 0) {
+            hull.pop_back();
+            K--;
+        }
+        hull.push_back(P[i]);
+        K++;
+    }
+
+    for (int i = 1; i < K; i++) {
+        if (hull[i - 1][1][0] != hull[i][1][0]) {
+            if (hull[i - 1][1][0] > hull[i][1][0]) {
+                swap(hull[i - 1], hull[i]);
+            }
+            cout << hull[i - 1][1][1] + 1 << " "
+                 << hull[i][1][1] + 1 << "\n";
+            return 0;
+        }
+    }
+
+    cout << "0 0\n";
 }
+
