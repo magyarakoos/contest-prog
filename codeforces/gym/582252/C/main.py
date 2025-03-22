e20c637fc355669b2e44f8bd5a29325d093cd14b
magyarakoos <ninjaoffical08@gmail.com>
2025-03-22 18:14:01 +0100

    sync: 2025-03-22 18:14:01

--- codeforces/gym/582252/C/main.py
+++ codeforces/gym/582252/C/main.py
@@ -3,8 +3,12 @@ f = open("input.txt")
 
 n = int(f.readline())
 a = []
-for l in map(strip, f.readlines()):
-    # a.append(["".join(seq) for seq in combinations(l[:-1], 4)])
+for _ in range(n):
+    l = f.readline().strip()
+    if len(l) >= 4:
+        a.append(["".join(seq) for seq in combinations(l, 4)])
+    else:
+        a.append([l])
 
 print(a)
 
