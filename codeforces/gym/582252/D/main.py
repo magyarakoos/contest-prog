95246a531c60a26cd16819b7f2c9dd7e44d82617
magyarakoos <ninjaoffical08@gmail.com>
2025-03-22 18:46:01 +0100

    sync: 2025-03-22 18:46:01

--- codeforces/gym/582252/D/main.py
+++ codeforces/gym/582252/D/main.py
@@ -4,5 +4,7 @@ for _ in range(int(input())):
     N = int(input())
     a = list(map(int, input().split()))
     used = [0] * N
-    pq = [(i, i, 0) for i in range(N)]
-    print(pq)
+    pq = [(i, i) for i in range(N)]
+    while len(pq) > 0:
+        i, j = heappop(pq)
+        d = abs(i - j)
