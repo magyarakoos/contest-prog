f57f79af5045a014f50088d7a49804db2edbeef2
magyarakoos <ninjaoffical08@gmail.com>
2025-03-23 20:48:00 +0100

    sync: 2025-03-23 20:48:00

--- codeforces/gym/582252/F/main.cpp
+++ codeforces/gym/582252/F/main.cpp
@@ -5,3 +5,12 @@ const int MAXN = 1e7 + 1;
 
 bitset<MAXN> s;
 vector<int> m[MAXN];
+
+int main() {
+    for (int i = 2; i < MAXN; i++) {
+        for (int j = i; j < MAXN * s[i]; j++) {
+            s[j] = 0;
+            m[j].push_back(i);
+        }
+    }
+}
