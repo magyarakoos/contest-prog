50737410021b804303b3362aff102058a70b4b44
magyarakoos <ninjaoffical08@gmail.com>
2025-03-23 21:00:01 +0100

    sync: 2025-03-23 21:00:01

--- codeforces/gym/582252/F/main.cpp
+++ codeforces/gym/582252/F/main.cpp
@@ -15,26 +15,21 @@ int main() {
             }
         }
     }
-    for (int i = 2; i < 100; i++) {
-        cout << i << " ";
-        for (int x : m[i]) { cout << x << " "; }
+    int N;
+    cin >> N;
+    vector ans(N, vector<int>(2, -1));
+    for (int i = 0, x; i < N; i++) {
+        cin >> x;
+        for (int y : m[x]) {
+            for (int z : m[x]) {
+                if (gcd(x, y + z) == 1) ans[i] = {y, z};
+            }
+        }
+    }
+    for (int j = 0; j < 2; j++) {
+        for (int i = 0; i < N; i++) {
+            cout << ans[i][j] << " ";
+        }
         cout << "\n";
     }
-    // int N;
-    // cin >> N;
-    // vector ans(N, vector<int>(2, -1));
-    // for (int i = 0, x; i < N; i++) {
-    //     cin >> x;
-    //     for (int y : m[x]) {
-    //         for (int z : m[x]) {
-    //             if (gcd(x, y + z) == 1) ans[i] = {y, z};
-    //         }
-    //     }
-    // }
-    // for (int j = 0; j < 2; j++) {
-    //     for (int i = 0; i < N; i++) {
-    //         cout << ans[i][j] << " ";
-    //     }
-    //     cout << "\n";
-    // }
 }
