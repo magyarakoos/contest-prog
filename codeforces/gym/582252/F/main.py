2b446419341609ca1d6e83a3686e7e2ca9ce117e
magyarakoos <ninjaoffical08@gmail.com>
2025-03-23 20:26:01 +0100

    sync: 2025-03-23 20:26:01

--- codeforces/gym/582252/F/main.py
+++ codeforces/gym/582252/F/main.py
@@ -1,16 +1,9 @@
-from collections import defaultdict
-
-
 N, MAXN = int(input()), 10**2 + 1
-
-m = [[[] for _ in range(MAXN)] for _ in range(2)]
-for i, x in enumerate(list(map(int, input().split()))):
-    m[0][x].append(i)
+a = list(map(int, input().split()))
+m = [[] for _ in range(MAXN)]
 
 s = [1] * MAXN
 for i in range(2, MAXN):
     for j in range(i, MAXN * s[i], i):
         s[j] = 0
-        m[1][j].append(i)
-
-print(m[1])
+        m[j].append(i)
