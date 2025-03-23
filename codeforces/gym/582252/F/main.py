4980471fc0a13fa2cf4998aa41d790952db892f7
magyarakoos <ninjaoffical08@gmail.com>
2025-03-23 20:27:01 +0100

    sync: 2025-03-23 20:27:01

--- codeforces/gym/582252/F/main.py
+++ codeforces/gym/582252/F/main.py
@@ -1,9 +1,11 @@
 N, MAXN = int(input()), 10**2 + 1
-a = list(map(int, input().split()))
-m = [[] for _ in range(MAXN)]
 
+m = [[] for _ in range(MAXN)]
 s = [1] * MAXN
 for i in range(2, MAXN):
     for j in range(i, MAXN * s[i], i):
         s[j] = 0
         m[j].append(i)
+
+for a in map(int, input().split()):
+    print(a)
