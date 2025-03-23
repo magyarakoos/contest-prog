9fd0d6cd9174c39fe2736ce87cbfb3f3d8a3e4a3
magyarakoos <ninjaoffical08@gmail.com>
2025-03-23 20:22:00 +0100

    sync: 2025-03-23 20:22:00

--- codeforces/gym/582252/F/main.py
+++ codeforces/gym/582252/F/main.py
@@ -3,7 +3,7 @@ from collections import defaultdict
 
 N, MAXN = int(input()), 10**2 + 1
 
-m = [[] for _ in range(MAXN)] * 2 
+m = [[[] for _ in range(MAXN)] for _ in range(2)]
 for i, x in enumerate(list(map(int, input().split()))):
     m[0][x].append(i)
 print(m)
