afa344d03b4da4fd581e0f4e65d189859fac9797
magyarakoos <ninjaoffical08@gmail.com>
2025-03-23 22:04:00 +0100

    sync: 2025-03-23 22:04:00

--- codeforces/gym/582252/F/main.cpp
+++ codeforces/gym/582252/F/main.cpp
@@ -2,26 +2,36 @@
 using namespace std;
 
 const int MAXN = 1e7 + 1;
-int m[MAXN], cnt[MAXN];
-bitset<MAXN> s;
 
 int main() {
+    cin.tie(0), ios::sync_with_stdio(0);
+
+    int N;
+    cin >> N;
+    vector<int> a(N);
+    bitset<MAXN> s, has;
+    map<int, vector<int>> m;
+    for (int& x : a) {
+        cin >> x;
+        has[x] = 1;
+    }
+
     for (int i = 2; i < MAXN; i++) {
         if (!s[i]) {
             for (int j = i; j < MAXN; j += i) {
                 s[j] = 1;
-                cnt[j]++;
-                if (!m[j]) m[j] = i;
+                if (has[j]) m[j].push_back(i);
             }
         }
     }
 
-    int N;
-    cin >> N;
     vector ans(N, vector<int>(2, -1));
-    for (int i = 0, x; i < N; i++) {
-        cin >> x;
-        if (cnt[x] >= 2) ans[i] = {m[x], x / m[x]};
+    for (int i = 0; i < N; i++) {
+        for (int y : m[a[i]]) {
+            for (int z : m[a[i]]) {
+                if (gcd(a[i], y + z) == 1) ans[i] = {y, z};
+            }
+        }
     }
     for (int j = 0; j < 2; j++) {
         for (int i = 0; i < N; i++) {
