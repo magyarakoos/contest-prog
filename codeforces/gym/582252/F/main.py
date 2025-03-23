a4bde496c2a77c9f50d3118ff404e6606f6596a7
magyarakoos <ninjaoffical08@gmail.com>
2025-03-23 22:08:00 +0100

    sync: 2025-03-23 22:08:00

---
+++ codeforces/gym/582252/F/main.py
@@ -0,0 +1,22 @@
+from math import gcd
+from itertools import combinations
+
+N = [int(input()), 10**7 + 1][1]
+m = [(0, 1) for _ in range(N)]
+s = [1] * N
+for i in range(2, N):
+    for j in range(i, N * s[i], i):
+        s[j] = 0
+
+
+a1, a2 = [], []
+for x in map(int, input().split()):
+    a1.append(-1)
+    a2.append(-1)
+    for d1, d2 in combinations(m[x], 2):
+        if gcd(d1 + d2, x) == 1:
+            a1[-1] = d1
+            a2[-1] = d2
+print(*a1)
+print(*a2)
+
