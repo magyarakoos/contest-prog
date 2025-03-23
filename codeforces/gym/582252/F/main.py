9414cd2b7cec6d649134d1e8df66ccd1de8f5a02
magyarakoos <ninjaoffical08@gmail.com>
2025-03-23 20:14:00 +0100

    sync: 2025-03-23 20:14:00

--- codeforces/gym/582252/F/main.py
+++ codeforces/gym/582252/F/main.py
@@ -2,7 +2,6 @@ N, MAXN = int(input()), 10**2 + 1
 a = list(map(int, input().split()))
 s = [1] * MAXN
 for i in range(2, MAXN):
-    if s[i]:
-        for j in range(i * 2, MAXN, i):
-            s[j] = 0
+    for j in range(i * 2, MAXN * s[i], i):
+        s[j] = 0
 print(s)
