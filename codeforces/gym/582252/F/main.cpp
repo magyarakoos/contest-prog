26c75d2f43094e8b4e9fbf8e2250d2193f877800
magyarakoos <ninjaoffical08@gmail.com>
2025-03-23 20:56:00 +0100

    sync: 2025-03-23 20:56:00

--- codeforces/gym/582252/F/main.cpp
+++ codeforces/gym/582252/F/main.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int MAXN = 1e7 + 1;
+const int MAXN = 1e5 + 1;
 
 bitset<MAXN> s;
 vector<int> m[MAXN];
@@ -13,15 +13,21 @@ int main() {
             m[j].push_back(i);
         }
     }
-    int N;
-    cin >> N;
-    vector ans(N, vector<int>(2, -1));
-    for (int i = 0, x; i < N; i++) {
-        cin >> x;
-        for (int y : m[x]) {
-            for (int z : m[x]) {
-                if (gcd(x, y + z) == 1) ans[i] = {y, z};
-            }
-        }
-    }
+    // int N;
+    // cin >> N;
+    // vector ans(N, vector<int>(2, -1));
+    // for (int i = 0, x; i < N; i++) {
+    //     cin >> x;
+    //     for (int y : m[x]) {
+    //         for (int z : m[x]) {
+    //             if (gcd(x, y + z) == 1) ans[i] = {y, z};
+    //         }
+    //     }
+    // }
+    // for (int j = 0; j < 2; j++) {
+    //     for (int i = 0; i < N; i++) {
+    //         cout << ans[i][j] << " ";
+    //     }
+    //     cout << "\n";
+    // }
 }
