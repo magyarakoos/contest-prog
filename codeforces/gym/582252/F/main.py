08d5c38d0e4c5858d3abdb4d8a30cb23f4de759c
magyarakoos <ninjaoffical08@gmail.com>
2025-03-23 20:13:00 +0100

    sync: 2025-03-23 20:13:00

--- codeforces/gym/582252/F/main.py
+++ codeforces/gym/582252/F/main.py
@@ -3,6 +3,6 @@ a = list(map(int, input().split()))
 s = [1] * MAXN
 for i in range(2, MAXN):
     if s[i]:
-        for j in range(i, MAXN, i):
+        for j in range(i * 2, MAXN, i):
             s[j] = 0
 print(s)
