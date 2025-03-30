19d4fcd0f4690285b5d821a623a4f7b3f263fe9d
magyarakoos <ninjaoffical08@gmail.com>
2025-03-30 21:35:01 +0200

    sync: 2025-03-30 21:35:01

--- codeforces/gym/582252/H/main.cpp
+++ codeforces/gym/582252/H/main.cpp
@@ -7,8 +7,10 @@ void solve(int ans) {
     int m = 1 << ans;
     vector<vector<array<int, 2>>> g(m);
     vector<int> deg(m);
+    multiset<array<int, 2>> ms;
     for (int i = 0; i < n; i++) {
         int u = a[i] & (m - 1), v = b[i] & (m - 1);
+        ms.insert({u, v});
         deg[u]++, deg[v]++;
         g[u].push_back({v, i});
         g[v].push_back({u, i});
@@ -28,16 +30,14 @@ void solve(int ans) {
             g[u].pop_back();
             if (!used[v[1]]) {
                 used[v[1]] = 1;
-                path.push_back({u, v});
+                path.push_back({u, v[0]});
                 euler(v[0]);
             }
         }
     };
     euler(start);
-    if (path.size() != n * 2) return;
+    if (path.size() != n) return;
     cout << ans << "\n";
-    for (int u : path) cout << u + 1 << " ";
-    cout << "\n";
     exit(0);
 }
 
