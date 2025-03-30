1c7c6cc75e2c324d5f99b72f08814e224ca460b5
magyarakoos <ninjaoffical08@gmail.com>
2025-03-30 20:54:00 +0200

    sync: 2025-03-30 20:54:00

--- codeforces/gym/582252/H/main.cpp
+++ codeforces/gym/582252/H/main.cpp
@@ -1,13 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int N, a[1 << 19], b[1 << 19];
+int n, a[1 << 19], b[1 << 19];
 
-bool solve(int ans) {}
+bool solve(int ans) {
+    int m = 1 << ans;
+    vector<vector<int>> g(m);
+    vector<int> deg(m);
+    for (int i = 0; i < n; i++) {
+        int u = a[i] & (m - 1), v = b[i] & (m - 1);
+    }
+}
 
 int main() {
     cin.tie(0), ios::sync_with_stdio(0);
-    cin >> N;
-    for (int i = 0; i < N; i++) cin >> a[i] >> b[i];
+    cin >> n;
+    for (int i = 0; i < n; i++) cin >> a[i] >> b[i];
     for (int i = 20; i >= 0; i--) solve(i);
 }
