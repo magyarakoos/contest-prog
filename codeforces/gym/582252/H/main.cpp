346b73e9d0b8e71b71dfff8ee08db2fc3271126f
magyarakoos <ninjaoffical08@gmail.com>
2025-03-30 20:50:01 +0200

    sync: 2025-03-30 20:50:01

--- codeforces/gym/582252/H/main.cpp
+++ codeforces/gym/582252/H/main.cpp
@@ -10,4 +10,5 @@ int main() {
     cin.tie(0), ios::sync_with_stdio(0);
     cin >> N;
     for (int i = 0; i < N; i++) cin >> a[i] >> b[i];
-    for (int i = 20; i >) }
+    for (int i = 20; i >= 0; i--) solve(i);
+}
