5f7cca6a5ba6569d7186c79d53280099018af8a6
magyarakoos <ninjaoffical08@gmail.com>
2025-03-30 21:44:01 +0200

    sync: 2025-03-30 21:44:01

--- codeforces/gym/582252/H/main.cpp
+++ codeforces/gym/582252/H/main.cpp
@@ -6,20 +6,18 @@ int n, a[1 << 19], b[1 << 19];
 void solve(int ans) {
     int m = 1 << ans;
     vector<vector<array<int, 2>>> g(m);
-    vector<int> deg(m);
     map<array<int, 2>, vector<int>> ms;
     for (int i = 0; i < n; i++) {
         int u = a[i] & (m - 1), v = b[i] & (m - 1);
         ms[{u, v}].push_back(i);
-        deg[u]++, deg[v]++;
         g[u].push_back({v, i});
         g[v].push_back({u, i});
     }
 
     int start = 0;
     for (int u = 0; u < m; u++) {
-        if (deg[u] & 1) return;
-        if (deg[u]) start = u;
+        if (g[u].size() & 1) return;
+        if (!g[u].empty()) start = u;
     }
 
     vector<array<int, 2>> path;
