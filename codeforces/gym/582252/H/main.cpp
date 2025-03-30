d632fe39e3933a385ed02a7d59715875a03c1545
magyarakoos <ninjaoffical08@gmail.com>
2025-03-30 20:56:00 +0200

    sync: 2025-03-30 20:56:00

--- codeforces/gym/582252/H/main.cpp
+++ codeforces/gym/582252/H/main.cpp
@@ -5,14 +5,16 @@ int n, a[1 << 19], b[1 << 19];
 
 bool solve(int ans) {
     int m = 1 << ans;
-    vector<vector<int>> g(m);
+    vector<vector<array<int, 2>>> g(m);
     vector<int> deg(m);
     for (int i = 0; i < n; i++) {
         int u = a[i] & (m - 1), v = b[i] & (m - 1);
         deg[u]++, deg[v]++;
-        g[u].push_back(v);
-        g[v].push_back(u);
+        g[u].push_back({v, i});
+        g[v].push_back({u, i});
     }
+    for (int i = 0; i < m; i++) { cout << deg[i] << " "; }
+    return 0;
 }
 
 int main() {
