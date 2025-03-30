153bece0b3506ed99e93f9c8639b43770c1365c1
magyarakoos <ninjaoffical08@gmail.com>
2025-03-30 20:49:00 +0200

    sync: 2025-03-30 20:49:00

--- codeforces/gym/582252/H/main.cpp
+++ codeforces/gym/582252/H/main.cpp
@@ -4,4 +4,10 @@ using namespace std;
 const int MAXN = 5e5 + 1;
 int N, a[MAXN], b[MAXN];
 
-bool solve() {}
+bool solve(int ans) {}
+
+int main() {
+    cin.tie(0), ios::sync_with_stdio(0);
+    cin >> N;
+    for (int i = 0; i < N; i++) cin >> a[i] >> b[i];
+    for (int i = 20; i >) }
