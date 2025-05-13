257343f9ec317d911556845f56e08712be807f00
magyarakoos <magyar.kende.22f@gmail.com>
2025-05-13 12:41:00 +0200

    sync: 2025-05-13 12:41:00

---
+++ atcoder/abc405/E/main.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int int64_t
+
+const int MOD = 998244353;
+
+int32_t main() {
+    cin.tie(0), ios::sync_with_stdio(0);
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+}
