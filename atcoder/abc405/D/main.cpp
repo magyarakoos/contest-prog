39363d51b61d7c90fd183b461360312f2282d0de
magyarakoos <magyar.kende.22f@gmail.com>
2025-05-13 07:58:01 +0200

    sync: 2025-05-13 07:58:01

--- atcoder/abc405/D/main.cpp
+++ atcoder/abc405/D/main.cpp
@@ -2,9 +2,26 @@
 using namespace std;
 #define int int64_t
 
+int di[] = {1, -1, 0, 0}, dj[] = {0, 0, 1, -1};
+
 int32_t main() {
     cin.tie(0), ios::sync_with_stdio(0);
     int h, w;
     cin >> h >> w;
     vector grid(h, string(w, 0));
+    vector dist(h, vector<int>(w, 1e9));
+    queue<array<int, 2>> q;
+    for (int i = 0; i < h; i++) {
+        cin >> grid[i];
+        for (int j = 0; j < w; j++) {
+            if (grid[i][j] == 'E') {
+                dist[i][j] = 0;
+                q.push({i, j});
+            }
+        }
+    }
+    while (!q.empty()) {
+        auto [i, j] = q.front();
+        q.pop();
+    }
 }
