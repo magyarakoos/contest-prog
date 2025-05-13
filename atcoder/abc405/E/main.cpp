057c27327ffb2e28dea62f24af281a34035838d9
magyarakoos <magyar.kende.22f@gmail.com>
2025-05-13 13:00:01 +0200

    sync: 2025-05-13 13:00:01

--- atcoder/abc405/E/main.cpp
+++ atcoder/abc405/E/main.cpp
@@ -7,10 +7,8 @@ const int MOD = 998244353, MAXN = 1e6 + 1;
 int inv(int a) {
     int res = 1, b = MOD - 2;
     while (b) {
-        if (b & 1) {
-            (a *= a) %= MOD;
-            (res *= a) %= MOD;
-        }
+        if (b & 1) (res *= a) %= MOD;
+        (a *= a) %= MOD;
         b /= 2;
     }
     return res;
