1f048aa20b1c1f209cce24e326af1370fa2ad40d
magyarakoos <magyar.kende.22f@gmail.com>
2025-06-23 13:02:00 +0200

    sync: 2025-06-23 13:02:00

--- codeforces/gym/605361/J/main.c
+++ codeforces/gym/605361/J/main.c
@@ -4,6 +4,26 @@ const int MAXN = 1e6 + 1;
 
 int n, a[MAXN], mem[MAXN];
 
+void merge(int l1, int r1, int l2, int r2) {
+    int ol1 = l1, mi = 0;
+    while (l1 <= r1 && l2 <= r2) {
+        if (a[l1] < a[l2]) {
+            mem[mi++] = a[l1++];
+        } else {
+            mem[mi++] = a[l2++];
+        }
+    }
+    while (l1 <= r1) mem[mi++] = a[l1++];
+    while (l2 <= r2) mem[mi++] = a[l2++];
+    for (int i = 0; i < mi; i++) a[i + ol1] = mem[i];
+}
+
+void sort(int l, int r) {
+    int m = (l + r) / 2;
+    if (l < m) sort(l, m);
+    if (m + 1 < r) sort(m + 1, r);
+}
+
 int main() {
     int n;
     scanf("%d", &n);
