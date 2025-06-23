459750faf74d5d02fdf6073747aa4f7b69e6db33
magyarakoos <magyar.kende.22f@gmail.com>
2025-06-23 11:18:01 +0200

    sync: 2025-06-23 11:18:01

--- codeforces/gym/605361/J/main.cpp
+++ codeforces/gym/605361/J/main.cpp
@@ -4,4 +4,11 @@ using namespace std;
 
 int32_t main() {
     cin.tie(0), ios::sync_with_stdio(0);
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int& x : a) cin >> x;
+    sort(a.begin(), a.end());
+    for (int x : a) cout << x << " ";
+    cout << "\n";
 }
