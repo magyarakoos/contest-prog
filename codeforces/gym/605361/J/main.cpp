d9a6a7591bce5c12f1bc95901deeb4c6344f37d6
magyarakoos <magyar.kende.22f@gmail.com>
2025-06-23 11:58:00 +0200

    sync: 2025-06-23 11:58:00

--- codeforces/gym/605361/J/main.cpp
+++ codeforces/gym/605361/J/main.cpp
@@ -10,9 +10,9 @@ int32_t main() {
     cin.tie(0), ios::sync_with_stdio(0);
     int n;
     cin >> n;
-    vector<int> a(n);
-    for (int& x : a) cin >> x;
-    sort(a.begin(), a.end());
-    for (int x : a) cout << x << " ";
-    cout << "\n";
+    while (n--) {
+        int x;
+        cin >> x;
+        buckets[x & ((1 << LEN) - 1)].push_back(x);
+    }
 }
