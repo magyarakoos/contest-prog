076183ad38b98dea2927a7c8bc06dd8db660dc8d
magyarakoos <magyar.kende.22f@gmail.com>
2025-06-23 11:20:01 +0200

    sync: 2025-06-23 11:20:01

--- codeforces/gym/605361/J/main.cpp
+++ codeforces/gym/605361/J/main.cpp
@@ -1,7 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define int int64_t
 
-int main() {
+int32_t main() {
+    cin.tie(0), ios::sync_with_stdio(0);
     int n;
     cin >> n;
     vector<int> a(n);
