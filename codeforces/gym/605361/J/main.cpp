503b4dd1f3d3e59d953d6e1e8cbd6e1ddf18e136
magyarakoos <magyar.kende.22f@gmail.com>
2025-06-23 12:02:00 +0200

    sync: 2025-06-23 12:02:00

--- codeforces/gym/605361/J/main.cpp
+++ codeforces/gym/605361/J/main.cpp
@@ -2,9 +2,7 @@
 #include <vector>
 using namespace std;
 
-const int LEN = 20;
-
-vector<int> buckets[1 << LEN];
+vector<int> buckets[1 << 20];
 
 int32_t main() {
     cin.tie(0), ios::sync_with_stdio(0);
@@ -13,6 +11,7 @@ int32_t main() {
     while (n--) {
         int x;
         cin >> x;
-        buckets[x >> 10].push_back(x);
+        buckets[x >> 10].push_back(x & ((1 << 10) - 1));
     }
+    for (int i = 0; i < (1 << 20); i++) {}
 }
