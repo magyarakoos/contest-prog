858a33373923a13be7bb09f2fc4a40a05296cb6c
magyarakoos <magyar.kende.22f@gmail.com>
2025-06-23 13:00:00 +0200

    sync: 2025-06-23 13:00:00

--- codeforces/gym/605361/J/main.c
+++ codeforces/gym/605361/J/main.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+const int MAXN = 1e6 + 1;
+
+int n, a[MAXN], mem[MAXN];
+
 int main() {
+    int n;
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) scanf("%d", &a[i]);
+
     return 0;
 }
