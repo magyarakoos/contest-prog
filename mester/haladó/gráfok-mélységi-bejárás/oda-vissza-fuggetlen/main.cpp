1757d1dd0de9860e74cc4ed18b82ba2b62650c63
magyarakoos <magyar.kende.22f@gmail.com>
2025-06-24 12:23:00 +0200

    sync: 2025-06-24 12:23:00

--- mester/haladó/gráfok-mélységi-bejárás/oda-vissza-fuggetlen/main.cpp
+++ mester/haladó/gráfok-mélységi-bejárás/oda-vissza-fuggetlen/main.cpp
@@ -4,9 +4,12 @@ using namespace std;
 
 int32_t main() {
     cin.tie(0), ios::sync_with_stdio(0);
+
     int n, m, a, b;
     cin >> n >> m >> a >> b;
     a--, b--;
+    vector<map<int, bool>> g(2 * n);
+    for (int u = 0; u < n; u++) g[u][u * 2] = 1;
     for (int i = 0, u, v; i < m; i++) {
         cin >> u >> v;
         u--, v--;
