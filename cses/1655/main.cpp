723c8c57d4db9bd1f3d50e2a5aa20cc9295da1ec
magyarakoos <magyar.kende.22f@gmail.com>
2025-06-24 09:09:00 +0200

    sync: 2025-06-24 09:09:00

--- cses/1655/main.cpp
+++ cses/1655/main.cpp
@@ -4,5 +4,12 @@ using namespace std;
 
 int32_t main() {
     cin.tie(0), ios::sync_with_stdio(0);
-}
 
+    int n;
+    cin >> n;
+    vector<int> a(n + 1);
+    for (int i = 1; i <= n; i++) {
+        cin >> a[i];
+        a[i] ^= a[i - 1];
+    }
+}
