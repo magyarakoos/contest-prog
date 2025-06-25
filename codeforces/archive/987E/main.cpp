e3e3e95b3efa8e2345545e8934f49eb6991ee059
magyarakoos <magyar.kende.22f@gmail.com>
2025-06-25 15:05:01 +0200

    sync: 2025-06-25 15:05:01

---
+++ codeforces/archive/987E/main.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int int64_t
+
+int32_t main() {
+    cin.tie(0), ios::sync_with_stdio(0);
+    int n;
+    cin >> n;
+    vector<int> p(n);
+    for (int& x : p) cin >> x, x--;
+}
