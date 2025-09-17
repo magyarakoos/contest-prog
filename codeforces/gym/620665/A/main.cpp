be50e62d25cf44aa0f2b90c80fa73d38a05d0cc9
magyarakoos <magyar.kende.22f@gmail.com>
2025-09-17 18:48:01 +0200

    sync: 2025-09-17 18:48:01

--- codeforces/gym/620665/A/main.cpp
+++ codeforces/gym/620665/A/main.cpp
@@ -6,8 +6,7 @@ int32_t main() {
     cin.tie(0), ios::sync_with_stdio(0);
     int n;
     cin >> n;
-    vector<int> a(n + 1), pre(n + 1), suf(n + 2, 1e9),
-        result;
+    vector<int> a(n + 1), pre(n + 1), suf(n + 2, 1e9);
     for (int i = 1; i <= n; i++) cin >> a[i];
     for (int i = 1; i <= n; i++) {
         pre[i] = max(pre[i - 1], a[i]);
@@ -15,7 +14,6 @@ int32_t main() {
     for (int i = n; i > 0; i--) {
         suf[i] = min(suf[i + 1], a[i]);
     }
-
     vector<int> result;
     for (int i = 1; i <= n; i++) {
         if (pre[i - 1] < a[i] && suf[i + 1] > a[i]) {
