e487ea15147072298a965b085a4e2383a60cb7c3
magyarakoos <magyar.kende.22f@gmail.com>
2025-09-17 20:40:00 +0200

    sync: 2025-09-17 20:40:00

--- codeforces/gym/620665/D/main.cpp
+++ codeforces/gym/620665/D/main.cpp
@@ -4,25 +4,4 @@ using namespace std;
 
 int32_t main() {
     cin.tie(0), ios::sync_with_stdio(0);
-    int n;
-    cin >> n;
-    vector<int> a(n);
-    for (int& x : a) cin >> x;
-    for (int i = 3; i <= n; i++) {
-        if (n % i || (i % 2 == 0 && i != 4)) continue;
-        for (int j = 0; j < n / i; j++) {
-            bool ok = 1;
-            for (int k = 0; k < i; k++) {
-                if (!a[k * (n / i) + j]) {
-                    ok = 0;
-                    break;
-                }
-            }
-            if (ok) {
-                cout << "YES\n";
-                return 0;
-            }
-        }
-    }
-    cout << "NO\n";
 }
