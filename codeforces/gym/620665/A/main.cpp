a23ee1023caced31d1190e99e14bc74aefc5b20e
magyarakoos <magyar.kende.22f@gmail.com>
2025-09-17 19:58:01 +0200

    sync: 2025-09-17 19:58:01

--- codeforces/gym/620665/A/main.cpp
+++ codeforces/gym/620665/A/main.cpp
@@ -10,8 +10,20 @@ struct DSU {
     int find(int x) {
         return x == par[x] ? x : par[x] = find(par[x]);
     }
+    void unite(int x, int y) {
+        x = find(x);
+        y = find(y);
+        if (x == y) return;
+        if (x < y) swap(x, y);
+        par[y] = x;
+        rank[x] += rank[y];
+    }
 };
 
 int32_t main() {
     cin.tie(0), ios::sync_with_stdio(0);
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int& x : a) cin >> x;
 }
