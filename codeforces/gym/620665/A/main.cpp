f6650b3ccaa21fc3a0a8ae52aad49aa5643dfea8
magyarakoos <magyar.kende.22f@gmail.com>
2025-09-17 19:53:01 +0200

    sync: 2025-09-17 19:53:01

--- codeforces/gym/620665/A/main.cpp
+++ codeforces/gym/620665/A/main.cpp
@@ -2,25 +2,11 @@
 using namespace std;
 #define int int64_t
 
+struct DSU {
+    vector<int> par, rank;
+    DSU(int n) : par(n), rank(n, 1) {}
+};
+
 int32_t main() {
     cin.tie(0), ios::sync_with_stdio(0);
-    int n;
-    cin >> n;
-    vector<int> a(n + 1), pre(n + 1), suf(n + 2, 1e9);
-    for (int i = 1; i <= n; i++) cin >> a[i];
-    for (int i = 1; i <= n; i++) {
-        pre[i] = max(pre[i - 1], a[i]);
-    }
-    for (int i = n; i > 0; i--) {
-        suf[i] = min(suf[i + 1], a[i]);
-    }
-    vector<int> result;
-    for (int i = 1; i <= n; i++) {
-        if (pre[i - 1] < a[i] && suf[i + 1] > a[i]) {
-            result.push_back(a[i]);
-        }
-    }
-    cout << result.size();
-    for (int x : result) cout << " " << x;
-    cout << "\n";
 }
