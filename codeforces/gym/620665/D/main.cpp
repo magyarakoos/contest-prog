aea6eff0c2ebfd388ed19efb904f5a1c89bb31a7
magyarakoos <magyar.kende.22f@gmail.com>
2025-09-24 12:20:01 +0200

    sync: 2025-09-24 12:20:01

--- codeforces/gym/620665/D/main.cpp
+++ codeforces/gym/620665/D/main.cpp
@@ -1,7 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define int int64_t
+using point = array<int, 2>;
+
+void solve() {
+    set < array<int, 2> int n;
+    cin >> n;
+    while (n--) {}
+}
 
 int32_t main() {
     cin.tie(0), ios::sync_with_stdio(0);
+    int t;
+    cin >> t;
+    while (t--) solve();
 }
