6eb0af51d17d44eee3467dd00766a47ac552248c
magyarakoos <magyar.kende.22f@gmail.com>
2025-09-27 12:57:01 +0200

    sync: 2025-09-27 12:57:01

--- cses/1753/main.cpp
+++ cses/1753/main.cpp
@@ -2,7 +2,12 @@
 using namespace std;
 #define int int64_t
 
+vector<int> z(const string& s) {}
+
 int32_t main() {
     cin.tie(0), ios::sync_with_stdio(0);
+    string s, t;
+    cin >> s >> t;
+    auto a = z(t + "#" + s);
+    cout << count(a.begin(), a.end(), t.size()) << "\n";
 }
-
