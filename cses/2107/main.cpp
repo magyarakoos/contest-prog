fa7a5ae44272c4537ad9f1f7e0be5f88c5e33b04
magyarakoos <magyar.kende.22f@gmail.com>
2025-09-27 12:39:00 +0200

    sync: 2025-09-27 12:39:00

--- cses/2107/main.cpp
+++ cses/2107/main.cpp
@@ -6,11 +6,14 @@ vector<int> z(const string& s) {
     vector<int> z(n);
     int l = 0, r = 0;
     for (int i = 1; i < n; i++) {
-
+        if (i < r) z[i] = min(r - i, z[i - l]);
         while (i + z[i] < n && s[i + z[i]] == s[z[i]]) {
             z[i]++;
         }
-        if (i + 1 > r) {}
+        if (i + z[i] > r) {
+            l = i;
+            r = i + z[i];
+        }
     }
     return z;
 }
