d997535e8a49d6819588602860fb1856316b42b0
magyarakoos <magyar.kende.22f@gmail.com>
2025-09-27 13:38:01 +0200

    sync: 2025-09-27 13:38:01

--- codeforces/archive/1198E/main.cpp
+++ codeforces/archive/1198E/main.cpp
@@ -7,8 +7,12 @@ int32_t main() {
     int n, m;
     cin >> n >> m;
     vector<int> row(1), col(1);
-    while (m--) {
-        int x1, y1, x2, y2;
+    vector<array<int, 4>> rectS(m);
+    for (auto& [x1, y1, x2, y2] : rectS) {
         cin >> x1 >> y1 >> x2 >> y2;
+        row.push_back(y1);
+        row.push_back(y2);
+        col.push_back(x1);
+        col.push_back(x2);
     }
 }
