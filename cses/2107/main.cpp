7723ed1cac0e13b0616e863da280703ab423df7b
magyarakoos <magyar.kende.22f@gmail.com>
2025-09-27 10:16:01 +0200

    sync: 2025-09-27 10:16:01

--- cses/2107/main.cpp
+++ cses/2107/main.cpp
@@ -1,9 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+vector<int> z(int n) {
+    vector<int> z(n);
+
+    return z;
+}
+
 int32_t main() {
     cin.tie(0), ios::sync_with_stdio(0);
     int n;
     cin >> n;
-    vector<int> z(n), pi(n);
 }
