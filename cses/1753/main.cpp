5c7ed762ae9240a2e8a2da8904c7071ba5402f06
magyarakoos <magyar.kende.22f@gmail.com>
2025-09-27 13:00:00 +0200

    sync: 2025-09-27 13:00:00

--- cses/1753/main.cpp
+++ cses/1753/main.cpp
@@ -2,7 +2,20 @@
 using namespace std;
 #define int int64_t
 
-vector<int> z(const string& s) {}
+vector<int> z(const string& s) {
+    int n = s.size(), l = 0, r = 0;
+    vector<int> z(n);
+    for (int i = 1; i < n; i++) {
+        z[i] = (i < r) * min(r - i, z[i - l]);
+        while (i + z[i] < n && s[z[i]] == s[i + z[i]])
+            z[i]++;
+        if (i + 1 > r) {
+            l = i;
+            r = i + z[i];
+        }
+    }
+    cout << count(z.begin(), z.end(), s.size()) << "\n";
+}
 
 int32_t main() {
     cin.tie(0), ios::sync_with_stdio(0);
