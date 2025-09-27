9d835b0959957a65472ac10076f6f2200e02eaea
magyarakoos <magyar.kende.22f@gmail.com>
2025-09-27 13:36:01 +0200

    sync: 2025-09-27 13:36:01

--- codeforces/archive/1198E/main.cpp
+++ codeforces/archive/1198E/main.cpp
@@ -4,5 +4,11 @@ using namespace std;
 
 int32_t main() {
     cin.tie(0), ios::sync_with_stdio(0);
+    int n, m;
+    cin >> n >> m;
+    vector<int> row(1), col(1);
+    while (m--) {
+        int x1, y1, x2, y2;
+        cin >> x1 >> y1 >> x2 >> y2;
+    }
 }
-
