a3de073998da264de2a5e8cb9d5bc9ef9f28fce4
magyarakoos <magyar.kende.22f@gmail.com>
2025-09-28 12:53:00 +0200

    sync: 2025-09-28 12:53:00

--- codeforces/archive/873D/main.cpp
+++ codeforces/archive/873D/main.cpp
@@ -2,7 +2,16 @@
 using namespace std;
 #define int int64_t
 
+void solve(int n, int k) {}
+
 int32_t main() {
     cin.tie(0), ios::sync_with_stdio(0);
-}
+    int n, k;
+    cin >> n >> k;
+    if (k % 2 == 0 || k >= 2 * n) {
+        cout << "-1\n";
+    } else {
 
+        solve(n, k);
+    }
+}
