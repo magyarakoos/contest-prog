120db85ef4aec955a04888e5fd96db09a81af16e
magyarakoos <magyar.kende.22f@gmail.com>
2025-09-28 13:09:00 +0200

    sync: 2025-09-28 13:09:00

--- codeforces/archive/873D/main.py
+++ codeforces/archive/873D/main.py
@@ -4,6 +4,7 @@ def solve(n, k):
     a = []
     for i in range(n // 2, 0, -1):
         a.append(i)
+    print (n // 2, n // 2 * 2 - 1, a)
     for i in solve(n - n // 2, k - n // 2 - 1):
         a.append(i + n // 2)
     print(n, k, a)
