69fe7082799247b980df364f3f322c31e7105056
magyarakoos <magyar.kende.22f@gmail.com>
2025-09-28 12:05:00 +0200

    sync: 2025-09-28 12:05:00

--- codeforces/archive/1198E/main.cpp
+++ codeforces/archive/1198E/main.cpp
@@ -8,7 +8,7 @@ int32_t main() {
     cin.tie(0), ios::sync_with_stdio(0);
     int n, m;
     cin >> n >> m;
-    vector<int> row(1, 1), col(1, 1);
+    vector<int> row({1, n}), col({1, n});
     vector<array<int, 4>> rectS(m);
     for (auto& [x1, y1, x2, y2] : rectS) {
         cin >> x1 >> y1 >> x2 >> y2;
