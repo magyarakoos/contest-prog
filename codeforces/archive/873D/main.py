2c66b0181ad0c34bd71bd36de31b97d08a2c7e46
magyarakoos <magyar.kende.22f@gmail.com>
2025-09-28 15:34:00 +0200

    sync: 2025-09-28 15:34:00

--- codeforces/archive/873D/main.py
+++ codeforces/archive/873D/main.py
@@ -7,4 +7,8 @@ def solve(n, k):
         return [i for i in range(1, n // 2 + 1)] + [x + n // 2 for x in solve(n - n // 2, k - 1)]
     else:
         return [i for i in range(n // 2, 0, -1)] + [x + n // 2 for x in solve(n - n // 2, k - (n // 2 * 2 - 1))]
-print(*solve(*map(int, input().split())))
+n, k = map(int, input().split())
+if k % 2 == 0 or k > n * 2 - 1:
+    print(-1)
+else:
+    print(*solve(n, k))
