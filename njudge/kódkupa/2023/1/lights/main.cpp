ac597b22136212928c5b33a6f2356ed51b945521
magyarakoos <magyar.kende.22f@gmail.com>
2025-10-17 14:54:00 +0200

    sync: 2025-10-17 14:54:00

--- njudge/kódkupa/2023/1/lights/main.cpp
+++ njudge/kódkupa/2023/1/lights/main.cpp
@@ -4,5 +4,10 @@ using namespace std;
 
 int32_t main() {
     cin.tie(0), ios::sync_with_stdio(0);
+    int n, c;
+    cin >> n >> c;
+    vector<int> a(n);
+    for (int& x : a) cin >> x;
+    set<int> needed;
+    for (int i = 0; i < c; i++) needed.insert(i);
 }
-
