4d6141ad193b4276c8c34691703ab1645dfd4843
magyarakoos <magyar.kende.22f@gmail.com>
2025-11-01 19:37:01 +0100

    sync: 2025-11-01 19:37:01

---
+++ atcoder/abc430/A/main.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int int64_t
+
+const int INF = 1e18, MAXN = 2e5, MOD = 1e9 + 7;
+
+#define cases 0
+void solve() {
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+    cout << ((c >= a && d >= b) || c < a ? "No" : "Yes")
+         << "\n";
+}
+
+int32_t main() {
+    cin.tie(0), ios::sync_with_stdio(0);
+    int T = 1;
+    if (cases) cin >> T;
+    while (T--) solve();
+}
