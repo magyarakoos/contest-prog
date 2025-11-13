dad93132bc80a7b58d9043f92f9ea55e792c3fdd
magyarakoos <magyar.kende.22f@gmail.com>
2025-11-13 19:52:00 +0100

    sync: 2025-11-13 19:52:00

--- njudge/oktv/2024/legtobbgyumolcs/main.cpp
+++ njudge/oktv/2024/legtobbgyumolcs/main.cpp
@@ -4,5 +4,13 @@ using namespace std;
 
 int32_t main() {
     cin.tie(0), ios::sync_with_stdio(0);
+    int n;
+    cin >> n;
+    vector<bool> a(n + 1);
+    for (int i = 0; i < n; i++) {
+        char c;
+        cin >> c;
+        a[i + 1] = c == 'K';
+    }
+    vector<int> ps(n + 1), ss(n + 2);
 }
-
