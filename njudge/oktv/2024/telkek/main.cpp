d8db5ec96ae057137e484d837c1661c3c092f111
magyarakoos <magyar.kende.22f@gmail.com>
2025-11-13 20:00:00 +0100

    sync: 2025-11-13 20:00:00

--- njudge/oktv/2024/telkek/main.cpp
+++ njudge/oktv/2024/telkek/main.cpp
@@ -4,19 +4,15 @@ using namespace std;
 
 int32_t main() {
     cin.tie(0), ios::sync_with_stdio(0);
-    int n, q;
-    cin >> n >> q;
-    vector<int> t(n + 1);
-    for (int i = 1; i <= n; i++) {
-        cin >> t[i];
-        t[i] = max(t[i], t[i - 1]);
-    }
-    t.push_back(2e9);
-    while (q--) {
-        int m;
-        cin >> m;
-        int i =
-            upper_bound(t.begin(), t.end(), m) - t.begin();
-        cout << i << "\n";
+    int n, p;
+    cin >> n >> p;
+    vector<int> a(n);
+    for (int& x : a) cin >> x;
+    int l = 0, r = 0, sum = 0;
+    while (r < n) {
+        while (r < n && sum + a[r] <= p) r++;
+
+        sum -= a[l];
+        l++;
     }
 }
