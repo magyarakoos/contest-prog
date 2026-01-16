c63e5218b9bb482eb5d454ae3b6d4f97dd07a8ab
magyarakoos <magyar.kende.22f@gmail.com>
2026-01-16 11:58:00 +0100

    sync: 2026-01-16 11:58:00

--- njudge/oktv/2023/kartyajatek/main.cpp
+++ njudge/oktv/2023/kartyajatek/main.cpp
@@ -4,5 +4,17 @@ using namespace std;
 
 int32_t main() {
     cin.tie(0), ios::sync_with_stdio(0);
+    int n;
+    cin >> n >> n;
+    vector<int> a(n), b;
+    for (int& x : a) cin >> x;
+    vector<bool> point(n);
+    for (int i = 0; i < n; i++) {
+        if (b.empty() || b.back() <= a[i]) {
+            b.push_back(a[i]);
+        } else {
+            int j = upper_bound(b.begin(), b.end(), a[i]) -
+                    b.begin();
+        }
+    }
 }
-
