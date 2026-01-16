b10a567c7a1f9a2d5cf3b38f44086aa2ab549be1
magyarakoos <magyar.kende.22f@gmail.com>
2026-01-16 11:42:01 +0100

    sync: 2026-01-16 11:42:01

--- njudge/oktv/2023/jarda2/main.cpp
+++ njudge/oktv/2023/jarda2/main.cpp
@@ -10,11 +10,13 @@ int32_t main() {
     cin >> n;
     vector<int> dp(n + 1);
     dp[0] = 1;
+    int ps = 0;
     for (int i = 1; i <= n; i++) {
         (dp[i] += 2 * dp[i - 1]) %= MOD;
         if (i >= 2) (dp[i] += 7 * dp[i - 2]) %= MOD;
-        for (int j = 0; j < i - 2; j++) {
-            (dp[i] += 8 * dp[j]) %= MOD;
+        if (i >= 3) {
+            (dp[i] += ((ps += 8 * dp[i - 3]) %= MOD)) %=
+                MOD;
         }
     }
     cout << dp[n] << "\n";
