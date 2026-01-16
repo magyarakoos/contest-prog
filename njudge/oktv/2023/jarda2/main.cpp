d54390c40f3d3e515a6d9ddde3fdb7f5e5cf132f
magyarakoos <magyar.kende.22f@gmail.com>
2026-01-16 11:37:00 +0100

    sync: 2026-01-16 11:37:00

--- njudge/oktv/2023/jarda2/main.cpp
+++ njudge/oktv/2023/jarda2/main.cpp
@@ -2,7 +2,20 @@
 using namespace std;
 #define int int64_t
 
+const int MOD = 2023;
+
 int32_t main() {
     cin.tie(0), ios::sync_with_stdio(0);
+    int n;
+    cin >> n;
+    vector<int> dp(n + 1);
+    dp[0] = 1;
+    for (int i = 1; i <= n; i++) {
+        (dp[i] += 2 * dp[i - 1]) %= MOD;
+        (dp[i] += 7 * dp[i - 2]) %= MOD;
+        for (int j = 0; j < i - 2; j++) {
+            (dp[i] += 4 * dp[j]) %= MOD;
+        }
+    }
+    cout << dp[n] << "\n";
 }
-
