3b972ad116f4447ca6ffec73e0f94f3e55ddc01f
magyarakoos <magyar.kende.22f@gmail.com>
2026-03-20 19:07:00 +0100

    sync: 2026-03-20 19:07:00

--- spoj/ADACLEAN/main.cpp
+++ spoj/ADACLEAN/main.cpp
@@ -4,32 +4,18 @@ using namespace std;
 
 const int MOD = 1e9 + 7, BMOD = 10;
 
-int mpow(int a, int b) {
-    int res = 1;
-    while (b) {
-        if (b % 2) {
-            res *= a;
-            res %= MOD;
-        }
-        a *= a;
-        a %= MOD;
-        b /= 2;
-    }
-    return res;
-}
-
 void solve() {
-    int n, k;
+    int n, k, pw = 1;
     string s;
     cin >> n >> k >> s;
-
+    for (int i = 0; i < k; i++) (pw *= BMOD) %= MOD;
     set<int> hS;
     int h = 0;
     for (int i = 0; i < n; i++) {
         h *= BMOD;
         h %= MOD;
         if (i - k >= 0) {
-            h += MOD - mpow(BMOD, k) * (s[i - k] - 'a' + 1);
+            h += MOD - pw * (s[i - k] - 'a' + 1);
             h %= MOD;
         }
         h += s[i] - 'a' + 1;
