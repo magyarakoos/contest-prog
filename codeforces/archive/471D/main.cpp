01b6a719c85493176571830d48deb54e04bd5f6b
magyarakoos <magyar.kende.22f@gmail.com>
2026-03-20 19:52:01 +0100

    sync: 2026-03-20 19:52:01

--- codeforces/archive/471D/main.cpp
+++ codeforces/archive/471D/main.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 #define int int64_t
 
-vector<int> z_function(string s) {
+vector<int> z_function(const vector<int>& s) {
     int n = s.size();
     vector<int> z(n);
     int l = 0, r = 0;
@@ -21,4 +21,9 @@ vector<int> z_function(string s) {
 
 int32_t main() {
     cin.tie(0), ios::sync_with_stdio(0);
+    int n, w;
+    cin >> n >> w;
+    vector<int> a(n), b(w);
+    for (int& x : a) cin >> x;
+    for (int& x : b) cin >> x;
 }
