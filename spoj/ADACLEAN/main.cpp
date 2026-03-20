4e5f066b94fb4d6862ba9210e34e8344a1832586
magyarakoos <magyar.kende.22f@gmail.com>
2026-03-20 18:49:01 +0100

    sync: 2026-03-20 18:49:01

--- spoj/ADACLEAN/main.cpp
+++ spoj/ADACLEAN/main.cpp
@@ -2,6 +2,23 @@
 using namespace std;
 #define int int64_t
 
+const int MOD = 1e9 + 7, BMOD = 29;
+
+void solve() {
+    int n, k;
+    string s;
+    cin >> n >> k >> s;
+    int h = 0;
+    for (int i = n - 1; ~i; i--) {
+        h *= BMOD;
+        h %= MOD;
+        h += s[i] - 'a' + 1;
+        h %= MOD;
+        cout << h << " ";
+    }
+    cout << "\n";
+}
+
 int32_t main() {
     cin.tie(0)->sync_with_stdio(0);
     int t;
