2c0dd2a2077c198ec466813c87592cded1c7b8f5
magyarakoos <magyar.kende.22f@gmail.com>
2026-03-21 12:19:01 +0100

    sync: 2026-03-21 12:19:01

--- onlinejudge/12910/main.cpp
+++ onlinejudge/12910/main.cpp
@@ -21,6 +21,7 @@ int32_t main() {
             if (i + 1 == w * h) return 0.;
             if (vis[i]) return dp[i];
             double result = 0;
+            int same = 0;
             for (int j = 1; j <= 6; j++) {
                 int k = i + j;
                 if (k >= w * h) {
@@ -33,7 +34,7 @@ int32_t main() {
                 }
             }
             vis[i] = 1;
-            return dp[i] = result / 6 + 1;
+            return dp[i] = result / (6. - same) + 1;
         };
         cout << solve(0) << "\n";
     }
