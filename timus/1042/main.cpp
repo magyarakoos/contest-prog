a6f23e1d08ea83a904e7eef1ae230c8cd7adeb25
magyarakoos <magyar.kende.22f@gmail.com>
2026-03-21 10:53:01 +0100

    sync: 2026-03-21 10:53:01

--- timus/1042/main.cpp
+++ timus/1042/main.cpp
@@ -8,26 +8,20 @@ int32_t main() {
     cin >> n;
     vector<bitset<5>> a(n);
     for (int i = 0, x; i < n; i++) {
-        while (cin >> x && x != -1) a[i][x - 1] = 1;
+        while (cin >> x && x != -1) a[x - 1][i] = 1;
         a[i][n] = 1;
     }
-    cout << "Step 0:\n";
-    for (int i = 0; i < n; i++) { cout << a[i] << "\n"; }
-    cout << "\n";
     for (int j = 0; j < n; j++) {
         for (int i = j; i < n; i++) {
             if (a[i][j]) {
                 swap(a[i], a[j]);
+                cout << i << " ";
                 break;
             }
         }
         for (int i = 0; i < n; i++) {
             if (i != j && a[i][j]) a[i] ^= a[j];
         }
-        cout << "Step " << j + 1 << ":\n";
-        for (int i = 0; i < n; i++) {
-            cout << a[i] << "\n";
-        }
-        cout << "\n";
     }
+    cout << "\n";
 }
