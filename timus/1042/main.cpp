06e727bfbcdba4965d270a49f503f05f055f221f
magyarakoos <magyar.kende.22f@gmail.com>
2026-03-21 10:27:00 +0100

    sync: 2026-03-21 10:27:00

--- timus/1042/main.cpp
+++ timus/1042/main.cpp
@@ -6,9 +6,18 @@ int32_t main() {
     cin.tie(0)->sync_with_stdio(0);
     int n;
     cin >> n;
-    vector<bitset<4>> a(n);
+    vector<bitset<250>> a(n);
     for (int i = 0, x; i < n; i++) {
         while (cin >> x && x != -1) a[i][x - 1] = 1;
     }
-    for (int i = 0; i < n; i++) { cout << a[i] << "\n"; }
+    for (int j = 0; j < n; j++) {
+        bool ok = 0;
+        for (int i = 0; i < n; i++) {
+            if (a[i][j]) {
+                swap(a[i], a[j]);
+                ok = 1;
+                break;
+            }
+        }
+    }
 }
