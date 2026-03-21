5ecd6d1ab8315eea9c585f6868ce7e0d9a90b415
magyarakoos <magyar.kende.22f@gmail.com>
2026-03-21 11:58:01 +0100

    sync: 2026-03-21 11:58:01

--- onlinejudge/12910/main.cpp
+++ onlinejudge/12910/main.cpp
@@ -3,5 +3,10 @@ using namespace std;
 #define int int64_t
 
 int32_t main() {
-    cin.tie(0), ios::sync_with_stdio(0);
-    do { } while (getline(cin, "")); }
+    cin.tie(0)->sync_with_stdio(0);
+    string nl;
+    do {
+        int w, h, s;
+        cin >> w >> h >> s;
+    } while (getline(cin, nl));
+}
