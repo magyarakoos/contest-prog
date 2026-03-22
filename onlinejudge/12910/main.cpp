e6d8f4791bb08f625a51549a83c7d2071fa84731
magyarakoos <magyar.kende.22f@gmail.com>
2026-03-22 09:59:11 +0100

    sync: 2026-03-22 09:59:11

--- onlinejudge/12910/main.cpp
+++ onlinejudge/12910/main.cpp
@@ -6,38 +6,19 @@ int32_t main() {
     cin.tie(0)->sync_with_stdio(0);
     int w, h, s;
     while (cin >> w >> h >> s) {
-        vector<int> p(w * h);
+        int n = w * h;
+        vector<int> p(n), np(n);
         iota(p.begin(), p.end(), 0);
         while (s--) {
             int u, v;
             cin >> u >> v;
             p[u - 1] = v - 1;
         }
-        vector<int> vis(w * h);
-        vector<double> dp(w * h);
-        function<double(int)> solve = [&](int i) {
-            cout << i << endl;
-            sleep(1);
-            if (i + 1 == w * h) return 0.;
-            if (vis[i]) return dp[i];
-            double result = 0;
-            int same = 0;
-            for (int j = 1; j <= 6; j++) {
-                int k = i + j;
-                if (k >= w * h) {
-                    cout << k << "+" << w * h << " "
-                         << w * h - (k - w * h + 2) << "\n";
-                    k = w * h - (k - w * h + 2);
-                }
-                if (i == k) {
-                    same++;
-                } else {
-                    result += solve(k);
-                }
-            }
-            vis[i] = 1;
-            return dp[i] = result / (6. - same) + 1;
-        };
-        cout << solve(0) << "\n";
+        for (int i = 0; i < n; i++) {
+            int j = p[i];
+            while (j != i) { j = p[j]; }
+        }
+        for (int i = 0; i < n; i++) { cout << p[i] << " "; }
+        cout << "\n";
     }
 }
