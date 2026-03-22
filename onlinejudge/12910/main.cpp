cbe7b8d532cc5f0fd0a1a48960fae7171100fee0
magyarakoos <magyar.kende.22f@gmail.com>
2026-03-22 10:15:01 +0100

    sync: 2026-03-22 10:15:01

--- onlinejudge/12910/main.cpp
+++ onlinejudge/12910/main.cpp
@@ -32,7 +32,7 @@ int32_t main() {
         }
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                cout << a[i][j] << " ";
+                cout << setw(2) << a[i][j] << " ";
             }
             cout << "| " << a[i][n];
             cout << "\n";
