d21f5230bb527d1372f13f580e9db56450ac84ca
magyarakoos <magyar.kende.22f@gmail.com>
2026-03-22 10:12:01 +0100

    sync: 2026-03-22 10:12:01

--- onlinejudge/12910/main.cpp
+++ onlinejudge/12910/main.cpp
@@ -20,13 +20,22 @@ int32_t main() {
         for (int i = n; i < n + 5; i++) {
             p[i] = p[n - (i - n + 2)];
         }
-        for (int i = 0; i < n + 5; i++)
-            cout << p[i] + 1 << " ";
-        cout << "\n";
+        // for (int i = 0; i < n + 5; i++)
+        //     cout << p[i] + 1 << " ";
+        // cout << "\n";
         vector a(n, vector<double>(n + 1));
         for (int i = 0; i < n; i++) {
             a[i][i] = a[i][n] = 6;
-            for (int j = 1; j <= 6; j++) {}
+            for (int j = 1; j <= 6; j++) {
+                a[i][p[i + j]]--;
+            }
         }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                cout << a[i][j] << " ";
+            }
+            cout << "\n";
+        }
+        cout << "\n";
     }
 }
