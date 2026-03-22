c59567dc1e2119a0d821b2614d985a383f65f0d3
magyarakoos <magyar.kende.22f@gmail.com>
2026-03-22 13:47:00 +0100

    sync: 2026-03-22 13:47:00

--- njudge/kódkupa/2023/1/periodicwords/main.cpp
+++ njudge/kódkupa/2023/1/periodicwords/main.cpp
@@ -3,6 +3,25 @@ using namespace std;
 #define int int64_t
 
 int32_t main() {
-    cin.tie(0), ios::sync_with_stdio(0);
+    cin.tie(0)->sync_with_stdio(0);
+    int n, s;
+    cin >> n >> s;
+    vector<bool> is_prime(1e5 + 1, 1);
+    vector<int> primes;
+    is_prime[0] = is_prime[1] = 0;
+    for (int i = 2; i <= 1e5; i++) {
+        if (!is_prime[i]) continue;
+        primes.push_back(i);
+        for (int j = i * 2; j <= 1e5; j += i) {
+            is_prime[j] = 1;
+        }
+    }
+    for (int i = 0; i < 15; i++) cout << primes[i] << " ";
+    cout << "\n";
+    int q;
+    cin >> q;
+    while (q--) {
+        int l, r;
+        cin >> l >> r;
+    }
 }
-
