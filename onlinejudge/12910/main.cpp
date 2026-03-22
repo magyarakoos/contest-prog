fbf40df392021ba0915c72ae8b73f67e7ef76d72
magyarakoos <magyar.kende.22f@gmail.com>
2026-03-22 10:37:00 +0100

    sync: 2026-03-22 10:37:00

--- onlinejudge/12910/main.cpp
+++ onlinejudge/12910/main.cpp
@@ -1,8 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define int int64_t
 
-int32_t main() {
+int main() {
     cin.tie(0)->sync_with_stdio(0);
     int w, h, s;
     while (cin >> w >> h >> s) {
