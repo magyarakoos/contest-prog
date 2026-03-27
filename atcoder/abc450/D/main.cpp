5e25db99bdcd7072d0252cf1b9d9a57fe11a8ae0
magyarakoos <magyar.kende.22f@gmail.com>
2026-03-27 19:25:01 +0100

    sync: 2026-03-27 19:25:01

--- atcoder/abc450/D/main.cpp
+++ atcoder/abc450/D/main.cpp
@@ -9,31 +9,10 @@ void solve() {
     int n, k;
     cin >> n >> k;
     vector<int> a(n);
-    set<int> s;
     for (int& x : a) {
         cin >> x;
-        s.insert(x % k);
+        x %= k;
     }
-    vector<int> b, mid, edge;
-    for (int x : s) b.push_back(x);
-    int m = b.size();
-    if (m % 2) {
-        mid.push_back(b[m / 2]);
-    } else {
-        mid.push_back(b[m / 2 - 1]);
-        mid.push_back(b[m / 2]);
-    }
-    edge.push_back(b[0]);
-    edge.push_back(b[m - 1]);
-    int result = 0;
-    for (int x : mid) {
-        for (int y : edge) {
-            result =
-                max(result, min(abs(x - y),
-                                min(x, y) + k - max(x, y)));
-        }
-    }
-    cout << result << "\n";
 }
 
 int32_t main() {
