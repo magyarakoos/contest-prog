aaaf36eeb17fdd7ebf4c044d5bb72d019a043e8f
magyarakoos <magyar.kende.22f@gmail.com>
2026-03-27 18:39:01 +0100

    sync: 2026-03-27 18:39:01

--- atcoder/abc450/B/main.cpp
+++ atcoder/abc450/B/main.cpp
@@ -5,7 +5,25 @@ using namespace std;
 const int INF = 1e18, MAXN = 2e5, MOD = 1e9 + 7;
 
 #define cases 0
-void solve() {}
+void solve() {
+    int n;
+    cin >> n;
+    vector c(n, vector<int>(n));
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) cin >> c[i][j];
+    }
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            for (int k = j + 1; k < n; k++) {
+                if (c[i][j] + c[i][k] < c[i][k]) {
+                    cout << "Yes\n";
+                    return;
+                }
+            }
+        }
+    }
+    cout << "No\n";
+}
 
 int32_t main() {
     cin.tie(0), ios::sync_with_stdio(0);
