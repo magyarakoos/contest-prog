de506c10de2af855796a505c960a632602511c62
magyarakoos <magyar.kende.22f@gmail.com>
2026-07-07 22:13:01 +0200

    sync: 2026-07-07 22:13:00

--- atcoder/awc0107/C/main.cpp
+++ atcoder/awc0107/C/main.cpp
@@ -6,4 +6,11 @@ int32_t main() {
     cin.tie(0)->sync_with_stdio(0);
     int n, q;
     cin >> n >> q;
+    map<int, vector<pair<string, int>>> mp;
+    for (int i = 0; i < n; i++) {
+        string s;
+        int x;
+        cin >> s >> x;
+        mp[x].push_back({s, i});
+    }
 }
