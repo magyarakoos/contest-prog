e0fc3111bea0c61fafdd5b3653647127103480b0
magyarakoos <magyar.kende.22f@gmail.com>
2026-07-07 22:25:01 +0200

    sync: 2026-07-07 22:25:01

--- atcoder/awc0107/C/main.cpp
+++ atcoder/awc0107/C/main.cpp
@@ -6,46 +6,17 @@ int32_t main() {
     cin.tie(0)->sync_with_stdio(0);
     int n, q;
     cin >> n >> q;
-    map<int, vector<pair<string, int>>> mp;
-    set<int> xS;
+    vector<pair<array<int, 2>, string>> a(n);
     for (int i = 0; i < n; i++) {
         string s;
         int x;
         cin >> s >> x;
-        xS.insert(x);
-        mp[x].push_back({s, i});
+        a[i] = {{x, -i}, s};
     }
+    sort(a.begin(), a.end());
     while (q--) {
         int x;
         cin >> x;
-        if (mp.contains(x)) {
-            for (auto p : mp[x]) { cout << p.first << " "; }
-            cout << "\n";
-        } else {
-            if (*xS.rbegin() == x) {
-                auto it = xS.rbegin();
-                it--;
-                cout << mp[*it][0].first << "\n";
-            } else if (*xS.begin() == x) {
-                auto it = xS.begin();
-                it++;
-                cout << mp[*it][0].first << "\n";
-            } else {
-                auto it = xS.upper_bound(x);
-                auto it2 = it;
-                it2--;
-                auto p1 = mp[*it][0];
-                auto p2 = mp[*it2][0];
-                if (*it - x < x - *it2) {
-                    cout << p1.first << "\n";
-                } else if (*it - x > x - *it2) {
-                    cout << p2.first << "\n";
-                } else if (p1.second < p2.second) {
-                    cout << p1.first << "\n";
-                } else {
-                    cout << p2.first << "\n";
-                }
-            }
-        }
+        auto it = lower_bound(a.begin(), a.end(), x);
     }
 }
